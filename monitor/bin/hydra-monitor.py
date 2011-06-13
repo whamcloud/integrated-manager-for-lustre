07e0f07de7b4f5acc537198c295fd64eead788ce
John Spray <john@whamcloud.com>
2011-06-13 11:57:20 +0100

    Add --once option to hydra-monitor for debugging

--- monitor/bin/hydra-monitor.py
+++ monitor/bin/hydra-monitor.py
@@ -8,13 +8,16 @@ from monitor.lib.lustre_audit import LustreAudit
 
 from time import sleep
 import os
+import sys
 
 if __name__=='__main__':
     try:
-        while(True):
+        if len(sys.argv) > 1 and sys.argv[1] == "--once":
             LustreAudit().audit_all()
-            sleep(5)
-        #LustreAudit().audit_all()
+        else:
+            while(True):
+                LustreAudit().audit_all()
+                sleep(5)
 
     except KeyboardInterrupt:
         print "Exiting..."
