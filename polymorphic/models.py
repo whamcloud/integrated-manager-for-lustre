adcfa5266ad22de28ea414b9f66e3a918788e97d
John Spray <john@whamcloud.com>
2011-07-20 00:08:01 +0100

    Fixes to polymorphic:  * Wrap save code in a transaction to avoid situation where e.g. an    Event is created before a LearnEvent, and another thread tries to    downcast the Event  * Replace the __subclasses__ iteration code with the old content_type    stuff, with the tweak that we use get_for_id instead of a ForeignKey    dereference, thus using the contenttypes cache and avoiding spurious    SELECTs when downcasting

--- polymorphic/models.py
+++ polymorphic/models.py
@@ -1,20 +1,57 @@
 from django.db import models
 from django.db.models.base import ModelBase
 from django.db.models.query import QuerySet
+from django.contrib.contenttypes.models import ContentType
+
+# http://djangosnippets.org/snippets/1343/
+def nested_commit_on_success(func):
+    """Like commit_on_success, but doesn't commit existing transactions.
+
+    This decorator is used to run a function within the scope of a 
+    database transaction, committing the transaction on success and
+    rolling it back if an exception occurs.
+
+    Unlike the standard transaction.commit_on_success decorator, this
+    version first checks whether a transaction is already active.  If so
+    then it doesn't perform any commits or rollbacks, leaving that up to
+    whoever is managing the active transaction.
+    """
+    from django.db import transaction
+ 
+    commit_on_success = transaction.commit_on_success(func)
+    def _nested_commit_on_success(*args, **kwds):
+        if transaction.is_managed():
+            return func(*args,**kwds)
+        else:
+            return commit_on_success(*args,**kwds)
+    return transaction.wraps(func)(_nested_commit_on_success)
 
 class PolymorphicMetaclass(ModelBase):
     def __new__(cls, name, bases, dct):
-        def downcast(self):
-            for subklass in self.__class__.__subclasses__():
-                attr = subklass.__name__.lower()
-                try:
-                    return getattr(self, attr)
-                except subklass.DoesNotExist:
-                    pass
+        def save(self, *args, **kwargs):
+            if(not self.content_type):
+                self.content_type = ContentType.objects.get_for_model(self.__class__)
 
-            return self
+            # We make sure this is happening inside a transaction 
+            # because otherwise another thread could try to get
+            # objects of the base class, this metaclass will try
+            # to downcast, and fail because the instance of the
+            # child DB object hasn't been saved yet.
+            @nested_commit_on_success
+            def base_save():
+                models.Model.save(self, *args, **kwargs)
+            base_save();
+
+
+        def downcast(self):
+            model = ContentType.objects.get_for_id(self.content_type_id).model_class()
+            if (model == self.__class__):
+                return self
+            return model.objects.get(id=self.id)  
 
         if issubclass(dct.get('__metaclass__', type), PolymorphicMetaclass):
+          dct['content_type'] = models.ForeignKey(ContentType, editable=False, null=True)
+          dct['save'] = save
           dct['downcast'] = downcast
 
         return super(PolymorphicMetaclass, cls).__new__(cls, name, bases, dct)
@@ -38,6 +75,4 @@ class DowncastQuerySet(QuerySet):
     def __iter__(self):
         for item in super(DowncastQuerySet, self).__iter__():
             yield item.downcast()
-
-
 
