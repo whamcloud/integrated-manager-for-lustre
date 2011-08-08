1cf44bf1133e92d4c20c3a6f0bf7e49075fc11b7
John Spray <john@whamcloud.com>
2011-08-08 16:25:34 +0100

    Fix indentation

--- hydra-rmmod.py
+++ hydra-rmmod.py
@@ -7,51 +7,54 @@ import sys
 extra_deps = {"lquota": set(["lov", "osc", "mgc", "mds", "mdc", "lmv"])}
 
 class Module:
-	def __init__(self, lsmod_line):
-		parts = lsmod_line.split()
-		self.name = parts[0]
-		depcount = int(parts[2])
-		try:
-			self.dependents = set(parts[3].split(","))
-		except IndexError:
-			self.dependents = set([])
-		#if depcount != len(self.dependents): 
-		#	print "Non-module dependencies on %s" % self.name
-		#	sys.exit(-1)
-		
+    def __init__(self, lsmod_line):
+        parts = lsmod_line.split()
+        self.name = parts[0]
+        depcount = int(parts[2])
+        try:
+            self.dependents = set(parts[3].split(","))
+        except IndexError:
+            self.dependents = set([])
+        #if depcount != len(self.dependents): 
+        #    print "Non-module dependencies on %s" % self.name
+        #    sys.exit(-1)
+        
 
 def load_lsmod():
-	modules = {}
+    modules = {}
 
-	lines = os.popen("/sbin/lsmod").read().split("\n")[1:-1]
-	for line in lines:
-		m = Module(line.strip())
-		modules[m.name] = m
+    lines = os.popen("/sbin/lsmod").read().split("\n")[1:-1]
+    for line in lines:
+        m = Module(line.strip())
+        modules[m.name] = m
 
-	for name,module in modules.items():
-		try:
-			module.dependents |= (extra_deps[name] & set(modules.keys()))
-		except KeyError:
-			pass
+    for name,module in modules.items():
+        try:
+            module.dependents |= (extra_deps[name] & set(modules.keys()))
+        except KeyError:
+            pass
 
-	return modules
+    return modules
 
 def remove_module(name, modules):
-	m = modules[name]
-	print "Removing %d dependents of %s : %s" % (len(m.dependents), name, m.dependents)
-	while (len(m.dependents) > 0):
-		remove_module(m.dependents.pop(), modules)
+    try:
+        m = modules[name]
+    except KeyError:
+        # It's not loaded, do nothing.
+        return
+    print "Removing %d dependents of %s : %s" % (len(m.dependents), name, m.dependents)
+    while (len(m.dependents) > 0):
+        remove_module(m.dependents.pop(), modules)
 
-	print "Removing %s" % name
-	rc = os.system("rmmod %s" % name)
-	if rc != 0:
-		raise "Failed to rmmod '%s'" % name
+    print "Removing %s" % name
+    rc = os.system("rmmod %s" % name)
+    if rc != 0:
+        raise "Failed to rmmod '%s'" % name
 
-	modules.pop(name)
-	for m in modules.values():
-		if name in m.dependents:
-			m.dependents.remove(name)
-		
+    modules.pop(name)
+    for m in modules.values():
+        if name in m.dependents:
+            m.dependents.remove(name)
 
 modules = load_lsmod()
 remove_module(sys.argv[1], modules)
