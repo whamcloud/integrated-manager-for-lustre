a8cca1f5b2b67e68558473559dc559c8aaa95164
John Spray <john@whamcloud.com>
2011-08-30 21:12:56 +0100

    Remove false dependency on ManagedHost

--- configure/plugins/lvm.py
+++ configure/plugins/lvm.py
@@ -7,55 +7,75 @@ from configure.lib.vendor_plugin import VendorResource, VendorPlugin
 from configure.lib.vendor_plugin import LocalId, GlobalId
 from configure.lib.vendor_plugin import ResourceAttribute
 
-# Access to database makes this a server-only plugin
-from configure.models import ManagedHost
-
 class LvmPlugin(VendorPlugin):
+    def simple_ssh(self, hostname, command):
+        """Like configure.lib.job.debug_ssh but doesn't require a Host instance"""
+        import paramiko
+        import socket
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        # How long it may take to establish a TCP connection
+        SOCKET_TIMEOUT = 3600
+        # How long it may take to get the output of our agent
+        # (including tunefs'ing N devices)
+        SSH_READ_TIMEOUT = 3600
+
+        args = {"hostname": hostname,
+                "username": 'root',
+                "timeout": SOCKET_TIMEOUT}
+        ssh.connect(**args)
+        transport = ssh.get_transport()
+        channel = transport.open_session()
+        channel.settimeout(SSH_READ_TIMEOUT)
+        channel.exec_command(command)
+        result_stdout = channel.makefile('rb').read()
+        result_stderr = channel.makefile_stderr('rb').read()
+        result_code = channel.recv_exit_status()
+        ssh.close()
+
+        self.log.debug("simple_ssh:%s:%s:%s" % (hostname, result_code, command))
+        if result_code != 0:
+            self.log.error("simple_ssh:%s:%s:%s" % (hostname, result_code, command))
+            self.log.error(result_stdout)
+            self.log.error(result_stderr)
+        return result_code, result_stdout, result_stderr
+
     def initial_scan(self):
         # Get the list of user-configured hosts to scan
         root_resources = self.get_root_resources()
         for lvm_host_resource in root_resources:
-            try:
-                # XXX only mapping to ManagedHost for convenience of debug_ssh
-                host = ManagedHost.objects.get(address = lvm_host_resource.hostname)
-            except ManagedHost.DoesNotExist:
-                self.log.error("No ManagedHost for LvmHost %s!" % ())
+            hostname = lvm_host_resource.hostname
+            code, out, err = self.simple_ssh(hostname, "vgs --units b --noheadings -o vg_name,vg_uuid,vg_size")
+            if code != 0:
+                self.log.error("Bad code %s from SSH call: %s %s" %(code, out, err))
                 continue
 
-            from configure.lib.job import debug_ssh
-            code, out, err = debug_ssh(host, "vgs --units b --noheadings -o vg_name,vg_uuid,vg_size")
-
             vol_group_resources = []
-
-            if code == 0:
-                try:
-                    lines = out.split("\n")
-                    for line in lines:
-                        name, uuid, size_str = line.split()
-                        size = int(size_str[0:-1], 10)
-                        self.log.info("Learned VG %s %s %s" % (name, uuid, size))
-                        group = LvmGroup(uuid = uuid, name = name, size = size)
-                        group.add_parent(lvm_host_resource)
-                        self.add_resource(group)
-                        vol_group_resources.append(group)
-                except ValueError:
-                    pass
-            else:
-                self.log.error("Bad code %s from SSH call: %s %s" %(code, out, err))
+            lines = [l for l in out.split("\n") if len(l) > 0]
+            for line in lines:
+                name, uuid, size_str = line.split()
+                size = int(size_str[0:-1], 10)
+                self.log.info("Learned VG %s %s %s" % (name, uuid, size))
+                group = LvmGroup(uuid = uuid, name = name, size = size)
+                group.add_parent(lvm_host_resource)
+                self.add_resource(group)
+                vol_group_resources.append(group)
 
             for vgr in vol_group_resources:
-                code, out, err = debug_ssh(host, "lvs --units b --noheadings -o lv_name,lv_uuid,lv_size %s" % vgr.name)
-                if code == 0:
-                    lines = [l for l in out.split("\n") if len(l) > 0]
-                    for line in lines:
-                        name, uuid, size_str = line.split()
-                        size = int(size_str[0:-1], 10)
-                        self.log.info("Learned LV %s %s %s" % (name, uuid, size))
-                        vol = LvmVolume(uuid = uuid, name = name, size = size)
-                        vol.add_parent(vgr)
-                        self.add_resource(vol)
-                else:
+                code, out, err = self.simple_ssh(hostname, "lvs --units b --noheadings -o lv_name,lv_uuid,lv_size %s" % vgr.name)
+                if code != 0:
                     self.log.error("Bad code %s from lvs call for %s: %s %s" % (code, vgr.name, out, err))
+                    continue
+
+                lines = [l for l in out.split("\n") if len(l) > 0]
+                for line in lines:
+                    name, uuid, size_str = line.split()
+                    size = int(size_str[0:-1], 10)
+                    self.log.info("Learned LV %s %s %s" % (name, uuid, size))
+                    vol = LvmVolume(uuid = uuid, name = name, size = size)
+                    vol.add_parent(vgr)
+                    self.add_resource(vol)
 
                 
 class LvmGroup(VendorResource):
