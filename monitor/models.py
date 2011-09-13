d4ae1b194d2d10abe073753d1b42476b0d0e3d3c
Brian J. Murrell <brian@whamcloud.com>
2011-09-13 10:53:52 -0400

    Implement AddHost to configure rsyslog.
    Requires hydra-agent c42390d93980ffcb66bfe280c1facce5cca93352.
    hydra-server now depends on rsyslog-mysql
    Introduce the rsyslog Model so that the schema is created for it
    by syncdb.
    Configure rsyslog.conf on the hydra-server to log to mysql.

--- monitor/models.py
+++ monitor/models.py
@@ -979,6 +979,34 @@ class TargetMountRecoveryInfo(models.Model):
         else:
             return "N/A"
 
+class Systemevents(models.Model):
+    id = models.AutoField(primary_key=True, db_column='ID') # Field name made lowercase.
+    customerid = models.BigIntegerField(null=True, db_column='CustomerID', blank=True) # Field name made lowercase.
+    receivedat = models.DateTimeField(null=True, db_column='ReceivedAt', blank=True) # Field name made lowercase.
+    devicereportedtime = models.DateTimeField(null=True, db_column='DeviceReportedTime', blank=True) # Field name made lowercase.
+    facility = models.IntegerField(null=True, db_column='Facility', blank=True) # Field name made lowercase.
+    priority = models.IntegerField(null=True, db_column='Priority', blank=True) # Field name made lowercase.
+    fromhost = models.CharField(max_length=60, db_column='FromHost', blank=True) # Field name made lowercase.
+    message = models.TextField(db_column='Message', blank=True) # Field name made lowercase.
+    ntseverity = models.IntegerField(null=True, db_column='NTSeverity', blank=True) # Field name made lowercase.
+    importance = models.IntegerField(null=True, db_column='Importance', blank=True) # Field name made lowercase.
+    eventsource = models.CharField(max_length=60, db_column='EventSource', blank=True) # Field name made lowercase.
+    eventuser = models.CharField(max_length=60, db_column='EventUser', blank=True) # Field name made lowercase.
+    eventcategory = models.IntegerField(null=True, db_column='EventCategory', blank=True) # Field name made lowercase.
+    eventid = models.IntegerField(null=True, db_column='EventID', blank=True) # Field name made lowercase.
+    eventbinarydata = models.TextField(db_column='EventBinaryData', blank=True) # Field name made lowercase.
+    maxavailable = models.IntegerField(null=True, db_column='MaxAvailable', blank=True) # Field name made lowercase.
+    currusage = models.IntegerField(null=True, db_column='CurrUsage', blank=True) # Field name made lowercase.
+    minusage = models.IntegerField(null=True, db_column='MinUsage', blank=True) # Field name made lowercase.
+    maxusage = models.IntegerField(null=True, db_column='MaxUsage', blank=True) # Field name made lowercase.
+    infounitid = models.IntegerField(null=True, db_column='InfoUnitID', blank=True) # Field name made lowercase.
+    syslogtag = models.CharField(max_length=60, db_column='SysLogTag', blank=True) # Field name made lowercase.
+    eventlogtype = models.CharField(max_length=60, db_column='EventLogType', blank=True) # Field name made lowercase.
+    genericfilename = models.CharField(max_length=60, db_column='GenericFileName', blank=True) # Field name made lowercase.
+    systemid = models.IntegerField(null=True, db_column='SystemID', blank=True) # Field name made lowercase.
+    class Meta:
+        db_table = u'SystemEvents'
+
 # Only do admin registration if we are not being imported
 # by the configure app
 #    from django.contrib import admin
