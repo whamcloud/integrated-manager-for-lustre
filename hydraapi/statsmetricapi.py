9059ffd9c85adc0f8b632fadd187b6981cc74fd2
Michael MacDonald <mjmac@whamcloud.com>
2011-10-21 12:12:01 -0400

    Latest stats API

--- hydraapi/statsmetricapi.py
+++ hydraapi/statsmetricapi.py
@@ -35,14 +35,14 @@ class GetFSTargetStats(AnonymousRequestHandler):
             return all_fs_stats
 
     def metrics_fetch(self,fs,target_kind,fetch_metrics,start_time,end_time,interval,datafunction='Average'):
-        import time
         if target_kind == 'OST':
             if start_time:
                 start_time = int(start_time)
+                start_time = getstartdate(start_time)
                 if fetch_metrics:
-                    fs_target_stats = fs.metrics.fetch(datafunction,ObjectStoreTarget,fetch_metrics=fetch_metrics.split(),start_time=int(time.time()-start_time))
+                    fs_target_stats = fs.metrics.fetch(datafunction,ObjectStoreTarget,fetch_metrics=fetch_metrics.split(),start_time=start_time)
                 else:
-                    fs_target_stats = fs.metrics.fetch(datafunction,ObjectStoreTarget,start_time=int(time.time()-start_time))
+                    fs_target_stats = fs.metrics.fetch(datafunction,ObjectStoreTarget,start_time=start_time)
             else:
                 if fetch_metrics:
                     fs_target_stats = fs.metrics.fetch_last(ObjectStoreTarget,fetch_metrics=fetch_metrics.split())
@@ -51,23 +51,28 @@ class GetFSTargetStats(AnonymousRequestHandler):
         elif target_kind == 'MDT':
             if start_time:
                 start_time = int(start_time)
+                start_time = getstartdate(start_time)
                 if fetch_metrics:
-                    fs_target_stats = fs.metrics.fetch(datafunction,MetadataTarget,fetch_metrics=fetch_metrics.split(),start_time=int(time.time()-start_time))
+                    fs_target_stats = fs.metrics.fetch(datafunction,MetadataTarget,fetch_metrics=fetch_metrics.split(),start_time=start_time)
                 else:
-                    fs_target_stats = fs.metrics.fetch(datafunction,MetadataTarget,start_time=int(time.time()-start_time))
+                    fs_target_stats = fs.metrics.fetch(datafunction,MetadataTarget,start_time=start_time)
             else:
                 if fetch_metrics:
                     fs_target_stats = fs.metrics.fetch_last(MetadataTarget,fetch_metrics=fetch_metrics.split())
                 else:
                     fs_target_stats = fs.metrics.fetch_last(MetadataTarget)   
+        anystats = []
         if fs_target_stats:
             if start_time:  
                 for stats_data in fs_target_stats:
                     stats_data[1]['filesystem'] = fs.name
+                    stats_data[1]['timestamp'] = stats_data[0]
+                    anystats.append(stats_data[1])
             else:
                 fs_target_stats[1]['filesystem'] = fs.name
                 fs_target_stats[1]['timestamp'] = fs_target_stats[0]
-        return fs_target_stats
+                anystats.append(fs_target_stats[1])
+        return anystats
 
 class GetFSServerStats(AnonymousRequestHandler):
     @extract_request_args('filesystem','starttime','endtime','datafunction','fetchmetrics')
@@ -78,39 +83,41 @@ class GetFSServerStats(AnonymousRequestHandler):
             fs = Filesystem.objects.get(name=filesystem)
             hosts = fs.get_servers()
             for host in hosts:
-                host_stats_metric.append(self.metrics_fetch(host,fetchmetrics,starttime,endtime,interval))
+                host_stats_metric.extend(self.metrics_fetch(host,fetchmetrics,starttime,endtime,interval))
             return host_stats_metric
         else:
-            all_host_stats_metrics = []
             for fs in Filesystem.objects.all():
                 hosts = fs.get_servers()
                 host_stats_metric = []  
-                for host in Host.objects.all():
-                    host_stats_metric.append(self.metrics_fetch(host,fetchmetrics,starttime,endtime,interval))
-                all_host_stats_metrics.extend(host_stats_metric)  
-            return all_host_stats_metrics
+                for host in hosts:
+                    host_stats_metric.extend(self.metrics_fetch(host,fetchmetrics,starttime,endtime,interval))
+            return host_stats_metric
 
     def metrics_fetch(self,host,fetch_metrics,start_time,end_time,interval,datafunction='Average'):
-        import time
         if start_time:
             start_time = int(start_time)
+            start_time = getstartdate(start_time)
             if fetch_metrics:
-                host_stats = host.metrics.fetch(datafunction,fetch_metrics=fetch_metrics.split(),start_time=int(time.time()-start_time))
+                host_stats = host.metrics.fetch(datafunction,fetch_metrics=fetch_metrics.split(),start_time=start_time)
             else:
-                host_stats = host.metrics.fetch(datafunction,start_time=int(time.time()-start_time))
+                host_stats = host.metrics.fetch(datafunction,start_time=start_time)
         else:
             if fetch_metrics:
                 host_stats = host.metrics.fetch_last(fetch_metrics=fetch_metrics.split())
             else:
                 host_stats = host.metrics.fetch_last()
+        onlystats = []   
         if host_stats:
             if start_time:
                 for stats_data in host_stats:
                     stats_data[1]['host'] = host.address
+                    stats_data[1]['timestamp'] = stats_data[0]
+                    onlystats.append(stats_data[1])      
             else:
                 host_stats[1]['host'] = host.address
                 host_stats[1]['timestamp'] = host_stats[0]
-        return host_stats
+                onlystats.append(host_stats[1]) 
+        return onlystats
 
 class GetFSMGSStats(AnonymousRequestHandler):
     @extract_request_args('filesystem','starttime','endtime' ,'datafunction','fetchmetrics')
@@ -130,13 +137,13 @@ class GetFSMGSStats(AnonymousRequestHandler):
             return all_mgs_stats_metric
 
     def metrics_fetch(self,mgs,fetch_metrics,start_time,end_time,interval,datafunction='Average'):
-        import time
         if start_time:
             start_time = int(start_time)
+            start_time = getstartdate(start_time)
             if fetch_metrics:
-                mgs_stats = mgs.metrics.fetch(datafunction,fetch_metrics=fetch_metrics.split(),start_time=int(time.time()-start_time))
+                mgs_stats = mgs.metrics.fetch(datafunction,fetch_metrics=fetch_metrics.split(),start_time=start_time)
             else:
-                mgs_stats = mgs.metrics.fetch(datafunction,start_time=int(time.time()-start_time))
+                mgs_stats = mgs.metrics.fetch(datafunction,start_time=start_time)
         else:
             if fetch_metrics:
                 mgs_stats = mgs.metrics.fetch_last(fetch_metrics=fetch_metrics.split())
@@ -163,13 +170,13 @@ class GetServerStats(AnonymousRequestHandler):
             raise Exception("Unable to find host with hostid=%s" %hostid)
 
     def metrics_fetch(self,host,fetch_metrics,start_time,end_time,interval,datafunction='Average'):
-        import time
         if start_time:
             start_time = int(start_time)
+            start_time = getstartdate(start_time)
             if fetch_metrics:
-                host_stats = host.metrics.fetch(datafunction,fetch_metrics=fetch_metrics.split(),start_time=int(time.time()-start_time))
+                host_stats = host.metrics.fetch(datafunction,fetch_metrics=fetch_metrics.split(),start_time=start_time)
             else:
-                host_stats = host.metrics.fetch(datafunction,start_time=int(time.time()-start_time))
+                host_stats = host.metrics.fetch(datafunction,start_time=start_time)
         else:
             if fetch_metrics:
                 host_stats = host.metrics.fetch_last(fetch_metrics=fetch_metrics.split())
@@ -198,13 +205,13 @@ class GetTargetStats(AnonymousRequestHandler):
             return self.metrics_fetch(target,fetchmetrics,starttime,endtime,interval)
 
     def metrics_fetch(self,target,fetch_metrics,start_time,end_time,interval,datafunction='Average'):
-        import time
         if start_time:
             start_time = int(start_time)
+            start_time = getstartdate(start_time)
             if fetch_metrics:
-                target_stats = target.metrics.fetch(datafunction,fetch_metrics=fetch_metrics.split(),start_time=int(time.time()-start_time))
+                target_stats = target.metrics.fetch(datafunction,fetch_metrics=fetch_metrics.split(),start_time=start_time)
             else:
-                target_stats = target.metrics.fetch(datafunction,start_time=int(time.time()-start_time))
+                target_stats = target.metrics.fetch(datafunction,start_time=start_time)
         else:
             if fetch_metrics:
                 target_stats = target.metrics.fetch_last(fetch_metrics=fetch_metrics.split())
@@ -212,6 +219,8 @@ class GetTargetStats(AnonymousRequestHandler):
                 target_stats = target.metrics.fetch_last()
         if target_stats:
             if start_time:
+                start_time=int(start_time)
+                start_time = getstartdate(start_time)             
                 for stats_data in target_stats:
                     stats_data[1]['target'] = target.name
             else:
@@ -233,23 +242,38 @@ class GetFSClientsStats(AnonymousRequestHandler):
             return client_stats
 
     def metrics_fetch(self,filesystem,start_time,end_time,interval,datafunction='Average'):
-        import time
         fetch_metrics="num_exports"
         client_stats = [] 
         if start_time:
             start_time = int(start_time)
-            client_stats = filesystem.metrics.fetch(datafunction,ObjectStoreTarget,fetch_metrics=fetch_metrics.split(),start_time=int(time.time()-start_time))
+            start_time = getstartdate(start_time)  
+            client_stats = filesystem.metrics.fetch(datafunction,ObjectStoreTarget,fetch_metrics=fetch_metrics.split(),start_time=start_time)
         else:
             try:
                 client_stats = filesystem.metrics.fetch_last(ObjectStoreTarget,fetch_metrics=fetch_metrics.split())
             except:
-                pass 
+                pass
+        onlystats = [] 
         if client_stats:
             if start_time:
                 for stats_data in client_stats:
                     stats_data[1]['filesystem'] = filesystem.name
                     stats_data[1]['timestamp'] = stats_data[0]
+                    onlystats.append(stats_data[1])       
             else:
                 client_stats[1]['filesystem'] = filesystem.name
                 client_stats[1]['timestamp'] = client_stats[0]
-        return client_stats
+                onlystats.append(client_stats[1])
+        return onlystats
+
+def getstartdate(start_time):
+    import datetime
+    now = lambda: datetime.datetime.now()
+    minutes = datetime.timedelta(minutes=1)
+    startdatetime = (now() - minutes * int(start_time)).isoformat()
+    return _str2dt(startdatetime)
+
+def _str2dt(in_string):
+    """Parse a string and return a datetime object."""
+    import dateutil.parser
+    return dateutil.parser.parse(in_string)
