4995c173774a1ef09b64a6cb34c8ed0d23a71592
John Spray <john@whamcloud.com>
2011-10-24 01:27:30 +0100

    Apply 'ignore filesystems with no targets' from legacyaudit to detect-scan

--- hydra_agent/actions/detect_scan.py
+++ hydra_agent/actions/detect_scan.py
@@ -184,6 +184,9 @@ def get_mgs_targets(local_targets):
         mgs_targets[fs] = []
         read_log("filesystem", fs, "%s-client" % fs)
         read_log("filesystem", fs, "%s-param" % fs)
+        # Don't bother reporting on a FS entry with no targets
+        if len(mgs_targets[fs]) == 0:
+            del mgs_targets[fs]
 
     # Read config logs "testfs-MDT0000" etc
     for target in targets:
