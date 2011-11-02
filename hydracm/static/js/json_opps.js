7a8d8383059665e9ce15cbb78b0474119b26cdc4
John Spray <john@whamcloud.com>
2011-11-02 23:21:16 +0000

    Remove spurious dialog on LNet job submission

--- hydracm/static/js/json_opps.js
+++ hydracm/static/js/json_opps.js
@@ -53,7 +53,6 @@ function Lnet_Operations(host_id, opps, confirm_mesg)
         var response = data.response;    
         if(response.status != "")
         {
-          jAlert("Host Lnet State changed to " + opps, ALERT_TITLE);
           $('#server_configuration').dataTable().fnClearTable();
           LoadServerConf_ServerConfig();
         }
