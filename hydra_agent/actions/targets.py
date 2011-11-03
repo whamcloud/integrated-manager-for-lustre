cffd1fda65c8e82b01bdcc805f31445ca968f472
Brian J. Murrell <brian@whamcloud.com>
2011-11-03 12:24:39 -0400

    Cleanup calls to shell.try_run()

--- hydra_agent/actions/targets.py
+++ hydra_agent/actions/targets.py
@@ -187,7 +187,7 @@ def configure_ha(args):
 
 def list_ha_targets(args):
     targets = []
-    for line in shell.try_run("crm_resource --list", shell=True).split("\n"):
+    for line in shell.try_run(['crm_resource', '--list']).split("\n"):
         match = re.match(r"^\s*([^\s]+).+hydra:Target", line)
         if match:
             targets.append(match.groups()[0])
@@ -205,22 +205,22 @@ def unmount_target(args):
 def start_target(args):
     from time import sleep
     unique_label = "%s_%s" % (args.label, args.serial)
-    shell.try_run("crm_resource -r %s -p target-role -m -v Started)" % unique_label)
+    shell.try_run(['crm_resource', '-r', unique_label, '-p', 'target-role',
+                   '-m', '-v', 'Started'])
 
     # now wait for it to start
     # FIXME: this may break on non-english systems or new versions of pacemaker
     timeout = 100
     n = 0
     while n < timeout:
-        stdout = shell.try_run("crm resource status %s 2>&1" % unique_label,
-                               shell=True)
+        stdout = shell.try_run("crm resource status %s 2>&1" % unique_label, shell=True)
         if stdout.startswith("resource %s is running on:" % unique_label):
             break
         sleep(1)
         n += 1
 
     # and make sure it didn't start but (the RA) fail(ed)
-    stdout = shell.try_run("crm status", shell=True)
+    stdout = shell.try_run(['crm', 'status'])
 
     failed = True
     for line in stdout.split("\n"):
@@ -230,7 +230,8 @@ def start_target(args):
 
     if failed:
         # try to leave things in a sane state for a failed mount
-        shell.try_run("crm_resource -r %s -p target-role -m -v Stopped)" % unique_label)
+        shell.try_run(['crm_resource', '-r', unique_label, '-p',
+                       'target-role', '-m', '-v', 'Stopped'])
         raise RuntimeError("failed to start target %s" % unique_label)
     else:
         location = get_resource_location(unique_label)
@@ -244,14 +245,16 @@ def stop_target(args):
 def _stop_target(label, serial):
     unique_label = "%s_%s" % (label, serial)
     from time import sleep
-    shell.try_run("crm_resource -r %s -p target-role -m -v Stopped)" % unique_label)
+    shell.try_run(['crm_resource', '-r', unique_label, '-p', 'target-role',
+                  '-m', '-v', 'Stopped'])
 
     # now wait for it
     # FIXME: this may break on non-english systems or new versions of pacemaker
     timeout = 100
     n = 0
     while n < timeout:
-        stdout = shell.try_run("crm resource status %s 2>&1" % unique_label, shell=True)
+        stdout = shell.try_run("crm resource status %s 2>&1" % unique_label,
+                               shell=True)
         if stdout.find("is NOT running") > -1:
             break
         sleep(1)
@@ -270,8 +273,10 @@ def unmigrate_target(args):
     from time import sleep
 
     # just remove the migration constraint
-    shell.try_run("crm configure delete %s-migrated)" % args.label, shell = True)
+    shell.try_run(['crm', 'configure', 'delete', '%s-migrated' % args.label])
     sleep(1)
     
-    shell.try_run("crm_resource -r %s -p target-role -m -v Stopped)" % args.label, shell = True)
-    shell.try_run("crm_resource -r %s -p target-role -m -v Started)" % args.label, shell = True)
+    shell.try_run(['crm_resource', '-r', args.label, '-p', 'target-role',
+                   '-m', '-v', 'Stopped'])
+    shell.try_run(['crm_resource', '-r', args.label, '-p', 'target-role',
+                   '-m', '-v', 'Started'])
