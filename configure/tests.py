2f21e991a3906fdd3b1c18946f7bf2af588bd6eb
John Spray <john@whamcloud.com>
2011-11-29 19:25:01 +0000

    Remove configure/tests.py, it's no longer needed when using nose to detect tests

--- configure/tests.py
+++
@@ -1,6 +0,0 @@
-
-
-from configure.lib.storage_plugin.tests.test_plugin import TestRegistration, TestCallbacks, TestAddRemove
-from configure.lib.storage_plugin.tests.test_manager import TestLoad, TestCornerCases
-from configure.lib.storage_plugin.tests.test_alert_conditions import TestAlertConditions
-from configure.lib.storage_plugin.tests.test_attributes import TestAttributes, TestReferenceAttribute
