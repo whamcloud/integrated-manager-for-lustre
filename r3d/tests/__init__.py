7147b95f9d56dfcb48ffbf71189b7fa83e426442
kelsey <kelsey@whamcloud.com>
2011-11-30 14:16:40 -0800

    Removing imports of tests into __init__'s - unnecessary for test discovery using nose.

--- r3d/tests/__init__.py
+++ r3d/tests/__init__.py
@@ -1,5 +1,3 @@
 ## Copyright 2011 Whamcloud, Inc.
 ## Authors: Michael MacDonald <mjmac@whamcloud.com>
 
-from r3d.tests.integration import *
-from r3d.tests.unit import *
--- r3d/tests/unit/__init__.py
+++ r3d/tests/unit/__init__.py
@@ -1,6 +1,3 @@
 ## Copyright 2011 Whamcloud, Inc.
 ## Authors: Michael MacDonald <mjmac@whamcloud.com>
 
-from r3d.tests.unit.lib import *
-from r3d.tests.unit.database import *
-#from r3d.tests.unit.datasources import *
