b8fba3455c3a3742b8d670273c7c142856c133cb
John Spray <jcspray@gmail.com>
2011-12-05 12:32:51 +0000

    Run 'migrate' on configure/ first

--- hydra-worker-init.sh
+++ hydra-worker-init.sh
@@ -40,6 +40,7 @@ start() {
         echo "create database hydra" | mysql
         # and populate it
         python $PYTHONPATH/manage.py syncdb --noinput
+        python $PYTHONPATH/manage.py migrate configure
         python $PYTHONPATH/manage.py migrate
         # reload rsyslog since it will have complained about the missing
         # table when it started
