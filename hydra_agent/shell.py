4c841b02688196a1f3e1d561ed5547d1b617c42d
John Spray <jcspray@gmail.com>
2011-12-19 13:02:26 +0000

    HYD-515 Add teardown code to avoid leaking PTYs
    PTYsHYD-515 Add teardown code to avoid leaking PTYsHYD-515 Add teardown
    code to avoid leaking PTYs

--- hydra_agent/shell.py
+++ hydra_agent/shell.py
@@ -39,43 +39,65 @@ def run(arg_list, shell = False):
     poll.register(p.stderr, all_poll_flags)
     poll.register(master, all_poll_flags)
 
-    # We will iterate on poll.poll until we have seen HUPs on both
-    # stdout and stderr.
-    stdout_buf = ""
-    stderr_buf = ""
-    
-    # Support older pythons which don't define select.PIPE_BUF
-    try:
-        pipe_buf = select.PIPE_BUF
-    except AttributeError:
-        pipe_buf = 4096
+    def teardown(p, poll, slave):
+        poll.unregister(p.stderr)
+        poll.unregister(master)
+        del poll
 
-    stdout_closed = False
-    stderr_closed = False
-    while (not stdout_closed) and (not stderr_closed):
-        result = poll.poll(100)
-        for fd, mask in result:
-            if fd == master and mask & (select.POLLIN | select.POLLPRI):
-                import os
-                stdout = stdout_file.read(pipe_buf)
-                # FIXME: wtf?  I'm getting DOS newlines sometimes
-                stdout = stdout.replace("\r\n", "\n")
-                stdout_buf = stdout_buf + stdout
-                sys.stderr.write(stdout)
-            elif fd == p.stderr.fileno() and mask & (select.POLLIN | select.POLLPRI):
-                stderr = p.stderr.read(pipe_buf)
-                stderr_buf = stderr_buf + stderr
-                sys.stderr.write(stderr)
-            elif mask & select.POLLHUP:
-                if fd == master:
-                    stdout_closed = True
-                elif fd == p.stderr.fileno():
-                    stderr_closed = True
+        if p.stdin:
+            p.stdin.close()
+        if p.stdout:
+            p.stdout.close()
+        if p.stderr:
+            p.stderr.close()
+        stdout_file.close()
+        #os.close(master) # bad file descriptor
+        os.close(slave)
+        del p
+
+    try:
+        # We will iterate on poll.poll until we have seen HUPs on both
+        # stdout and stderr.
+        stdout_buf = ""
+        stderr_buf = ""
+        
+        # Support older pythons which don't define select.PIPE_BUF
+        try:
+            pipe_buf = select.PIPE_BUF
+        except AttributeError:
+            pipe_buf = 4096
+
+        stdout_closed = False
+        stderr_closed = False
+        while (not stdout_closed) and (not stderr_closed):
+            result = poll.poll(100)
+            for fd, mask in result:
+                if fd == master and mask & (select.POLLIN | select.POLLPRI):
+                    import os
+                    stdout = stdout_file.read(pipe_buf)
+                    # FIXME: wtf?  I'm getting DOS newlines sometimes
+                    stdout = stdout.replace("\r\n", "\n")
+                    stdout_buf = stdout_buf + stdout
+                    sys.stderr.write(stdout)
+                elif fd == p.stderr.fileno() and mask & (select.POLLIN | select.POLLPRI):
+                    stderr = p.stderr.read(pipe_buf)
+                    stderr_buf = stderr_buf + stderr
+                    sys.stderr.write(stderr)
+                elif mask & select.POLLHUP:
+                    if fd == master:
+                        stdout_closed = True
+                    elif fd == p.stderr.fileno():
+                        stderr_closed = True
+                    else:
+                        raise RuntimeError("Unexpected select() result %s" % ((fd, mask),))
                 else:
                     raise RuntimeError("Unexpected select() result %s" % ((fd, mask),))
-            else:
-                raise RuntimeError("Unexpected select() result %s" % ((fd, mask),))
-    rc = p.wait()
+        rc = p.wait()
+
+        teardown(p, poll, slave)
+    except Exception:
+        teardown(p, poll, slave)
+        raise
 
     return rc, stdout_buf, stderr_buf
 
