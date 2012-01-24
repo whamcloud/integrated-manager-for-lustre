0e6de6bc5890d6028a7115a62289bf26b0dd043b
John Spray <jcspray@gmail.com>
2012-01-24 13:33:38 +0000

    Test HTTP access to server before saving server config

--- hydra_agent/actions/server_conf.py
+++ hydra_agent/actions/server_conf.py
@@ -4,12 +4,21 @@
 
 from hydra_agent.plugins import AgentPlugin
 
+def _validate_conf(server_conf):
+    from hydra_agent.main_loop import MainLoop
+    result = MainLoop()._send_update(server_conf['url'], server_conf['token'], None, {})
+    if result == None:
+        from socket import getfqdn
+        raise RuntimeError("Cannot contact server URL %s from %s" % (server_conf['url'], getfqdn()))
 
 def set_server_conf(args = None):
     import simplejson as json
-    data = json.loads(args.args)
+    server_conf = json.loads(args.args)
+
+    _validate_conf(server_conf)
+
     from hydra_agent.store import AgentStore
-    AgentStore.set_server_conf(data)
+    AgentStore.set_server_conf(server_conf)
 
 
 def remove_server_conf(args = None):
