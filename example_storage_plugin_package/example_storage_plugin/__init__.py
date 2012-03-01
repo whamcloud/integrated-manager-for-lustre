a1b0971122f3bfe38c8104211082ed3cb662f963
Brian J. Murrell <brian@whamcloud.com>
2012-03-01 13:38:25 -0500

    HYD-220 Example vendor storage plugin packaging
    Change-Id: I7d3284b738c518083ee3edaefd328de712fbfb61

---
+++ example_storage_plugin_package/example_storage_plugin/__init__.py
@@ -0,0 +1,7 @@
+
+# ==============================
+# Copyright 2011 Whamcloud, Inc.
+# ==============================
+
+from example_storage_plugin import ExamplePlugin, Couplet, Controller, \
+                                   HardDrive, RaidPool, Lun
