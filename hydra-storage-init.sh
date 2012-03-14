7162595e23b5ed86d266d33124fce252e94d157e
John Spray <john@whamcloud.com>
2012-03-14 17:05:15 +0000

    log stdout and stderr from hydra-storage

--- hydra-storage-init.sh
+++ hydra-storage-init.sh
@@ -12,6 +12,7 @@ export SERVICE_NAME=hydra-storage
 export HYDRA_PATH=/usr/share/hydra-server 
 export DAEMON_PATH=${HYDRA_PATH}/chroma_core/bin/storage_daemon
 export PID_FILE=/var/run/hydra-storage.pid
+export LOG_DIR=/var/log/hydra
 if ! [ -f ${DAEMON_PATH} ]
 then
 	echo "Daemon not found at " ${DAEMON_PATH}
@@ -21,7 +22,7 @@ fi
 export PYTHONPATH=${HYDRA_PATH}
 
 start() {
-    action "Starting ${SERVICE_NAME}" daemon --pidfile ${PID_FILE} '${DAEMON_PATH} >/dev/null 2>/dev/null & echo $! > ${PID_FILE}'
+    action "Starting ${SERVICE_NAME}" daemon --pidfile ${PID_FILE} '${DAEMON_PATH} > ${LOG_DIR}/startup_${SERVICE_NAME}_out.log 2>${LOG_DIR}/startup_${SERVICE_NAME}_err.log & echo $! > ${PID_FILE}'
     echo
 }
 
