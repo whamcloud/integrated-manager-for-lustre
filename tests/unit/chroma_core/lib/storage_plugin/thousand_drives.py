8a0d37671445a7f61b5988d4b7e513a74a59db48
John Spray <john@whamcloud.com>
2012-03-15 12:00:43 +0000

    HYD-641: test plugin with reasonable number of drives+statistics

---
+++ tests/unit/chroma_core/lib/storage_plugin/thousand_drives.py
@@ -0,0 +1,36 @@
+
+from chroma_core.lib.storage_plugin.plugin import StoragePlugin
+from chroma_core.lib.storage_plugin.resource import StorageResource
+from chroma_core.lib.storage_plugin import attributes
+from chroma_core.lib.storage_plugin import statistics
+from chroma_core.lib.storage_plugin.resource import GlobalId, ScannableResource
+
+import random
+
+DRIVE_COUNT = 1000
+
+
+class Controller(StorageResource, ScannableResource):
+    name = attributes.String()
+    identifier = GlobalId('name')
+
+
+class DiskDrive(StorageResource):
+    identifier = GlobalId('serial')
+    serial = attributes.String()
+    read_bytes_sec = statistics.Gauge(units = "B/s", label = "Read bandwidth")
+    write_bytes_sec = statistics.Counter(units = "B/s", label = "Write bandwidth")
+
+
+class TestPlugin(StoragePlugin):
+    def initial_scan(self, controller):
+        self.drives = []
+        for i in xrange(0, DRIVE_COUNT):
+            serial = "%s_%d" % (controller.name, i)
+            drive, created = self.update_or_create(DiskDrive, serial = serial)
+            self.drives.append(drive)
+
+    def update_scan(self, controller):
+        for drive in self.drives:
+            drive.read_bytes_sec = random.randint(0, 1000)
+            drive.write_bytes_sec = random.randint(0, 1000)
