836d84c20b78d427793588222ab5fd183e1ef6d7
Shardul Latkar <shardul.latkar@clogeny.com>
2012-03-17 00:29:39 +0530

    Fixed tests to read data from config file passed in on command line,  Added a sample config file that can be used for selenium tests
    Change-Id: I6c18666972ab128314d0841869c83b9a10d684f5

--- tests/selenium/utils/sample_data.py
+++ tests/selenium/utils/sample_data.py
@@ -1,26 +1,22 @@
-""" Test Data """
-import json
+from testconfig import config
 
 
 class Testdata:
-    """This class reads and returns data from config file that is used by the test cases
+    """
+    This class reads and returns data from config file that is used by the test cases
     """
 
-    def __init__(self):
-        """ Loads data from config file
-        """
-
-        self.json_data = open('../sample_data/example.json')
-        self.test_data = json.load(self.json_data)
-
     def get_test_data_for_server_configuration(self):
-        host_list = self.test_data["hosts"]
+        # read and return host related data from config file
+        host_list = config["hosts"]
         return host_list
 
     def get_test_data_for_mgt_configuration(self):
-        mgs_list = self.test_data["mgss"]
+        # read and return mgs related data from config file
+        mgs_list = config["mgss"]
         return mgs_list
 
     def get_test_data_for_filesystem_configuration(self):
-        fs_list = self.test_data["filesystems"]
+        # read and return file system related data from config file
+        fs_list = config["filesystems"]
         return fs_list
--- tests/selenium/views/login.py
+++ tests/selenium/views/login.py
@@ -1,8 +1,8 @@
 
-import test_parameters
 from selenium.common.exceptions import WebDriverException
 from time import sleep
 from base import wait_for_element
+from testconfig import config
 
 
 class Login:
@@ -29,10 +29,10 @@ class Login:
                     continue
 
     def login_superuser(self):
-        for user in test_parameters.USERS:
+        for user in config['hydra_servers']['users']:
             if user['is_superuser']:
                 self.username.send_keys(user['username'])
                 self.password.send_keys(user['password'])
                 self.login_button.click()
                 return
-        raise RuntimeError("No superuser in test_parameters.USERS")
+        raise RuntimeError("No superuser in config file")
