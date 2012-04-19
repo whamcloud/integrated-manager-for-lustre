85a6dd0734eb0c57357214820629feecd37a5e9f
Robert Read <rread@whamcloud.com>
2012-04-19 15:50:47 -0700

    HYD-948 -  read/write balance in large OST IO graph
    Instead of a second series, this subtacts write from
    reads, so this shows how "balanced" reads and writes
    are. This makes for some cool looking graphs, but
    with the caveat that idle and really busy filesystms
    look the same. However, we have the small IO graph
    for absolute values.
    
    Change-Id: I15f8f84dc46227b6e6f1b3760152c8896a137c96

--- chroma-manager/chroma_ui/static/js/dashboard.js
+++ chroma-manager/chroma_ui/static/js/dashboard.js
@@ -526,9 +526,11 @@ var Dashboard = function(){
         _.each(data, function(series_data, target_id) {
           var update_data = [];
           _.each(series_data, function(datapoint) {
-            var timestamp = new Date(datapoint.ts).getTime();
-            update_data.push([timestamp, ( datapoint.data.stats_read_bytes + datapoint.data.stats_write_bytes )])
-          });
+              var timestamp = new Date(datapoint.ts).getTime();
+              var read_bytes = _.isUndefined(datapoint.data.stats_read_bytes)? 0 : datapoint.data.stats_read_bytes;
+              var write_bytes =  _.isUndefined(datapoint.data.stats_write_bytes)? 0 : datapoint.data.stats_write_bytes;
+              update_data.push([timestamp, read_bytes - write_bytes]);
+            });
 
           var target = ApiCache.get('target', target_id);
           var label;
@@ -551,7 +553,7 @@ var Dashboard = function(){
         chart: {
           renderTo: 'global_ost_bandwidth'
         },
-        title: { text: 'OST read/write bandwidth'},
+        title: { text: 'OST read/write balance'},
         xAxis: {type:'datetime'},
         yAxis: [{
           title: null,
