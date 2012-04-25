d362d66ff5a36d5b6f76ab0cddb22d0b6fa4923d
Michael MacDonald <mjmac@whamcloud.com>
2012-04-25 18:38:30 -0400

    Fix up copyright for r3d/cli.py, ignore r3d/prettytable.py
    Change-Id: Ice02266402c85d74291a1a54b8f5edd668d60206

--- chroma-manager/r3d/cli.py
+++ chroma-manager/r3d/cli.py
@@ -1,6 +1,8 @@
+#
 # ========================================================
 # Copyright (c) 2012 Whamcloud, Inc.  All rights reserved.
 # ========================================================
+
 
 import re
 import warnings
