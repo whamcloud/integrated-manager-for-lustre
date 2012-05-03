9f403ee7b512203619d681cdb4208e3a5e8ef0d4
Robert Read <rread@whamcloud.com>
2012-05-02 17:06:27 -0700

    director: add --master option to new_cluster
    This will install latest chroma build.

--- chroma-director/provisioning/management/commands/new_cluster.py
+++ chroma-director/provisioning/management/commands/new_cluster.py
@@ -9,7 +9,6 @@ import time
 
 
 class Command(BaseCommand):
-    args = "[recover]"
     help = "Create a new chroma cluster. "
     can_import_settings = True
     option_list = BaseCommand.option_list + (
@@ -17,12 +16,16 @@ class Command(BaseCommand):
             help="name of the cluster"),
         make_option("--oss", type=int, default=1,
             help="number of OSS nodes to create"),
-        make_option("--clients", type=int, default=0,
-            help="number of client nodes "),
-        make_option("--type", type=str, default="t1.micro",
-            help="instance type for lustre nodes"),
+        make_option("--master", action="store_true",
+            help="Update from master repository"),
+        #make_option("--clients", type=int, default=0,
+        #    help="number of client nodes "),
+        #make_option("--type", type=str, default="t1.micro",
+        #    help="instance type for lustre nodes"),
         make_option("--volumes", type=int, default=4,
-            help="number of EBS volumes per OSS")
+            help="number of EBS volumes per OSS"),
+        make_option("--recover", type=int, default=0,
+            help="Attempt to just perform configuration of nodes")
     )
 
 
@@ -30,7 +33,7 @@ class Command(BaseCommand):
         manager = ChromaManager.create(self.options.name)
         ChromaAppliance.create(manager, "mds1")
         for i in range(0, self.options.oss):
-            ChromaAppliance.create(manager, "oss%d" % i+1)
+            ChromaAppliance.create(manager, "oss%d" % (i+1))
 
         return manager
 
@@ -43,7 +46,7 @@ class Command(BaseCommand):
         appliances = ChromaAppliance.objects.filter(chroma_manager = manager)
         manager_ops = ChromaManagerOps(manager)
 
-        manager_ops.update_deps()
+        manager_ops.update_deps(self.options.master)
         manager_ops.add_etc_hosts([manager.node] + [a.node for a in appliances])
         manager_ops.set_hostname()
         manager_ops.setup_chroma()
@@ -54,7 +57,7 @@ class Command(BaseCommand):
             storage_ops = ChromaStorageOps(appliance)
             storage_ops.set_key(manager_key)
             storage_ops.add_etc_hosts([manager.node] + [a.node for a in appliances])
-            storage_ops.configure()
+            storage_ops.configure(self.options.master)
             if appliance.node.name == "mds1":
                 storage_ops.configure_mds()
             else:
@@ -67,12 +70,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.options = LazyStruct(**options)
-        if len(args) == 0:
+        if self.options.recover > 0:
+            manager = ChromaManager.objects.get(id = self.options.recover)
+        else:
             manager = self._create_instances()
-            self._setup_instances(manager)
-        elif args[0] == 'recover':
-            manager = ChromaManager.objects.get(id = int(args[1]))
-            self._setup_instances(manager)
+        self._setup_instances(manager)
 
 
             # TODO: wait for completion of server add and device discovery
