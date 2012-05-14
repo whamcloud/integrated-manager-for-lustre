c26e386c97ed6cbbc43799b01d76b082c2137ecc
John Spray <john@whamcloud.com>
2012-05-14 14:51:54 +0100

    Simple autodetection test, test that Chroma can detect a basic filesystem that it created itself.

---
+++ chroma-manager/tests/integration/shared_storage_configuration/test_autodetection.py
@@ -0,0 +1,39 @@
+
+from testconfig import config
+from tests.integration.core.testcases import ChromaIntegrationTestCase
+from tests.utils.http_requests import AuthorizedHttpRequests
+
+
+class TestAutodetection(ChromaIntegrationTestCase):
+    def setUp(self):
+        user = config['chroma_managers'][0]['users'][0]
+        self.chroma_manager = AuthorizedHttpRequests(user['username'], user['password'],
+            server_http_url = config['chroma_managers'][0]['server_http_url'])
+        self.reset_cluster(self.chroma_manager)
+        self.erase_volumes()
+
+    def test_simple_detection(self):
+        self.create_filesystem_simple()
+        host_id = self.chroma_manager.get("/api/host").json['objects'][0]['id']
+
+        command = self.chroma_manager.post("/api/command/", body = {
+            'jobs': [{'class_name': 'ForceRemoveHostJob', 'args': {'host_id': host_id}}],
+            'message': "Test force remove hosts"
+        }).json
+
+        self.wait_for_command(self.chroma_manager, command['id'])
+
+        self.assertEqual(len(self.chroma_manager.get("/api/host").json['objects']), 0)
+
+        self.add_hosts([config['lustre_servers'][0]['address']])
+
+        response = self.chroma_manager.post("/api/command/", body = {
+            'jobs': [{'class_name': 'DetectTargetsJob', 'args': {}}],
+            'message': "Test detect targets"
+        })
+        self.assertEqual(response.status_code, 201, response.content)
+        self.wait_for_command(self.chroma_manager, response.json['id'])
+
+        fs = self.chroma_manager.get("/api/filesystem/").json['objects'][0]
+        self.assertEqual(len(fs['mdts']), 1)
+        self.assertEqual(len(fs['osts']), 1)
