49338cda3cc01663d94099ae63a6289960ca9e1d
John Spray <john@whamcloud.com>
2012-05-15 01:07:53 +0100

    Safer way of patching tastypie.fields.DateTimeField

--- chroma-manager/urls.py
+++ chroma-manager/urls.py
@@ -7,28 +7,10 @@
 from django.conf.urls.defaults import patterns, include
 
 from django.contrib import admin
+
 admin.autodiscover()
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-
-# HYD-646: Patch tastypie to assume timezone-naive datetimes are in UTC
-# to match our DB convention (see WorkaroundDateTimeField)
-from dateutil import tz
-import datetime
-
-import tastypie.fields
-
-
-class ZuluDateTimeField(tastypie.fields.DateTimeField):
-    def convert(self, value):
-        if isinstance(value, datetime.datetime):
-            if value.tzinfo == None:
-                value = value.replace(tzinfo = tz.tzutc())
-
-        return value
-
-tastypie.fields.DateTimeField = ZuluDateTimeField
 
 import chroma_ui.urls
 import chroma_api.urls
