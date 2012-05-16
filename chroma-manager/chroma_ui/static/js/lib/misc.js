b66712c5a07fae75fa6ae6b813cdc1eb94f6f947
John Spray <john@whamcloud.com>
2012-05-16 18:31:55 +0100

    Remove unused formatNumber function

--- chroma-manager/chroma_ui/static/js/lib/misc.js
+++ chroma-manager/chroma_ui/static/js/lib/misc.js
@@ -1,24 +1,4 @@
 
-/* http://snipplr.com/view/5945/ */
-function formatNumber(number, decimals, dec_point, thousands_sep) {
-    // http://kevin.vanzonneveld.net
-    // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +     bugfix by: Michael White (http://crestidg.com)
-    // +     bugfix by: Benjamin Lupton
-    // +     bugfix by: Allan Jensen (http://www.winternet.no)
-    // +    revised by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)    
-    // *     example 1: number_format(1234.5678, 2, '.', '');
-    // *     returns 1: 1234.57     
- 
-    var n = number, c = isNaN(decimals = Math.abs(decimals)) ? 2 : decimals;
-    var d = dec_point == undefined ? "." : dec_point;
-    var t = thousands_sep == undefined ? "," : thousands_sep, s = n < 0 ? "-" : "";
-    var i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
- 
-    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
-}
-
 /* returns a floating point nubmer with fixed precision */
 function formatNumberPrecision(number, precision) {
 
