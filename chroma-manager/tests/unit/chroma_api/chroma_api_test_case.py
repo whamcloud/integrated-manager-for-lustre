26da52d010dc48f7aefe92aa453e3ef245454f43
John Spray <john@whamcloud.com>
2012-05-25 16:11:00 +0100

    Superficial check for errors in API gets

--- chroma-manager/tests/unit/chroma_api/chroma_api_test_case.py
+++ chroma-manager/tests/unit/chroma_api/chroma_api_test_case.py
@@ -23,3 +23,16 @@ class ChromaApiTestCase(JobTestCaseWithHost, ResourceTestCase):
 
             ResourceTestCase.tearDown(self)
             JobTestCaseWithHost.tearDown(self)
+
+    def spider_api(self):
+        from chroma_api.urls import api
+        for name, resource in api._registry.items():
+            if 'get' in resource._meta.list_allowed_methods:
+                response = self.api_client.get(resource.get_resource_list_uri())
+                self.assertHttpOK(response)
+                if 'get' in resource._meta.detail_allowed_methods:
+                    objects = self.deserialize(response)['objects']
+
+                    for o in objects:
+                        self.api_client.get(o['resource_uri'])
+                        self.assertHttpOK(response)
