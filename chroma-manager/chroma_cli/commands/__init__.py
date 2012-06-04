f82861e1698d101dc61ca8891b38e68f57262334
Michael MacDonald <mjmac@whamcloud.com>
2012-06-03 22:09:27 -0400

    Remove some cruft that was accidentally pushed
    Change-Id: If75577316398c9d02230882766463f00aa13efd9

--- chroma-manager/chroma_cli/commands/__init__.py
+++ chroma-manager/chroma_cli/commands/__init__.py
@@ -2,7 +2,3 @@
 # ========================================================
 # Copyright (c) 2012 Whamcloud, Inc.  All rights reserved.
 # ========================================================
-
-
-from chroma_cli.commands.dispatcher import CommandDispatcher
-CommandDispatcher  # stupid pyflakes
