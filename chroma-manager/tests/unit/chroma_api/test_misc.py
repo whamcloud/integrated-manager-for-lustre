72e1069bfef4d395d9520b8f71c21572da9f1fd2
John Spray <john@whamcloud.com>
2012-06-18 13:23:27 +0100

    Fix race in test (latest on created_at only has second resolution)

--- chroma-manager/tests/unit/chroma_api/test_misc.py
+++ chroma-manager/tests/unit/chroma_api/test_misc.py
@@ -53,7 +53,7 @@ class TestMisc(ChromaApiTestCase):
             response = self.api_client.put(host['resource_uri'], data = host)
             self.assertHttpAccepted(response)
             # Check we created an exception
-            self.assertNotEqual(StepResult.objects.latest('created_at').exception, None)
+            self.assertNotEqual(StepResult.objects.latest('id').exception, None)
         finally:
             MockAgent.succeed = True
 
