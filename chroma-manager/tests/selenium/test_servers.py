be27b06e920671700fd607b563872c8071b21807
John Spray <john@whamcloud.com>
2012-07-25 15:22:08 +0100

    HYD-1306 Clear add host validation errors on 'add another'
    Change-Id: If2479a450c82999334a2f52042747c37ed74fd9d

--- chroma-manager/tests/selenium/test_servers.py
+++ chroma-manager/tests/selenium/test_servers.py
@@ -6,8 +6,6 @@
 
 from tests.selenium.views.servers import Servers
 from tests.selenium.base import SeleniumBaseTestCase
-from tests.selenium.utils.constants import static_text
-from tests.selenium.utils.constants import wait_time
 from utils.sample_data import Testdata
 from tests.selenium.views.volumes import Volumes
 
@@ -16,9 +14,6 @@ class TestServer(SeleniumBaseTestCase):
     """Test cases for server related operations"""
     def setUp(self):
         super(TestServer, self).setUp()
-
-        self.long_wait = wait_time['long']
-        self.medium_wait = wait_time['medium']
 
         # Getting test data for servers
         self.test_data = Testdata()
@@ -35,6 +30,22 @@ class TestServer(SeleniumBaseTestCase):
             # Check LNet state
             self.assertEqual('LNet up', self.server_page.get_lnet_state(host_name))
 
+    def test_create_server_validation(self):
+        # Enter something invalid, check a validation message appears
+        self.server_page.add_server_open()
+        self.server_page.add_server_enter_address("")
+        self.server_page.add_server_submit_address()
+        self.assertEqual(self.server_page.add_server_error, "This field is mandatory")
+
+        # Enter something valid, complete adding and opt to add another
+        self.server_page.add_server_enter_address(self.host_list[0]['address'])
+        self.server_page.add_server_submit_address()
+        self.server_page.add_server_confirm()
+        self.server_page.add_server_add_another()
+
+        # Check that the validation message is not visible
+        self.assertEqual(self.server_page.add_server_error, None)
+
     def test_volume_config_for_added_server(self):
         """Test for verifying that volumes appear for newly added server"""
 
@@ -45,16 +56,22 @@ class TestServer(SeleniumBaseTestCase):
     def test_start_and_stop_lnet_on_server(self):
         """Test for starting and stopping LNet on server"""
 
+        address = self.host_list[0]['address']
         self.server_page.add_servers(self.host_list)
-        self.stop_lnet_on_servers()
-        self.start_lnet_on_servers()
+        self.server_page.transition(address, 'lnet_down')
+        self.assertEqual(self.server_page.get_lnet_state(address), "LNet down")
+        self.server_page.transition(address, 'lnet_up')
+        self.assertEqual(self.server_page.get_lnet_state(address), "LNet up")
 
     def test_load_and_unload_lnet_on_server(self):
         """Test for loading and unloading LNet on server"""
 
+        address = self.host_list[0]['address']
         self.server_page.add_servers(self.host_list)
-        self.unload_lnet_on_servers()
-        self.load_lnet_on_servers()
+        self.server_page.transition(address, 'lnet_unloaded')
+        self.assertEqual(self.server_page.get_lnet_state(address), "LNet unloaded")
+        self.server_page.transition(address, 'lnet_down')
+        self.assertEqual(self.server_page.get_lnet_state(address), "LNet down")
 
     def check_volume_config_for_added_server(self):
         self.navigation.go('Volumes')
@@ -65,35 +82,3 @@ class TestServer(SeleniumBaseTestCase):
 
             # Verifying that volume configuration appear for added server
             self.assertTrue(volumes_page.check_primary_volumes(host_name), 'Volumes not recognized for server: ' + host_name)
-
-    def stop_lnet_on_servers(self):
-        for host in self.host_list:
-            host_name = host["address"]
-            self.server_page.transition(host_name, static_text['stop_lnet'])
-
-            # Check LNet state
-            self.assertEqual('lnet_down', self.server_page.get_lnet_state(host_name), 'LNet not stopped')
-
-    def start_lnet_on_servers(self):
-        for host in self.host_list:
-            host_name = host["address"]
-            self.server_page.transition(host_name, static_text['start_lnet'])
-
-            # Check LNet state
-            self.assertEqual('lnet_up', self.server_page.get_lnet_state(host_name), 'LNet not started')
-
-    def unload_lnet_on_servers(self):
-        for host in self.host_list:
-            host_name = host["address"]
-            self.server_page.transition(host_name, static_text['unload_lnet'])
-
-            # Check LNet state
-            self.assertEqual('lnet_unloaded', self.server_page.get_lnet_state(host_name), 'LNet not unloaded')
-
-    def load_lnet_on_servers(self):
-        for host in self.host_list:
-            host_name = host["address"]
-            self.server_page.transition(host_name, static_text['load_lnet'])
-
-            # Check LNet state
-            self.assertEqual('lnet_down', self.server_page.get_lnet_state(host_name), 'LNet not loaded')
