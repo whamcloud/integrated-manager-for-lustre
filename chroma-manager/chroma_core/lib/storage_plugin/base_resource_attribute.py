ab7b81bcb95fca7f03eb3fb6bea48cce36e141e8
John Spray <john.spray@intel.com>
2012-09-18 13:17:56 +0100

    HYD-1427 Storage plugin doc cleanup:  * Remove dead provide/subscribe constructor arguments from BaseStorageAttribute  * Add documentation for generic attribute constructor arguments

--- chroma-manager/chroma_core/lib/storage_plugin/base_resource_attribute.py
+++ chroma-manager/chroma_core/lib/storage_plugin/base_resource_attribute.py
@@ -9,7 +9,9 @@ from chroma_core.models import StorageResourceAttributeSerialized
 
 class BaseResourceAttribute(object):
     """Base class for declared attributes of BaseStorageResource.  This is
-       to BaseStorageResource as models.fields.Field is to models.Model"""
+    to BaseStorageResource as models.fields.Field is to models.Model
+
+    """
 
     # This is a hack to store the order in which attributes are declared so
     # that I can sort the BaseStorageResource attribute dict for presentation in the same order
@@ -19,15 +21,23 @@ class BaseResourceAttribute(object):
     model_class = StorageResourceAttributeSerialized
 
     def __init__(self,
-                 subscribe = None,
-                 provide = None,
                  optional = False,
                  label = None,
                  hidden = False,
                  user_read_only = False):
+        """
+        :param optional: If this is True, the attribute may be left unassigned (i.e. null).  Otherwise,
+            a non-null value must be provided for all instances.
+        :param label: Human readable string for use in the user interface.  Use this if the programmatic
+            attribute name in the resource declaration is not appropriate for presentation to the user.
+        :param hidden: If this is True, this attribute will not be included as a column in the tabular view
+            of storage resources.
+        :param user_read_only: If this is True, this attribute can only be set internally by the plugin, not
+            by the user.  For example, a controller might have some attributes entered by the user, and some
+            read from the hardware: those read from the hardware would be marked `user_read_only`.  Attributes
+            which are `user_read_only` must also be `optional`.
+        """
         self.optional = optional
-        self.subscribe = subscribe
-        self.provide = provide
         self.label = label
         self.hidden = hidden
         self.user_read_only = user_read_only
