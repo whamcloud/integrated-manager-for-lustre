3afd1a224c43bbd8c4b7c25040b463a171942789
Aric Coady <aric.coady@intel.com>
2012-10-11 13:04:31 -0700

    HYD-1450 Plugins support default and custom charts.

--- chroma-manager/tests/unit/chroma_core/lib/storage_plugin/test_resource.py
+++ chroma-manager/tests/unit/chroma_core/lib/storage_plugin/test_resource.py
@@ -1,6 +1,6 @@
 
 from django.test import TestCase
-from chroma_core.lib.storage_plugin.api import attributes
+from chroma_core.lib.storage_plugin.api import attributes, statistics
 from chroma_core.lib.storage_plugin.api.identifiers import GlobalId
 from chroma_core.lib.storage_plugin.base_resource import BaseStorageResource
 
@@ -18,17 +18,22 @@ class TestDefaults2(BaseStorageResource):
 
     name = attributes.String()
     name_scope = attributes.String()
+    read = statistics.Gauge()
+    write = statistics.Gauge()
 
 
 class TestOverrides(BaseStorageResource):
     class Meta:
         identifier = GlobalId('name')
         label = "Alpha"
+        charts = [{'title': 'IO', 'series': ['read', 'write']}]
 
     def get_label(self):
         return "Bravo"
 
     name = attributes.String()
+    read = statistics.Gauge()
+    write = statistics.Gauge()
 
 
 class TestDisplayNames(TestCase):
@@ -38,8 +43,10 @@ class TestDisplayNames(TestCase):
 
         td2 = TestDefaults2(name = "foo", name_scope = "bar")
         self.assertEqual(td2.get_label(), "TestDefaults2 ('foo', 'bar')")
+        self.assertEqual(len(td2.get_charts()), 2)
 
     def test_overrides(self):
         to = TestOverrides(name = "foo")
         self.assertEqual(to.get_label(), "Bravo")
         self.assertEqual(to._meta.label, "Alpha")
+        self.assertEqual(len(to.get_charts()), 1)
