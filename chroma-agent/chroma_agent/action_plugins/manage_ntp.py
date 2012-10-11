069050d1f5143058f126a4cfb5d5667482df95d4
John Spray <john.spray@intel.com>
2012-10-11 11:23:11 +0100

    manage_ntp robustness:  * Cope with inserting server line even if there is no existing    server line to insert next to  * Don't try to sync time when unconfiguring

--- chroma-agent/chroma_agent/action_plugins/manage_ntp.py
+++ chroma-agent/chroma_agent/action_plugins/manage_ntp.py
@@ -23,19 +23,29 @@ def configure_ntp(args):
     tmp_f, tmp_name = mkstemp(dir = '/etc')
     f = open('/etc/ntp.conf', 'r')
     added_server = False
+    COMMENT_PREFIX = "# Commented by chroma-agent: "
+    ADD_SUFFIX = " # Added by chroma-agent"
     for line in f.readlines():
-        if args.node == "":
-            if line.startswith("server "):
-                continue
-            elif line.startswith("# Commented by chroma-agent: "):
-                line = line[29:]
-        else:
+        if args.node:
+            # Comment out existing server lines and add one of our own
             if line.startswith("server "):
                 if not added_server:
-                    os.write(tmp_f, "server %s\n" % args.node)
+                    os.write(tmp_f, "server %s%s\n" % (args.node, ADD_SUFFIX))
                     added_server = True
-                line = "# Commented by chroma-agent: %s" % line
+                line = "%s%s" % (COMMENT_PREFIX, line)
+        else:
+            # Remove anything we added, and uncomment anything we commented
+            if line.startswith("server "):
+                continue
+            elif line.startswith(COMMENT_PREFIX):
+                line = line[len(COMMENT_PREFIX):]
         os.write(tmp_f, line)
+
+    if args.node and not added_server:
+        # This can happen if there was no existing 'server' line for
+        # us to insert before
+        os.write(tmp_f, "server %s%s\n" % (args.node, ADD_SUFFIX))
+
     f.close()
     os.close(tmp_f)
     os.chmod(tmp_name, 0644)
@@ -43,25 +53,29 @@ def configure_ntp(args):
         os.rename("/etc/ntp.conf", "/etc/ntp.conf.pre-chroma")
     os.rename(tmp_name, "/etc/ntp.conf")
 
-    # make sure the time is very close before letting ntpd take over
-    shell.try_run(['service', 'ntpd', 'stop'])
+    if args.node:
+        # If we have a server, sync time to it now before letting ntpd take over
+        shell.try_run(['service', 'ntpd', 'stop'])
 
-    timeout = 300
-    sleep_time = 5
-    while timeout > 0:
-        rc, stdout, stderr = shell.run(['service', 'ntpdate', 'restart'])
-        if rc == 0:
-            break
-        else:
-            sleep(sleep_time)
-            timeout = timeout - sleep_time
+        timeout = 300
+        sleep_time = 5
+        while timeout > 0:
+            rc, stdout, stderr = shell.run(['service', 'ntpdate', 'restart'])
+            if rc == 0:
+                break
+            else:
+                sleep(sleep_time)
+                timeout = timeout - sleep_time
 
-    # did we time out?
-    if timeout < 1:
-        raise RuntimeError("Timed out waiting for time sync from the Chroma Manager.  You could try waiting a few minutes and clicking \"Set up server\" for this server")
+        # did we time out?
+        if timeout < 1:
+            raise RuntimeError("Timed out waiting for time sync from the Chroma Manager.  You could try waiting a few minutes and clicking \"Set up server\" for this server")
 
-    # signal the process
-    shell.try_run(['service', 'ntpd', 'start'])
+        # signal the process
+        shell.try_run(['service', 'ntpd', 'start'])
+    else:
+        # With no server, just restart ntpd, don't worry about the sync
+        shell.try_run(['service', 'ntpd', 'restart'])
 
 
 class RsyslogPlugin(ActionPlugin):
