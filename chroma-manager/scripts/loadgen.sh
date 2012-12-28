84a465c97ac63900f1560bc8119388b6af084ed2
Brian J. Murrell <brian@whamcloud.com>
2012-12-28 16:25:06 -0500

    Allow Multiple threads with loadgen.sh
    Also added another mount retry loop similar to
    f26d0f43daf2c1894de93e416ee7740e029ae1b9.

--- chroma-manager/scripts/loadgen.sh
+++ chroma-manager/scripts/loadgen.sh
@@ -24,37 +24,52 @@ if [[ $2 ]]; then
 fi
 
 FSNAME=${FSNAME:-"lustre"}
-LUSTRE=${LUSTRE:-"/mnt/client/$FSNAME"}
+LUSTRE_BASE=${LUSTRE:-"/mnt/client/$FSNAME"}
 MGSNID=${MGSNID:-"10.10.0.2@tcp"}
 STRIPE_COUNT=${STRIPE_COUNT:=-1}
 MGSPATH=${MGSPATH:-$MGSNID:/$FSNAME}
+THREADS=${THREADS:=1}
 
-if ! grep -q $LUSTRE /proc/mounts; then
-  mkdir -p $LUSTRE
-  mount -tlustre $MGSPATH $LUSTRE
-#  lfs setstripe $LUSTRE -c $STRIPE_COUNT
-else
-  MGSPATH=$(grep $LUSTRE /proc/mounts | sed -e 's/ .*//')
-fi
-
-
-while true; do
-    name=$LUSTRE/cptest.$(uname -n)/
-    cp -pr /lib/ $name
-    sleep 1
-    rm -rf $name
-
-    sleep 1
-    name=$LUSTRE/iotest-$(uname -n)
-    rm -f $name
-    lfs setstripe -c $STRIPE_COUNT $name
-    dd if=/dev/zero of=$name bs=128k count=10k
-    umount $LUSTRE
-    while ! mount -tlustre $MGSPATH $LUSTRE; do
-        echo "failed to mount.  trying again in a sec..."
+for ((thread=1; $thread <= $THREADS; thread++)); do
+(
+    if [ $THREADS -gt 1 ]; then
+        LUSTRE="$LUSTRE_BASE-$thread"
+    else
+        LUSTRE="$LUSTRE_BASE"
+    fi
+    if ! grep -q $LUSTRE /proc/mounts; then
+        mkdir -p $LUSTRE
+        while ! mount -tlustre $MGSPATH $LUSTRE; do
+            echo "failed to mount.  trying again in a sec..."
+            sleep 1
+        done
+        #lfs setstripe $LUSTRE -c $STRIPE_COUNT
+    else
+        MGSPATH=$(grep $LUSTRE /proc/mounts | sed -e 's/ .*//')
+    fi
+    
+    
+    while true; do
+        name=$LUSTRE/cptest.$(uname -n)/
+        cp -pr /lib/ $name
         sleep 1
+        rm -rf $name
+    
+        sleep 1
+        name=$LUSTRE/iotest-$(uname -n)
+        rm -f $name
+        lfs setstripe -c $STRIPE_COUNT $name
+        dd if=/dev/zero of=$name bs=128k count=10k
+        umount $LUSTRE
+        while ! mount -tlustre $MGSPATH $LUSTRE; do
+            echo "failed to mount.  trying again in a sec..."
+            sleep 1
+        done
+        dd of=/dev/zero if=$name bs=128k count=10k
+        rm -f $name
+        sleep $((RANDOM % 100 / 4))
     done
-    dd of=/dev/zero if=$name bs=128k count=10k
-    rm -f $name
-    sleep $((RANDOM % 100 / 4))
+) &
 done
+
+wait
