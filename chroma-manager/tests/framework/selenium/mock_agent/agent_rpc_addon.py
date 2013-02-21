f965e3dcdaac21f0ba520a0ae54bb9eb17cdaef6
John Spray <john.spray@intel.com>
2013-02-21 12:23:11 +0000

    Monkey patch ManagedHost.corosync_reported_up to default to True
    This avoids the lnet state always being 'lnet down' in the UI
    when running against MockAgent.
    
    Change-Id: Ib62c5df8f2c6e414df018fadf7e69a9021cd1c09

--- chroma-manager/tests/framework/selenium/mock_agent/agent_rpc_addon.py
+++ chroma-manager/tests/framework/selenium/mock_agent/agent_rpc_addon.py
@@ -19,3 +19,8 @@ MockAgentRpc.mock_servers = {
 import json
 MockAgentRpc.mock_servers['kp-ss-storage-appliance-1']['device-plugin'] = json.load(open('/usr/share/chroma-manager/kp-ss-storage-appliance-1.json'))['result']
 MockAgentRpc.mock_servers['kp-ss-storage-appliance-2']['device-plugin'] = json.load(open('/usr/share/chroma-manager/kp-ss-storage-appliance-2.json'))['result']
+
+# In the absence of monitoring input, ManagedHosts will usually always say 'lnet down' in
+# the UI because they're considered offline from a corosync POV: override this for MockAgent
+from chroma_core.models import ManagedHost
+[f for f in ManagedHost._meta.fields if f.name == 'corosync_reported_up'][0].default = True
