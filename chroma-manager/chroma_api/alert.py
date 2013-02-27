whamcloud/integrated-manager-for-lustre

commit 7efb756c3c31118351fe2e675d13e2f4910e00ba

HYD-1705: Doc language changes from REST_API_doc_changes.pdf and some added help_text attributes.
Change-Id: I8f0c12496256f715258f2f22318ad904dfcdd2d8

chroma-manager/chroma_api/alert.py
@@ -68,6 +68,10 @@ class AlertSubscriptionResource(ModelResource):
 
 
 class AlertTypeResource(Resource):
+    """
+    A list of possible alert types.  Use for
+    populating alert subscriptions.
+    """
     id = fields.CharField()
     description = fields.CharField()
 
@@ -118,9 +122,9 @@ class AlertTypeResource(Resource):
 
 class AlertResource(ModelResource):
     """
-    A bad health state.  Alerts refer to particular objects (such as
-    servers or targets), and can either be active (this is a current
-    problem) or inactive (this is a historical record of a problem).
+    Notification of a bad health state.  Alerts refer to particular objects (such as
+    servers or targets), and can either be active (indicating this is a current
+    problem) or inactive (indicating this is a historical record of a problem).
 
     The ``alert_item_content_type_id`` and ``alert_item_id`` attributes
     together provide a unique reference to the object to which the
@@ -128,15 +132,17 @@ class AlertResource(ModelResource):
     """
     message = fields.CharField(readonly = True, help_text = "Human readable description\
             of the alert, about one sentence")
-    alert_item_content_type_id = fields.IntegerField()
-    alert_item = fields.CharField()
+    alert_item_content_type_id = fields.IntegerField(help_text = "ID of affected item")
+    alert_item_content_id = fields.IntegerField(help_text = "Content type ID of affected item")
+    alert_item = fields.CharField(help_text = "URI of affected item")
     active = fields.BooleanField(attribute = 'active', null = True,
-            help_text = "True if the alert is a current issue, false\
-            if it is historical")
+                                 help_text = "``true`` if the alert is a current issue, ``false`` if it is historical")
 
     affected = fields.ListField(null = True, help_text = "List of objects which\
             are affected by the alert (e.g. a target alert also affects the\
-            filesystem to which the target belongs)")
+            file system to which the target belongs)")
+
+    dismissed = fields.BooleanField(help_text = "If ``true``, the alert should not be presented for operator attention")
 
     def dehydrate_alert_item(self, bundle):
         from chroma_api.urls import api
@@ -155,7 +161,7 @@ class AlertResource(ModelResource):
 
         from chroma_core.models import StorageResourceAlert, StorageAlertPropagated
         from chroma_core.models import Volume
-        from chroma_core.models import  ManagedMgs
+        from chroma_core.models import ManagedMgs
         from chroma_core.models import FilesystemMember
         from chroma_core.models import TargetOfflineAlert, TargetRecoveryAlert, TargetFailoverAlert, HostContactAlert
 
@@ -194,7 +200,7 @@ class AlertResource(ModelResource):
                 "id": ao.pk,
                 "content_type_id": ct.pk,
                 "resource_uri": api.get_resource_uri(ao)
-                })
+            })
 
         return result
         # <<
@@ -215,8 +221,8 @@ class AlertResource(ModelResource):
         return bundle.obj.alert_item_type.id
 
     alert_item_str = fields.CharField(readonly = True,
-            help_text = "A human readable noun describing the object\
-            that is the subject of the alert")
+                                      help_text = "A human readable noun describing the object\
+                                      that is the subject of the alert")
 
     class Meta:
         queryset = AlertState.objects.all()

chroma-manager/chroma_api/command.py
@@ -50,17 +50,20 @@ class CommandValidation(Validation):
 
 class CommandResource(ModelResource):
     """
-    Commands are created for asynchronous user actions (202 ACCEPTED responses
-    contain command objects).  The command resource can be used to query the details
-    and state of these asynchronous operations.
+    Asynchronous user-initiated operations which create, remove or modify resources are
+    represented by ``command`` objects.  When a PUT, POST, PATCH or DELETE to
+    a resource returns a 202 ACCEPTED response, the response body contains
+    a command identifier.  The ``command`` resource is used to query the status
+    of these asynchronous operations.
 
     Typically this is used to poll a command for completion and find out whether it
     succeeded.
     """
     jobs = fields.ToManyField("chroma_api.job.JobResource", 'jobs',
-            help_text = "Jobs belonging to this command")
+                              help_text = "Jobs belonging to this command")
 
-    logs = fields.CharField()
+    logs = fields.CharField(help_text = "String.  Concatentation of all user-visible logs from the"
+                            "``job`` objects associated with this command.")
 
     def dehydrate_logs(self, bundle):
         command = bundle.obj
@@ -77,7 +80,7 @@ class CommandResource(ModelResource):
         validation = CommandValidation()
         always_return_data = True
 
-    def obj_create(self, bundle, request = None):
+    def obj_create(self, bundle, request = None, **kwargs):
         for job in bundle.data['jobs']:
             # FIXME: HYD-1367: This is a hack to work around the inability of
             # the Job class to handle m2m references properly, serializing hosts

chroma-manager/chroma_api/configuration.py
@@ -102,16 +102,13 @@ class Configuration(object):
 
 class ConfigurationResource(Resource):
     """
-    By default this will give a configuration dump that is suitable
-    for re-creating a Chroma server managing the same targets that
-    are managed by this Chroma server.
-
-    Pass the 'template=True' as a parameter to a GET in order to receive
-    a template suitable for creating a new filesystem on identically named
-    hosts/devices.
+    A configuration dump that is suitable
+    for re-creating a Command Center server managing the same targets that
+    are managed by this Command Center server.
     """
-    mgts = fields.ListField()
-    hosts = fields.ListField()
+    mgts = fields.ListField(help_text = "List of MGTs (see ``target`` resource)")
+    hosts = fields.ListField(help_text = "List of hosts (see ``host`` resource)."
+                                         "Only the ``address`` attribute is populated.")
 
     class Meta:
         resource_name = 'configuration'

chroma-manager/chroma_api/event.py
@@ -24,23 +24,31 @@ STR_TO_SEVERITY = dict([(logging.getLevelName(level), level) for level in [
 
 class EventResource(ModelResource):
     """
-    Events are messages generated by Chroma to represent what
-    changes on hosts or filesystems it monitors.  Events are essentially
+    Events are messages generated by the Command Center to represent what
+    changes on hosts or file systems it monitors.  Events are essentially
     log messages, separate from the ``log`` resource because they are
     internally synthesized rather than gathered from syslog.
+
+    An event is a message generated by the Command Center to indicate a
+    change on a host or in a file system being monitored. Events are similar
+    to log messages, but do not appear in the log resource because they are
+    internally synthesized rather than received via syslog.
     """
-    host_name = fields.CharField(attribute = 'host', blank = True, null = True,
-            help_text = "The ``label`` attribute of the host on which the event\
+    host_name = fields.CharField(
+        attribute = 'host', blank = True, null = True,
+        help_text = "The ``label`` attribute of the host on which the event\
                     occurred, or null if the event is not specific to a single\
                     host")
-    host = fields.ToOneField('chroma_api.host.HostResource', 'host', null = True,
-            help_text = "The host on which the event occurred, or null if the event\
+    host = fields.ToOneField(
+        'chroma_api.host.HostResource', 'host', null = True,
+        help_text = "The host on which the event occurred, or null if the event\
                     is not specific to a single host")
     message = fields.CharField()
 
-    severity = fields.CharField(attribute='severity',
-            help_text = "String indicating the severity of the\
-            event, one of %s" % STR_TO_SEVERITY.keys())
+    severity = fields.CharField(
+        attribute='severity',
+        help_text = "String indicating the severity of the\
+                    event, one of %s" % STR_TO_SEVERITY.keys())
 
     class Meta:
         queryset = Event.objects.all()

chroma-manager/chroma_api/filesystem.py
@@ -29,7 +29,7 @@ class FilesystemValidation(Validation):
     def _validate_put(self, bundle, request):
         errors = defaultdict(list)
 
-        if 'conf_params' in bundle.data and bundle.data['conf_params'] != None:
+        if 'conf_params' in bundle.data and bundle.data['conf_params'] is not None:
             try:
                 fs = ManagedFilesystem.objects.get(pk = bundle.data['id'])
             except ManagedFilesystem.DoesNotExist:
@@ -265,28 +265,29 @@ class FilesystemValidation(Validation):
 
 class FilesystemResource(MetricResource, ConfParamResource):
     """
-    A Lustre filesystem, consisting of one or mode MDTs, and one or more OSTs.
+    A Lustre file system, associated with exactly one MGT and consisting of
+    one or mode MDTs and one or more OSTs.
 
-    When POSTing to create a filesystem, specify volumes to use like this:
+    When using POST to create a file system, specify volumes to use like this:
     ::
 
         {osts: [{volume_id: 22}],
         mdt: {volume_id: 23},
         mgt: {volume_id: 24}}
 
-    To create a filesystem using an existing MGT instead of creating a new
+    To create a file system using an existing MGT instead of creating a new
     MGT, set the `id` attribute instead of the `volume_id` attribute for
     that target (i.e. `mgt: {id: 123}`).
 
-    Note: Lustre filesystems are owned by an MGT, and the ``name`` of a filesystem
+    Note: A Lustre file system is owned by an MGT, and the ``name`` of the file system
     is unique within that MGT.  Do not use ``name`` as a globally unique identifier
-    for filesystems in your application.
+    for a file system in your application.
     """
     bytes_free = fields.IntegerField()
     bytes_total = fields.IntegerField()
     files_free = fields.IntegerField()
     files_total = fields.IntegerField()
-    client_count = fields.IntegerField()
+    client_count = fields.IntegerField(help_text = "Number of Lustre clients which are connected to this file system")
 
     mount_command = fields.CharField(null = True, help_text = "Example command for\
             mounting this file system on a Lustre client, e.g. \"mount -t lustre 192.168.0.1:/testfs /mnt/testfs\"")
@@ -295,16 +296,16 @@ class FilesystemResource(MetricResource, ConfParamResource):
             on a Lustre client, e.g. \"192.168.0.1:/testfs\"")
 
     osts = fields.ToManyField('chroma_api.target.TargetResource', null = True,
-            attribute = lambda bundle: ManagedOst.objects.filter(filesystem = bundle.obj),
-            help_text = "List of OSTs which belong to this file system")
+                              attribute = lambda bundle: ManagedOst.objects.filter(filesystem = bundle.obj),
+                              help_text = "List of OSTs which belong to this file system")
     # NB a filesystem must always report an MDT, although it may be deleted just before
     # the filesystem is deleted, so use _base_manager
-    mdts = fields.ToManyField('chroma_api.target.TargetResource',
-            attribute = lambda bundle: ManagedMdt._base_manager.filter(filesystem = bundle.obj), full = True,
-            help_text = "List of MDTs in this file system, should be at least 1 unless the file system\
-            is in the process of being deleted")
+    mdts = fields.ToManyField('chroma_api.target.TargetResource', full = True,
+                              attribute = lambda bundle: ManagedMdt._base_manager.filter(filesystem = bundle.obj),
+                              help_text = "List of MDTs in this file system, should be at least 1 unless the "
+                                          "file system is in the process of being deleted")
     mgt = fields.ToOneField('chroma_api.target.TargetResource', attribute = 'mgs', full = True,
-            help_text = "The MGT on which this file system is registered")
+                            help_text = "The MGT on which this file system is registered")
 
     def _get_stat_simple(self, bundle, klass, stat_name, factor = 1):
         try:
@@ -402,5 +403,7 @@ class FilesystemResource(MetricResource, ConfParamResource):
         filesystem_data = self.full_dehydrate(self.build_bundle(obj = fs)).data
 
         raise custom_response(self, request, http.HttpAccepted,
-                {'command': dehydrate_command(command),
-                 'filesystem': filesystem_data})
+                              {
+                                  'command': dehydrate_command(command),
+                                  'filesystem': filesystem_data
+                              })

chroma-manager/chroma_api/group.py
@@ -16,7 +16,7 @@ class GroupResource(ModelResource):
     A user group.  Users inherit the permissions
     of groups of which they are a member.
 
-    Chroma groups are used internally to refer
+    Groups are used internally to refer
     to factory-configured profiles, so this resource
     is read-only.
     """

chroma-manager/chroma_api/help.py
@@ -18,15 +18,16 @@ class HelpResource(Resource):
     """
     This resource provides contextual help for use in user interfaces.
 
-    ``/conf_param/`` sub-url provides help for Lustre configuration parameters.  GET
-    this resource in one of two ways:
+    GETs to the ``/conf_param/`` sub-url respond with help for Lustre configuration
+    parameters.  There are two ways to do this GET:
 
     * Set the ``keys`` parameter to a comma-separated list of configuration parameter
       names to get help for particular parameters.
     * Set the ``kind`` parameter to one of 'OST', 'MDT' or 'FS' to get all possible
       configuration parameters for this type of object.
 
-    The response is a dictionary of configuration parameter name to help string.
+    The response is a dictionary where the key is a configuration parameter name
+    and the value is a help string.
     """
     class Meta:
         object_class = dict
@@ -37,7 +38,7 @@ class HelpResource(Resource):
         authentication = AnonymousAuthentication()
 
     def override_urls(self):
-        from django.conf.urls.defaults import url
+        from django.conf.urls import url
         return [
             url(r"^(?P<resource_name>%s)/conf_param/$" % self._meta.resource_name, self.wrap_view('conf_param_help'), name="api_conf_param_help"),
         ]

chroma-manager/chroma_api/host.py
@@ -49,7 +49,11 @@ class HostValidation(Validation):
 
 class HostResource(MetricResource, StatefulModelResource):
     """
-    Represents a Lustre server which Chroma server is monitoring or managing.  When PUTing, requires the ``state`` field.  When POSTing, requires the ``address`` field.
+    Represents a Lustre server that is being monitored and managed from the Command Center.
+
+    PUTs to this resource must have the ``state`` attribute set.
+
+    POSTs to this resource must have the ``address`` attribute set.
     """
     nids = fields.ListField(null = True)
 
@@ -71,7 +75,7 @@ class HostResource(MetricResource, StatefulModelResource):
 
         filtering = {'id': ['exact'],
                      'fqdn': ['exact', 'startswith'],
-                    'role': ['exact']}
+                     'role': ['exact']}
 
     def obj_create(self, bundle, request = None, **kwargs):
 

commit 0be65567213f15779935b82ad1391a3d86ef6093

Fix the chroma_api.alert resource (broke it in 7efb756c)
Change-Id: I4f6287a0df2943f70ff64a360d904d3fefc6acd3

chroma-manager/chroma_api/alert.py
@@ -142,7 +142,8 @@ class AlertResource(ModelResource):
             are affected by the alert (e.g. a target alert also affects the\
             file system to which the target belongs)")
 
-    dismissed = fields.BooleanField(help_text = "If ``true``, the alert should not be presented for operator attention")
+    dismissed = fields.BooleanField(attribute = 'dismissed',
+                                    help_text = "If ``true``, the alert should not be presented for operator attention")
 
     def dehydrate_alert_item(self, bundle):
         from chroma_api.urls import api