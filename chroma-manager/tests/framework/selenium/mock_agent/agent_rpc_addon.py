2c0f651ef77f722120306852f862c8bda80e96e6
John Spray <john.spray@intel.com>
2013-04-16 11:14:44 +0100

    HYD-1897: Update MockAgent for HYD-1715, and relax UI display of device nodes which were not chosen by the balancing code.
    Also put a usable copy of the CI's selenium config into
    git so that I can reproduce its behaviour.
    
    Change-Id: If3d0e90fd5e4ec1b4a034df72e78e88b09adfc14

--- chroma-manager/tests/framework/selenium/mock_agent/agent_rpc_addon.py
+++ chroma-manager/tests/framework/selenium/mock_agent/agent_rpc_addon.py
@@ -1,4 +1,3 @@
-
 from tests.unit.chroma_core.helper import MockAgentRpc
 from tests.unit.chroma_core.helper import MockAgentSsh
 AgentRpc = MockAgentRpc
@@ -17,10 +16,44 @@ MockAgentRpc.mock_servers = {
 }
 
 import json
-MockAgentRpc.mock_servers['kp-ss-storage-appliance-1']['device-plugin'] = json.load(open('/usr/share/chroma-manager/kp-ss-storage-appliance-1.json'))['result']
-MockAgentRpc.mock_servers['kp-ss-storage-appliance-2']['device-plugin'] = json.load(open('/usr/share/chroma-manager/kp-ss-storage-appliance-2.json'))['result']
+import os
+CI_LOCATION = '/usr/share/chroma-manager/'
+DEV_LOCATION = "tests/framework/selenium/mock_agent/"
+LOCATION = DEV_LOCATION if os.path.exists(DEV_LOCATION) else CI_LOCATION
+MockAgentRpc.mock_servers['kp-ss-storage-appliance-1']['device-plugin'] = json.load(open(os.path.join(LOCATION, 'kp-ss-storage-appliance-1.json')))['result']
+MockAgentRpc.mock_servers['kp-ss-storage-appliance-2']['device-plugin'] = json.load(open(os.path.join(LOCATION, 'kp-ss-storage-appliance-2.json')))['result']
 
 # In the absence of monitoring input, ManagedHosts will usually always say 'lnet down' in
 # the UI because they're considered offline from a corosync POV: override this for MockAgent
 from chroma_core.models import ManagedHost
 [f for f in ManagedHost._meta.fields if f.name == 'corosync_reported_up'][0].default = True
+
+from chroma_core.services.job_scheduler.job_scheduler import JobScheduler
+old_create_host = JobScheduler.create_host
+
+
+def create_host(self, *args, **kwargs):
+    from chroma_core.models import ManagedHost
+    from django.db.models import Q
+    host_id, command_id = old_create_host(self, *args, **kwargs)
+    added_host = ManagedHost.objects.get(id=host_id)
+    for host in ManagedHost.objects.filter(~Q(id=host_id)):
+        added_host.ha_cluster_peers.add(host)
+
+    return host_id, command_id
+
+JobScheduler.create_host = create_host
+
+
+def test_host_contact(self, address):
+    ok = address in MockAgentRpc.mock_servers
+    return {
+        'address': ok,
+        'resolve': ok,
+        'ping': ok,
+        'agent': ok,
+        'reverse_resolve': ok,
+        'reverse_ping': ok
+    }
+
+JobScheduler.test_host_contact = test_host_contact
