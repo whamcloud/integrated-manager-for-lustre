9aee7b154a33a0c10f373b1c2a122fa397f3f831
Michael MacDonald <michael.macdonald@intel.com>
2013-05-10 09:58:58 -0400

    HYD-2014: fence_chroma should support --action
    This seems to be the preferred name (instead of --option) for
    most stdin and CLI arguments.
    
    Change-Id: I57a123ce3f09694c358413af6e140f9cc61205a6

--- chroma-agent/chroma_agent/fence_chroma.py
+++ chroma-agent/chroma_agent/fence_chroma.py
@@ -54,6 +54,8 @@ def stdin_to_args(stdin_lines=None):
 
 
 def main(args=None):
+    VALID_ACTIONS = ["off", "on", "reboot", "metadata", "list", "monitor"]
+
     epilog = """
 With no command line argument, arguments are read from standard input.
 Arguments read from standard input take the form of:
@@ -61,67 +63,58 @@ Arguments read from standard input take the form of:
     arg1=value1
     arg2=value2
 
-  option                Action to perform (off, on, reboot, metadata, monitor, list)
-  nodename              Name of node on which to perform action
-  port                  Optional port parameter, aliases to nodename
-"""
+  action                Action to perform (%s)
+  port                  Name of node on which to perform action
+""" % ", ".join(VALID_ACTIONS)
 
     parser = ArgumentParser(description="Chroma Fence Agent",
                             formatter_class=RawDescriptionHelpFormatter,
                             epilog=epilog)
 
-    parser.add_argument("-o", "--option", choices=["off", "on", "reboot", "metadata", "list", "monitor"])
-    parser.add_argument("-H", "--nodename", help="Name of node on which to perform action")
-    parser.add_argument("-n", "--port", help="Optional port parameter, aliases to nodename")
+    parser.add_argument("-o", "--option", "--action", dest="action", choices=VALID_ACTIONS)
+    parser.add_argument("-n", "--plug", "--nodename", "--port", dest="port", help="Name of node on which to perform action")
     ns = parser.parse_args(args)
 
-    if not ns.option and not ns.nodename:
+    if not ns.action and not ns.port:
         ns = parser.parse_args(stdin_to_args())
 
-    # This all bears some explanation. When pacemaker invokes stonith, it
-    # sends parameters via stdin rather than via command-line options. It
-    # always sends nodename, hence the preference for its use.
-    if ns.nodename and ns.port and not ns.nodename == ns.port:
-        sys.stderr.write("Both nodename and port were supplied, but do not match!\n")
-        sys.exit(1)
-    elif ns.port and not ns.nodename:
-        ns.nodename = ns.port
-
-    if ns.option == "metadata":
+    if ns.action == "metadata":
         print """
 <?xml version="1.0" ?>
 <resource-agent name="fence_chroma" shortdesc="Fence agent for Intel Manager for Lustre Storage Servers">
 <longdesc>fence_chroma is an I/O Fencing agent which can be used with Intel Manager for Lustre Storage Servers.</longdesc>
 <parameters>
-    <parameter name="nodename">
-        <getopt mixed="-H" />
+    <parameter name="port">
+        <getopt mixed="-p" />
         <content type="string" />
         <shortdesc lang="en">Storage Server (machine name) to fence</shortdesc>
     </parameter>
-    <parameter name="option">
+    <parameter name="action">
         <getopt mixed="-o" />
-        <content type="string" default="reboot" />
-        <shortdesc lang="en">Fencing action ([reboot], metadata, list)</shortdesc>
+        <content type="string" />
+        <shortdesc lang="en">Fencing action (%s)</shortdesc>
     </parameter>
 </parameters>
 <actions>
     <action name="reboot" />
+    <action name="off" />
+    <action name="on" />
     <action name="metadata" />
     <action name="list" />
     <action name="monitor" />
 </actions>
 </resource-agent>
-"""
-    elif ns.option in ["on", "off"]:
-        node = p_cfg.get_node(ns.nodename)
-        getattr(node, "fence_%s" % ns.option)()
-    elif ns.option == "reboot":
-        manage_node.stonith(ns.nodename)
-    elif ns.option == "monitor":
+""" % ", ".join(VALID_ACTIONS)
+    elif ns.action in ["on", "off"]:
+        node = p_cfg.get_node(ns.port)
+        getattr(node, "fence_%s" % ns.action)()
+    elif ns.action == "reboot":
+        manage_node.stonith(ns.port)
+    elif ns.action == "monitor":
         # TODO: What does "monitor" mean for this agent? We have to have it
         # to keep pacemaker happy, but longer-term it might make sense to
-        # make this a meta-monitor, in that it invokes the monitor option for
+        # make this a meta-monitor, in that it invokes the monitor action for
         # all sub-agents and aggregates the results.
         sys.exit(0)
-    elif ns.option == "list":
+    elif ns.action == "list":
         list_fenceable_nodes()
