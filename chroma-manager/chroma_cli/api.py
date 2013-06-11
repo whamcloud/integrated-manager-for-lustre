whamcloud/integrated-manager-for-lustre

commit ba3fa2d6cab3f02d383b930a4847ff2e58abca7c

Handle server profiles in the CLI
CLI: Add support for specifying a server profile while adding a server
API: Rename 'profile' resource to 'server_profile', and make it a true
ToOneField (i.e. URIs instead of IDs)

Change-Id: I09b59e3860837cec79416fffcaf010ebea41efe6

chroma-manager/chroma_cli/api.py
@@ -21,6 +21,7 @@
 
 
 import json
+from urlparse import urljoin
 
 from chroma_cli.exceptions import InvalidApiResource, UnsupportedFormat, NotFound, TooManyMatches, BadRequest, InternalError, UnauthorizedRequest, AuthenticationFailure, ApiConnectionError
 
@@ -71,7 +72,6 @@ class ChromaSessionClient(object):
 
     @property
     def session_uri(self):
-        from urlparse import urljoin
         return urljoin(self.api_uri, "session/")
 
     def start_session(self):
@@ -240,7 +240,6 @@ class ApiHandle(object):
         if not re.search(r"^http(s)?://", base_uri):
             base_uri = "http://" + base_uri
         if not re.search(r"/api(/?)$", base_uri):
-            from urlparse import urljoin
             base_uri = urljoin(base_uri, "/api/")
 
         return base_uri
@@ -259,7 +258,6 @@ class ApiHandle(object):
             return content
 
     def send_and_decode(self, method_name, relative_url, data=None):
-        from urlparse import urljoin
         full_url = urljoin(self.base_url, relative_url)
 
         from requests import ConnectionError
@@ -369,7 +367,7 @@ class ApiEndpoint(object):
     def fields(self):
         return self.schema['fields']
 
-    def resolve_id(self, query):
+    def resolve_uri(self, query):
         try:
             # Slight hack here -- relies on the "name" field usually being
             # first in a reverse-sort in order to optimize for the most
@@ -393,22 +391,21 @@ class ApiEndpoint(object):
 
                     try:
                         if query in candidates[0][field]:
-                            return candidates[0]['id']
+                            return candidates[0]['resource_uri']
                     except (IndexError, KeyError):
                         continue
         except KeyError:
             # No filtering possible?
             pass
 
-        raise NotFound("Unable to resolve id for %s/%s" % (self.name, query))
+        raise NotFound("Unable to resolve URI for %s/%s" % (self.name, query))
 
     def resource_uri(self, subject):
         try:
             id = int(subject)
+            urljoin(self.uri, "%s/" % id)
         except (ValueError, TypeError):
-            id = self.resolve_id(subject)
-        from urlparse import urljoin
-        return urljoin(self.uri, "%s/" % id)
+            return urljoin(self.uri, self.resolve_uri(subject))
 
     def get_decoded(self, uri=None, **data):
         if not uri:

chroma-manager/chroma_api/host.py
@@ -21,7 +21,7 @@
 
 
 from collections import defaultdict
-from chroma_core.services.job_scheduler import job_scheduler_client
+from chroma_core.services.job_scheduler.job_scheduler_client import JobSchedulerClient
 from chroma_core.services.rpc import RpcError
 from tastypie.validation import Validation
 
@@ -101,6 +101,17 @@ def _host_params(bundle):
             'pkey_pw': bundle.data.get('private_key_passphrase')}
 
 
+class ServerProfileResource(ModelResource):
+    class Meta:
+        queryset = ServerProfile.objects.all()
+        resource_name = 'server_profile'
+        authentication = AnonymousAuthentication()
+        authorization = DjangoAuthorization()
+        list_allowed_methods = ['get']
+        readonly = ['ui_name']
+        filtering = {'name': ['exact']}
+
+
 class HostResource(MetricResource, StatefulModelResource):
     """
     Represents a Lustre server that is being monitored and managed from the Command Center.
@@ -115,6 +126,8 @@ class HostResource(MetricResource, StatefulModelResource):
                                                "public key on the new server.")
     private_key_passphrase = fields.CharField(help_text = "passphrase to "
                                                           "decrypt private key")
+
+    server_profile = fields.ToOneField(ServerProfileResource, 'server_profile')
 
     def dehydrate_nids(self, bundle):
         return [n.nid_string for n in Nid.objects.filter(
@@ -139,13 +152,15 @@ class HostResource(MetricResource, StatefulModelResource):
                      'role': ['exact']}
 
     def obj_create(self, bundle, request = None, **kwargs):
-
-        # FIXME: we get errors back just fine when something goes wrong
+        # FIXME HYD-1657: we get errors back just fine when something goes wrong
         # during registration, but the UI tries to format backtraces into
         # a 'validation errors' dialog which is pretty ugly.
 
+        # Resolve a server profile URI to a record
+        profile = self.fields['server_profile'].hydrate(bundle).obj
+
         try:
-            host, command = job_scheduler_client.JobSchedulerClient.create_host_ssh(profile=bundle.data['profile'],
+            host, command = JobSchedulerClient.create_host_ssh(server_profile=profile.name,
                                                                                     **_host_params(bundle))
         except RpcError, e:
             # Return 400, a failure here could mean the address was already occupied, or that
@@ -184,16 +199,6 @@ class HostResource(MetricResource, StatefulModelResource):
         return objects.distinct()
 
 
-class ServerProfileResource(ModelResource):
-    class Meta:
-        queryset = ServerProfile.objects.all()
-        resource_name = 'profile'
-        authentication = AnonymousAuthentication()
-        authorization = DjangoAuthorization()
-        list_allowed_methods = ['get']
-        readonly = ['ui_name']
-
-
 class HostTestResource(Resource):
     """
     A request to test a potential host address for accessibility, typically
@@ -229,7 +234,7 @@ class HostTestResource(Resource):
 
     def obj_create(self, bundle, request = None, **kwargs):
 
-        result = job_scheduler_client.JobSchedulerClient.test_host_contact(
+        result = JobSchedulerClient.test_host_contact(
             **_host_params(bundle))
 
         raise custom_response(self, request, http.HttpAccepted, result)

chroma-manager/chroma_cli/api_resource.py
@@ -96,6 +96,12 @@ class ApiResource(object):
             return out_time.strftime("%H:%M:%S")
 
 
+class ServerProfile(ApiResource):
+    def __init__(self, *args, **kwargs):
+        super(ServerProfile, self).__init__(*args, **kwargs)
+        self.list_columns.extend(["name", "ui_name", "ui_description", "managed", "bundles"])
+
+
 class Host(ApiResource):
     def __init__(self, *args, **kwargs):
         super(Host, self).__init__(*args, **kwargs)

chroma-manager/chroma_cli/handlers.py
@@ -282,7 +282,13 @@ class ServerHandler(Handler):
             raise BadUserInput("\n".join([message] + failures))
 
     def add(self, ns):
-        kwargs = {'address': ns.subject, 'profile': 'default'}
+        kwargs = {'address': ns.subject}
+
+        if not ns.server_profile:
+            raise BadUserInput("No server_profile supplied.")
+
+        kwargs['server_profile'] = self.api.endpoints['server_profile'].show(ns.server_profile[0])['resource_uri']
+
         if not ns.force:
             self.test_host(ns, **kwargs)
         self.output(self.api_endpoint.create(**kwargs))
@@ -304,6 +310,16 @@ class ServerHandler(Handler):
             'message': "Initiating a shutdown on host %s" % host.label
         }
         self.output(self.api.endpoints['command'].create(**kwargs))
+
+
+class ServerProfileHandler(Handler):
+    nouns = ['server_profile']
+    verbs = ['list']
+    intransitive_verbs = ['list']
+
+    def __init__(self, *args, **kwargs):
+        super(ServerProfileHandler, self).__init__(*args, **kwargs)
+        self.api_endpoint = self.api.endpoints['server_profile']
 
 
 class FilesystemHandler(Handler):

chroma-manager/chroma_core/models/server_profile.py
@@ -30,6 +30,13 @@ class ServerProfile(models.Model):
         help_text = "The bundles specified by this profile"
     )
 
+    @property
+    def id(self):
+        """
+        Work around tastypie bug, when calling get_resource_uri it looks for .id
+        """
+        return self.pk
+
     # TODO: add a 'default' flag so that we can consistently
     # present a default in the UI
 

chroma-manager/chroma_core/services/job_scheduler/job_scheduler_client.py
@@ -263,7 +263,7 @@ class JobSchedulerClient(object):
         JobSchedulerRpc().cancel_job(job_id)
 
     @classmethod
-    def create_host_ssh(cls, address, profile, root_pw=None, pkey=None, pkey_pw=None):
+    def create_host_ssh(cls, address, server_profile, root_pw=None, pkey=None, pkey_pw=None):
         """
         Create a host which will be set up using SSH
 
@@ -272,7 +272,7 @@ class JobSchedulerClient(object):
         """
         from chroma_core.models import ManagedHost, Command
 
-        host_id, command_id = JobSchedulerRpc().create_host_ssh(address, profile, root_pw, pkey, pkey_pw)
+        host_id, command_id = JobSchedulerRpc().create_host_ssh(address, server_profile, root_pw, pkey, pkey_pw)
         return ManagedHost.objects.get(pk=host_id), Command.objects.get(pk=command_id)
 
     @classmethod

chroma-manager/chroma_ui/static/js/server_configuration.js
@@ -206,10 +206,10 @@ function add_host_dialog() {
   });
 
   function get_profiles() {
-    Api.get("profile/", {limit: 0}, success_callback = function(data) {
+    Api.get("server_profile/", {limit: 0}, success_callback = function(data) {
       $.each(data.objects, function(i, profile) {
-        var option = "<option value='" + profile.name + "'>"+ profile.ui_name + "</option>";
-        $('div.add_host_dialog select[name=\'profile\']').prepend(option)
+        var option = "<option value='" + profile.resource_uri + "'>"+ profile.ui_name + "</option>";
+        $('div.add_host_dialog select[name=\'server_profile\']').prepend(option)
       });
     });
   }
@@ -237,7 +237,7 @@ function add_host_dialog() {
           .find('form')
           .find('#id_add_host_address').val();
 
-      post_params['profile'] = element
+      post_params['server_profile'] = element
           .find('form')
           .find('.add_server_profile').val();
 

chroma-manager/tests/feature/cli/features/environment.py
@@ -49,7 +49,6 @@ def before_all(context):
 def before_feature(context, feature):
     context.runner.setup_test_environment()
     context.old_db_config = context.runner.setup_databases()
-    context.cli_failure_expected = False
 
     from tests.unit.services.job_scheduler.job_test_case import JobTestCase
 
@@ -70,7 +69,7 @@ def before_feature(context, feature):
     from chroma_core.models import ManagedHost, Command
     from chroma_core.services.job_scheduler.agent_rpc import AgentRpc
 
-    def create_host_ssh(address, profile, root_pw, pkey, pkey_pw):
+    def create_host_ssh(address, server_profile, root_pw, pkey, pkey_pw):
         host_data = AgentRpc.mock_servers[address]
         host = synthetic_host(address, nids = host_data['nids'], fqdn=host_data['fqdn'], nodename=host_data['nodename'])
         ObjectCache.add(ManagedHost, host)
@@ -190,3 +189,6 @@ def after_feature(context, feature):
 #--    context.runner.teardown_databases(context.old_db_config)
 #--    context.runner.teardown_test_environment()
 #--    # Bob's your uncle.
+
+def before_scenario(context, scenario):
+    context.cli_failure_expected = False

commit 5c6194d773173ce697e60b67d0babf942eabf2ed

Fix CLI bug introduced with ba3fa2d6
Change-Id: I199c304ce0285057d8a86d5c5c86321d27751bf9

chroma-manager/chroma_cli/api.py
@@ -403,7 +403,7 @@ class ApiEndpoint(object):
     def resource_uri(self, subject):
         try:
             id = int(subject)
-            urljoin(self.uri, "%s/" % id)
+            return urljoin(self.uri, "%s/" % id)
         except (ValueError, TypeError):
             return urljoin(self.uri, self.resolve_uri(subject))
 