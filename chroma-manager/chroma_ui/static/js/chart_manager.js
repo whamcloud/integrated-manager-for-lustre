7076d03b41211658e81cd4e3ce6d578f0b71aba3
John Spray <john.spray@intel.com>
2013-06-21 12:32:54 +0100

    Remove some rogue code that was flagged in an IP scan
    Change-Id: If5272b888645579ff0599cfe591e9d63c58882cd

--- chroma-manager/chroma_ui/static/js/chart_manager.js
+++ chroma-manager/chroma_ui/static/js/chart_manager.js
@@ -99,30 +99,7 @@ Highcharts.Series.prototype.tooltipHeaderFormatter = function (key) {
  *     - ChartManager - Generator for a chart manager object
  *     - chart_manager - Global instance of the ChartManager
  */
-function dump(arr,level) {
-  var dumped_text = "";
-  if(!level) level = 0;
 
-  //The padding given at the beginning of the line.
-  var level_padding = "";
-  for(var j=0;j<level+1;j++) level_padding += "    ";
-
-  if(typeof(arr) == 'object') { //Array/Hashes/Objects
-    for(var item in arr) {
-      var value = arr[item];
-
-      if(typeof(value) == 'object') { //If it is an array,
-        dumped_text += level_padding + "'" + item + "' ...\n";
-        dumped_text += dump(value,level+1);
-      } else {
-        dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
-      }
-    }
-  } else { //Strings/Chars/Numbers etc.
-    dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
-  }
-  return dumped_text;
-}
 var ChartModel = function(options) {
 
     var config = $.extend(true,  {
