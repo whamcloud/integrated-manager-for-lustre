216c7349bac09bf568401bd90128ce424d776045
Brian J. Murrell <brian.murrell@intel.com>
2013-07-15 09:12:35 -0400

    HYD-1896 Don't fail cache_get() silently
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>
    Change-Id: I68f37b519502bdaaf29de7d112c0bd1b9bc1cb89

--- chroma-dependencies/include/cache.sh
+++ chroma-dependencies/include/cache.sh
@@ -1,8 +1,29 @@
 CACHE="${CACHE:-$(pwd)}"
 mkdir -p "$CACHE"
 
+file_valid() {
+    local file="$1"
+    local type="$2"
+
+
+    if [ ! -s "$file" ]; then
+        return 1
+    fi
+    if [ -n "$type" ]; then
+        file_type=$(file $file)
+        file_type=${file_type##*: }
+        if [[ $file_type != $type* ]]; then
+            return 1
+        fi
+    fi
+
+    return 0
+
+}
+
 cache_get() {
     local location="$1"
+    local type="$2"
 
     local name="${location##*/}"
     local entry="$CACHE/$name"
@@ -11,9 +32,14 @@ cache_get() {
     if [ -f "$entry" ]; then
         # and is not in the process of being created
         if [ ! -f "$lock" ]; then
-            # so it's all good
-            cp "$entry" .
-            return 0
+            if file_valid "$entry" "$type"; then
+                # so it's all good
+                cp "$entry" .
+                return 0
+            else
+                # but it's a 0-byte file, so invalid, remove and carry on
+                rm -f "$entry"
+            fi
         fi
         # still being created, so wait for it to be finished
         # TODO: detect the writer's death
@@ -40,7 +66,13 @@ cache_get() {
 
     # fetch to temporary location to make "appearance" atomic
     local tmp_slot="$entry.tmp"
-    curl -L -o "$tmp_slot" "$location" 2> "$lock"
+    if ! curl -k -L -o "$tmp_slot" "$location" 2> "$lock" ||
+        ! file_valid "$tmp_slot" "$type"; then
+        echo "Failed to fetch $location"
+        cat "$lock"
+        rm -f "$lock" "$tmp_slot"
+        exit 1
+    fi
     mv -f "$tmp_slot" "$entry"
 
     # got it, remove the lock so others can read it
