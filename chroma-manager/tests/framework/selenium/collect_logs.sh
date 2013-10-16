b392c50806ba2a33af3aef7f9d0a092f3d671b4a
kelsey <kelsey@whamcloud.com>
2013-10-16 13:51:59 -0700

    Fix missing screenshots and chromedriver logs. Better test logging.
    Fixing a bug in gathering the screenshots and chromedriver logs.
    Also changed the configuration of the logging for the test so that
    it is saved to a file so we can view logged actions during tests
    even when there isn't a test failure.
    
    Change-Id: I1d79d5f92425b7c26393a8102369cf4e7bf63153

--- chroma-manager/tests/framework/selenium/collect_logs.sh
+++ chroma-manager/tests/framework/selenium/collect_logs.sh
@@ -12,9 +12,10 @@ eval $(python $CHROMA_DIR/chroma-manager/tests/utils/json_cfg2sh.py "$CLUSTER_CO
 MEASURE_COVERAGE=${MEASURE_COVERAGE:-false}
 
 ssh root@$CHROMA_MANAGER "tar -czf - -C /var/log/chroma . -C /var/log/httpd . -C /var/log messages" | tar -xzf - -C $WORKSPACE/test_logs
-scp root@$TEST_RUNNER:.vnc/*.log $WORKSPACE/test_logs/
-scp -r root@$TEST_RUNNER:chroma_test_env/chroma/chroma-manager/failed-screen-shots/ $WORKSPACE
-scp -r root@$TEST_RUNNER:chroma_test_env/chroma/chroma-manager/failed-browser-logs/ $WORKSPACE
+scp chromatest@$TEST_RUNNER:.vnc/*.log $WORKSPACE/test_logs/
+scp chromatest@$TEST_RUNNER:chroma_test_env/chroma/chroma-manager/*log $WORKSPACE/test_logs/
+scp -r chromatest@$TEST_RUNNER:chroma_test_env/chroma/chroma-manager/failed-screen-shots/ $WORKSPACE
+scp -r chromatest@$TEST_RUNNER:chroma_test_env/chroma/chroma-manager/failed-browser-logs/ $WORKSPACE
 
 if $MEASURE_COVERAGE; then
   ssh root@$CHROMA_MANAGER <<EOF
