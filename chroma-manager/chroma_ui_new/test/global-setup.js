2a0bf29698eaab5c9ad00d590a5e1c872b358010
Joe Grund <joe.grund@intel.com>
2014-01-26 13:05:42 -0500

    HYD-2864: Update Mock to recursively spy on instance methods, add conveniences for mocking inline
    Change-Id: Ibc9e5d8af09a07a8ca8f4d18495032ee285386ac

--- chroma-manager/chroma_ui_new/test/global-setup.js
+++ chroma-manager/chroma_ui_new/test/global-setup.js
@@ -3,30 +3,8 @@ beforeEach(module('fixtures', 'imlMocks'));
 (function () {
   'use strict';
 
+  // Go around the injector and set this globally for tests.
+  window.mock = new window.Mock();
 
-  jasmine.createRecursiveStubObj = function createStubObj(baseName, stubbings, obj) {
-    var name, stubbing;
-    if (stubbings.constructor === Array) {
-      return jasmine.createSpyObj(baseName, stubbings);
-    } else {
-      obj = obj || {};
-
-      // jshint forin: false
-      for (name in stubbings) {
-        stubbing = stubbings[name];
-        if (_.isFunction(stubbing)) {
-          obj[name] = jasmine.createSpy(baseName + '.' + name);
-          obj[name].andCallFake(stubbing);
-        } else if (_.isObject(stubbing)) {
-          obj[name] = {};
-
-          createStubObj(baseName, stubbing, obj[name]);
-        } else {
-          obj[name] = stubbing;
-        }
-      }
-      return obj;
-    }
-  };
 }());
 
--- chroma-manager/chroma_ui_new/test/mocks/mock.js
+++ chroma-manager/chroma_ui_new/test/mocks/mock.js
@@ -6,6 +6,55 @@
   }
 
   /**
+   * Takes an instance, "copies" it's properties, and spies on all it's methods.
+   * This recursively iterates an object, spying on it's methods.
+   * This iterates on all properties of an object (even prototype ones) and should therefore
+   * be considered a resemblance of the original object, and not a clone with prototypal inheritance.
+   * You will need to spy on returns from method calls yourself.
+   * @static
+   * @param {Object} instance A fully instantiated object
+   * @returns {Object} The copied object.
+   */
+  Mock.spyInstance = function spyInstance (instance, obj, visited, refs) {
+    var name;
+
+    var out = obj || {};
+
+    visited = visited || [];
+    refs = refs || [];
+
+    // jshint forin: false
+    for (name in instance)
+      _mockValue(instance[name], name, out, refs, visited);
+
+    return out;
+  };
+
+  function _mockValue(value, name, out, refs, visited) {
+    var visitedIndex;
+
+    if (_.isFunction(value)) {
+      out[name] = value;
+
+      spyOn(out, name);
+
+    } else if (_.isPlainObject(value)) {
+      visitedIndex = visited.indexOf(value);
+
+      if (visitedIndex !== -1) {
+        out[name] = refs[visitedIndex];
+      } else {
+        out[name] = {};
+        refs.push(out[name]);
+        visited.push(value);
+        Mock.spyInstance(value, out[name], visited, refs);
+      }
+    } else {
+      out[name] = value;
+    }
+  }
+
+  /**
    * Add a new mock to the list.
    * @param {string} name
    * @param {function} setup
@@ -15,6 +64,26 @@
       this.mocks[name.name] = name;
     else
       this.mocks[name] = setup;
+  };
+
+  Mock.prototype.decorator = function (func) {
+    var name = getFunctionName(func);
+
+    this.register({
+      type: 'decorator',
+      name: name,
+      setup: func
+    });
+  };
+
+  Mock.prototype.factory = function (func) {
+    var name = getFunctionName(func);
+
+    this.register({
+      type: 'factory',
+      name: name,
+      setup: func
+    });
   };
 
   /**
@@ -57,6 +126,14 @@
     }));
   };
 
-  // Go around the injector and set this globally for tests.
-  window.mock = new Mock();
+  window.Mock = Mock;
+
+  function getFunctionName(func) {
+    var name = func.toString().match(/^\s*function\s*([$\w]*)\s*\(/)[1];
+
+    if (name === '')
+      throw new Error('Function cannot be anonymous!');
+
+    return name;
+  }
 }());
