d2476251ffb88788c8320c617ca8c9d2c8217731
Michael MacDonald <michael.macdonald@intel.com>
2014-03-25 11:53:46 -0400

    HYD-3076: Manage /etc/fstab entry for worker mount
    The worker node support neglected to include management of
    an /etc/fstab entry to persist the client mount across reboots.
    
    Change-Id: I22314e5813df3e9662fa8364dc0af30bde96b7b1

--- chroma-agent/chroma_agent/action_plugins/manage_client_mounts.py
+++ chroma-agent/chroma_agent/action_plugins/manage_client_mounts.py
@@ -26,6 +26,37 @@ import errno
 from chroma_agent import shell
 
 
+FSTAB_ENTRY_TEMPLATE = "%s\t%s\t\tlustre\tdefaults,_netdev\t0 0\n"
+
+
+def delete_fstab_entry(mountspec, *unused):
+    old_path = "/etc/fstab"
+    new_path = "/etc/fstab.iml.edit"
+    new_lines = []
+
+    needs_update = False
+    with open(old_path, "r") as old:
+        for line in old:
+            if len(line) > 1 and mountspec == line.split()[0]:
+                needs_update = True
+            else:
+                new_lines.append(line)
+
+    if needs_update:
+        with open(new_path, "w") as new:
+            for line in new_lines:
+                new.write(line)
+        os.rename(new_path, old_path)
+
+
+def create_fstab_entry(mountspec, mountpoint):
+    # Ensure that we're not appending this twice
+    delete_fstab_entry(mountspec, mountpoint)
+
+    with open("/etc/fstab", "a") as f:
+        f.write(FSTAB_ENTRY_TEMPLATE % (mountspec, mountpoint))
+
+
 def mount_lustre_filesystem(mountspec, mountpoint):
     try:
         os.makedirs(mountpoint, 0755)
@@ -33,7 +64,8 @@ def mount_lustre_filesystem(mountspec, mountpoint):
         if e.errno != errno.EEXIST:
             raise
 
-    shell.try_run(['/sbin/mount.lustre', mountspec, mountpoint])
+    create_fstab_entry(mountspec, mountpoint)
+    shell.try_run(['/bin/mount', mountpoint])
 
 
 def mount_lustre_filesystems(filesystems):
@@ -42,6 +74,7 @@ def mount_lustre_filesystems(filesystems):
 
 
 def unmount_lustre_filesystem(mountspec, mountpoint):
+    delete_fstab_entry(mountspec, mountpoint)
     shell.try_run(['/bin/umount', mountpoint])
 
 
--- chroma-agent/tests/test_manage_client_mounts.py
+++ chroma-agent/tests/test_manage_client_mounts.py
@@ -1,5 +1,6 @@
 import os
-from mock import patch
+from StringIO import StringIO
+from mock import patch, mock_open
 
 from tests.command_capture_testcase import CommandCaptureTestCase
 
@@ -14,32 +15,103 @@ class TestClientMountManagement(CommandCaptureTestCase):
         self.mountspec = '1.2.3.4@tcp:/%s' % self.fsname
         self.mountpoint = os.path.join(client_root, self.fsname)
 
-    def test_mount_lustre_filesystem(self):
+    @patch('chroma_agent.action_plugins.manage_client_mounts.delete_fstab_entry')
+    @patch('chroma_agent.action_plugins.manage_client_mounts.create_fstab_entry')
+    def test_mount_lustre_filesystem(self, create, delete):
         from chroma_agent.action_plugins.manage_client_mounts import mount_lustre_filesystem
 
         with patch('os.makedirs') as makedirs:
             mount_lustre_filesystem(self.mountspec, self.mountpoint)
             makedirs.assert_called_with(self.mountpoint, 0755)
 
-        self.assertRan(['/sbin/mount.lustre', self.mountspec, self.mountpoint])
+        self.assertRan(['/bin/mount', self.mountpoint])
+        create.assert_called_with(self.mountspec, self.mountpoint)
 
-    def test_mount_lustre_filesystems(self):
+    @patch('chroma_agent.action_plugins.manage_client_mounts.delete_fstab_entry')
+    @patch('chroma_agent.action_plugins.manage_client_mounts.create_fstab_entry')
+    def test_mount_lustre_filesystems(self, *patches):
         from chroma_agent.action_plugins.manage_client_mounts import mount_lustre_filesystems
 
         with patch('chroma_agent.action_plugins.manage_client_mounts.mount_lustre_filesystem') as mlf:
             mount_lustre_filesystems([(self.mountspec, self.mountpoint)])
             mlf.assert_called_with(self.mountspec, self.mountpoint)
 
-    def test_unmount_lustre_filesystem(self):
+    @patch('chroma_agent.action_plugins.manage_client_mounts.delete_fstab_entry')
+    def test_unmount_lustre_filesystem(self, *patches):
         from chroma_agent.action_plugins.manage_client_mounts import unmount_lustre_filesystem
 
         unmount_lustre_filesystem(self.mountspec, self.mountpoint)
 
         self.assertRan(['/bin/umount', self.mountpoint])
 
-    def test_unmount_lustre_filesystems(self):
+    @patch('chroma_agent.action_plugins.manage_client_mounts.delete_fstab_entry')
+    def test_unmount_lustre_filesystems(self, *patches):
         from chroma_agent.action_plugins.manage_client_mounts import unmount_lustre_filesystems
 
         with patch('chroma_agent.action_plugins.manage_client_mounts.unmount_lustre_filesystem') as ulf:
             unmount_lustre_filesystems([(self.mountspec, self.mountpoint)])
             ulf.assert_called_with(self.mountspec, self.mountpoint)
+
+    @patch('chroma_agent.action_plugins.manage_client_mounts.delete_fstab_entry')
+    def test_create_fstab_entry(self, delete):
+        from chroma_agent.action_plugins.manage_client_mounts import FSTAB_ENTRY_TEMPLATE, create_fstab_entry
+
+        with patch('chroma_agent.action_plugins.manage_client_mounts.open',
+                   mock_open(), create=True) as mo:
+            create_fstab_entry(self.mountspec, self.mountpoint)
+            mo().write.assert_called_once_with(FSTAB_ENTRY_TEMPLATE %
+                                               (self.mountspec, self.mountpoint))
+        delete.assert_called_with(self.mountspec, self.mountpoint)
+
+    @patch('os.rename')
+    def test_delete_fstab_entry(self, rename):
+        from chroma_agent.action_plugins.manage_client_mounts import delete_fstab_entry
+
+        fake_fstab = """#
+# /etc/fstab
+# blah
+
+/dev/foo        /bar        something   defaults    1 1
+%(mountspec)s              %(mountpoint)s          lustre      defaults,_netdev    0 0
+%(mountspec)s1              %(mountpoint)s1          lustre      defaults,_netdev    0 0
+
+1.2.3.4:/baz    /qux        nfs         defaults    0 0
+""" % dict(mountspec = self.mountspec, mountpoint = self.mountpoint)
+        data = StringIO(fake_fstab)
+
+        with patch('chroma_agent.action_plugins.manage_client_mounts.open',
+                   mock_open(), create=True) as mo:
+            mo.return_value.__iter__.return_value = data
+            delete_fstab_entry(self.mountspec, self.mountpoint)
+
+            new_fstab = [c[1][0] for c in mo().write.mock_calls]
+            self.assertMatchInList(r"^# /etc/fstab", new_fstab)
+            self.assertMatchInList(r'^%s1\s' % self.mountspec, new_fstab)
+            self.assertNotMatchInList(r'^%s\s' % self.mountspec, new_fstab)
+            self.assertMatchInList(r"^1.2.3.4:/baz", new_fstab)
+        rename.assert_called_with("/etc/fstab.iml.edit", "/etc/fstab")
+
+    def assertMatchInList(self, pattern, array):
+        import re
+        if isinstance(pattern, basestring):
+            pattern = re.compile(pattern)
+
+        for string in array:
+            if re.search(pattern, string):
+                return True
+
+        msg = "No matches found: %r not found in list" % pattern.pattern
+        raise AssertionError(msg)
+
+    def assertNotMatchInList(self, pattern, array):
+        found = False
+        try:
+            found = self.assertMatchInList(pattern, array)
+        except AssertionError:
+            pass
+
+        if found:
+            msg = "Unexpected match found: %r was found in list" % pattern
+            raise AssertionError(msg)
+
+        return True
