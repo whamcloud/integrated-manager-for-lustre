9a1335fd12d1ebe527017fecf192e2260d864282
Chris Gearing <chris.gearing@intel.com>
2014-04-23 12:02:24 +0100

    HYD-3139 Host add hung - UpdateDevices step in Load LNet module
    The problem is that the normalized devices routine would allow circular
    references to be entered, i.e. A normalized to A this then causes a hang
    when we try and retrieve them.
    
    As an additional measure I added a counter to detect larger circular
    references such as A->B->C->A in this case we don't know which is the
    normalized value so just drop out once we see that we are about to
    got back to the starting value.
    
    Signed-off-by: Chris Gearing <chris.gearing@intel.com>
    Change-Id: I7dd8489d0af4a04ceeaf8a91b1b41c92d649db1e
    Reviewed-on: http://review.whamcloud.com/10065
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Joe Grund <joe.grund@intel.com>

--- chroma-agent/chroma_agent/device_plugins/linux.py
+++ chroma-agent/chroma_agent/device_plugins/linux.py
@@ -155,9 +155,16 @@ class DeviceHelper(object):
         # normal path can actually be normalized further. So if the root to normalization takes multiple
         # steps this will deal with it
         # So if /dev/sdx normalizes to /dev/mmapper/special-device
-        # but /dev/mmapper/special-device normalizaes to /dev/md/mdraid1
+        # but /dev/mmapper/special-device normalizes to /dev/md/mdraid1
         # /dev/sdx will normalize to /dev/md/mdraid1
-        while normalized_path in DeviceHelper.normalize_device_cache:
+
+        # As an additional measure  a counter to detect circular
+        # references such as A->B->C->A in this case we don't know which is the
+        # normalized value so just drop out after circular_reference_limit redirections.
+        visited = set()
+
+        while (normalized_path not in visited) and (normalized_path in DeviceHelper.normalize_device_cache):
+            visited.add(normalized_path)
             normalized_path = DeviceHelper.normalize_device_cache[normalized_path]
 
         return normalized_path
@@ -167,11 +174,16 @@ class DeviceHelper(object):
         Add an entry to the normalized path list, adding to often does no harm and adding something that
         is not completely canonical does no harm either, because the search routine is recursive so if
         A=>B and B=>C then A,B and C will all evaluate to the canonical value of C.
+
+        This routine does not add circular references, it is better to detect them in here than in the caller
+
         :param path: device path
         :param normalized_path: canonical path
         :return: No return value
         '''
-        DeviceHelper.normalize_device_cache[path] = normalized_path
+
+        if (path != normalized_path):                           # Normalizing to itself makes no sense
+            DeviceHelper.normalize_device_cache[path] = normalized_path
 
 
 class BlockDevices(DeviceHelper):
