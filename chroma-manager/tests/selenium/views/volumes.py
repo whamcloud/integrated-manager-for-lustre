e2d316c86eabaf3461760653e10721f21cbd97d1
Joe Grund <joe.grund@intel.com>
2014-06-23 16:43:14 -0400

    HYD-3313 Have volumes auto refresh in a poll loop
      - Small syntax error fixes.
      - Small stylistic fixes.
      - Cache jQuery selectors.
      - Add select-box-it lib since we can't tell when a native select is
        open.
      - Update volume page to do dirty checking.
      - Update volume page to do polling.
      - Update volume selenium tests to work with select box it.
    
    Change-Id: I06630ae6857934d38ec91b8545f2618f64d46231
    Reviewed-on: http://review.whamcloud.com/10874
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Will Johnson <william.c.johnson@intel.com>
    Tested-by: Wayne Kenneth Seymour III <wayne.k.seymour.iii@intel.com>
    Tested-by: Will Johnson <william.c.johnson@intel.com>
    Reviewed-by: Joe Grund <joe.grund@intel.com>
    Tested-by: Joe Grund <joe.grund@intel.com>

--- chroma-manager/tests/selenium/views/volumes.py
+++ chroma-manager/tests/selenium/views/volumes.py
@@ -1,5 +1,4 @@
-from selenium.webdriver.support.ui import Select
-
+from tests.selenium.utils.select_box_it import SelectBoxIt
 from tests.selenium.base_view import DatatableView
 from tests.selenium.utils.element import (
     wait_for_element_by_css_selector, find_visible_element_by_css_selector
@@ -24,40 +23,30 @@ class Volumes(DatatableView):
         """
         # Iterating through the rows of volume configuration table
         for tr in self.rows:
-            select_tags = tr.find_elements_by_tag_name("select")
+            selects = tr.find_elements_by_css_selector(SelectBoxIt.CONTAINER_SELECTOR)
 
             # Get primary and failover 'select' dropdown elements
-            primary_select = select_tags.__getitem__(0)
-            failover_select = select_tags.__getitem__(1)
+            primary_select = SelectBoxIt(self.driver, selects[0])
+            failover_select = SelectBoxIt(self.driver, selects[1])
 
-            if primary_select.is_enabled():
-                primary_select_id = primary_select.get_attribute("id")
-                failover_select_id = failover_select.get_attribute("id")
+            if not primary_select.is_disabled():
+                current_primary_text = primary_select.get_selected()
 
-                # Get all options of primary select box
-                primary_options = primary_select.find_elements_by_tag_name('option')
-                primary_option_values = [option.get_attribute('value') for option in primary_options]
-
-                current_primary_value = Select(self.driver.find_element_by_id(primary_select.get_attribute("id"))).first_selected_option.get_attribute("value")
-
-                # Select another option different from its original value from primary select dropdown
-                for x in primary_option_values:
-                    if int(x) != -1 and int(x) != int(current_primary_value):
-                        Select(self.driver.find_element_by_id(primary_select.get_attribute("id"))).select_by_value(str(x))
+                # Select another option different from its original value for primary select dropdown
+                for primary_text in primary_select.get_options_text():
+                    if primary_text != SelectBoxIt.BLANK_OPTION_TEXT and primary_text != current_primary_text:
+                        primary_select.select_option(primary_text)
                         break
+
+                new_primary_text = primary_select.get_selected()
+                new_failover_text = failover_select.get_selected()
 
                 # Click Apply button
                 self.driver.find_element_by_css_selector('#btnApplyConfig').click()
                 wait_for_element_by_css_selector(self.driver, self.volume_error_dialog, 10)
 
-                primary_text = Select(self.driver.find_element_by_id(primary_select_id)).first_selected_option.text
-                failover_text = ''
-
-                if failover_select_id != '':
-                    failover_text = Select(self.driver.find_element_by_id(failover_select_id)).first_selected_option.text
-
                 # If selected text for primary and failover dropdowns are same then return display status of error dialog
-                if primary_text == failover_text:
+                if new_primary_text == new_failover_text:
                     return find_visible_element_by_css_selector(self.driver, self.volume_error_dialog)
 
         raise RuntimeError("Primary and failover servers cannot be same for a particular volume")
@@ -67,24 +56,25 @@ class Volumes(DatatableView):
         # server available
         for tr in self.rows:
             row_volume_name = tr.find_elements_by_tag_name('td')[0].text
-            primary_select = Select(tr.find_elements_by_tag_name("select")[0])
-            failover_select = Select(tr.find_elements_by_tag_name("select")[1])
+            selects = tr.find_elements_by_css_selector(SelectBoxIt.CONTAINER_SELECTOR)
+            primary_select = SelectBoxIt(self.driver, selects[0])
+            failover_select = SelectBoxIt(self.driver, selects[1])
+
             if row_volume_name == unicode(volume_name):
-                if primary_server_hostname in [option.text for option in primary_select.options]:
-                    if primary_select.first_selected_option.text != primary_server_hostname:
-                        # Set the desired primary
-                        primary_select.select_by_visible_text(primary_server_hostname)
+                if primary_select.has_option(primary_server_hostname):
+                    if not primary_select.is_selected(primary_server_hostname):
+                        primary_select.select_option(primary_server_hostname)
 
                         # Pick another server for failover, if one is available, else set it blank (-1)
-                        failover_options = failover_select.options
+                        failover_options = failover_select.get_options_text()
                         failover_server_hostname = None
                         for option in failover_options:
-                            if option.get_attribute('value') != -1 and option.text != primary_server_hostname:
-                                failover_server_hostname = option.text
+                            if option != SelectBoxIt.BLANK_OPTION_TEXT and option != primary_server_hostname:
+                                failover_server_hostname = option
                         if failover_server_hostname is None:
-                            failover_select.select_by_value(-1)
+                            failover_select.select_option(SelectBoxIt.BLANK_OPTION_TEXT)
                         else:
-                            failover_select.select_by_visible_text(failover_server_hostname)
+                            failover_select.select_option(failover_server_hostname)
 
                         self.driver.find_element_by_css_selector('#btnApplyConfig').click()
                         wait_for_element_by_css_selector(self.driver, '#transition_confirm_button', 10)
@@ -96,8 +86,9 @@ class Volumes(DatatableView):
         debug_info = []
         for tr in self.rows:
             row_volume_name = tr.find_elements_by_tag_name('td')[0].text
-            primary_select = [o.text for o in Select(tr.find_elements_by_tag_name("select")[0]).options]
-            debug_info.append((row_volume_name, primary_select))
+            primary_select = SelectBoxIt(self.driver, tr.find_elements_by_class_name(SelectBoxIt.CONTAINER_CLASS)[0])
+            options_text = primary_select.get_options_text()
+            debug_info.append((row_volume_name, options_text))
 
         raise RuntimeError("No row found with volume name '%s' that had server '%s' available to be selected as a primary server. Debug info: '%s'" % (volume_name, primary_server_hostname, debug_info))
 
@@ -105,54 +96,40 @@ class Volumes(DatatableView):
         """Changing volume configuration"""
         # TODO: Lots of this needs refactored into the test_volumes module, where we can do proper asserts, etc.
         for tr in self.rows:
-            select_tags = tr.find_elements_by_tag_name("select")
-            assert len(select_tags) == 2, select_tags
-            primary_select = select_tags[0]
-            failover_select = select_tags[1]
+            selects = tr.find_elements_by_css_selector(SelectBoxIt.CONTAINER_SELECTOR)
+            assert len(selects) == 2, selects
+            primary_select = SelectBoxIt(self.driver, selects[0])
+            primary_select_id = selects[0].get_attribute('id')
+            failover_select = SelectBoxIt(self.driver, selects[1])
+            failover_select_id = selects[1].get_attribute('id')
 
-            # Get primary and failover 'select' dropdown elements
-            primary_select_id = primary_select.get_attribute('id')
-            failover_select_id = failover_select.get_attribute('id')
-            original_primary_value = Select(self.driver.find_element_by_id(primary_select_id)).first_selected_option.get_attribute('value')
-            original_failover_value = ''
-            if failover_select_id != '':
-                original_failover_value = Select(self.driver.find_element_by_id(failover_select_id)).first_selected_option.get_attribute('value')
-
-            if primary_select.is_enabled() and failover_select.is_enabled():
-                primary_options = primary_select.find_elements_by_tag_name('option')
-                primary_option_values = [option.get_attribute('value') for option in primary_options]
+            if not primary_select.is_disabled() and not failover_select.is_disabled():
+                # Get primary and failover 'select' dropdown elements
+                original_primary_text = primary_select.get_selected()
+                original_failover_text = failover_select.get_selected()
 
                 # Select another option different from its original value for primary select dropdown
-                for x in primary_option_values:
-                    if int(x) != -1 and int(x) != int(original_primary_value):
-                        Select(self.driver.find_element_by_id(primary_select_id)).select_by_value(str(x))
+                for primary_text in primary_select.get_options_text():
+                    if primary_text != SelectBoxIt.BLANK_OPTION_TEXT and primary_text != original_primary_text:
+                        primary_select.select_option(primary_text)
                         break
-
-                failover_options = failover_select.find_elements_by_tag_name('option')
-                failover_option_values = [option.get_attribute('value') for option in failover_options]
 
                 # Select another option different from its original value for failover select dropdown
-                for y in failover_option_values:
-                    if int(y) != -1 and int(y) != int(original_failover_value):
-                        Select(self.driver.find_element_by_id(failover_select_id)).select_by_value(str(y))
-                        break
+                for failover_text in failover_select.get_options_text():
+                    if failover_text != SelectBoxIt.BLANK_OPTION_TEXT and failover_text != original_failover_text:
+                        failover_select.select_option(failover_text)
+
+                new_primary_text = primary_select.get_selected()
+                new_failover_text = failover_select.get_selected()
 
                 # Click Apply button
                 self.driver.find_element_by_css_selector('#btnApplyConfig').click()
 
-                # Get new values for primary and failover dropdowns
-                new_primary_text = Select(self.driver.find_element_by_id(primary_select_id)).first_selected_option.text
-                new_primary_value = Select(self.driver.find_element_by_id(primary_select_id)).first_selected_option.get_attribute("value")
-                new_failover_text = Select(self.driver.find_element_by_id(failover_select_id)).first_selected_option.text
-                new_failover_value = Select(self.driver.find_element_by_id(failover_select_id)).first_selected_option.get_attribute("value")
-
                 # If texts are different then volume configuration setting should be successful and returns a success message
                 if new_primary_text == new_failover_text:
                     self.driver.find_element_by_css_selector(self.error_ok_button).click()
-                    Select(self.driver.find_element_by_id(primary_select_id)).select_by_visible_text(original_primary_value)
-                    Select(self.driver.find_element_by_id(failover_select_id)).select_by_visible_text(original_failover_value)
-                elif original_primary_value == new_primary_value and original_failover_value == new_failover_value:
-                    pass
+                    primary_select.select_option(original_primary_text)
+                    failover_select.select_option(original_failover_text)
                 else:
                     self.driver.find_element_by_css_selector('#transition_confirm_button').click()
                     self.quiesce()
@@ -160,10 +137,14 @@ class Volumes(DatatableView):
                     # Verify values persist through a refresh
                     self.driver.refresh()
                     self.quiesce()
-                    stored_primary_value = Select(self.driver.find_element_by_id(primary_select_id)).first_selected_option.text
-                    stored_failover_value = Select(self.driver.find_element_by_id(failover_select_id)).first_selected_option.text
-                    assert stored_primary_value == new_primary_text, "%s != %s" % (stored_primary_value, new_primary_text)
-                    assert stored_failover_value == new_failover_text, "%s != %s" % (stored_failover_value, new_failover_text)
+
+                    stored_primary_text = SelectBoxIt(self.driver, self.driver.find_element_by_id(primary_select_id)).get_selected()
+                    stored_failover_text = SelectBoxIt(self.driver, self.driver.find_element_by_id(failover_select_id)).get_selected()
+
+                    assert stored_primary_text == new_primary_text, "%s != %s" % (
+                        stored_primary_text, new_primary_text)
+                    assert stored_failover_text == new_failover_text, "%s != %s" % (
+                        stored_failover_text, new_failover_text)
                     return True
 
         raise RuntimeError("No primary / failover servers available for changing volume configuration")
@@ -172,16 +153,14 @@ class Volumes(DatatableView):
         """Checks whether primary volumes for given server appear or not"""
         # Iterating through the rows of volume configuration table
         for tr in self.rows:
-            select_tags = tr.find_elements_by_tag_name("select")
-            if len(select_tags) > 0:
-                primary_select = select_tags.__getitem__(0)
-                if primary_select.get_attribute("id") != '':
-                    primary_options = primary_select.find_elements_by_tag_name('option')
-                    primary_option_names = [option.text for option in primary_options]
+            selects = tr.find_elements_by_css_selector(SelectBoxIt.CONTAINER_SELECTOR)
+            if len(selects) > 0:
+                primary_select = SelectBoxIt(self.driver, selects[0])
 
-                    # Check whether the given server is listed in primary server list for current volume
-                    for option_value in primary_option_names:
-                        if option_value == server_name:
+                # Check whether the given server is listed in primary server list for current volume
+                if not primary_select.is_disabled():
+                    for primary_text in primary_select.get_options_text():
+                        if primary_text == server_name:
                             return True
 
         return False
