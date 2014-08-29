121eb13c30219ed77d583d7f637b0e77544ac940
Brian J. Murrell <brian.murrell@intel.com>
2014-08-29 16:04:06 -0400

    HYD-2592 Libraryize some common test code
    As our test scripts have grown over time, we've ended
    up with a redundant definition of a number of variables
    that repeat themselves in every test script. This patch
    moves the variables into a common file, defaults.sh, and
    includes the file at the beginning of each script.
    
    This definition in one shared location is a small step towards
    removing the redundancy in our test scripts.
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>
    Change-Id: I33c70c25f791d458c2160ef5d2a6e1b7f4547775
    Reviewed-on: http://review.whamcloud.com/11674
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Kelsey Prantis <kelsey.prantis@intel.com>
    Reviewed-by: Chris Gearing <chris.gearing@intel.com>

---
+++ chroma-manager/tests/framework/integration/utils/defaults.sh
@@ -0,0 +1,35 @@
+# auth.sh contains the JENKINS_PULL environmental variable so we can avoid
+# printing it into the console in plaintext calling this script.
+set +x  # DONT REMOVE/COMMENT or you will risk exposing the jenkins-pull api token in the console logs.
+. $HOME/auth.sh
+set -x
+
+[ -r localenv ] && . localenv
+
+spacelist_to_commalist() {
+    echo $@ | tr ' ' ','
+}
+
+d=${0%/*}
+if [[ $d != /* ]]; then
+    d=${PWD}/$d
+fi
+while [ ! -f $d/include/Makefile.version ]; do
+    d=${d%/*}
+done
+export IEEL_VERSION=$(make -f $d/include/Makefile.version .ieel_version)
+
+PROVISIONER=${PROVISIONER:-"$HOME/provisionchroma -v -S --provisioner /home/bmurrell/provisioner"}
+
+
+MEASURE_COVERAGE=${MEASURE_COVERAGE:-false}
+
+# Variables that we expect to be set upstream, no "default"
+set +x  # DONT REMOVE/COMMENT or you will risk exposing the jenkins-pull api token in the console logs.
+export JENKINS_USER=${JENKINS_USER:-jenkins-pull}
+export JENKINS_PULL=${JENKINS_PULL:?"Need to set JENKINS_PULL"}
+set -x
+BUILD_JOB_NAME=${BUILD_JOB_NAME:?"Need to set BUILD_JOB_NAME"}
+BUILD_JOB_BUILD_NUMBER=${BUILD_JOB_BUILD_NUMBER:?"Need to set BUILD_JOB_BUILD_NUMBER"}
+JOB_URL=${JOB_URL:?"Need to set JOB_URL"}
+WORKSPACE=${WORKSPACE:?"Need to set WORKSPACE"}
