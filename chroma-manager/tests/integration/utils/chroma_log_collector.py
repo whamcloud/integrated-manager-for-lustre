76fa6c1dde1a6dd1bdb0a82c38bf4c32be168962
Kelsey Prantis <kelsey.prantis@intel.com>
2014-10-09 12:25:14 -0700

    TEI-876: Collect chroma-diagnostics for automated tests.
    Lets eat our own dogfood and use chroma-diagnostics to help us
    debug our production style automated tests. This will also give
    us valuable insight into what would be useful to add to CD, and
    CD in turn provides some information we weren't gathering before.
    
    Change-Id: If59418afe744933a974d09dd48472fefaf5604c1
    Reviewed-on: http://review.whamcloud.com/12271
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Tested-by: Kelsey Prantis <kelsey.prantis@intel.com>
    Reviewed-by: Gene Campbell <gene.campbell@intel.com>
    Reviewed-by: Joe Grund <joe.grund@intel.com>

--- chroma-manager/tests/integration/utils/chroma_log_collector.py
+++ chroma-manager/tests/integration/utils/chroma_log_collector.py
@@ -8,6 +8,7 @@
 # Ex usage: ./chroma_log_collection.py ss_log_collection_test some_machine:~/cluster_cfg.json
 
 import json
+import re
 import subprocess
 import sys
 
@@ -26,55 +27,57 @@ class ChromaLogCollector(object):
 
         for test_runner in self.test_runners:
             self.fetch_log(test_runner, '/var/log/chroma_test.log', "%s-chroma_test.log" % test_runner)
-            self.fetch_log(test_runner, '/var/log/messages', '%s-messages.log' % test_runner)
+            self.fetch_log(test_runner, '/var/log/messages', "%s-messages.log" % test_runner)
 
-        for chroma_manager in self.chroma_managers:
-            self.fetch_log_dir(chroma_manager, '/var/log/chroma/')
-            self.fetch_log(chroma_manager, '/var/log/messages', '%s-messages.log' % chroma_manager)
-            self.fetch_log_dir(chroma_manager, '/var/log/httpd/')
-            self.fetch_log(chroma_manager, '/var/log/yum.log', '%s-yum.log' % chroma_manager)
-
-        for lustre_server in self.lustre_servers:
-            self.fetch_log(lustre_server, '/var/log/chroma-agent-console.log', '%s-chroma-agent-console.log' % lustre_server)
-            self.fetch_log(lustre_server, '/var/log/chroma-agent.log', '%s-chroma-agent.log' % lustre_server)
-            self.fetch_log(lustre_server, '/var/log/messages', '%s-messages.log' % lustre_server)
-            self.fetch_log(lustre_server, '/var/log/yum.log', '%s-yum.log' % lustre_server)
-            self.fetch_pacemaker_configuration(lustre_server)
+        for server in self.chroma_managers + self.lustre_servers:
+            self.fetch_log(server, '/var/log/yum.log', "%s-yum.log" % server)
+            self.fetch_chroma_diagnostics(server)
 
     def fetch_log(self, server, source_log_path, destination_log_filename):
         print "Fetching %s from %s to %s/%s" % (source_log_path, server, self.destination_path, destination_log_filename)
-        subprocess.call(['scp', "%s:%s" % (server, source_log_path), "%s/%s" % (self.destination_path, destination_log_filename)])
+        subprocess.call(['scp', "%s:%s" % (server, source_log_path), "%s/%s" % (
+            self.destination_path, destination_log_filename)])
 
     def fetch_log_dir(self, server, dir):
         logs = subprocess.Popen(['ssh', server, "ls %s | xargs -n1 basename" % dir], stdout=subprocess.PIPE).communicate()[0]
         for log in logs.split():
             destination_log_filename = "%s-%s-%s" % (server, dir.strip('/').split('/')[-1], log)
-            self.fetch_log(server, '%s/%s' % (dir, log), destination_log_filename)
+            self.fetch_log(server, "%s/%s" % (dir, log), destination_log_filename)
 
-    def fetch_pacemaker_configuration(self, lustre_server):
-        # Only attempt to fetch if pacemaker exists on the lustre server
-        if subprocess.call(['ssh', lustre_server, 'which pcs']) == 0:
-            pcs_status = subprocess.Popen(['ssh', lustre_server, 'pcs status'], stdout=subprocess.PIPE).communicate()[0]
-            f = open('%s/%s-pcs-status.log' % (self.destination_path, lustre_server), 'w')
-            try:
-                f.write(pcs_status)
-            finally:
-                f.close()
+    def fetch_chroma_diagnostics(self, server):
+        # Check that chroma-diagnostics is installed. May not be if installation failed, etc.
+        if not subprocess.call(['ssh', server, 'which chroma-diagnostics']) == 0:
+            print "chroma-diagnostics not installed on %s. skipping." % server
+            return
 
-            pcs_configuration = subprocess.Popen(['ssh', lustre_server, '''mkdir -p /etc/cluster && cat <<EOF > /etc/cluster/cluster.conf
-<cluster name="IML" config_version="1">
-        <clusternodes>
-        <clusternode name="" nodeid="1">
-        </clusternode>
-        </clusternodes>
-</cluster>
-EOF
-pcs config; rm -f /etc/cluster/cluster.conf'''], stdout=subprocess.PIPE).communicate()[0]
-            f = open('%s/%s-pcs-configuration.log' % (self.destination_path, lustre_server), 'w')
-            try:
-                f.write(pcs_configuration)
-            finally:
-                f.close()
+        # Generate the diagnostics from the server
+        pipe = subprocess.Popen(['ssh', server, 'chroma-diagnostics'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+        # Find the diagnostics filename from the chroma-diagnostics output
+        cd_out = ''.join([x.decode('utf8') for x in pipe.stderr.readlines()])
+        match = re.compile('/var/log/(.*)').search(cd_out)
+        if not match:
+            print "Did not find diagnostics filepath in chroma-diagnostics output"
+            sys.exit(1)
+        diagnostics = match.group(1).strip()
+
+        # Copy and expand the diagnostics locally, so they will be able to be read in browser in Jenkins.
+        self.fetch_log(server, "/var/log/%s" % diagnostics, '')
+        self.fetch_log(server, "chroma-diagnostics.log", '%s-chroma-diagnostics.log' % server)
+        if diagnostics.endswith('tar.lzma'):
+            subprocess.call(
+                ['tar', '--lzma', '-xvf', "%s/%s" % (self.destination_path, diagnostics),
+                    '-C', self.destination_path], stdout=subprocess.PIPE)
+        elif diagnostics.endswith('tar.gz'):
+            subprocess.call(
+                ['tar', '-xvzf', "%s/%s" % (self.destination_path, diagnostics),
+                    '-C', self.destination_path], stdout=subprocess.PIPE)
+        else:
+            print "Didnt recognize chroma-diagnostics file format"
+            sys.exit(1)
+
+        subprocess.call(['rm', '-f', "%s/%s" % (self.destination_path, diagnostics)],
+                stdout=subprocess.PIPE)
 
 
 if __name__ == '__main__':
@@ -83,7 +86,7 @@ if __name__ == '__main__':
     cluster_cfg_path = sys.argv[2]
 
     subprocess.call(['mkdir', '-p', destination_path])
-    subprocess.call(['rm', '-f', '%s.tgz' % destination_path])
+    subprocess.call(['rm', '-f', "%s.tgz" % destination_path])
     cluster_cfg_json = open(cluster_cfg_path)
     cluster_cfg = json.loads(cluster_cfg_json.read())
 
@@ -93,7 +96,7 @@ if __name__ == '__main__':
 
     lustre_servers = []
     for lustre_server in cluster_cfg['lustre_servers']:
-        if not lustre_server['distro'] == "mock":
+        if not lustre_server['distro'] == 'mock':
             lustre_servers.append("root@%s" % lustre_server['address'])
 
     test_runners = []
@@ -102,4 +105,3 @@ if __name__ == '__main__':
 
     log_collector = ChromaLogCollector(destination_path, chroma_managers, lustre_servers, test_runners)
     log_collector.collect_logs()
-    subprocess.call(['tar', '-czf', '%s.tgz' % destination_path, destination_path])
