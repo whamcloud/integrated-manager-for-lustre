c70b2bc1ec0e9146caaafee9456850f0ec1df37b
Kelsey Prantis <kelsey.prantis@intel.com>
2014-11-21 11:42:02 -0800

    TEI-2956: Reduce io from development-style test jobs.
    Our Jenkins instance is currently being slow, and we believe this
    may be related to the IO being put on the master builder from the
    "developer-style" tests - particularly where it tars up the repo
    and transfers it over to avoid a full git clone each run.
    
    Change-Id: I185997e77eef611ed199cadd3016af43103bd012
    Reviewed-on: http://review.whamcloud.com/12812
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Brian J. Murrell <brian.murrell@intel.com>
    Reviewed-by: Joe Grund <joe.grund@intel.com>

--- chroma-manager/tests/utils/pip_install_requirements.py
+++ chroma-manager/tests/utils/pip_install_requirements.py
@@ -15,13 +15,12 @@ class PipInstallRequirements(object):
     # The max amount of time we will continue trying to install.
     PIP_TIMEOUT = datetime.timedelta(hours=1)
 
-    def __init__(self, pip_packages_dir, *args, **kwargs):
+    def __init__(self, pip_packages_dir, packages_to_install, *args, **kwargs):
         super(PipInstallRequirements, self).__init__(*args, **kwargs)
         self.pip_packages_dir = pip_packages_dir
+        self.packages_to_install = packages_to_install or open('requirements.txt')
 
     def install(self):
-        requirements = open('requirements.txt')
-
         virtual_env = os.environ.get('VIRTUAL_ENV')
         print "VIRTUAL_ENV=%s" % virtual_env
         if not virtual_env:
@@ -33,38 +32,39 @@ class PipInstallRequirements(object):
         build_dir = os.path.join(virtual_env, 'build')
         print "BUILD_DIR=%s" % build_dir
 
-        for line in requirements:
-            line = line.strip()
-            if line and not line[0] == '#':
-                if re.match("https?://", line):
+        for package in packages_to_install:
+            package = package.strip()
+            if package and not package[0] == '#':
+                if re.match("https?://", package):
                     # The url based requirements, such as those from GitHub,
                     # need slightly different handling. They will still try
                     # to reach out to their external dependency even if it is
                     # in the package dir as --no-index only tells it not to
                     # talk to PyPi, not other sites. It will, however, use the
                     # previously downloaded package if we give it the exact
-                    # location instead of just the line from requirements.txt.
+                    # location instead of just the package description.
                     expected_package_location = os.path.join(
                         self.pip_packages_dir,
-                        line.rsplit('/', 1)[-1]
+                        package.rsplit('/', 1)[-1]
                     )
                     if os.path.exists(expected_package_location):
                         subprocess.check_call(['pip', 'install',
                                                '--build', build_dir, '--no-index', '--pre',
                                                expected_package_location])
                     else:
-                        print "Failed to install %s" % line.rsplit('/', 1)[-1]
+                        print "Failed to install %s" % package.rsplit('/', 1)[-1]
                         sys.exit(1)
                 else:
                     exit_status = subprocess.call(['pip', 'install',
                                                    '--build', build_dir, '--no-index', '--pre',
-                                                   '--find-links', 'file://%s' % self.pip_packages_dir, line])
+                                                   '--find-links', 'file://%s' % self.pip_packages_dir, package])
                     if not exit_status == 0:
-                        print "Failed to install %s" % line
+                        print "Failed to install %s" % package
                         sys.exit(1)
 
 
 if __name__ == '__main__':
     pip_packages_dir = sys.argv[1]
-    installer = PipInstallRequirements(pip_packages_dir)
+    packages_to_install = sys.argv[2:]
+    installer = PipInstallRequirements(pip_packages_dir, packages_to_install)
     installer.install()
