a921ddc5ad530b732b3be1b4140d671e12f62f51
Joe Grund <joe.grund@intel.com>
2015-03-24 11:26:16 -0400

    HYD-4291 Create through modules
    We have a number of highland through plugins that can be reusable.
    
    As a first step, move them into ui-modules/node.
    
    Later, we can bundle them using browserify and move them to
    ui-modules/isomorphic.
    
    Change-Id: Ieefd18e7bb0c3637765d92ef2a50fb9107daad3b
    Reviewed-on: http://review.whamcloud.com/14154
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Will Johnson <william.c.johnson@intel.com>
    Reviewed-by: Wayne Kenneth Seymour III <wayne.k.seymour.iii@intel.com>
    Reviewed-by: Joe Grund <joe.grund@intel.com>
    Tested-by: Joe Grund <joe.grund@intel.com>

---
+++ chroma-manager/ui-modules/node/through/sort-by.js
@@ -0,0 +1,36 @@
+//
+// INTEL CONFIDENTIAL
+//
+// Copyright 2013-2014 Intel Corporation All Rights Reserved.
+//
+// The source code contained or described herein and all documents related
+// to the source code ("Material") are owned by Intel Corporation or its
+// suppliers or licensors. Title to the Material remains with Intel Corporation
+// or its suppliers and licensors. The Material contains trade secrets and
+// proprietary and confidential information of Intel or its suppliers and
+// licensors. The Material is protected by worldwide copyright and trade secret
+// laws and treaty provisions. No part of the Material may be used, copied,
+// reproduced, modified, published, uploaded, posted, transmitted, distributed,
+// or disclosed in any way without Intel's prior express written permission.
+//
+// No license under any patent, copyright, trade secret or other intellectual
+// property right is granted to or conferred upon you by disclosure or delivery
+// of the Materials, either expressly, by implication, inducement, estoppel or
+// otherwise. Any license under such intellectual property rights must be
+// express and approved by Intel in writing.
+
+'use strict';
+
+var _ = require('lodash-mixins');
+
+/**
+ * Sorts the stream by the comparator
+ * @param {Function} cmp
+ * @param {Highland.Stream} s
+ * @returns {Highland.Stream} A stream.
+ */
+module.exports = _.curry(function sortBy (cmp, s) {
+  return s.collect()
+    .invoke('sort', [cmp])
+    .sequence();
+});
