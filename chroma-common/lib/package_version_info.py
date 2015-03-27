0424842a825b3543737f4a4aced9e84e2f36965c
Chris Gearing <chris.gearing@intel.com>
2015-03-27 20:07:57 +0000

    HYD-4312 Upgrade testing fails due to __cmp__ of VersionInfo being misimplemented
    VersionInfo which is used to compare package versions overrides _cmp_ however it
    inherits from namedtuple which implements all of the rich comparisons like _lt_
    and _gt_ and these rich comparisons take precedence over the _cmp_ method.
    
    What is required is a version of VersionInfo that only includes _cmp_ and
    includes some unit test to make sure it actually works!
    
    This patch turns VersionInfo into a standalone class with tests. The tests are
    not landed as part of this patch because of some CI issues, which means they will
    follow in a later patch.
    
    The move of the class into a dedicated file has a couple of advantages. Firstly it
    can be used from multiple components and second it allows the pairing of code and tests
    to be more obvious.
    
    At present VersionInfo cannot be used from chroma_agent (lustre.py) because it is
    not seriablizable as json. Fixing that seemed like feature creep.
    
    Change-Id: I6d762cec4b723ec644730e7da927537d4436eb10
    Signed-off-by: Chris Gearing <chris.gearing@intel.com>
    Reviewed-on: http://review.whamcloud.com/14267
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Mathieu Drapeau <mathieu.drapeau@intel.com>
    Reviewed-by: Joe Grund <joe.grund@intel.com>
    Signed-off-by: Chris Gearing <chris.gearing@intel.com>
    Reviewed-on: http://review.whamcloud.com/14365
    Reviewed-on: http://review.whamcloud.com/14652

---
+++ chroma-common/lib/package_version_info.py
@@ -0,0 +1,55 @@
+#
+# INTEL CONFIDENTIAL
+#
+# Copyright 2013-2014 Intel Corporation All Rights Reserved.
+#
+# The source code contained or described herein and all documents related
+# to the source code ("Material") are owned by Intel Corporation or its
+# suppliers or licensors. Title to the Material remains with Intel Corporation
+# or its suppliers and licensors. The Material contains trade secrets and
+# proprietary and confidential information of Intel or its suppliers and
+# licensors. The Material is protected by worldwide copyright and trade secret
+# laws and treaty provisions. No part of the Material may be used, copied,
+# reproduced, modified, published, uploaded, posted, transmitted, distributed,
+# or disclosed in any way without Intel's prior express written permission.
+#
+# No license under any patent, copyright, trade secret or other intellectual
+# property right is granted to or conferred upon you by disclosure or delivery
+# of the Materials, either expressly, by implication, inducement, estoppel or
+# otherwise. Any license under such intellectual property rights must be
+# express and approved by Intel in writing.
+
+
+# import so that this module can be imported in pure python environments as well as on Linux.
+# Doing it this way means that rpm_lib can mocked.
+try:
+    import rpm as rpm_lib
+except:
+    class rpm_lib(object):
+        @classmethod
+        def labelCompare(cls, a, b):
+            return cmp(a, b)
+
+
+class VersionInfo(object):
+    """
+    A convenient way of storing package version information that can be printed and compared with ease.
+
+    At present this class is not serializable and so cannot be converted easily into json. This highlights
+    a limitation of our current use of plain json for messages in that all but the basic types have to be
+    discarded during their transportation.
+    """
+    def __init__(self, epoch, version, release, arch):
+        self.epoch = str(epoch)
+        self.version = str(version)
+        self.release = str(release)
+        self.arch = str(arch)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return "epoch='%s', version='%s', release='%s', arch='%s'" % (self.epoch, self.version, self.release, self.arch)
+
+    def __cmp__(self, other):
+        return rpm_lib.labelCompare((self.epoch, self.version, self.release), (other.epoch, other.version, other.release))
