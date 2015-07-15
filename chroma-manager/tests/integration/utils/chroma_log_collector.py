c4915dc2cbb3f671eff7009d2a4385acc10d0d70
Chris Gearing <chris.gearing@intel.com>
2015-07-15 11:30:30 +0200

    HYD-4629 chroma-diagnostics often hangs stalling the whole test queue.
    chroma-diagnostics often hangs stalling the whole test queue, this could
    be because of a number of things but fundamentally we must time it out
    so that the queue does not stall.
    
    Generally we should be very careful of calling external processes without
    some timeout mechanism - so this ticket creates a function that can
    be called in other places as well.
    
     - Add safe_shell module with run that has a timeout mechanism
     - Change chroma_log_collector.py to use that run instead of Popen
    
    Change-Id: I5d9e450bb91d38bf941e1e95b152846ecd9addde
    Signed-off-by: Chris Gearing <chris.gearing@intel.com>
    Reviewed-on: http://review.whamcloud.com/15602
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Brian J. Murrell <brian.murrell@intel.com>
    Reviewed-by: Joe Grund <joe.grund@intel.com>
    (cherry picked from commit f65de040be575ed30328cf433045dcb69c262244)
    Reviewed-on: http://review.whamcloud.com/16002

--- chroma-manager/tests/integration/utils/chroma_log_collector.py
+++ chroma-manager/tests/integration/utils/chroma_log_collector.py
@@ -9,8 +9,9 @@
 
 import json
 import re
-import subprocess
 import sys
+
+import safe_shell
 
 
 class ChromaLogCollector(object):
@@ -23,70 +24,111 @@ class ChromaLogCollector(object):
         self.test_runners = test_runners
 
     def collect_logs(self):
-        subprocess.call(['rm', '-rf', "%s/*.log" % destination_path])
+        """
+        Collect the logs from the target
+        :return: empty list on success or error messages that can be used for diagnosing what went wrong.
+        """
+        if safe_shell.run(['rm', '-rf', "%s/*.log" % destination_path]).rc:
+            return "Fail to clear out destination path for logs collection: %s" % destination_path
+
+        errors = []
 
         for test_runner in self.test_runners:
-            self.fetch_log(test_runner, '/var/log/chroma_test.log', "%s-chroma_test.log" % test_runner)
-            self.fetch_log(test_runner, '/var/log/messages', "%s-messages.log" % test_runner)
+            errors.append(self.fetch_log(test_runner, '/var/log/chroma_test.log', "%s-chroma_test.log" % test_runner))
+            errors.append(self.fetch_log(test_runner, '/var/log/messages', "%s-messages.log" % test_runner))
 
         for server in self.chroma_managers + self.lustre_servers:
-            self.fetch_log(server, '/var/log/yum.log', "%s-yum.log" % server)
-            self.fetch_chroma_diagnostics(server)
+            errors.append(self.fetch_log(server, '/var/log/yum.log', "%s-yum.log" % server))
+            errors.extend(self.fetch_chroma_diagnostics(server))
+
+        return [error for error in errors if error]
 
     def fetch_log(self, server, source_log_path, destination_log_filename):
-        print "Fetching %s from %s to %s/%s" % (source_log_path, server, self.destination_path, destination_log_filename)
-        subprocess.call(['scp', "%s:%s" % (server, source_log_path), "%s/%s" % (
-            self.destination_path, destination_log_filename)])
+        """
+        Collect the log from the target
+        :return: None on success or error message that can be used for diagnosing what went wrong.
+        """
+        action = "Fetching %s from %s to %s/%s" % (source_log_path, server, self.destination_path, destination_log_filename)
+        print action
+        if safe_shell.run(['scp', "%s:%s" % (server, source_log_path), "%s/%s" % (
+                self.destination_path, destination_log_filename)]).rc:
+            error = "Failed %s" % action
+
+            with open(destination_log_filename, "w+") as f:
+                f.write(error)
+            return error
+
+        return None
 
     def fetch_log_dir(self, server, dir):
-        logs = subprocess.Popen(['ssh', server, "ls %s | xargs -n1 basename" % dir], stdout=subprocess.PIPE).communicate()[0]
-        for log in logs.split():
+        """
+        Collect the log directory from the target
+        :return: None on success or error message that can be used for diagnosing what went wrong.
+        """
+        logs = safe_shell.run(['ssh', server, "ls %s | xargs -n1 basename" % dir])
+
+        if logs.rc:
+            return "Failed fecthing log dir %s from %s" % (server, dir)
+
+        for log in logs.stdout.split():
             destination_log_filename = "%s-%s-%s" % (server, dir.strip('/').split('/')[-1], log)
             self.fetch_log(server, "%s/%s" % (dir, log), destination_log_filename)
 
+        return None
+
     def fetch_chroma_diagnostics(self, server):
+        """
+        Collect the chroma diagnostics from the target
+        :return: empty list on success or list of error messages that can be used for diagnosing what went wrong.
+        """
+
         # Check that chroma-diagnostics is installed. May not be if installation failed, etc.
-        if not subprocess.call(['ssh', server, 'which chroma-diagnostics']) == 0:
-            print "chroma-diagnostics not installed on %s. skipping." % server
-            return
+        if safe_shell.run(['ssh', server, 'which chroma-diagnostics']).rc:
+            return["chroma-diagnostics not installed on %s. skipping." % server]
 
         # Generate the diagnostics from the server
-        pipe = subprocess.Popen(['ssh', server, 'chroma-diagnostics'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        result = safe_shell.run(['ssh', server, 'chroma-diagnostics'])
+
+        if result.timeout:
+            return["Chroma Diagnostics timed-out"]
 
         # Find the diagnostics filename from the chroma-diagnostics output
-        cd_out = ''.join([x.decode('utf8') for x in pipe.stderr.readlines()])
+        cd_out = result.stderr.decode('utf8')
         match = re.compile('/var/log/(.*)').search(cd_out)
         if not match:
-            print "Did not find diagnostics filepath in chroma-diagnostics output"
-            sys.exit(1)
+            return ["Did not find diagnostics filepath in chroma-diagnostics output"]
+
         diagnostics = match.group(1).strip()
 
+        errors = []
+
         # Copy and expand the diagnostics locally, so they will be able to be read in browser in Jenkins.
-        self.fetch_log(server, "/var/log/%s" % diagnostics, '')
-        self.fetch_log(server, "chroma-diagnostics.log", '%s-chroma-diagnostics.log' % server)
+        errors.append(self.fetch_log(server, "/var/log/%s" % diagnostics, ''))
+        errors.append(self.fetch_log(server, "chroma-diagnostics.log", '%s-chroma-diagnostics.log' % server))
+
         if diagnostics.endswith('tar.lzma'):
-            subprocess.call(
-                ['tar', '--lzma', '-xvf', "%s/%s" % (self.destination_path, diagnostics),
-                    '-C', self.destination_path], stdout=subprocess.PIPE)
+            if safe_shell.run(['tar', '--lzma', '-xvf', "%s/%s" % (self.destination_path, diagnostics),
+                            '-C', self.destination_path]).rc:
+                errors.append("Error tar --lzma the chroma diagnostics file")
         elif diagnostics.endswith('tar.gz'):
-            subprocess.call(
-                ['tar', '-xvzf', "%s/%s" % (self.destination_path, diagnostics),
-                    '-C', self.destination_path], stdout=subprocess.PIPE)
+            if safe_shell.run(['tar', '-xvzf', "%s/%s" % (self.destination_path, diagnostics),
+                            '-C', self.destination_path]).rc:
+                errors.append("Error tar -xvzf the chroma diagnostics file")
         else:
-            print "Didnt recognize chroma-diagnostics file format"
-            sys.exit(1)
+            errors = "Didnt recognize chroma-diagnostics file format"
 
-        subprocess.call(['rm', '-f', "%s/%s" % (self.destination_path, diagnostics)],
-                stdout=subprocess.PIPE)
+        if safe_shell.run(['rm', '-f', "%s/%s" % (self.destination_path, diagnostics)]).rc:
+            errors.append("Unable to remove the diagnostics %s/%s" % (self.destination_path, diagnostics))
 
+        return errors
 
 if __name__ == '__main__':
 
     destination_path = sys.argv[1]
     cluster_cfg_path = sys.argv[2]
 
-    subprocess.call(['mkdir', '-p', destination_path])
-    subprocess.call(['rm', '-f', "%s.tgz" % destination_path])
+    safe_shell.run(['mkdir', '-p', destination_path])
+    safe_shell.run(['rm', '-f', "%s.tgz" % destination_path])
     cluster_cfg_json = open(cluster_cfg_path)
     cluster_cfg = json.loads(cluster_cfg_json.read())
 
@@ -104,4 +146,8 @@ if __name__ == '__main__':
         test_runners.append("root@%s" % test_runner['address'])
 
     log_collector = ChromaLogCollector(destination_path, chroma_managers, lustre_servers, test_runners)
-    log_collector.collect_logs()
+    errors = log_collector.collect_logs()
+
+    if errors:
+        print '\n'.join(errors)
+        sys.exit(1)
