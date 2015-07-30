7638290978e0b115546a590f9049432e9b6e5f9c
Joe Grund <joe.grund@intel.com>
2015-07-30 10:52:52 -0400

    HYD-4662 Create d3 Line model
    In our work towards generic charting, we need an easy way to create
    lines.
    
    Currently we use nvd3 for this, but it is buggy and not as configurable
    as we'd like.
    
    Instead of relying on this lib, we can create a simple
    line model with very few lines of code.
    
    This will enable us to use lines
    easily and on demand (charts, spark-lines) and not be beholden to nvd3.
    
      - Added get-line module.
      - Added associated tests.
      - Added flushD3Transitions to
        move transitions to end states.
    
    Change-Id: I98f83e600e18964d255a9fe723d3fccbb189bf8b
    Signed-off-by: Joe Grund <joe.grund@intel.com>
    Reviewed-on: http://review.whamcloud.com/15805
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Will Johnson <william.c.johnson@intel.com>
    Reviewed-by: Wayne Kenneth Seymour III <wayne.k.seymour.iii@intel.com>

--- chroma-manager/chroma_ui_new/test/global-setup.js
+++ chroma-manager/chroma_ui_new/test/global-setup.js
@@ -3,6 +3,13 @@ beforeEach(module('fixtures', 'imlMocks'));
 (function () {
   'use strict';
 
+  window.flushD3Transitions = function flushD3Transitions () {
+    var now = Date.now;
+    Date.now = function mockNow () { return Infinity; };
+    window.d3.timer.flush();
+    Date.now = now;
+  };
+
   // Go around the injector and set this globally for tests.
   window.mock = new window.Mock();
 
