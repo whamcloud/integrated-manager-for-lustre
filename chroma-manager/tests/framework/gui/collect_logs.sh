127caf1433b7dd555b089d29ad39e9e1884adb2b
Joe Grund <joe.grund@intel.com>
2015-12-30 09:20:18 -0500

    HYD-4881 Jenkins gui tests: Only provision one node
    The selenium_cluster_config.json can be cleaned up considerably.
    
    In addition, it can be updated to only provision
    a single node instead of two.
    
      - Update selenium_cluster_config.json to only provision one node.
      - Remove unneeded exports.
      - Remove unneeded dependencies.
    
    Change-Id: Ib47e985c0fe34466a2d0146e83094db61bd0f7be
    Signed-off-by: Joe Grund <joe.grund@intel.com>
    Reviewed-on: http://review.whamcloud.com/17753
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Brian J. Murrell <brian.murrell@intel.com>
    Reviewed-by: Chris Gearing <chris.gearing@intel.com>

--- chroma-manager/tests/framework/gui/collect_logs.sh
+++ chroma-manager/tests/framework/gui/collect_logs.sh
@@ -11,20 +11,6 @@ eval $(python $CHROMA_DIR/chroma-manager/tests/utils/json_cfg2sh.py "$CLUSTER_CO
 
 MEASURE_COVERAGE=${MEASURE_COVERAGE:-false}
 
-ssh root@$CHROMA_MANAGER "tar -czf - -C /var/log/chroma . -C /var/log/nginx . -C /var/log messages" | tar -xzf - -C $WORKSPACE/test_logs
 scp chromatest@$TEST_RUNNER:.vnc/*.log $WORKSPACE/test_logs/
-scp chromatest@$TEST_RUNNER:chroma_test_env/chroma/chroma-manager/*log $WORKSPACE/test_logs/
-scp -r chromatest@$TEST_RUNNER:chroma_test_env/chroma/chroma-manager/debug-screen-shots/ $WORKSPACE
-scp -r chromatest@$TEST_RUNNER:chroma_test_env/chroma/chroma-manager/failed-screen-shots/ $WORKSPACE
-scp -r chromatest@$TEST_RUNNER:chroma_test_env/chroma/chroma-manager/failed-browser-logs/ $WORKSPACE
-
-if $MEASURE_COVERAGE; then
-  ssh root@$CHROMA_MANAGER <<EOF
-    chroma-config stop
-    cd /var/tmp/
-    coverage combine
-EOF
-  scp root@$CHROMA_MANAGER:/var/tmp/.coverage $WORKSPACE/.coverage.raw
-fi
 
 exit 0
