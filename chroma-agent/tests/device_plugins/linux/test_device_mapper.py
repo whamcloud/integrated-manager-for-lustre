02cb24985ef3002be77c48e89a59a623f169581b
Chris Gearing <chris.gearing@intel.com>
2016-02-11 16:32:47 +0000

    HYD-5480 Chroma agent causes constant UDEV change events
    On systems running the chroma-agent process, "udevadm monitor" reports constant
    UDEV change events for all disk drives (/dev/sd*) and device mapper drives
    (/dev/dm-*). The events come in bursts about every one second.
    
    Stopping the chroma-agent process with "/etc/init.d/chroma-agent stop" causes
    the events to stop.
    
    These constant change events are likely not good for the storage subsystem and
    could affect Lustre performance negatively.
    
    This was reported by HP as HP-44
    
    The solution is to code more defensively when dealing with multimap devices. It
    is quite possible that a multimap devices reports a 'child' device that is not
    actually present in the system. Whilst debugging this it was also discovered that
    the agent is not resilient to lvm2 not being installed and so the code was updated
    to make that more resilient.
    
     - Check that devices are reported by blkid before trying to add them as children of
       the parent multimap device.
     - Deal gracefully with the case of vgs or lvg not being installed.
    
    Change-Id: I277504cdcaaf6c01b84ea1bbeea2c20159207275
    Signed-off-by: Chris Gearing <chris.gearing@intel.com>
    Reviewed-on: http://review.whamcloud.com/18423
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Tom Nabarro <tom.nabarro@intel.com>
    Reviewed-by: Abiola Saraki <abiola.saraki@intel.com>

---
+++ chroma-agent/tests/device_plugins/linux/test_device_mapper.py
@@ -0,0 +1,34 @@
+import errno
+
+from tests.command_capture_testcase import CommandCaptureTestCase, CommandCaptureCommand
+from chroma_agent.device_plugins.linux_components.device_mapper import DmsetupTable
+
+
+class TestDmsetupTable(CommandCaptureTestCase):
+
+    def test_lvm2_not_installed(self):
+        self.add_commands(CommandCaptureCommand(('vgs', '--units', 'b', '--noheadings', '-o', 'vg_name,vg_uuid,vg_size'),
+                                                rc=errno.ENOENT),
+                          CommandCaptureCommand(('dmsetup', 'table'),
+                                                stdout="No devices found"))
+
+        dm_setup_table = DmsetupTable({})
+
+        self.assertEqual(dm_setup_table.vgs, {})
+        self.assertEqual(dm_setup_table.lvs, {})
+
+        self.assertRanAllCommandsInOrder()
+
+        self.reset_command_capture()
+        self.add_commands(CommandCaptureCommand(('vgs', '--units', 'b', '--noheadings', '-o', 'vg_name,vg_uuid,vg_size'),
+                                                rc=errno.ENOENT),
+                          CommandCaptureCommand(('lvs', '--units', 'b', '--noheadings', '-o', 'lv_name,lv_uuid,lv_size,lv_path', 'bob'),
+                                                rc=errno.ENOENT))
+
+        for vgs in dm_setup_table._get_vgs():
+            self.assertFalse(True, '_get_vgs did not deal with missing vgs')
+
+        for lvs in dm_setup_table._get_lvs('bob'):
+            self.assertFalse(True, '_get_lvs did not deal with missing lvg')
+
+        self.assertRanAllCommandsInOrder()
