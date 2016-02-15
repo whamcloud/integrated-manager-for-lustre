b177a1c9ddf3b25a7b7800913bfe3c257c841137
Tom Nabarro <tom.nabarro@intel.com>
2016-02-15 16:33:31 +0000

    HYD-5460 Extend FirewallControl class to handle remote firewall management
    Currently code within test_firewall_access.py and remote_operations.py issue
    shell commands remotely which are not EL7 compatible. Solution is to create
    RemoteFirewallControl class to return compatible strings to issue over SSH
    connection after establishing correct target firewall application.
    
    Also extend the classes to process firewall application output to identify
    existing ip rules through iptables (el6) or firewalld (el7).
    
    This change extends from HYD-5448 and should also include verifying that
    duplicate rules do not exist after IML configuration.
    
      - create RemoteFirewallControl class and subclasses with methods returning
        strings for remote shell actions
      - implement process_rules methods in RemoteFirewallControl subclasses
        to build rulelist from firewall list command stdout
      - assert exact number of expected rules exist in firewall
        rather than allowing for duplicates in test_firewall_access
      - add necessary methods to RemoteFirewallControl (integrating changes
        necessary for HYD-5426) handling remote_operations.py firewall functionality
      - take remote access function as parameter to RemoteFirewallControl.create()
      - change remote_operations.py RealRemoteOperations._ssh_access() to return
        Shell.RunResult named tuple (common code)
      - implement object caching and error handling inside class
      - create() decides on class applicablity based firewall application preference
        (firewall-cmd preferred over lokkit)
      - add unit tests for create/fw app detection, command string output and
        rules processing
      - move to chroma-manager/tests/utils as this is test code
      - move tests to chroma-manager/tests/unit/lib
      - fix port-proto filter testing for rules in test_firewall_access.py
      - process firewall-cmd output if new-line or space separated
      - fix selinux detection to work with both el6 & el7 (HYD-5626)
      - when creating RFC with _ssh_address func, pass expected_return_code=None
      - to remove port on iptables, find rule using rule-spec that matches
        lokkit added rule, iptables-save used so removed rule not reinstated
    
    Change-Id: I297e1489ba651ffec4197b054bafde57dc89c05f
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>
    Reviewed-on: http://review.whamcloud.com/18458
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Brian J. Murrell <brian.murrell@intel.com>
    Reviewed-by: Chris Gearing <chris.gearing@intel.com>

--- chroma-agent/tests/chroma_common/lib/test_firewall_control.py
+++ chroma-agent/tests/chroma_common/lib/test_firewall_control.py
@@ -5,8 +5,7 @@ import abc
 from chroma_agent.chroma_common.lib import util
 from chroma_agent.chroma_common.lib.firewall_control import FirewallControl
 from chroma_agent.chroma_common.lib.firewall_control import FirewallControlEL6
-from tests.command_capture_testcase import CommandCaptureTestCase
-from tests.command_capture_testcase import CommandCaptureCommand
+from tests.command_capture_testcase import CommandCaptureTestCase, CommandCaptureCommand
 
 
 class BaseTestFC:
