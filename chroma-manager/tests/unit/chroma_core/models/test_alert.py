dc8760673cda6cde1bdb1ef213dec68fcc7019a6
Chris Gearing <chris.gearing@intel.com>
2016-04-12 19:22:16 +0200

    HYD-5827 Alert messages do not change when the AlertType changes
    The solution to HYD-5736 caused Alert messages to be cached rather
    than regenerated. This made it possible for the GUI to search them
    but also made it possible for the AlertItems referenced to be deleted
    with the message becoming impossible to generate.
    
    However when an Alert is cast to a different Alert for example
    CommandRunningAlert becomes CommandCancelledAlert the message must
    be regenerated or it may well be incorrect.
    
    The cast function for Alert's needs to regenerate the message after
    the alert is cast.
    
     - Regenerate the Alert message in the AlertStateBase.cast method
     - Add a new test_alert.py in the unit tests to verify this change.
    
    Change-Id: Ide3bc8c5cfab67dfacd6b77dc0414b594a164612
    Signed-off-by: Chris Gearing <chris.gearing@intel.com>
    Reviewed-on: http://review.whamcloud.com/19483
    Tested-by: Jenkins
    Reviewed-by: Tom Nabarro <tom.nabarro@intel.com>
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Abiola Saraki <abiola.saraki@intel.com>

---
+++ chroma-manager/tests/unit/chroma_core/models/test_alert.py
@@ -0,0 +1,27 @@
+from tests.unit.lib.iml_unit_test_case import IMLUnitTestCase
+
+from chroma_core.models import CommandRunningAlert
+from chroma_core.models import CommandCancelledAlert
+from chroma_core.models import AlertState
+
+
+class TestAlert(IMLUnitTestCase):
+
+    def test_message_regeneration_on_cast(self):
+        command = self.make_command(message='Houston we have a problem')
+
+        CommandRunningAlert.notify(command, True)
+
+        alerts = AlertState.objects.all()
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].message(), 'Command Houston we have a problem running')
+
+        # Now cast it somewhere else.
+        command_alert = alerts[0].cast(CommandCancelledAlert)
+
+        self.assertEqual(command_alert.message(), 'Command Houston we have a problem cancelled')
+
+        # Ensure we still have only 1 alert.
+        alerts = AlertState.objects.all()
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].message(), 'Command Houston we have a problem cancelled')
