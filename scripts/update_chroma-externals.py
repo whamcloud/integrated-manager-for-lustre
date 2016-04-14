c3170eec919355c4b1e9a9de4f7d95791c349f38
Brian J. Murrell <brian.murrell@intel.com>
2016-04-14 13:16:24 -0400

    HYD-4775 Remove need for chroma-externals pragma
    The chroma-externals pragma works well but has an overhead that
    when a number of patches exist after a chroma-externals pragma
    they must all have the commit message change.
    
    We can change to use the sha1 in the original commit to find
    the gerrit commit to pull.
    
    Process is
    
    See if git submodule update works
    If not get the sha1 that we require from the error message, this
    is not pretty but I couldn't work out how to do it another way.
    query git to findout whic commit this sha1 is.
    Fetch that commit from gerrit.
    Finally to ensure all is well git submodule update should now work.
    
     - New update_chroma-externals script.
     - Change calls to use the new script.
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>
    Change-Id: I651caca7b56af02e39e711fa4d09d88502971e42
    Reviewed-on: http://review.whamcloud.com/19560
    Tested-by: Jenkins
    Tested-by: Chroma Test User <hpdd-devops@intel.com>
    Reviewed-by: Tom Nabarro <tom.nabarro@intel.com>
    Reviewed-by: Will Johnson <william.c.johnson@intel.com>
    Reviewed-by: Joe Grund <joe.grund@intel.com>

---
+++ scripts/update_chroma-externals.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+#
+# INTEL CONFIDENTIAL
+#
+# Copyright 2013-2016 Intel Corporation All Rights Reserved.
+#
+# The source code contained or described herein and all documents related
+# to the source code ("Material") are owned by Intel Corporation or its
+# suppliers or licensors. Title to the Material remains with Intel Corporation
+# or its suppliers and licensors. The Material contains trade secrets and
+# proprietary and confidential information of Intel or its suppliers and
+# licensors. The Material is protected by worldwide copyright and trade secret
+# laws and treaty provisions. No part of the Material may be used, copied,
+# reproduced, modified, published, uploaded, posted, transmitted, distributed,
+# or disclosed in any way without Intel's prior express written permission.
+#
+# No license under any patent, copyright, trade secret or other intellectual
+# property right is granted to or conferred upon you by disclosure or delivery
+# of the Materials, either expressly, by implication, inducement, estoppel or
+# otherwise. Any license under such intellectual property rights must be
+# express and approved by Intel in writing.
+
+
+import sys
+import re
+import os
+import json
+from collections import namedtuple
+
+from chroma_common.lib import shell
+
+
+def gerrit_changes(user, change_id):
+    changes = []
+
+    for line in run_command(['ssh',
+                             '-p',
+                             '29418',
+                             '%s@review.whamcloud.com' % user,
+                             'gerrit query',
+                             '--format', 'JSON',
+                             '--patch-sets', change_id]).stdout.split('\n'):
+        if line:
+            change = json.loads(line.strip())
+
+            # Only include the changes not the gerrit stats
+            if 'type' not in change:
+                changes.append(change)
+
+    return changes
+
+
+def run_command(args, exit_on_error=True):
+    result = namedtuple("result", "rc stdout stderr")
+
+    result.rc, result.stdout, result.stderr = shell.run(args)
+
+    if result.rc and exit_on_error:
+        print "%s exited with error code %s, stdout %s" % (' '.join(args), result.rc, result.stderr)
+        sys.exit(result.rc)
+
+    print "%s returned %s" % (" ".join(args), result.rc)
+
+    return result
+
+
+def git_user():
+    try:
+        fetch_url = run_command(['bash', '-c', 'git remote show origin | grep "Fetch URL:"']).stdout
+        user = re.search("Fetch URL: [a-z]*://([a-z]*)", fetch_url).groups()[0]
+    except:
+        user = 'hudson'
+
+    print "Git user is %s" % user
+
+    return user
+
+
+def make_pristine():
+    # Ensure that the submodules repository is absolutely pristine and unchanged. exit with rc !=0 if changes have occurred.
+
+    # We can only make it pristine if it actually exists. It may be that there is not repo yet because we have only
+    # done the git submodule init, so the repo is not truely in existence.
+    if os.path.isdir('chroma-externals'):
+        os.chdir('chroma-externals')
+        run_command(['git', 'reset', '--hard'])
+        run_command(['git', 'clean', '-dfx'])
+        os.chdir('..')
+
+
+def fetch_chroma_externals(user, externals_sha1):
+    changes = gerrit_changes(user, externals_sha1)
+
+    if not changes:
+        print "Unable to find git commit for chroma-externals sha1 %s" % externals_sha1
+        sys.exit(-1)
+
+    # Find the patch set ref
+    try:
+        ref = next(change['ref'] for change in changes[0]['patchSets'] if change['revision'] == externals_sha1)
+    except StopIteration:
+        print "Unable to find gerrit patchset for chroma-externals sha1 %s" % externals_sha1
+        sys.exit(-1)
+
+    # Now fetch the appropriate sha1 from gerrit.
+    os.chdir('chroma-externals')
+    run_command(['git',
+                 'fetch',
+                 'ssh://%s@review.whamcloud.com:29418/chroma-externals' % user,
+                 ref])
+    run_command(['git', 'checkout', 'FETCH_HEAD'])
+    os.chdir('..')
+
+    # Submodule init should now work just fine.
+    run_command(['git', 'submodule', 'update'])
+
+
+# End of functions, start of execution.
+run_command(['git', 'submodule', 'init'])
+
+make_pristine()
+
+fetch_externals = run_command(['git', 'submodule', 'update'], False)
+
+if fetch_externals.rc:
+    if 'fatal: reference is not a tree' not in fetch_externals.stderr:
+        print "git submodule update returned an unexpected error."
+        print "rc=%s" % fetch_externals.rc
+        print "stdout=%s" % fetch_externals.stdout
+        print "stderr=%s" % fetch_externals.stderr
+        sys.exit(-1)
+
+    user = git_user()
+
+    externals_sha1_re = re.search('fatal: reference is not a tree: ([a-z0-9]+)', fetch_externals.stderr)
+    externals_sha1 = externals_sha1_re.groups()[0]
+
+    fetch_chroma_externals(user, externals_sha1)
