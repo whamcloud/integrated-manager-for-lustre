30c89a3812a05ff426209b8f6c434ca3418018a5
Kelsey Prantis <kelsey.prantis@intel.com>
2016-04-22 02:16:16 -0400

    HYD-5895 Use "best run" instead of "latest run" in test aggregation
    Currently the test reporting aggregation script takes the results
    of the most recent run for each test suite. This can lead to some
    frustrating situations such as two people both kick off re-runs,
    the first one passes and marks the review with a check, but then
    the second one hits an intermittent failure and undoes the check.
    Even if you abort the second one, it is still counted. Also, now
    that we have a matrix of distros to test, it can be difficult to
    get both distros to pass at once when intermittent failures are
    frequent.
    
    This patch changes the test aggregation to instead pick the "best"
    test run. This means that it will take the most recent passing run,
    or the most recent failing run if none are passing. This should make
    it easier to get past test while still making sure that every test
    suite passes at least once.
    
      - Modify chroma-manager/tests/framework/test_reporting/aggregate_test_results.py
        to select the "best run" rather than the "most recent run".
      - Also fixes a potential false positive situation. If any of the selected runs
        fail, the overall report needs to be a fail.
      - Makes some improvements to logging to be able to see which runs are
        considered, and which ones are ultimately selected.
      - Is smart enough to exclude currently running tests and tests that have
        fallen out of history.
      - Upgrade jenkinsapi to 0.2.28 to get fix for bug in is_good and
        efficiency improvements. Does not upgrade all the way to 0.2.29 as
        that is incompatible with our current version of jenkins.
    
    Change-Id: I2990e248907b5d84736913e1b289222a36285680
    Signed-off-by: Kelsey Prantis <kelsey.prantis@intel.com>
    Reviewed-on: http://review.whamcloud.com/19727
    Tested-by: Jenkins
    Tested-by: Chroma Test User
    Reviewed-by: Chris Gearing <chris.gearing@intel.com>
    Reviewed-by: Tom Nabarro <tom.nabarro@intel.com>
    Reviewed-by: Joe Grund <joe.grund@intel.com>

--- chroma-manager/tests/framework/test_reporting/aggregate_test_results.py
+++ chroma-manager/tests/framework/test_reporting/aggregate_test_results.py
@@ -4,12 +4,13 @@
 #
 # Usage: ./aggregate_test_results.py jenkins_url username password build_job_name build_job_build_number valid_test_jobs required_tests
 
+from collections import defaultdict
 import errno
 import os
 import re
 import sys
-from jenkinsapi.utils.requester import Requester
 
+from jenkinsapi.utils.requester import Requester
 from jenkinsapi import api
 
 import logging
@@ -27,6 +28,98 @@ def mkdir_p(path):
             pass
         else:
             raise
+
+
+def compile_unique_runs(usages):
+    """
+    Take the usage data from Jenkins, and convert it into a dict of test suites
+    mapped to the runs of that suite for this build.
+
+    Usage data is a record from Jenkins of which jobs used a given fingerprint.
+    Fingerprints are how our test jobs and their associated builds are linked
+    in Jenkins. So it is from this data we must dig out which test runs are for
+    our build. Please see the jenkins api docs for details on the exact formats
+    of usages and fingerprints.
+
+    :param usages: A list of usage data from the fingerprints for a build job.
+
+    :returns: A dict where they keys are the test suite names, and the value is
+        the list of build numbers of that test suite that the usages data
+        indicated were downstream of our build job.
+    """
+    runs = {}
+    for usage in usages:
+        for use in usage:
+            # Get the pure job name, minus any extra stuff about the distro, etc.
+            job_name = use['name'].split('/')[0]
+
+            # Use the ranges of build numbers provided in the usage and
+            # extrapolate out into a complete list of each number in the range,
+            # each of which is a test run in Jenkins associated with our build.
+            build_nums = []
+            for ranges in use['ranges'].values():
+                for range in ranges:
+                    build_nums.append(range['end'] - 1)
+
+            # The complete set of fingerprints will end up giving references to
+            # the same build numbers multiple times. So we keep it to a unique
+            # set so we don't waste time re-evaluating the same runs over and
+            # over again with costly calls to Jenkins.
+            runs[job_name] = list(set(runs.get(job_name, []) + build_nums))
+
+    for job_name, unique_build_nums  in runs.iteritems():
+        logging.info("Found these build numbers for '%s': '%s'" % (job_name, unique_build_nums))
+
+    return runs
+
+
+def get_best_runs(job_name, build_nums):
+    """
+    Return the most recent passing run for each distro, or, if none have passed,
+    the most recent non-passing run.
+
+    :param job_name: The name of the test suite in Jenkins, eg, chroma-unit-tests.
+    :param build_nums: The build numbers in Jenkins for that test runs of that suite
+        that we want to find the best run of.
+
+    :return: List of the best builds to use in the reporting from this test suite.
+    """
+    # Get the build object from jenkins for each run
+    test_runs = defaultdict(list)
+    for build_num in build_nums:
+        master_job = jenkins.get_job(job_name)
+        try:
+            master_build = master_job.get_build(build_num)
+        except KeyError:
+            # Tests that are still running or have fallen out of history will have a KeyError
+            continue
+
+        for run in master_build.get_matrix_runs():
+            try:
+                distro = re.search('distro=(.*?),', run.baseurl).group(1)
+            except:
+                distro = re.search('slave=(.*?)%', run.baseurl).group(1)
+            test_runs[distro].append(run)
+
+    # Pick out the most recent passing build if one exists, else the most recent non-passing
+    best_runs = []
+    for distro, runs in test_runs.iteritems():
+        most_recent_pass = None
+        most_recent_fail = None
+
+        # By first sorting the runs by build number, we ensure that we have the
+        # most recent for each at the end.
+        for run in sorted(runs, key = lambda item: item.buildno):
+            if run.is_good():
+                most_recent_pass = run
+            else:
+                most_recent_fail = run
+
+        best_run = most_recent_pass if most_recent_pass else most_recent_fail
+        logging.info("Selected build num '%s' as the best run for '%s' distro '%s'" % (best_run.buildno, job_name, distro))
+        best_runs.append(best_run)
+
+    return best_runs
 
 
 if __name__ == '__main__':
@@ -48,41 +141,27 @@ if __name__ == '__main__':
     job = jenkins.get_job(build_job_name)
     build = job.get_build(build_job_build_number)
 
-    test_runs = []
+    # Gather the fingerprints that link together the jobs
     downstream_jobs_names = build.job.get_downstream_job_names()
     fingerprint_data = build.get_data("%s?depth=2&tree=fingerprint[fileName,usage[name,ranges[ranges[end]]]]" % build.python_api_url(build.baseurl))
+    usages = []  # Usages are a record of a fingerprint being used in specific test runs
+
     for fingerprint in fingerprint_data['fingerprint']:
         if fingerprint['fileName'] == 'build_info.txt':
-            usage = fingerprint['usage']
-            for use in usage:
-                job_name = use['name'].split('/')[0]
-                if job_name in downstream_jobs_names and job_name in valid_test_jobs:
-                    build_nums = []
-                    for k, ranges in use['ranges'].iteritems():
-                        for range in ranges:
-                            build_nums.append(range['end'] - 1)
-                    latest_build = max(build_nums)
-                    try:
-                        if job_name == use['name']:
-                            # Job is not multiconfiguration
-                            test_runs.append(jenkins.get_job(job_name).get_build(latest_build))
-                        else:
-                            # Job is multiconfiguration
-                            master_job = jenkins.get_job(job_name)
-                            master_build = master_job.get_build(latest_build)
-                            for run in master_build.get_matrix_runs():
-                                test_runs.append(run)
-                        logging.info("Added '%s' build num '%s' to the list of test runs" % (job_name, latest_build))
-                    except Exception, e:
-                        # Can happen for things like old disabled jobs.
-                        # Any real missing builds will be caught by the
-                        # check for required runs below.
-                        logging.warning("Exception trying to get '%s' build num '%s': '%s'" % (job_name, latest_build, e))
-                        raise e
+            usages.append(fingerprint['usage'])
 
-    # Double check all jobs have finished running
-    for test_run in test_runs:
-        test_run.block_until_complete()
+    # Use the fingerprint data to get a unique list of test runs for the build
+    unique_runs = compile_unique_runs(usages)
+
+    # Select the best run (most recent pass, or failure if no passes)
+    test_runs = []
+    for job_name, unique_build_nums in unique_runs.iteritems():
+        if (job_name in downstream_jobs_names) and (job_name in valid_test_jobs):
+            best_runs = get_best_runs(job_name, unique_build_nums)
+            if best_runs:
+                test_runs.extend(best_runs)
+
+    logging.info("Final list of runs we will report on '%s'" % test_runs)
 
     # Gather test and coverage reports from test jobs
     mkdir_p('coverage_files')
@@ -99,6 +178,7 @@ if __name__ == '__main__':
         test_reports = [v for k, v in artifacts.iteritems() if re.match(".*test|TEST.*.xml", k)]
         logging.info("Found the following reports for %s: '%s'" % (test_run.name, [r.filename for r in test_reports]))
         mkdir_p("test_reports/%s" % os.path.normpath(test_run.name))
+
         for test_report in test_reports:
             test_report.save_to_dir("test_reports/%s/" % os.path.normpath(test_run.name))
 
@@ -112,13 +192,16 @@ if __name__ == '__main__':
     found_tests = set([t.job.name for t in test_runs if os.listdir("test_reports/%s" % t.name)])
     logging.info("Found these tests: '%s'" % found_tests)
     missing_tests = required_tests.difference(found_tests)
+
     if missing_tests:
         print "MISSING TEST RESULTS! Did not find an expected test report for these tests: '%s'." % ', '.join(missing_tests)
         exit(1)
 
     # Check if there were test failures
-    passing_tests = [t for t in test_runs if t.is_good()]
-    if len(passing_tests) == len(test_runs):
-        print "SUCCESS"
-    else:
+    failing_tests = [t for t in test_runs if not t.is_good()]
+    if failing_tests:
+        logging.info("Failing tests: '%s'" % failing_tests)
         print "FAILED"
+        exit(1)
+    else:
+        print "SUCCESS"
