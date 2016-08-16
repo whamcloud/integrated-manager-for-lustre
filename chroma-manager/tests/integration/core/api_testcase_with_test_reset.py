2b2756656563a24eae1121f2969dc46ec2cb15de
Kelsey Prantis <kelsey.prantis@intel.com>
2016-08-16 18:21:43 -0400

    HYD-6250 Rename tarball from 'ieel' to 'ee'
    Product management has passed down that we need to change
    the 'ieel' in the tarball name to just 'ee' because 'Intel'
    is never supposed to be abbreviated.
    
    This patch changes 'ieel' to 'ee' and updates a few
    references to it.
    
      - Change SHORT_ARCHIVE_NAME from 'ieel' to 'ee' in
        include/Makefile.version.
      - Make the code in the upgrade tests that automatically finds the
        tarball name from the jenkins run for the old version we are
        upgrading from flexible enough it can match tarballs labelled
        either 'ieel' or 'ee'.
      - Update the path to the tarball in api_test_case_with_test_reset.
    
    Change-Id: Id499c9775927483fdeb2b257d42435c60507655b
    Signed-off-by: Kelsey Prantis <kelsey.prantis@intel.com>
    Reviewed-on: http://review.whamcloud.com/21959
    Tested-by: Jenkins
    Tested-by: Chroma Test User
    Reviewed-by: Will Johnson <william.c.johnson@intel.com>
    Reviewed-by: Tom Nabarro <tom.nabarro@intel.com>
    Reviewed-by: Joe Grund <joe.grund@intel.com>

--- chroma-manager/tests/integration/core/api_testcase_with_test_reset.py
+++ chroma-manager/tests/integration/core/api_testcase_with_test_reset.py
@@ -676,7 +676,7 @@ class ApiTestCaseWithTestReset(UtilityTestCase):
 
             result = self.remote_command(
                 chroma_manager['address'],
-                "ls /tmp/ieel-*/",
+                "ls /tmp/ee-*/",
                 expected_return_code = None
             )
             installer_contents = result.stdout
@@ -690,7 +690,7 @@ class ApiTestCaseWithTestReset(UtilityTestCase):
             logger.debug("Found these profiles: %s" % profiles)
             result = self.remote_command(
                 chroma_manager['address'],
-                "for profile_pat in %s; do chroma-config profile register /tmp/ieel-*/$profile_pat; done &> config_profile.log" % profiles,
+                "for profile_pat in %s; do chroma-config profile register /tmp/ee-*/$profile_pat; done &> config_profile.log" % profiles,
                 expected_return_code = None
             )
             chroma_config_exit_status = result.exit_status
