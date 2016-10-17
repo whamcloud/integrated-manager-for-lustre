fac56ca5f15093511478bbf2258730d8f7599e71
Kelsey Prantis <kelsey.prantis@intel.com>
2016-10-17 08:56:52 -0700

    HYD-6596 Update to Lustre build version 2.7.18.6
    The Lustre team has provided us with ther RC build for EE 3.1, which
    needs integrated into overall EE.
    
    Update to the Lustre version 2.7.18.6 by changing the following build
    parameters:
    
      JENKINS_BUILD_NUMBER = 133
    
    The following chroma-dependencies are effected:
      lustre[-client]
      zfs
    
      - Update chroma-dependencies/include/Makefile.lustre_build_meta
    
    Change-Id: Icb7ced9e62469a5499cbc7074bfda4888355dc3d
    Signed-off-by: Kelsey Prantis <kelsey.prantis@intel.com>
    Reviewed-on: http://review.whamcloud.com/23207
    Tested-by: Jenkins
    Tested-by: Chroma Test User
    Reviewed-by: Tom Nabarro <tom.nabarro@intel.com>
    Reviewed-by: Will Johnson <william.c.johnson@intel.com>
    Reviewed-by: Chris Gearing <chris.gearing@intel.com>

--- cluster-sim/cluster_sim/simulator.py
+++ cluster-sim/cluster_sim/simulator.py
@@ -246,7 +246,7 @@ class ClusterSimulator(Persisted):
                 'lustre-modules': (0, '2.1.4', '1', 'x86_64'),
                 'lustre-osd-ldiskfs': (0, '2.1.4', '1', 'x86_64'),
                 'lustre-osd-zfs': (0, '2.1.4', '1', 'x86_64'),
-                'kernel-devel-3.10.0-327.28.2.el7_lustre': (0, '2.6.32', '1', 'x86_64'),
+                'kernel-devel-3.10.0-327.36.2.el7_lustre': (0, '2.6.32', '1', 'x86_64'),
                 'zfs': (0, '0.6.5.3', '1', 'x86_64')
             },
             'worker': {
