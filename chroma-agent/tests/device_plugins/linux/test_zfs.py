9b61ea52cbfbc46446850f59f994fee32dd46425
Tom Nabarro <tom.nabarro@intel.com>
2017-03-27 21:15:56 +0100

    HYD-7280 Use global locks to stop zpool import-export sequence interruption
    Zpools can be imported and exported from other processes (which don't
    have knowledge of thread level locks) and interrupt the import-export
    sequences of IML. In order to avoid these race conditions,
    global/file locks should be implemented which provide protection from
    these interruptions effective between different processes, not just
    between threads in the same process.
    
    Implementing these global locks within the IML code will protect
    against situation where pacemaker calls chroma-agent from a different
    process than the chroma-agent daemon (and therefore not aware of any
    locks being held). In that situation, global locks set from IML code
    will be detectable by any process running on the same host.
    
    This patch is only the first stage of the work and mimics old
    behaviour by appending pid to lockfile dir, thus making locks
    specific to process. This stage replaces thread specific Locks
    with file locks.
    
      - create necessary temporary directory during ZfsDevice.__init__
        (this patch appends the process ID to the directory name)
      - create lock object during ZfsDevice.__init__
      - acquire and release lock file on lock_pool and unlock_pool
        respectively
      - if lock cannot be acquired, keep trying with a minute timeout
      - make locks reentrant by applying reference counting per thread
        and lock object combination, only unlock when reference count
        is 0, increment reference count after lock has been acquired or
        if thread is already locking
      - update and improve relevant unit tests
    
    Change-Id: I2fb19617d40312b0b090683e09c120c4ddfe1aac

--- chroma-agent/tests/device_plugins/linux/test_zfs.py
+++ chroma-agent/tests/device_plugins/linux/test_zfs.py
@@ -1,4 +1,4 @@
-from mock import patch
+from mock import patch, Mock
 
 from chroma_agent.device_plugins.linux import ZfsDevices
 from chroma_agent.device_plugins.linux_components.block_devices import BlockDevices
@@ -45,6 +45,8 @@ class TestZfs(LinuxAgentTests, CommandCaptureTestCase):
     @patch('logging.Logger.debug', mock_debug)
     @patch('chroma_agent.utils.BlkId', dict)
     @patch('chroma_agent.device_plugins.linux_components.block_devices.BlockDevices.find_block_devs', mock_empty_dict)
+    @patch('chroma_agent.device_plugins.linux_components.zfs.ZfsDevice.lock_pool', Mock())
+    @patch('chroma_agent.device_plugins.linux_components.zfs.ZfsDevice.unlock_pool', Mock())
     def _setup_zfs_devices(self):
         blockdevices = BlockDevices()
 
