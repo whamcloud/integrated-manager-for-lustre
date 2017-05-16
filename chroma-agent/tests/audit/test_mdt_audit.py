626489658c35e5125007e6bda3d56e3ea0692e74
Brian J. Murrell <brian.murrell@intel.com>
2017-05-16 13:55:14 -0400

    Get lustre, zfs, e2fsprogs and robinhood from external repos
    For now we will just use the Jenkins job lustre-b2_10 and the yum
    repo that is created in it's artefacts.  When a final Lustre release
    is made this should switch to the official download location.
    
    This means removing their corresponding bundles.  As a short-term
    workaround until we completely excise bundles, convert removed
    bundles to "external" psuedo-bundle.
    
    While we are pulling from their Jenkins (i.e. before 2.10 is GA) we
    need to use build.whamcloud.com to get around silly proxy restrictions.
    
    Remove the data['packages'] from bundle validation as it looks
    like it was only ever to cover lazy profile writing in any case.
    data['bundles'] should be the canonical list of required bundles
    and should [have] always been complete instead of inferring bundles
    from packages.
    
    Configure client repo to point at the lustre jenkins build.
    Also disable GPG checking on the lustre repos.  Remove duplicate
    client repo creation.
    
    Simplify (greatly!) the lustre client installation
    
    Given the packages provided by Lustre in their master branch, adapt
    the EFS test set up to install the correct packages.
    
    Update create_installer to install from repos also.  This is so
    simple now that it can probably go away at some point.
    
    Various "hardening" techniques to deal with Jenkins being a flakey
    HTTP server.
    
    Some Lustre /proc files were moved to /sys
    - lots of fixes for unit tests
    - remove old 1.8.7.80 data
    - add lustre 2.9.58 data to agent unit tests
    
    Don't need to call cleanup in run_tests.
    
    Fix sim failure in test_updates by forcing a package scan before
    checking for package update alerts.
    
    Add a "unit_tests" target in the top-level Makefile that sets up
    the virtual environment and then runs the chroma-manager->unit_tests
    target.
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- chroma-agent/tests/audit/test_mdt_audit.py
+++ chroma-agent/tests/audit/test_mdt_audit.py
@@ -7,7 +7,7 @@ from tests.test_utils import PatchedContextTestCase
 class TestMdtAudit(PatchedContextTestCase):
     def setUp(self):
         tests = os.path.join(os.path.dirname(__file__), '..')
-        self.test_root = os.path.join(tests, "data/lustre_versions/2.0.66/mds_mgs")
+        self.test_root = os.path.join(tests, "data/lustre_versions/2.9.58_86_g2383a62/mds_mgs")
         super(TestMdtAudit, self).setUp()
         self.audit = MdtAudit()
 
--- chroma-agent/tests/metrics/test_node.py
+++ chroma-agent/tests/metrics/test_node.py
@@ -7,14 +7,14 @@ from tests.test_utils import PatchedContextTestCase
 class TestNodeMetrics(PatchedContextTestCase):
     def setUp(self):
         tests = os.path.join(os.path.dirname(__file__), '..')
-        self.test_root = os.path.join(tests, "data/lustre_versions/2.0.66/mds_mgs")
+        self.test_root = os.path.join(tests, "data/lustre_versions/2.9.58_86_g2383a62/mds_mgs")
         super(TestNodeMetrics, self).setUp()
         self.audit = NodeAudit()
         self.metrics = self.audit.metrics()
 
     def test_node_cpustats(self):
-        self.assertEqual(self.metrics['raw']['node']['cpustats']['total'], 3540537)
-        self.assertEqual(self.metrics['raw']['node']['cpustats']['user'], 24601)
+        self.assertEqual(self.metrics['raw']['node']['cpustats']['total'], 10309070)
+        self.assertEqual(self.metrics['raw']['node']['cpustats']['user'], 360581)
 
     def test_node_meminfo(self):
-        self.assertEqual(self.metrics['raw']['node']['meminfo']['MemTotal'], 3991680)
+        self.assertEqual(self.metrics['raw']['node']['meminfo']['MemTotal'], 1883716)
