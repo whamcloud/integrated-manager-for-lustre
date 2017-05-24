7dc56d3471866bb276cefbeade1981bb6418be74
Brian J. Murrell <brian.murrell@intel.com>
2017-05-24 15:51:08 -0400

    Simplify (greatly!) the lustre client installation

--- chroma-manager/tests/framework/integration/utils/install_client.sh
+++ chroma-manager/tests/framework/integration/utils/install_client.sh
@@ -14,41 +14,8 @@ sslverify=0
 gpgcheck=0
 EOF
 
-# it is tempting to just use:
-#   yum install --exclude=kernel-debug -y lustre-client-modules lustre-client
-#   KERNEL_VERSION_AND_RELEASE=\"\$(rpm -qR lustre-client-modules | sed -n -e '/^kernel =/s/.* = //p')\";
-# here, however if a newer kernel than what the Lustre client wants is
-# installed then yum complains and refuses to install the older kernel
-# pity
+# disable the server repo
+yum-config-manager --disable lustre
 
-yumdownloader lustre-client lustre-client-modules
-
-# Extract what kernel version and release we need to match the client rpms
-# rpm -qpr will list the dependencies of the rpm, and the sed strips out the
-# other dependencies and the 'kernel = ' at the beggining of the line.
-KERNEL_VERSION_AND_RELEASE=\"\$(rpm -qpR lustre-client-modules-* | sed -n -e '/^kernel =/s/.* = //p')\";
-
-if [[ \$(lsb_release -sr) = 6.* ]]; then
-    yumdownloader kernel-firmware-\$KERNEL_VERSION_AND_RELEASE
-
-    if ! rpm -q \$(rpm -qp kernel-firmware-*.rpm); then
-        rpm -Uvh --oldpackage kernel-firmware-*.rpm
-    fi
-    rm kernel-firmware-*.rpm
-fi
-
-yumdownloader kernel-\$KERNEL_VERSION_AND_RELEASE # Have to install explicitly to avoid getting kernel-debug in the automatic dependency resolution
-
-if ! rpm -q \$(rpm -qp kernel-*.rpm); then
-    rpm -ivh --oldpackage kernel*.rpm
-fi
-
-grubby --set-default=/boot/vmlinuz-\${KERNEL_VERSION_AND_RELEASE}.x86_64
-
-yum install -y lustre-client-modules-*.rpm lustre-client-*.rpm
-
-# Removed and installed a kernel, so need a reboot
-sync
-sync
-nohup bash -c \"sleep 2; init 6\" >/dev/null 2>/dev/null </dev/null & exit 0"
+yum -y install kmod-lustre-client"
 
