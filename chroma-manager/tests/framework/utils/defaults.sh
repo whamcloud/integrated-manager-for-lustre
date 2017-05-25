f0889395e282d2d1e98f096a46a209dbe4d4a426
Joe Grund <joe.grund@intel.com>
2017-05-25 12:14:32 -0400

    Remove Jenkins auth
    Remove all instances of jenkins auth.
    We will not need it going forward.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- chroma-manager/tests/framework/utils/defaults.sh
+++ chroma-manager/tests/framework/utils/defaults.sh
@@ -1,9 +1,3 @@
-# auth.sh contains the JENKINS_PULL environmental variable so we can avoid
-# printing it into the console in plaintext calling this script.
-set +x  # DONT REMOVE/COMMENT or you will risk exposing the jenkins-pull api token in the console logs.
-. $HOME/auth.sh
-set -x
-
 [ -r localenv ] && . localenv
 
 spacelist_to_commalist() {
@@ -42,12 +36,6 @@ set_defaults() {
         MEASURE_COVERAGE="false"
     fi
     rm -f /tmp/env-"$JOB_NAME"-"$BUILD_NUMBER"
-
-    # Variables that we expect to be set upstream, no "default"
-    export JENKINS_USER=${JENKINS_USER:-jenkins-pull}
-    set +x  # DONT REMOVE/COMMENT or you will risk exposing the jenkins-pull api token in the console logs.
-    export JENKINS_PULL=${JENKINS_PULL:?"Need to set JENKINS_PULL"}
-    set -x
 
     JOB_NAME=${JOB_NAME%%/*}
     export JOB_NAME=${JOB_NAME:?"Need to set JOB_NAME"}
