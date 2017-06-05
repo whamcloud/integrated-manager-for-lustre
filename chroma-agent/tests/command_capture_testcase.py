dfd832cf5716e650dd61c5c30b710621a3238a8f
Tom Nabarro <tom.nabarro@intel.com>
2017-06-05 23:14:57 +0100

    copyright notice update

--- chroma-agent/tests/command_capture_testcase.py
+++ chroma-agent/tests/command_capture_testcase.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2017 Intel Corporation. All rights reserved.
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file.
+
+
 import mock
 import errno
 
