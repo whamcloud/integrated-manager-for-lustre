693bed79387b7ff2e15164c86ffe4c6c56dea6ba
Tom Nabarro <tom.nabarro@intel.com>
2017-06-07 11:59:24 +0100

    correct agent_teardown_functions.py comments

--- chroma-agent/chroma_agent/lib/agent_teardown_functions.py
+++ chroma-agent/chroma_agent/lib/agent_teardown_functions.py
@@ -9,7 +9,7 @@ agent_daemon_teardown_functions = []
 def agent_daemon_teardown_function():
     """
     A decorator for connecting method to teardown. A method decorated with this will be called each time the
-    daemon is run. The call will occur just before the agent starts receiving commands from the manager.
+    daemon is terminated normally. The call will occur just before the agent stops receiving commands from the manager.
 
         @receiver(post_save, sender=MyModel)
         def signal_receiver(sender, **kwargs):
