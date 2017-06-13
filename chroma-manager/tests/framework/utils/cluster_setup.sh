b0c1ed48071e664510bf260bda4e0f11089da7a3
Brian J. Murrell <brian.murrell@intel.com>
2017-06-13 14:13:42 -0400

    Install RH key; Use proper Copr .repo file
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- chroma-manager/tests/framework/utils/cluster_setup.sh
+++ chroma-manager/tests/framework/utils/cluster_setup.sh
@@ -28,16 +28,14 @@ http_caching=packages
 .
 wq
 EOF
-if [ -f /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7]; then
-    rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
-fi
+for key in CentOS-7 redhat-release; do
+    if [ -f /etc/pki/rpm-gpg/RPM-GPG-KEY-$key ]; then
+        rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-$key
+    fi
+done
 yum-config-manager --enable addon-epel\$(rpm --eval %rhel)-x86_64
-yum-config-manager --add-repo https://copr-be.cloud.fedoraproject.org/results/managerforlustre/manager-for-lustre/epel-7-x86_64/
+yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/managerforlustre/manager-for-lustre/repo/epel-7/managerforlustre-manager-for-lustre-epel-7.repo
 yum-config-manager --add-repo http://mirror.centos.org/centos/7/extras/x86_64/
-yum -y install distribution-gpg-keys-copr
-if ! ls /usr/share/distribution-gpg-keys/copr/copr-*manager-for-lustre*; then
-    rpm --import https://copr-be.cloud.fedoraproject.org/results/managerforlustre/manager-for-lustre/pubkey.gpg
-fi" | dshbak -c
 if [ ${PIPESTATUS[0]} != 0 ]; then
     exit 1
 fi
