c55a556d0bedcff07422f7fc48c633d212d21265
Tom Nabarro <tom.nabarro@intel.com>
2017-06-22 23:50:25 +0100

    debugwq failing client install

--- chroma-manager/tests/framework/integration/utils/install_client.sh
+++ chroma-manager/tests/framework/integration/utils/install_client.sh
@@ -12,9 +12,19 @@ baseurl=https://build.whamcloud.com/job/lustre-master/lastSuccessfulBuild/arch=x
 enabled=1
 sslverify=0
 gpgcheck=0
+
+[lustre-server]
+name=lustre-server
+baseurl=https://build.whamcloud.com/job/lustre-master/lastSuccessfulBuild/arch=x86_64%2Cbuild_type=server%2Cdistro=el7%2Cib_stack=inkernel/artifact/artifacts/
+enabled=1
+sslverify=0
+gpgcheck=0
 EOF
 
 # disable the server repo
-yum-config-manager --disable lustre
+# yum-config-manager --disable lustre
+yum -y install --disablerepo=* --enablerepo=lustre-server kernel kernel-devel kernel-headers
 
-yum -y install lustre-client"
+yum -y install lustre-client
+
+modprobe lustre"
