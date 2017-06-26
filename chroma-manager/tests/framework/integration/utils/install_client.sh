db94d27d8f8c0fcb0cffdf30508b1985ba80a274
Tom Nabarro <tom.nabarro@intel.com>
2017-06-26 19:48:16 +0100

    revert fixes related to lustre-client install problems

--- chroma-manager/tests/framework/integration/utils/install_client.sh
+++ chroma-manager/tests/framework/integration/utils/install_client.sh
@@ -1,4 +1,4 @@
-# Install the IEEL Lustre client
+# Install the Lustre client
 ssh root@$CLIENT_1 "exec 2>&1; set -xe
 
 mkdir lustre_client_rpms
@@ -17,18 +17,6 @@ EOF
 # disable the server repo
 yum-config-manager --disable lustre
 
-# install kernel kmod-lustre-client was built for
-# TODO: derive this rather than hard coding
-yum -y install kernel-3.10.0-514.21.1.el7
-
-# Installed a kernel, so need a reboot
-sync
-sync
-nohup bash -c \"sleep 2; init 6\" >/dev/null 2>/dev/null </dev/null & exit 0"
-
-sleep 240
-
-ssh root@$CLIENT_1 "exec 2>&1; set -xe
 yum -y install lustre-client
 
 modinfo lustre
