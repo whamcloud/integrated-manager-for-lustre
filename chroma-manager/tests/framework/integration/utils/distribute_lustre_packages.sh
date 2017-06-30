852c8f148dc2e5fbcc5e7156049706c8e989cebc
Tom Nabarro <tom.nabarro@intel.com>
2017-06-30 22:06:59 +0100

    typo

--- chroma-manager/tests/framework/integration/utils/distribute_lustre_packages.sh
+++ chroma-manager/tests/framework/integration/utils/distribute_lustre_packages.sh
@@ -6,7 +6,7 @@ TAR_NAME=lustre-kernel-packages.tar.gz
 
 ssh root@$CLIENT_1 "exec 2>&1; set -xe
 
-ed -i -e '/enabled/a keepcache=1' /etc/yum.repos.d/build.whamcloud.com_job_lustre-b2_10_lastSuccessfulBuild_arch\=x86_64\,build_type\=server\,distro\=el7\,ib_stack\=inkernel_artifact_artifacts_.repo
+sed -i -e '/enabled/a keepcache=1' /etc/yum.repos.d/build.whamcloud.com_job_lustre-b2_10_lastSuccessfulBuild_arch\=x86_64\,build_type\=server\,distro\=el7\,ib_stack\=inkernel_artifact_artifacts_.repo
 yum install --downloadonly --disablerepo=* --enablerepo=lustre kernel-*_lustre
 cd $CACHE_DIR
 tar -cvf ~/$TAR_NAME ./*"
