6c9a7bdab4eeba4f8469690d56e634088a5808f4
Tom Nabarro <tom.nabarro@intel.com>
2017-06-30 23:59:17 +0100

    enable more repos when downloading packages

--- chroma-manager/tests/framework/integration/utils/distribute_lustre_packages.sh
+++ chroma-manager/tests/framework/integration/utils/distribute_lustre_packages.sh
@@ -7,7 +7,7 @@ TAR_NAME=lustre-kernel-packages.tar.gz
 ssh root@$CLIENT_1 "exec 2>&1; set -xe
 
 sed -i -e '/enabled/a keepcache=1' /etc/yum.repos.d/build.whamcloud.com_job_lustre-b2_10_lastSuccessfulBuild_arch\=x86_64\,build_type\=server\,distro\=el7\,ib_stack\=inkernel_artifact_artifacts_.repo
-yum install --downloadonly --disablerepo=* --enablerepo=lustre kernel-*_lustre
+yum install --downloadonly kernel-*_lustre
 cd $CACHE_DIR
 tar -cvf ~/$TAR_NAME ./*"
 
