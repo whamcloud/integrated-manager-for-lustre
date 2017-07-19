5c85769152cc7cb0275a90337056a4cfade0658e
Brian J. Murrell <brian.murrell@intel.com>
2017-07-19 16:02:06 -0400

    use build.whamcloud.com name
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>
    Run-tests: integration-tests-shared-storage-configuration

--- chroma-manager/tests/framework/utils/cluster_setup.sh
+++ chroma-manager/tests/framework/utils/cluster_setup.sh
@@ -41,7 +41,7 @@ if [[ \$HOSTNAME = *vm*2 ]]; then
     build_type=client
     #yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/managerforlustre/lustre-client/repo/epel-7/managerforlustre-lustre-\$build_type-epel-7.repo
     #rpm --import https://copr-be.cloud.fedoraproject.org/results/managerforlustre/manager-for-lustre/pubkey.gpg
-    yum-config-manager --add-repo https://build.hpdd.intel.com/lustre-reviews/configurations/axis-arch/\\\$basearch/axis-build_type/client/axis-distro/el\\\$releasever/axis-ib_stack/inkernel/builds/48896/archive/artifacts/
+    yum-config-manager --add-repo https://build.whamcloud.com/lustre-reviews/configurations/axis-arch/\\\$basearch/axis-build_type/client/axis-distro/el\\\$releasever/axis-ib_stack/inkernel/builds/48896/archive/artifacts/
     ed /etc/yum.repos.d/build.hpdd.intel.com_lustre-reviews_configurations_axis-arch_\\\$basearch_axis-build_type_client_axis-distro_el\\\$releasever_axis-ib_stack_inkernel_builds_48896_archive_artifacts_.repo << EOF
 1d
 1s/\\\\\$//g
