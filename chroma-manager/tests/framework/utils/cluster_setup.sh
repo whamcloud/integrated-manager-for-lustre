92d8e8bb2f87a88046a7c8317b668371adf456f5
Brian J. Murrell <brian.murrell@intel.com>
2017-07-22 21:24:33 -0400

    Update the repo file name for the new review build too
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- chroma-manager/tests/framework/utils/cluster_setup.sh
+++ chroma-manager/tests/framework/utils/cluster_setup.sh
@@ -42,7 +42,7 @@ if [[ \$HOSTNAME = *vm*2 ]]; then
     #yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/managerforlustre/lustre-client/repo/epel-7/managerforlustre-lustre-\$build_type-epel-7.repo
     #rpm --import https://copr-be.cloud.fedoraproject.org/results/managerforlustre/manager-for-lustre/pubkey.gpg
     yum-config-manager --add-repo https://build.whamcloud.com/lustre-reviews/configurations/axis-arch/\\\$basearch/axis-build_type/client/axis-distro/el7/axis-ib_stack/inkernel/builds/49006/archive/artifacts/
-    ed /etc/yum.repos.d/build.whamcloud.com_lustre-reviews_configurations_axis-arch_\\\$basearch_axis-build_type_client_axis-distro_el7_axis-ib_stack_inkernel_builds_48896_archive_artifacts_.repo << EOF
+    ed /etc/yum.repos.d/build.whamcloud.com_lustre-reviews_configurations_axis-arch_\\\$basearch_axis-build_type_client_axis-distro_el7_axis-ib_stack_inkernel_builds_49006_archive_artifacts_.repo << EOF
 1d
 1s/\\\\\$//g
 /^$/i
