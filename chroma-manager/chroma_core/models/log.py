af3518de29ce959c990bd67298954b025f07b616
Peter Robinson <peter.timothy.robinson@gmail.com>
2017-09-12 14:05:55 -0600

    Fix Log Sorting   - #274   - Changed the log sorting to be based off of "datetime" instead of "id"
    Signed-off-by: Peter Robinson <peter.timothy.robinson@gmail.com>

--- chroma-manager/chroma_core/models/log.py
+++ chroma-manager/chroma_core/models/log.py
@@ -23,21 +23,23 @@ class MessageClass:
     def to_string(cls, n):
         """Convert a MessageClass ID to a string"""
         if not hasattr(cls, '_to_string'):
-            cls._to_string = dict([(v, k) for k, v in cls.__dict__.items() if not k.startswith('_') and isinstance(v, int)])
+            cls._to_string = dict([(v, k) for k, v in cls.__dict__.items(
+            ) if not k.startswith('_') and isinstance(v, int)])
         return cls._to_string[n]
 
     @classmethod
     def from_string(cls, s):
         """Convert a string to a MessageClass ID"""
         if not hasattr(cls, '_from_string'):
-            cls._from_string = dict([(k, v) for k, v in cls.__dict__.items() if not k.startswith('_') and isinstance(v, int)])
+            cls._from_string = dict([(k, v) for k, v in cls.__dict__.items(
+            ) if not k.startswith('_') and isinstance(v, int)])
         return cls._from_string[s]
 
 
 class LogMessage(models.Model):
     class Meta:
         app_label = 'chroma_core'
-        ordering = ['id']
+        ordering = ['-datetime']
 
     datetime = models.DateTimeField()
     # Note: storing FQDN rather than ManagedHost ID because:
@@ -45,12 +47,14 @@ class LogMessage(models.Model):
     #    the relational ID of a host is a less sound ID than its name
     #  * It is efficient to avoid looking up fqdn to host ID on insert (log messages
     #    are inserted much more than they are queried).
-    fqdn = models.CharField(max_length = 255,
-                            help_text = "FQDN of the host from which the message was received.  Note that this host may"
+    fqdn = models.CharField(max_length=255,
+                            help_text="FQDN of the host from which the message was received.  Note that this host may"
                             "no longer exist or its FQDN may have changed since.")
-    severity = models.SmallIntegerField(help_text = "Integer data. `RFC5424 severity <http://tools.ietf.org/html/rfc5424#section-6.2.1>`_")
-    facility = models.SmallIntegerField(help_text = "Integer data. `RFC5424 facility <http://tools.ietf.org/html/rfc5424#section-6.2.1>`_")
-    tag = models.CharField(max_length = 63)
+    severity = models.SmallIntegerField(
+        help_text="Integer data. `RFC5424 severity <http://tools.ietf.org/html/rfc5424#section-6.2.1>`_")
+    facility = models.SmallIntegerField(
+        help_text="Integer data. `RFC5424 facility <http://tools.ietf.org/html/rfc5424#section-6.2.1>`_")
+    tag = models.CharField(max_length=63)
     message = models.TextField()
     message_class = models.SmallIntegerField()
 
