ccab20a9a52c3088151ce868edcb119fe7db7e6c
Brian J. Murrell <brian.murrell@intel.com>
2017-09-15 10:22:11 -0400

    Debug clear_ha_el7
    Probe MMP status of all disks after pacemaker is supposed to
    be shut down.
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>
    Run-tests: integration-tests-shared-storage-configuration

--- chroma-manager/tests/integration/core/clear_ha_el7.sh
+++ chroma-manager/tests/integration/core/clear_ha_el7.sh
@@ -1,9 +1,70 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+set -x
 
-systemctl stop corosync pacemaker pcsd
-systemctl disable corosync pacemaker pcsd
+mmp_status() {
+    local verbose="$1"
+    local disk="$2"
+
+    read interval mmp_time host < <(debugfs -c -R dump_mmp $disk 2>/dev/null |
+                                    sed -ne '/^update_interval: /s///p' \
+                                         -e '/^node_name: /s///p' \
+                                         -e '/^time:/s/.*: \([0-9][0-9]*\).*/\1/p' |
+                                    tr '\n' ' ')
+    if [ -z "$interval" -o -z "$mmp_time" -o -z "$host" ]; then
+        if $verbose; then
+            echo "Could not read MMP block from $1"
+        fi
+        return 4
+    fi
+
+    now=$(date +%s)
+    diff=$((now - mmp_time))
+    #echo $diff $mmp_time $now $host $interval
+    if [ $diff -gt $interval ]; then
+        if $verbose; then
+            echo "not in use"
+        fi
+        rc=0
+    else
+        if [ $host = $HOSTNAME ]; then
+            host="this host"
+            rc=1
+        else
+            rc=2
+        fi
+        if $verbose; then
+            echo "$disk in use on $host, last updated $diff seconds ago"
+        fi
+    fi
+    return $rc
+}
+
+date
+pcs status
+
+systemctl stop pcsd pacemaker corosync
+systemctl disable pcsd pacemaker corosync
+
+date
+
+cat /proc/mounts
+
+for d in a b c d e; do
+    mmp_status -v /dev/sd$d || true
+done
+sleep 60
+if grep " lustre " /proc/mounts; then
+    umount -t lustre -a
+    cat /proc/mounts
+    sleep 60
+fi
+cat /proc/mounts
+# if any devices report !0 now, they are still active and they should not be!
+for d in a b c d e; do
+    mmp_status -v /dev/sd$d
+done
 
 # figure it out for ourselves if we can
 # otherwise the caller needs to have set it
