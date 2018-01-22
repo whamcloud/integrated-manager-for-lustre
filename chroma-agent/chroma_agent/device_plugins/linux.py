1e3201b26bded09d0b88ff8bd8a8d3da3e5e6d00
Tom Nabarro <tom.nabarro@intel.com>
2018-01-22 23:59:39 +0000

    only process changes, don't send any data from agent plug-in
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- chroma-agent/chroma_agent/device_plugins/linux.py
+++ chroma-agent/chroma_agent/device_plugins/linux.py
@@ -22,7 +22,8 @@ class LinuxDevicePlugin(DevicePlugin):
 
     def _quick_scan(self):
         """Lightweight enumeration of available block devices"""
-        return BlockDevices.quick_scan()
+        # return BlockDevices.quick_scan()
+        return {}
 
     def _full_scan(self):
         # If we are a worker node then return nothing because our devices are not of interest. This is a short term
@@ -33,41 +34,42 @@ class LinuxDevicePlugin(DevicePlugin):
 
         # Before we do anything do a partprobe, this will ensure that everything gets an up to date view of the
         # device partitions. partprobe might throw errors so ignore return value
-        AgentShell.run(["partprobe"])
+        # AgentShell.run(["partprobe"])
 
         # Map of block devices major:minors to /dev/ path.
-        block_devices = BlockDevices()
+        # block_devices = BlockDevices()
 
         # fixme: implement inside block_devices using device_scanner output
         # EMCPower Devices
         # emcpowers = EMCPower(block_devices).all()
 
-        LinuxDevicePlugin.devices_scanned = True
+        # LinuxDevicePlugin.devices_scanned = True
 
-        block_device_dict = {s: getattr(block_devices, s) for s in
-                             ['local_fs', 'mds', 'vgs', 'lvs', 'zfspools', 'zfsdatasets', 'zfsvols']}
+        # block_device_dict = {s: getattr(block_devices, s) for s in
+        #                      ['local_fs', 'mds', 'vgs', 'lvs', 'zfspools', 'zfsdatasets', 'zfsvols']}
 
-        block_device_dict['devs'] = block_devices.block_device_nodes
+        # block_device_dict['devs'] = block_devices.block_device_nodes
 
         # block_device_dict['emcpowers'] = emcpowers
 
-        return block_device_dict
+        # return block_device_dict
+        return {}
 
     def _scan_devices(self, scan_always):
-        full_scan_result = None
+        # full_scan_result = None
 
-        if scan_always or (self._quick_scan() != self._last_quick_scan_result):
-            self._last_quick_scan_result = self._quick_scan()
-            full_scan_result = self._full_scan()
-            self._last_full_scan_result = full_scan_result
-        elif self._safety_send < DevicePlugin.FAILSAFEDUPDATE:
-            self._safety_send += 1
-        else:
+        # if scan_always or (self._quick_scan() != self._last_quick_scan_result):
+        self._last_quick_scan_result = self._quick_scan()
+        full_scan_result = self._full_scan()
+        self._last_full_scan_result = full_scan_result
+        # elif self._safety_send < DevicePlugin.FAILSAFEDUPDATE:
+        #     self._safety_send += 1
+        # else:
             # The purpose of this is to cause the ResourceManager to re-evaluate the device-graph for this
             # host which may lead to different results if the devices reported from other hosts has changed
             # This should not really be required but is a harmless work around while we get the manager code
             # in order
-            full_scan_result = self._last_full_scan_result
+            # full_scan_result = self._last_full_scan_result
 
         if full_scan_result is not None:
             self._safety_send = 0
