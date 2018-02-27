9f8f8189be555adac3f3e99d16e6ad3a9ae91644
Brian J. Murrell <brian.murrell@intel.com>
2018-02-27 17:33:14 -0500

    See if time helps
    Environment: NOSE_ARGS=-x TESTS=tests/integration/shared_storage_configuration/test_conf_params.py:TestConfParams.test_writeconf_conf_params
    Run-tests: integration-tests-shared-storage-configuration
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- chroma-agent/chroma_agent/action_plugins/manage_conf_params.py
+++ chroma-agent/chroma_agent/action_plugins/manage_conf_params.py
@@ -3,12 +3,27 @@
 # license that can be found in the LICENSE file.
 
 
+import time
 from chroma_agent.lib.shell import AgentShell
+from chroma_agent.log import daemon_log
 
-
-def set_conf_param(key = None, value = None):
+def set_conf_param(key=None, value=None):
     if value is not None:
-        AgentShell.try_run(['lctl', 'conf_param', "%s=%s" % (key, value)])
+        result = AgentShell.run(['lctl', 'conf_param', "%s=%s" % (key, value)])
+        i = 0
+        while result.rc != 0 and i < 5:
+            i += 1
+            daemon_log.info("try %s: lctl conf_param %s=%s failed (%s).\n"
+                            "stdout: %s\nstderr: %s" % (i, key, value,
+                                                        result.rc,
+                                                        result.stdout,
+                                                        result.stderr))
+
+            time.sleep(5)
+            result = AgentShell.run(['lctl', 'conf_param', "%s=%s" % (key, value)])
+        if result.rc != 0:
+            raise RuntimeError("failed to lctl conf_param"
+                               "%s=%s failed (%s)." % (key, value, result.rc))
     else:
         AgentShell.try_run(['lctl', 'conf_param', "-d", key])
 
