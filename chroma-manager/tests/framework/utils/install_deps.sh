e2668f996f349ffb8f87bf933209c3f85c15a481
Tom Nabarro <tom.nabarro@intel.com>
2018-02-28 22:06:36 +0000

    use copr fork for in other places
    Signed-off-by: Tom Nabarro <tom.nabarro@intel.com>

--- chroma-manager/tests/framework/utils/install_deps.sh
+++ chroma-manager/tests/framework/utils/install_deps.sh
@@ -4,5 +4,5 @@ mkdir -p chroma_test_env
 tar -C chroma_test_env -xizf -"
 
 ssh root@$CHROMA_MANAGER "set -ex
-yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/tanabarr/manager-for-lustre/repo/epel-7/managerforlustre-manager-for-lustre-epel-7.repo
+yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/tanabarr/manager-for-lustre/repo/epel-7/tanabarr-manager-for-lustre-epel-7.repo
 yum -y install nodejs npm nginx libuv iml-gui iml-srcmap-reverse iml-online-help"
