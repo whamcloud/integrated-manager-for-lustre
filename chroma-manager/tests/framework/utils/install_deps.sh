e254dbcf5de459be368fdda797d93b3e0c1d73dd
Joe Grund <joe.grund@intel.com>
2018-05-03 21:03:14 -0400

    temporarliy revert to joegrund fork
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- chroma-manager/tests/framework/utils/install_deps.sh
+++ chroma-manager/tests/framework/utils/install_deps.sh
@@ -4,5 +4,5 @@ mkdir -p chroma_test_env
 tar -C chroma_test_env -xizf -"
 
 ssh root@$CHROMA_MANAGER "set -ex
-yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/managerforlustre/manager-for-lustre-devel/repo/epel-7/managerforlustre-manager-for-lustre-devel-epel-7.repo
+yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/joegrund/manager-for-lustre/repo/epel-7/managerforlustre-manager-for-lustre-epel-7.repo
 yum -y install nodejs npm nginx libuv iml-gui iml-srcmap-reverse iml-online-help"
