17520c32e5b1efcda9d1b92eafec2d48a2424304
Joe Grund <joe.grund@intel.com>
2018-05-11 10:14:41 -0400

    Revert "Assume yes answer to {vg,lv}create (#486)"
    This reverts commit 36a6a37c9e9819ec6dbb5bd6f2d1e4851f72a399.

--- chroma-manager/tests/integration/utils/test_blockdevices/test_blockdevice_lvm.py
+++ chroma-manager/tests/integration/utils/test_blockdevices/test_blockdevice_lvm.py
@@ -20,10 +20,8 @@ class TestBlockDeviceLvm(TestBlockDevice):
     # Create a lvm on the device.
     @property
     def prepare_device_commands(self):
-        # FIXME: the use of --yes in the {vg,lv}create commands is a work-around for #500
-        # and should be reverted when #500 is fixed
         return [
-            "vgcreate --yes %s %s; lvcreate --yes --wipesignatures n -l 100%%FREE --name %s %s"
+            "vgcreate %s %s; lvcreate --wipesignatures n -l 100%%FREE --name %s %s"
             % (self.vg_name, self._device_path, self.lv_name, self.vg_name)
         ]
 
