545490d4f9837e1b1f0bf4a8c175dffed5d519a7
Will Johnson <wjohnson@whamcloud.com>
2018-07-18 12:03:37 -0400

    Update copyright
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- scripts/loadgen.sh
+++ scripts/loadgen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2017 Intel Corporation. All rights reserved.
+# Copyright (c) 2017 DDN. All rights reserved.
 # Use of this source code is governed by a MIT-style
 # license that can be found in the LICENSE file.
 #
