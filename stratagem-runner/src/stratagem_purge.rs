ed45baee9229d1867bd14c98635444b52d9b2050
Nathaniel Clark <nclark@whamcloud.com>
2019-04-10 10:37:11 -0400

    Unify input handling for stratagem runners
    Use shared input_to_iter() function.
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- stratagem-runner/src/stratagem_purge.rs
+++ stratagem-runner/src/stratagem_purge.rs
@@ -4,12 +4,9 @@
 
 // Usage: stratagem_purge FS [NID1] [NID2] ...
 
-use std::fs::File;
-use std::io;
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::process::exit;
 use structopt::StructOpt;
+mod cli;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "stratagem_purge")]
@@ -31,33 +28,9 @@ fn main() {
     env_logger::init();
     let opt = Opt::from_args();
     let device = opt.fsname;
-    let input: Box<Iterator<Item = String>> = match opt.input {
-        None => {
-            if opt.fidlist.len() == 0 {
-                Box::new(BufReader::new(io::stdin()).lines().map(|x| x.unwrap()))
-            } else {
-                Box::new(opt.fidlist.iter().map(|x| x.to_owned()))
-            }
-        }
-        Some(name) => {
-            let buf: Box<BufRead> = match name.as_ref() {
-                "-" => Box::new(BufReader::new(io::stdin())),
-                _ => {
-                    let f = match File::open(&name) {
-                        Ok(x) => x,
-                        Err(e) => {
-                            log::error!("Failed to open {}: {}", &name, e);
-                            exit(-1);
-                        }
-                    };
-                    Box::new(BufReader::new(f))
-                }
-            };
-            Box::new(buf.lines().map(|x| x.unwrap()))
-        }
-    };
+    let input = cli::input_to_iter(opt.input, opt.fidlist);
 
-    if let Err(_) = liblustreapi::rmfid(&device, input) {
+    if liblustreapi::rmfid(&device, input).is_err() {
         exit(-1);
     }
 }
--- stratagem-runner/src/stratagem_warning.rs
+++ stratagem-runner/src/stratagem_warning.rs
@@ -8,11 +8,10 @@ use libc;
 use std::ffi::CStr;
 use std::fs::File;
 use std::io;
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::process::exit;
 use stratagem_runner::error::StratagemError;
 use structopt::StructOpt;
+mod cli;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "stratagem_warning")]
@@ -105,36 +104,12 @@ fn main() {
     let opt = Opt::from_args();
     let device = opt.fsname;
     let output: Box<io::Write> = match opt.output {
-        Some(file) => Box::new(File::create(file).unwrap()),
+        Some(file) => Box::new(File::create(file).expect("Failed to create file")),
         None => Box::new(io::stdout()),
     };
+    let input = cli::input_to_iter(opt.input, opt.fidlist);
 
-    let input: Box<Iterator<Item = String>> = match opt.input {
-        None => {
-            if opt.fidlist.len() == 0 {
-                Box::new(BufReader::new(io::stdin()).lines().map(|x| x.unwrap()))
-            } else {
-                Box::new(opt.fidlist.iter().map(|x| x.to_owned()))
-            }
-        }
-        Some(name) => {
-            let buf: Box<BufRead> = match name.as_ref() {
-                "-" => Box::new(BufReader::new(io::stdin())),
-                _ => {
-                    let f = match File::open(&name) {
-                        Ok(x) => x,
-                        Err(e) => {
-                            log::error!("Failed to open {}: {}", &name, e);
-                            exit(-1);
-                        }
-                    };
-                    Box::new(BufReader::new(f))
-                }
-            };
-            Box::new(buf.lines().map(|x| x.unwrap()))
-        }
-    };
-    if let Err(_) = write_records(&device, input, output) {
+    if write_records(&device, input, output).is_err() {
         exit(-1);
     }
 }
