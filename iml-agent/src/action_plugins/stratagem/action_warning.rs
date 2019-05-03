9ff0da885fd15eae6adbe32378bec891cc1ef3b2
Nathaniel Clark <nclark@whamcloud.com>
2019-05-03 10:26:13 -0400

    Add function to read stratagem warning mailbox
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- iml-agent/src/action_plugins/stratagem/action_warning.rs
+++ iml-agent/src/action_plugins/stratagem/action_warning.rs
@@ -2,15 +2,18 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-use crate::agent_error::ImlAgentError;
+use crate::{agent_error::ImlAgentError, env, http_comms::crypto_client};
 use csv;
+use futures::future::Future;
+use futures::stream::Stream;
 use libc;
 use std::ffi::CStr;
 use std::io;
+use std::sync::mpsc::channel;
 
 #[derive(Debug, serde::Serialize)]
 #[serde(rename_all = "SCREAMING-KEBAB-CASE")]
-struct Record<'a> {
+struct Record {
     path: String,
     user: String,
     uid: u32,
@@ -18,10 +21,10 @@ struct Record<'a> {
     atime: i64,
     mtime: i64,
     ctime: i64,
-    fid: &'a str,
+    fid: String,
 }
 
-fn fid2record<'a>(mntpt: &String, fid: &'a String) -> Result<Record<'a>, ImlAgentError> {
+fn fid2record(mntpt: &String, fid: &String) -> Result<Record, ImlAgentError> {
     let path = match liblustreapi::fid2path(&mntpt, &fid) {
         Ok(p) => p,
         Err(e) => {
@@ -53,7 +56,7 @@ fn fid2record<'a>(mntpt: &String, fid: &'a String) -> Result<Record<'a>, ImlAgen
         atime: stat.st_atime,
         mtime: stat.st_mtime,
         ctime: stat.st_ctime,
-        fid: &fid,
+        fid: fid.to_string(),
     })
 }
 
@@ -72,11 +75,56 @@ pub fn write_records(
     for fid in args {
         let rec = match fid2record(&mntpt, &fid) {
             Ok(r) => r,
-            Err(_e) => continue,
+            Err(_) => continue,
         };
         wtr.serialize(rec)?;
     }
 
+    wtr.flush()?;
+
+    Ok(())
+}
+
+pub fn read_mailbox(device: &str, mailbox: &str, out: impl io::Write) -> Result<(), ImlAgentError> {
+    let mntpt = liblustreapi::search_rootpath(&device).map_err(|e| {
+        log::error!("Failed to find rootpath({}) -> {:?}", device, e);
+        e
+    })?;
+
+    let query: Vec<(String, String)> = Vec::new();
+    let mut wtr = csv::Writer::from_writer(out);
+    let message_endpoint = env::MANAGER_URL.join("/mailbox/")?.join(mailbox)?;
+    let identity = crypto_client::get_id(&env::PFX)?;
+    let client = crypto_client::create_client(identity)?;
+
+    let (sender, recv) = channel();
+
+    // Spawn off an expensive computation
+    tokio::spawn(
+        stream_lines::strings(crypto_client::get_stream(&client, message_endpoint, &query))
+            // @@ add multithreading here
+            .for_each(move |fid| {
+                if let Ok(rec) = fid2record(&mntpt, &fid) {
+                    sender.send(rec).map_err(|_| ImlAgentError::SendError)?;
+                }
+                Ok(())
+            })
+            .map_err(|e| {
+                log::error!("Failed {:?}", e);
+                ()
+            }),
+    );
+
+    loop {
+        match recv.recv() {
+            Ok(rec) => {
+                if let Err(err) = wtr.serialize(&rec) {
+                    log::error!("Failed to write record for fid {}: {}", rec.fid, err);
+                }
+            }
+            Err(_) => break,
+        }
+    }
     wtr.flush()?;
 
     Ok(())
