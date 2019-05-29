a301d1ed261ab69646b0f1106531ef0ae43b9d44
Joe Grund <jgrund@whamcloud.io>
2019-05-29 16:39:01 -0400

    Update scan output
    Update the scan output so original rule names (arguments) are
    preserved. The output of the scan will now look like:
    
    ```json
    {
      "Ok": [
        {
          "group_counters": [
            {
              "counters": [
                {
                  "count": 1,
                  "have_flist": false,
                  "name": "Other"
                },
                {
                  "count": 0,
                  "have_flist": false,
                  "name": "smaller_than_1M"
                },
                {
                  "count": 0,
                  "have_flist": false,
                  "name": "not_smaller_than_1M_and_smaller_than_1G"
                },
                {
                  "count": 1,
                  "have_flist": false,
                  "name": "not_smaller_than_1G"
                },
                {
                  "count": 0,
                  "have_flist": false,
                  "name": "not_smaller_than_1T"
                }
              ],
              "name": "size_distribution"
            },
            {
              "counters": [
                {
                  "count": 2,
                  "have_flist": false,
                  "name": "Other"
                },
                {
                  "count": 0,
                  "have_flist": true,
                  "name": "shell_cmd_of_rule_0"
                },
                {
                  "count": 0,
                  "have_flist": true,
                  "name": "shell_cmd_of_rule_1"
                }
              ],
              "name": "warn_purge_times"
            }
          ]
        },
        "/tmp/83c3fc49-06ab-4197-82ea-a7b36b17bd89/",
        [
          [
            "/tmp/83c3fc49-06ab-4197-82ea-a7b36b17bd89/warn_purge_times/shell_cmd_of_rule_0",
            "fids_expiring_soon"
          ],
          [
            "/tmp/83c3fc49-06ab-4197-82ea-a7b36b17bd89/warn_purge_times/shell_cmd_of_rule_1",
            "fids_expired"
          ]
        ]
      ]
    }
    ```
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- iml-agent/src/cli.rs
+++ iml-agent/src/cli.rs
@@ -189,7 +189,7 @@ fn main() {
                 println!("{}", termion::clear::BeforeCursor);
 
                 match result {
-                    Ok((output, results_dir)) => {
+                    Ok((output, results_dir, _)) => {
                         println!(
                             "{}✔ Scan finished{}. Results located in {}",
                             green, reset, results_dir
