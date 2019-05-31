141ae4e60d3ba6677e28f3c8b670f516723e4434
Joe Grund <jgrund@whamcloud.io>
2019-05-31 09:52:47 -0400

    Add initial rust manager cli (#967)
    * Add initial rust manager cli
    
    This is the bare bones needed for a rust cli against the manager API.
    
    It uses API key auth so no password config file is needed.
    
    As a POC I've implemented a tabular list for the host endpoint.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>
    
    * formatting fixes
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>
    
    * Add command to kick stratagem scan
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

---
+++ iml-manager-cli/src/manager_cli_error.rs
@@ -0,0 +1,50 @@
+// Copyright (c) 2019 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+pub type Result<T> = std::result::Result<T, ImlManagerCliError>;
+
+#[derive(Debug)]
+pub enum ImlManagerCliError {
+    Reqwest(reqwest::Error),
+    InvalidHeaderValue(reqwest::header::InvalidHeaderValue),
+    UrlParseError(url::ParseError),
+}
+
+impl std::fmt::Display for ImlManagerCliError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            ImlManagerCliError::Reqwest(ref err) => write!(f, "{}", err),
+            ImlManagerCliError::InvalidHeaderValue(ref err) => write!(f, "{}", err),
+            ImlManagerCliError::UrlParseError(ref err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl std::error::Error for ImlManagerCliError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+            ImlManagerCliError::Reqwest(ref err) => Some(err),
+            ImlManagerCliError::InvalidHeaderValue(ref err) => Some(err),
+            ImlManagerCliError::UrlParseError(ref err) => Some(err),
+        }
+    }
+}
+
+impl From<reqwest::Error> for ImlManagerCliError {
+    fn from(err: reqwest::Error) -> Self {
+        ImlManagerCliError::Reqwest(err)
+    }
+}
+
+impl From<reqwest::header::InvalidHeaderValue> for ImlManagerCliError {
+    fn from(err: reqwest::header::InvalidHeaderValue) -> Self {
+        ImlManagerCliError::InvalidHeaderValue(err)
+    }
+}
+
+impl From<url::ParseError> for ImlManagerCliError {
+    fn from(err: url::ParseError) -> Self {
+        ImlManagerCliError::UrlParseError(err)
+    }
+}
