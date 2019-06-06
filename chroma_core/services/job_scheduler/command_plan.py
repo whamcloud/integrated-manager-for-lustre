f210948196900ad936764992b004f0ac15b9f8b9
Will Johnson <wjohnson@whamcloud.com>
2019-06-06 17:06:46 -0400

    - Don't use the database and instead pass job dependencies in the command plan
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- chroma_core/services/job_scheduler/command_plan.py
+++ chroma_core/services/job_scheduler/command_plan.py
@@ -111,7 +111,7 @@ class CommandPlan(object):
 
         return locks
 
-    def add_jobs(self, jobs, command, job_deps_map=None):
+    def add_jobs(self, jobs, command, job_deps_map):
         """Add a job, and any others which are required in order to reach its prerequisite state"""
         # Important: the Job must not be committed until all
         # its dependencies and locks are in.
@@ -196,7 +196,7 @@ class CommandPlan(object):
 
         return {"transition_job": transition_job, "dependency_jobs": depended_jobs}
 
-    def _create_dependencies(self, job, locks, job_deps_map=[]):
+    def _create_dependencies(self, job, locks, job_deps_map):
         """Examine overlaps between a job's locks and those of
            earlier jobs which are still pending, and generate wait_for
            dependencies when we have a write lock and they have a read lock
@@ -234,8 +234,11 @@ class CommandPlan(object):
                     # See comment by locked_state in StateReadLock
                     wait_fors.add(prior_write_lock.job.id)
 
-        job_deps_list = dict(filter(lambda (k,v): k == job.id, job_deps_map.items())).get(job.id) or []
-        wait_fors = list(wait_fors) + job_deps_list
+        job_deps_list = []
+        if job in job_deps_map:
+            job_deps_list = job_deps_map[job]
+
+        wait_fors = list(wait_fors) + map(lambda job: job.id, job_deps_list)
         
         job.wait_for_json = json.dumps(wait_fors)
 
@@ -335,7 +338,7 @@ class CommandPlan(object):
             job = d.to_job()
             locks = self._create_locks(job)
             job.locks_json = json.dumps([l.to_dict() for l in locks])
-            self._create_dependencies(job, locks)
+            self._create_dependencies(job, locks, {})
             job.save()
             jobs.append(job)
             for l in locks:
@@ -492,14 +495,14 @@ class CommandPlan(object):
             job_klass = ContentType.objects.get_by_natural_key("chroma_core", job["class_name"].lower()).model_class()
             depends_on_job_range = None
             if "depends_on_job_range" in job["args"]:
-                depends_on_job_range = job["args"].depends_on_job_range
+                depends_on_job_range = job["args"].get('depends_on_job_range')
                 del job["args"]["depends_on_job_range"]
             
             job_instance = job_klass(**job["args"])
-            job_deps_map[job_instance.id] = depends_on_job_range
+            job_deps_map[job_instance] = depends_on_job_range
             jobs.append(job_instance)
 
-        job_deps_map = {k:v for k,v in job_deps_map.items() if v != None}
+        job_deps_map = {k:map(lambda idx: jobs[idx], v) for k,v in job_deps_map.items() if v != None}
 
         with transaction.atomic():
             command = Command.objects.create(message=message)
