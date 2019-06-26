39e3da92884edf64094294cb5c9e22092169a87a
Joe Grund <jgrund@whamcloud.io>
2019-06-26 11:48:54 -0400

    Create popover component
    Create a popover component in seed.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

---
+++ iml-wasm-components/iml-popover/src/lib.rs
@@ -0,0 +1,50 @@
+// Copyright (c) 2019 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+use seed::{class, div, h3, prelude::*};
+
+#[derive(Debug, Clone)]
+pub enum Placement {
+    Left,
+    Right,
+    Top,
+    Bottom,
+}
+
+impl From<&Placement> for &str {
+    fn from(p: &Placement) -> Self {
+        match p {
+            Placement::Left => "left",
+            Placement::Right => "right",
+            Placement::Top => "top",
+            Placement::Bottom => "bottom",
+        }
+    }
+}
+
+impl Default for Placement {
+    fn default() -> Self {
+        Placement::Left
+    }
+}
+
+pub fn popover<T>(open: bool, placement: Placement, children: Vec<El<T>>) -> El<T> {
+    if !open {
+        return seed::empty();
+    }
+
+    div![
+        class!["fade", "popover", "in", (&placement).into()],
+        div![class!["arrow"]],
+        children
+    ]
+}
+
+pub fn popover_title<T>(el: El<T>) -> El<T> {
+    h3![class!["popover-title"], el]
+}
+
+pub fn popover_content<T>(el: El<T>) -> El<T> {
+    div![class!["popover-content"], el]
+}
