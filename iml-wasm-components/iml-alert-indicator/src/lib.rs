997a1e4719c0ede5e619d0c5887a4e196e22e6e0
Joe Grund <jgrund@whamcloud.io>
2019-06-26 15:25:43 -0400

    fixups
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- iml-wasm-components/iml-alert-indicator/src/lib.rs
+++ iml-wasm-components/iml-alert-indicator/src/lib.rs
@@ -18,26 +18,74 @@ fn get_message(alerts: &[&Alert]) -> String {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+pub enum WatchState {
+    Watching,
+    Open,
+    Close,
+}
+
 #[derive(Debug, Default)]
 pub struct PopoverStates {
-    inner: HashMap<u32, bool>,
+    inner: HashMap<u32, WatchState>,
+}
+
+fn is_open(x: WatchState) -> bool {
+    match x {
+        WatchState::Open => true,
+        _ => false,
+    }
+}
+
+fn is_watching(x: WatchState) -> bool {
+    match x {
+        WatchState::Watching => true,
+        _ => false,
+    }
+}
+
+pub fn handle_window_click(p: &mut PopoverStates) {
+    for k in p.get_open() {
+        p.update(k, WatchState::Close);
+    }
+
+    for k in p.get_watching() {
+        p.update(k, WatchState::Open);
+    }
 }
 
 impl PopoverStates {
     pub fn is_open(&self, id: u32) -> bool {
-        *self.inner.get(&id).unwrap_or(&false)
+        self.inner.get(&id).filter(|&&x| is_open(x)).is_some()
     }
-    pub fn update(&mut self, id: u32, state: bool) {
-        if state {
-            self.inner.insert(id, true);
-        } else {
-            self.inner.remove(&id);
+    pub fn get_watching(&self) -> Vec<u32> {
+        self.inner
+            .iter()
+            .filter(|(_, &v)| is_watching(v))
+            .map(|(k, _)| *k)
+            .collect()
+    }
+    pub fn get_open(&self) -> Vec<u32> {
+        self.inner
+            .iter()
+            .filter(|(_, &v)| is_open(v))
+            .map(|(k, _)| *k)
+            .collect()
+    }
+    pub fn update(&mut self, id: u32, state: WatchState) {
+        match state {
+            WatchState::Watching | WatchState::Open => {
+                self.inner.insert(id, state);
+            }
+            WatchState::Close => {
+                self.inner.remove(&id);
+            }
         }
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct AlertIndicatorPopoverState(pub (u32, bool));
+pub struct AlertIndicatorPopoverState(pub (u32, WatchState));
 
 pub fn alert_indicator(
     alerts: &[Alert],
@@ -45,9 +93,7 @@ pub fn alert_indicator(
     resource_uri: &str,
     open: bool,
 ) -> El<AlertIndicatorPopoverState> {
-    log::debug!("Alerts {:?}", alerts);
-    log::debug!("id {}", id);
-    log::debug!("resource_uri {}", resource_uri);
+    log::debug!("Alerts {:#?}", alerts);
 
     let alerts: Vec<&Alert> = alerts
         .iter()
@@ -64,13 +110,16 @@ pub fn alert_indicator(
             if alerts.is_empty() {
                 vec![i![class!["fa", "fa-check-circle"]]]
             } else {
+                let mut i = i![class!["fa", "activate-popover", "fa-exclamation-circle"]];
+
+                if !open {
+                    i.listeners.push(mouse_ev(Ev::Click, move |_| {
+                        AlertIndicatorPopoverState((id, WatchState::Watching))
+                    }));
+                }
+
                 vec![
-                    i![
-                        class!["fa", "activate-popover", "fa-exclamation-circle"],
-                        mouse_ev(Ev::Click, move |_| {
-                            AlertIndicatorPopoverState((id, !open))
-                        })
-                    ],
+                    i,
                     popover(
                         open,
                         iml_popover::Placement::Bottom,
