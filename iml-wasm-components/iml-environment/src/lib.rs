11bec70160ee24f4b5ba2c1bbd0a725347f1ea48
Will Johnson <wjohnson@whamcloud.com>
2019-07-12 15:39:05 -0400

    - Small cleanups
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- iml-wasm-components/iml-environment/src/lib.rs
+++ iml-wasm-components/iml-environment/src/lib.rs
@@ -4,13 +4,12 @@
 
 use seed::document;
 
-/// Will return https://<domain>:<port>/ui/
+/// Returns https://<domain>:<port>/ui/
 pub fn ui_root() -> String {
   document().base_uri().unwrap().unwrap_or_default()
 }
 
-/// Will return https://<domain>:<port>/grafana/
+/// Returns https://<domain>:<port>/grafana/
 pub fn grafana_root() -> String {
-  let url = document().base_uri().unwrap().unwrap_or_default();
-  url.replace("/ui/", "/grafana/")
+  ui_root().replace("/ui/", "/grafana/")
 }
