748557f90bd36f1e8a1c10d5224534fc6294b61b
Will Johnson <wjohnson@whamcloud.com>
2019-07-12 14:04:04 -0400

    GH-1050 - Create grafana chart iframe and iml-stratagem module
    Fixes #1050.
    
    Grafana charts will be inserted via an iframe. Create the `iml-grafana`
    module that will return an iframe that loads the specified chart. Also
    create the `iml-stratagem` module with a view function to load the size
    distribution chart.
    
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

---
+++ iml-wasm-components/iml-stratagem/src/lib.rs
@@ -0,0 +1,9 @@
+use iml_grafana_chart::grafana_chart;
+use seed::prelude::*;
+
+static DASHBOARD_ID: &str = "OBdCS5IWz";
+static DASHBOARD_NAME: &str = "stratagem";
+
+pub fn size_distribution_chart_view<T>() -> El<T> {
+  grafana_chart(DASHBOARD_ID, DASHBOARD_NAME, "10s", 2, "100%", "600")
+}
