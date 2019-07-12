e7a5f9251455b560e8c3b743d0b002f29ae151c3
Will Johnson <wjohnson@whamcloud.com>
2019-07-12 15:42:52 -0400

    - rename function
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- iml-wasm-components/iml-stratagem/src/lib.rs
+++ iml-wasm-components/iml-stratagem/src/lib.rs
@@ -4,6 +4,6 @@ use seed::prelude::*;
 static DASHBOARD_ID: &str = "OBdCS5IWz";
 static DASHBOARD_NAME: &str = "stratagem";
 
-pub fn size_distribution_chart_view<T>() -> El<T> {
+pub fn size_distribution_chart<T>() -> El<T> {
     grafana_chart(DASHBOARD_ID, DASHBOARD_NAME, "10s", 2, "100%", "600")
 }
