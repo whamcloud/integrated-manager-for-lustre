72820c37262d374819cf414f52632814e5b7ebe2
Joe Grund <jgrund@whamcloud.io>
2019-07-18 12:12:35 -0400

    Implement stratagem configuration component
    Fixes #1059.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- iml-wasm-components/iml-stratagem/src/lib.rs
+++ iml-wasm-components/iml-stratagem/src/lib.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 mod inode_error;
 mod inode_table;
 
@@ -9,4 +13,209 @@ static DASHBOARD_NAME: &str = "stratagem";
 
 pub fn size_distribution_chart<T>() -> El<T> {
     grafana_chart(DASHBOARD_ID, DASHBOARD_NAME, "10s", 2, "100%", "600")
+}
+
+use cfg_if::cfg_if;
+use iml_duration_picker::{self, duration_picker};
+use iml_toggle::toggle;
+use seed::{
+    class, div, dom_types::MessageMapper as _, h4, prelude::*, style, table, tbody, td, th, thead,
+    tr,
+};
+use web_sys::Element;
+
+cfg_if! {
+    if #[cfg(feature = "console_log")] {
+        fn init_log() {
+            use log::Level;
+            match console_log::init_with_level(Level::Trace) {
+                Ok(_) => (),
+                Err(e) => log::info!("{:?}", e)
+            };
+        }
+    } else {
+        fn init_log() {}
+    }
+}
+
+#[derive(Debug)]
+struct Model {
+    pub destroyed: bool,
+    pub run_config: iml_duration_picker::Model,
+    pub report_active: bool,
+    pub report_config: iml_duration_picker::Model,
+    pub purge_config: iml_duration_picker::Model,
+}
+
+impl Default for Model {
+    fn default() -> Self {
+        Model {
+            run_config: iml_duration_picker::Model {
+                value: "30".into(),
+                exclude_units: vec![
+                    iml_duration_picker::Unit::Minutes,
+                    iml_duration_picker::Unit::Seconds,
+                ],
+                ..Default::default()
+            },
+            report_active: true,
+            report_config: iml_duration_picker::Model {
+                value: "30".into(),
+                exclude_units: vec![
+                    iml_duration_picker::Unit::Minutes,
+                    iml_duration_picker::Unit::Seconds,
+                ],
+                ..Default::default()
+            },
+            purge_config: iml_duration_picker::Model {
+                value: "30".into(),
+                exclude_units: vec![
+                    iml_duration_picker::Unit::Minutes,
+                    iml_duration_picker::Unit::Seconds,
+                ],
+                ..Default::default()
+            },
+            destroyed: false,
+        }
+    }
+}
+
+// Update
+
+#[derive(Clone, Debug)]
+enum Msg {
+    Destroy,
+    TogglePurge(iml_toggle::Active),
+    ToggleReport(iml_toggle::Active),
+    RunConfig(iml_duration_picker::Msg),
+    ReportConfig(iml_duration_picker::Msg),
+    PurgeConfig(iml_duration_picker::Msg),
+    WindowClick,
+}
+
+fn update(msg: Msg, model: &mut Model, _orders: &mut Orders<Msg>) {
+    log::trace!("Msg: {:#?}", msg);
+
+    match msg {
+        Msg::Destroy => model.destroyed = true,
+        Msg::RunConfig(msg) => iml_duration_picker::update(msg, &mut model.run_config),
+        Msg::ReportConfig(msg) => iml_duration_picker::update(msg, &mut model.report_config),
+        Msg::PurgeConfig(msg) => iml_duration_picker::update(msg, &mut model.purge_config),
+        Msg::ToggleReport(iml_toggle::Active(active)) => {
+            model.report_config.disabled = !active;
+        }
+        Msg::TogglePurge(iml_toggle::Active(active)) => {
+            model.purge_config.disabled = !active;
+        }
+        Msg::WindowClick => {
+            if model.run_config.watching.should_update() {
+                model.run_config.watching.update();
+            }
+
+            if model.report_config.watching.should_update() {
+                model.report_config.watching.update();
+            }
+
+            if model.purge_config.watching.should_update() {
+                model.purge_config.watching.update();
+            }
+        }
+    }
+
+    log::trace!("Model: {:#?}", model);
+}
+
+// View
+fn view(model: &Model) -> El<Msg> {
+    let style_override =
+        style! { "display" => "flex", "align-items" => "center", "line-height" => "unset" };
+
+    div![
+        class!["container", "container-full"],
+        h4![class!["section-header"], "Top inode Users"],
+        table![
+            class!["table"],
+            thead![tr![th!["Name"], th!["Count"]]],
+            tbody![tr![td!["uid_1"], td!["5000"]]],
+            tbody![tr![td!["uid_4"], td!["3000"]]],
+            tbody![tr![td!["uid_3"], td!["2000"]]],
+            tbody![tr![td!["uid_2"], td!["1000"]]],
+            tbody![tr![td!["uid_0"], td!["500"]]],
+        ],
+        div![
+            class!["detail-panel"],
+            h4![class!["section-header"], "Stratagem Configuration"],
+            div![
+                class!["detail-row"],
+                div![style_override.clone(), "Scan filesystem every"],
+                div![
+                    style_override.clone(),
+                    div![
+                        class!["input-group"],
+                        duration_picker(&model.run_config).map_message(Msg::RunConfig)
+                    ]
+                ]
+            ],
+            div![
+                class!["detail-row"],
+                div![
+                    style_override.clone(),
+                    "Generate report on files older than"
+                ],
+                div![
+                    style_override.clone(),
+                    div![
+                        class!["input-group"],
+                        duration_picker(&model.report_config).map_message(Msg::ReportConfig)
+                    ],
+                    toggle(!model.report_config.disabled).map_message(Msg::ToggleReport),
+                ],
+            ],
+            div![
+                class!["detail-row"],
+                div![style_override.clone(), "Purge Files older than"],
+                div![
+                    style_override,
+                    div![
+                        class!["input-group"],
+                        duration_picker(&model.purge_config).map_message(Msg::PurgeConfig)
+                    ],
+                    toggle(!model.purge_config.disabled).map_message(Msg::TogglePurge)
+                ]
+            ]
+        ]
+    ]
+}
+
+fn window_events(model: &Model) -> Vec<seed::events::Listener<Msg>> {
+    if model.destroyed {
+        return vec![];
+    }
+
+    vec![simple_ev(Ev::Click, Msg::WindowClick)]
+}
+
+#[wasm_bindgen]
+pub struct StratagemCallbacks {
+    app: seed::App<Msg, Model, El<Msg>>,
+}
+
+#[wasm_bindgen]
+impl StratagemCallbacks {
+    pub fn destroy(&self) {
+        self.app.update(Msg::Destroy);
+    }
+}
+
+#[wasm_bindgen]
+pub fn render(el: Element) -> StratagemCallbacks {
+    init_log();
+
+    let app = seed::App::build(Model::default(), update, view)
+        .mount(el)
+        .window_events(window_events)
+        .finish()
+        .run();
+
+    StratagemCallbacks { app: app.clone() }
 }
