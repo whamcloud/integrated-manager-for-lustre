9887f3cad334cbac0005814911c55e037ba98971
Joe Grund <jgrund@whamcloud.io>
2019-07-18 12:12:35 -0400

    Implement stratagem configuration component
    Fixes #1059.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- iml-wasm-components/iml-wasm/src/lib.rs
+++ iml-wasm-components/iml-wasm/src/lib.rs
@@ -9,6 +9,8 @@ pub mod hsm_dropdown;
 pub mod multi_action_dropdown;
 
 pub use iml_fs::{fs_detail_page, fs_page};
+pub use iml_stratagem;
+
 cfg_if! {
     if #[cfg(feature = "console_log")] {
         fn init_log() {
