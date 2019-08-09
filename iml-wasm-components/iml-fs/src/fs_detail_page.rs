061b15f6422dc56dd093bd26427005c41eb6636e
Will Johnson <wjohnson@whamcloud.com>
2019-08-08 20:40:39 -0400

    - run fust format
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- iml-wasm-components/iml-fs/src/fs_detail_page.rs
+++ iml-wasm-components/iml-fs/src/fs_detail_page.rs
@@ -431,13 +431,22 @@ fn target_table(
                     style! {"word-break" => "break-all"},
                     ui_link(&format!("target/{}", x.id), &x.name)
                 ],
-                td![class!["col-sm-3", "col-xs-2"], style! {"word-break" => "break-all"}, x.volume_name],
-                td![class!["hidden-xs"], server_link(&x.primary_server, &x.primary_server_name)],
-                td![class!["hidden-xs"],
-                server_link(
-                    &x.failover_servers.first().unwrap_or(&"".into()),
-                    &x.failover_server_name
-                )],
+                td![
+                    class!["col-sm-3", "col-xs-2"],
+                    style! {"word-break" => "break-all"},
+                    x.volume_name
+                ],
+                td![
+                    class!["hidden-xs"],
+                    server_link(&x.primary_server, &x.primary_server_name)
+                ],
+                td![
+                    class!["hidden-xs"],
+                    server_link(
+                        &x.failover_servers.first().unwrap_or(&"".into()),
+                        &x.failover_server_name
+                    )
+                ],
                 td![server_link(
                     x.active_host.as_ref().unwrap_or(&"".into()),
                     &x.active_host_name
