aa73e5b8adf274003418ff49531d98ffc7250d5d
Joe Grund <jgrund@whamcloud.io>
2019-08-18 17:03:17 -0400

    Fix warp-drive to start server after API request finishes.
    Fixes #1153.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- iml-warp-drive/src/main.rs
+++ iml-warp-drive/src/main.rs
@@ -69,6 +69,30 @@ fn main() {
 
     let api_client = get_client().unwrap();
 
+    // GET -> messages stream
+    let routes = warp::get2()
+        .and(warp::sse())
+        .and(warp::any().map(move || Arc::clone(&user_state2)))
+        .and(warp::any().map(move || Arc::clone(&lock_state2)))
+        .and(warp::any().map(move || Arc::clone(&api_cache_state2)))
+        .map(
+            |sse: warp::sse::Sse,
+             users: users::SharedUsers,
+             locks: SharedLocks,
+             api_cache: SharedCache| {
+                // reply using server-sent events
+                let stream =
+                    users::user_connected(users, locks.lock().clone(), api_cache.lock().clone());
+                sse.reply(warp::sse::keep_alive().stream(stream))
+            },
+        )
+        .with(warp::log("iml-warp-drive::api"));
+
+    log::info!("IML warp drive starting");
+
+    let (_, fut) =
+        warp::serve(routes).bind_with_graceful_shutdown(iml_manager_env::get_warp_drive_addr(), rx);
+
     tokio::run(lazy(move || {
         warp::spawn(
             populate_from_api(Arc::clone(&api_cache_state))
@@ -87,57 +111,79 @@ fn main() {
 
                     log::debug!("Cache state {:?}", api_cache_state);
 
+                    warp::spawn(fut);
+
                     warp::spawn(
                         stream
-                        .from_err()
-                            .for_each(move |msg| -> Box<Future<Item = (), Error = failure::Error> + Send> {
-                                let _c3 = &c2;
+                            .from_err()
+                            .for_each(
+                                move |msg| -> Box<
+                                    dyn Future<Item = (), Error = failure::Error> + Send,
+                                > {
+                                    let _c3 = &c2;
 
-                                let api_client = api_client.clone();
-                                let api_cache_state = Arc::clone(&api_cache_state);
-                                let user_state4 = Arc::clone(&user_state4);
+                                    let api_client = api_client.clone();
+                                    let api_cache_state = Arc::clone(&api_cache_state);
+                                    let user_state4 = Arc::clone(&user_state4);
 
-                                match msg {
-                                    iml_postgres::AsyncMessage::Notification(n) => {
-                                        if n.channel() == "table_update" {
-                                            let fut = listen::into_db_record(n.payload())
-                                                .into_future()
-                                                .from_err()
-                                                .and_then(|r| {
-                                                    cache::db_record_to_change_record(r, api_client)
+                                    match msg {
+                                        iml_postgres::AsyncMessage::Notification(n) => {
+                                            if n.channel() == "table_update" {
+                                                let fut = listen::into_db_record(n.payload())
+                                                    .into_future()
+                                                    .from_err()
+                                                    .and_then(|r| {
+                                                        cache::db_record_to_change_record(
+                                                            r, api_client,
+                                                        )
                                                         .from_err()
-                                                })
-                                                .map(move |record_change| {
-                                                    match record_change.clone() {
-                                                        cache::RecordChange::Delete(r) => {
-                                                            let removed = api_cache_state.lock().remove_record(&r);
+                                                    })
+                                                    .map(move |record_change| {
+                                                        match record_change.clone() {
+                                                            cache::RecordChange::Delete(r) => {
+                                                                let removed = api_cache_state
+                                                                    .lock()
+                                                                    .remove_record(&r);
 
-                                                            if removed {
-                                                                users::send_message(Message::RecordChange(record_change), Arc::clone(&user_state4));
+                                                                if removed {
+                                                                    users::send_message(
+                                                                        Message::RecordChange(
+                                                                            record_change,
+                                                                        ),
+                                                                        Arc::clone(&user_state4),
+                                                                    );
+                                                                }
                                                             }
-                                                        }
-                                                        cache::RecordChange::Update(r) => {
-                                                            api_cache_state.lock().insert_record(r);
+                                                            cache::RecordChange::Update(r) => {
+                                                                api_cache_state
+                                                                    .lock()
+                                                                    .insert_record(r);
 
-                                                            users::send_message(Message::RecordChange(record_change), Arc::clone(&user_state4));
-                                                        }
-                                                    };
-                                                });
+                                                                users::send_message(
+                                                                    Message::RecordChange(
+                                                                        record_change,
+                                                                    ),
+                                                                    Arc::clone(&user_state4),
+                                                                );
+                                                            }
+                                                        };
+                                                    });
 
                                                 Box::new(fut)
-                                        } else {
-                                            log::warn!("unknown channel: {}", n.channel());
+                                            } else {
+                                                log::warn!("unknown channel: {}", n.channel());
 
-                                            Box::new(future::ok(()))
+                                                Box::new(future::ok(()))
+                                            }
                                         }
+                                        iml_postgres::AsyncMessage::Notice(err) => {
+                                            log::error!("Error from postgres {}", err);
+                                            Box::new(future::err(err).from_err())
+                                        }
+                                        _ => unreachable!(),
                                     }
-                                    iml_postgres::AsyncMessage::Notice(err) => {
-                                        log::error!("Error from postgres {}", err);
-                                        Box::new(future::err(err).from_err())
-                                    }
-                                    _ => unreachable!()
-                                }
-                            })
+                                },
+                            )
                             .map_err(move |e| {
                                 exit.trigger();
                                 tx2.trigger();
@@ -150,13 +196,12 @@ fn main() {
                         let mut c = c.lock();
 
                         c.simple_query("LISTEN table_update")
-                        .for_each(|_| Ok(()))
-                        .from_err()
+                            .for_each(|_| Ok(()))
+                            .from_err()
                     };
 
                     fut.and_then(move |_| {
-                        populate_from_db(Arc::clone(&api_cache_state3), client)
-                        .from_err()
+                        populate_from_db(Arc::clone(&api_cache_state3), client).from_err()
                     })
                 })
                 .inspect(|_| log::info!("Started listening to NOTIFY events"))
@@ -167,93 +212,53 @@ fn main() {
                 }),
         );
 
-        warp::spawn(
-            iml_rabbit::connect_to_rabbit()
-                .and_then(create_locks_consumer)
-                .and_then(move |stream| {
-                    log::info!("Started consuming locks");
+        iml_rabbit::connect_to_rabbit()
+            .and_then(create_locks_consumer)
+            .and_then(move |stream| {
+                log::info!("Started consuming locks");
 
-                    let stream = valve2.wrap(stream);
+                let stream = valve2.wrap(stream);
 
-                    stream
-                        .for_each(move |message| {
-                            log::debug!("got message {:?}", std::str::from_utf8(&message.data));
+                stream
+                    .for_each(move |message| {
+                        log::debug!("got message {:?}", std::str::from_utf8(&message.data));
 
-                            let lock_change: locks::Changes =
-                                serde_json::from_slice(&message.data).unwrap();
+                        let lock_change: locks::Changes =
+                            serde_json::from_slice(&message.data).unwrap();
 
-                            log::debug!("decoded message: {:?}", lock_change);
+                        log::debug!("decoded message: {:?}", lock_change);
 
-                            match lock_change {
-                                locks::Changes::Locks(l) => {
-                                    let mut hm = lock_state.lock();
-                                    hm.clear();
-                                    hm.extend(l.result);
+                        match lock_change {
+                            locks::Changes::Locks(l) => {
+                                let mut hm = lock_state.lock();
+                                hm.clear();
+                                hm.extend(l.result);
 
-                                    users::send_message(
-                                        Message::Locks(hm.clone()),
-                                        Arc::clone(&user_state),
-                                    );
-                                }
-                                locks::Changes::LockChange(l) => {
-                                    locks::update_locks(&mut lock_state.lock(), l);
+                                users::send_message(
+                                    Message::Locks(hm.clone()),
+                                    Arc::clone(&user_state),
+                                );
+                            }
+                            locks::Changes::LockChange(l) => {
+                                locks::update_locks(&mut lock_state.lock(), l);
 
-                                    let data = {
-                                        let locks = lock_state.lock();
-                                        locks.clone()
-                                    };
+                                let data = {
+                                    let locks = lock_state.lock();
+                                    locks.clone()
+                                };
 
-                                    users::send_message(
-                                        Message::Locks(data),
-                                        Arc::clone(&user_state),
-                                    );
-                                }
-                            };
+                                users::send_message(Message::Locks(data), Arc::clone(&user_state));
+                            }
+                        };
 
-                            Ok(())
-                        })
-                        .from_err()
-                })
-                .map_err(move |err| {
-                    exit3.trigger();
-                    tx.trigger();
-                    log::error!("Unhandled error {}", err)
-                }),
-        );
-
-        let users = warp::any().map(move || Arc::clone(&user_state2));
-
-        let locks = warp::any().map(move || Arc::clone(&lock_state2));
-
-        let api_cache = warp::any().map(move || Arc::clone(&api_cache_state2));
-
-        // GET -> messages stream
-        let routes = warp::get2()
-            .and(warp::sse())
-            .and(users)
-            .and(locks)
-            .and(api_cache)
-            .map(
-                |sse: warp::sse::Sse,
-                 users: users::SharedUsers,
-                 locks: SharedLocks,
-                 api_cache: SharedCache| {
-                    // reply using server-sent events
-                    let stream = users::user_connected(
-                        users,
-                        locks.lock().clone(),
-                        api_cache.lock().clone(),
-                    );
-                    sse.reply(warp::sse::keep_alive().stream(stream))
-                },
-            )
-            .with(warp::log("iml-warp-drive::api"));
-
-        log::info!("IML warp drive starting");
-
-        let (_, fut) = warp::serve(routes)
-            .bind_with_graceful_shutdown(iml_manager_env::get_warp_drive_addr(), rx);
-
-        fut
+                        Ok(())
+                    })
+                    .from_err()
+            })
+            .map_err(move |err| {
+                exit3.trigger();
+                tx.trigger();
+                log::error!("Unhandled error {}", err)
+            })
     }));
 }
