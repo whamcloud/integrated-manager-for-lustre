aeab88184d6f8b250dac73a5537f9610ce13bb56
Alex Talker <AlexTalker@users.noreply.github.com>
2019-08-19 18:20:59 +0300

    Temporary solution for race between plugin_runner and device-aggregator (#1036)
    * Temporary solution for race between plugin_runner and device-aggregator
    
    Signed-off-by: Alex Talker <alextalker@ya.ru>
    
    * Update chroma_core/plugins/block_devices.py
    
    Co-Authored-By: Joe Grund <grundjoseph@gmail.com>
    Signed-off-by: Alex Talker <alextalker@ya.ru>
    
    * Fix up a comment
    
    Signed-off-by: Alex Talker <alextalker@ya.ru>
    
    * Fix up error message
    
    Signed-off-by: Alex Talker <alextalker@ya.ru>
    
    * Include override for device-aggregator.socket
    
    Signed-off-by: Alex Talker <alextalker@ya.ru>
    
    * Refresh on latest master
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>
    
    * fixup
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- chroma_core/plugins/block_devices.py
+++ chroma_core/plugins/block_devices.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a MIT-style
 # license that can be found in the LICENSE file.
 
-import json
+import time
 from logging import DEBUG
 
 import settings
@@ -12,23 +12,50 @@ log = log_register("plugin_runner")
 log.setLevel(DEBUG)
 
 
-def _fetch_aggregator():
+def _fetch_aggregator(timeout):
     import requests
 
-    resp = requests.get(settings.DEVICE_AGGREGATOR_PROXY_PASS)
-    payload = resp.text
+    summary = 0.0
+    timeout = timeout if timeout >= 0 else 0
+    while summary <= timeout:
+        resp = requests.get(settings.DEVICE_AGGREGATOR_PROXY_PASS)
+        summary += resp.elapsed.seconds
+        summary += resp.elapsed.microseconds / 10.0 ** 6
 
-    return json.loads(payload)
+        try:
+            return resp.json(), summary
+        except ValueError as e:
+            # This error might be casused by device-aggregator not being ready
+            # yet or communication breakdown inside IML server side
+            log.error(
+                "iml-device-aggregator is not providing expected data, ensure "
+                "this is not caused by race condition (%s)" % e
+            )
+            # So it is better to wait for service startup
+            if summary < timeout:
+                time.sleep(0.1)
+                summary += 0.1
+    return {}, summary
 
 
-def get_devices(fqdn):
-    try:
-        _data = _fetch_aggregator()
-        return _data[fqdn]
-    except Exception as e:
-        log.error(
-            "iml-device-aggregator is not providing expected data, ensure "
-            "iml-device-scanner package is installed and relevant "
-            "services are running on storage servers (%s)" % e
-        )
-        return {}
+def get_devices(fqdn, timeout=0):
+    summary = 0.0
+    timeout = timeout if timeout >= 0 else 0
+
+    while summary <= timeout:
+        try:
+            _data, elapsed = _fetch_aggregator(timeout - summary)
+            summary += elapsed
+            return _data[fqdn]
+        except (KeyError, TypeError) as e:
+            # This error might be caused by fact that device-aggregator
+            # hasn't yet received device information update on startup
+            log.error(
+                "iml-device-aggregator is not providing expected data for fqdn {}. Waited: {},"
+                "Trying again for: {}, Error: {}".format(fqdn, summary, timeout - summary, e)
+            )
+            # So it is better to wait for it if possible
+            if summary < timeout:
+                time.sleep(0.1)
+                summary += 0.1
+    return {}
