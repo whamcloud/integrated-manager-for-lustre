1c9b53f18894ed3ca1159947ba822347b6f2a35a
Nathaniel Clark <nclark@whamcloud.com>
2019-08-19 17:05:43 -0400

    Print debugging to stdout also
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- tests/integration/core/clear_ha_el7.sh
+++ tests/integration/core/clear_ha_el7.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+debug() {
+    logger -t clear_ha "$@"
+    echo "clear_ha: $@"
+}
+
 set -e
 
-logger -t clear_ha "Stopping cluster"
+debug "Stopping cluster"
 if pcs status ; then
     pcs cluster stop --all
 fi
@@ -12,12 +17,12 @@ if [ -z "$ring0_iface" ] && [ -f /etc/corosync/corosync.conf ]; then
     ring0_iface=$(ip route get "$(sed -ne '/ringnumber: 0/{s///;n;s/.*: //p}' /etc/corosync/corosync.conf)" | sed -ne 's/.* dev \([^ ]*\)  *src.*/\1/p')
 fi
 
-logger -t clear_ha "Destroying cluster"
+debug "Destroying cluster"
 pcs cluster destroy
-logger -t clear_ha "Stopping cluster services"
+debug "Stopping cluster services"
 systemctl disable --now pcsd pacemaker corosync
 
-logger -t clear_ha "Stopping interface $ring0_iface"
+debug "Stopping interface $ring0_iface"
 if [ -n "$ring0_iface" ] && ip link show "$ring0_iface"; then
     ifconfig "$ring0_iface" 0.0.0.0 down
     rm -f /etc/sysconfig/network-scripts/ifcfg-"$ring0_iface"
@@ -27,4 +32,5 @@ rm -f /etc/corosync/corosync.conf
 rm -f /var/lib/pacemaker/cib/*
 rm -f /var/lib/corosync/*
 
+debug "Finished"
 exit 0
