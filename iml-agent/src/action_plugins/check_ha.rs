4ac537ba84e0bbef9688c17f96f7f93aae671c42
Nathaniel Clark <nclark@whamcloud.com>
2019-09-06 17:33:19 -0400

    Assimilate review comments
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- iml-agent/src/action_plugins/check_ha.rs
+++ iml-agent/src/action_plugins/check_ha.rs
@@ -4,11 +4,13 @@
 
 use crate::{agent_error::ImlAgentError, cmd::cmd_output_success};
 use elementtree::Element;
-use futures::{future, Future};
-//use tracing_futures::Instrument;
+use futures::{Future, future};
+use tracing::{span, Level};
+use tracing_futures::Instrument;
 use std::collections::HashMap;
+use std::fmt;
 
-// standard:provider:ocftype (e.g. ocf:heartbeat:ZFS, or stonith:fence_ipmilan)
+/// standard:provider:ocftype (e.g. ocf:heartbeat:ZFS, or stonith:fence_ipmilan)
 #[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
 pub struct ResourceAgentType {
     pub standard: String,         // e.g. ocf, lsb, stonith, etc..
@@ -26,11 +28,11 @@ impl ResourceAgentType {
     }
 }
 
-impl From<ResourceAgentType> for String {
-    fn from(agent: ResourceAgentType) -> Self {
-        match agent.provider {
-            Some(provider) => format!("{}:{}:{}", agent.standard, provider, agent.ocftype),
-            None => format!("{}:{}", agent.standard, agent.ocftype),
+impl fmt::Display for ResourceAgentType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self.provider {
+            Some(provider) => write!(f, "{}:{}:{}", self.standard, provider, self.ocftype),
+            None => write!(f, "{}:{}", self.standard, self.ocftype),
         }
     }
 }
@@ -44,48 +46,48 @@ pub struct AgentInfo {
 }
 
 impl AgentInfo {
-    pub fn new(elem: &Element, group: Option<String>) -> AgentInfo {
+    pub fn create(elem: &Element, group: Option<String>) -> Self {
         AgentInfo {
             agent: ResourceAgentType::new(
-                elem.get_attr("class").unwrap().to_string(),
+                elem.get_attr("class").unwrap_or("").to_string(),
                 elem.get_attr("provider").map(|s| s.to_string()),
-                elem.get_attr("type").unwrap().to_string(),
+                elem.get_attr("type").unwrap_or("").to_string(),
             ),
             group: group,
-            id: elem.get_attr("id").unwrap().to_string(),
+            id: elem.get_attr("id").unwrap_or("").to_string(),
             args: match elem.find("instance_attributes") {
-                None => vec![],
+                None => HashMap::new(),
                 Some(e) => e
                     .find_all("nvpair")
                     .map(|nv| {
                         (
-                            nv.get_attr("name").unwrap().to_string(),
-                            nv.get_attr("value").unwrap().to_string(),
+                            nv.get_attr("name").unwrap_or("").to_string(),
+                            nv.get_attr("value").unwrap_or("").to_string(),
                         )
                     })
                     .collect(),
-            }
-            .iter()
-            .cloned()
-            .collect(),
+            },
         }
     }
 }
 
 pub fn check_ha(_: ()) -> impl Future<Item = Vec<AgentInfo>, Error = ImlAgentError> {
-    cmd_output_success("cibadmin", &["--query", "--xpath", "//resources"]).and_then(|output| {
-        match Element::from_reader(output.stdout.as_slice()) {
-            Err(err) => future::err(ImlAgentError::XmlError(err)),
-            Ok(elem) => future::ok(
-                elem.find_all("group")
-                    .flat_map(|g| {
-                        let name = g.get_attr("id").unwrap().to_string();
-                        g.find_all("primitive")
-                            .map(move |p| AgentInfo::new(p, Some(name.clone())))
-                    })
-                    .chain(elem.find_all("primitive").map(|p| AgentInfo::new(p, None)))
-                    .collect(),
-            ),
-        }
-    })
+    cmd_output_success("cibadmin", &["--query", "--xpath", "//resources"])
+        .instrument(span!(Level::INFO, "Read cib"))
+        .and_then(|output| {
+            // This cannot split between map/map_err because Element does not implement Send
+            match Element::from_reader(output.stdout.as_slice()) {
+                Err(err) => future::err(ImlAgentError::XmlError(err)),
+                Ok(elem) => future::ok(
+                    elem.find_all("group")
+                        .flat_map(|g| {
+                            let name = g.get_attr("id").unwrap_or("").to_string();
+                            g.find_all("primitive")
+                                .map(move |p| AgentInfo::create(p, Some(name.clone())))
+                        })
+                        .chain(elem.find_all("primitive").map(|p| AgentInfo::create(p, None)))
+                        .collect(),
+                ),
+            }
+        }).from_err()
 }
