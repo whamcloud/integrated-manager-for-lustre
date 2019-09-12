849df4d114925949cf008920f495c93b5ead4c01
Will Johnson <wjohnson@whamcloud.com>
2019-09-12 16:43:09 -0400

    - Updates
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- iml-util/util.rs
+++ iml-util/util.rs
@@ -6,15 +6,12 @@ pub trait Flatten<T> {
     fn flatten(self) -> Option<T>;
 }
 
+/// Implement flatten for the `Option` type.
+/// See https://doc.rust-lang.org/std/option/enum.Option.html#method.flatten
+/// for more information.
 impl<T> Flatten<T> for Option<Option<T>> {
     fn flatten(self) -> Option<T> {
         self.unwrap_or(None)
-    }
-}
-
-impl<T> Flatten<T> for Option<Option<Option<T>>> {
-    fn flatten(self) -> Option<T> {
-        self.unwrap_or(None).unwrap_or(None)
     }
 }
 
@@ -23,26 +20,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_option_of_Some() {
+    fn test_option_of_some() {
         let x = Some(Some(7));
         assert_eq!(x.flatten(), Some(7));
     }
 
     #[test]
-    fn test_option_of_None() {
-        let x = Some(Some(None));
+    fn test_option_of_none() {
+        let x: Option<Option<i32>> = Some(None);
         assert_eq!(x.flatten(), None);
     }
 
     #[test]
     fn test_option_of_option_of_some() {
         let x = Some(Some(Some(7)));
-        assert_eq!(x.flatten(), Some(7));
+        assert_eq!(x.flatten().flatten(), Some(7));
     }
 
     #[test]
-    fn test_option_of_option_of_None() {
-        let x = Some(Some(None));
-        assert_eq!(x.flatten(), None);
+    fn test_option_of_option_of_none() {
+        let x: Option<Option<Option<u32>>> = Some(Some(None));
+        assert_eq!(x.flatten().flatten(), None);
     }
 }
