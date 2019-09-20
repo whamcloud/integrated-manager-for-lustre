d9baf9184741a57194227a27e979718ec3126f80
Nathaniel Clark <nclark@whamcloud.com>
2019-09-20 14:58:01 -0400

    Fill out stonith checking
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- iml-agent/src/action_plugins/check_stonith.rs
+++ iml-agent/src/action_plugins/check_stonith.rs
@@ -8,10 +8,74 @@ use crate::{
 };
 use elementtree::Element;
 use futures::Future;
+use std::process::Command;
 use tracing::{span, Level};
 use tracing_futures::Instrument;
 
-fn stonith_ok(elem: &Element) -> Result<(bool, String), ImlAgentError> {
+
+fn inst_attr_problem(elem: &Option<&Element>, node: &String) -> Option<String> {
+    match elem {
+        None => None,
+        Some(instance) => {
+            let mut hc: Option<bool> = None;
+            let mut hl: Option<bool> = None;
+            for e in instance.children() {
+                match e.get_attr("name") {
+                    Some("pcmk_host_list") =>
+                        match e.get_attr("value") {
+                            None => return None,
+                            Some(val) => {
+                                if val.contains(node) {
+                                    match hc {
+                                        Some(true) => return None,
+                                        Some(false) => return Some(format!("{} missing from pcmk_host_list", node)),
+                                        None => hl = Some(true),
+                                    }
+                                } else {
+                                    hl = Some(false);
+                                }
+                            }
+                        },
+                    Some("pcmk_host_check") =>
+                        match e.get_attr("value") {
+                            Some("static-list") => {
+                                match hl {
+                                    Some(true) => return None,
+                                    Some(false) => return Some(format!("{} missing from pcmk_host_list", node)),
+                                    None => hc = Some(true),
+                                }
+                            },
+                            _ => return None,
+                        },
+                    _ => (),
+                }
+            }
+            None
+        }
+    }
+}
+
+fn meta_attr_problem(elem: &Option<&Element>) -> Option<String> {
+    match elem {
+        None => None,
+        Some(meta) => {
+            for e in meta.children() {
+                match e.get_attr("name") {
+                    Some("target-role") =>
+                        match e.get_attr("value") {
+                            Some("Started") => return None,
+                            Some(other) => return Some(format!("role: {}", other)),
+                            None => (),
+                        }
+                    _ => ()
+                }
+            }
+            None
+        }
+    }
+}
+
+fn stonith_ok(elem: &Element, nodename: &String) -> Result<(bool, String), ImlAgentError> {
     match elem.get_attr("type") {
         None => Err(ImlAgentError::CibError(CibError(format!(
             "{} is missing type attribute",
@@ -21,24 +85,26 @@ fn stonith_ok(elem: &Element) -> Result<(bool, String), ImlAgentError> {
             None => Ok((false, "fence_chroma - unconfigured".to_string())),
             Some(_) => Ok((true, "fence_chroma".to_string())),
         },
-        Some(t) => Ok((true, t.to_string())),
-        // @@TODO: This should check for node name in
-        // instance_attributes:pcmk_host_list if
-        // pcmk_host_check=static-list
-
-        // @@TODO: This should check meta_attributes:target-role is
-        // not Stopped (disabled agent)
+        Some(t) => {
+            if let Some(err) = meta_attr_problem(&elem.find("meta_attributes")) {
+                return Ok((false, format!("{} - {}", t, err)))
+            }
+            if let Some(err) = inst_attr_problem(&elem.find("instance_attributes"), nodename) {
+                return Ok((false, format!("{} - {}", t, err)))
+            }
+            Ok((true, t.to_string()))
+        }
     }
 }
 
-fn _check_stonith(output: &[u8]) -> Result<(bool, String), ImlAgentError> {
-    match Element::from_reader(output) {
+fn _check_stonith(xml: &[u8], nodename: &String) -> Result<(bool, String), ImlAgentError> {
+    match Element::from_reader(xml) {
         Err(err) => Err(ImlAgentError::XmlError(err)),
         Ok(elem) => match elem.tag().name() {
-            "primitive" => stonith_ok(&elem),
+            "primitive" => stonith_ok(&elem, nodename),
             "xpath-query" => {
                 for el in elem.find_all("primitive") {
-                    if let Ok((true, msg)) = stonith_ok(el) {
+                    if let Ok((true, msg)) = stonith_ok(el, nodename) {
                         return Ok((true, msg));
                     }
                 }
@@ -62,7 +128,8 @@ pub fn check_stonith(_: ()) -> impl Future<Item = (bool, String), Error = ImlAge
         if !x.status.success() {
             return Ok((false, "No pacemaker".to_string()));
         }
-        _check_stonith(x.stdout.as_slice())
+        let cmd = Command::new("crm_node").args(&["-n"]).output()?;
+        _check_stonith(x.stdout.as_slice(), &String::from_utf8(cmd.stdout)?)
     })
     .from_err()
 }
@@ -73,29 +140,25 @@ mod tests {
     use crate::agent_error;
 
     #[test]
-    fn test_stonith_unconfigured_fence_chroma() -> agent_error::Result<()> {
+    fn test_stonith_unconfigured_fence_chroma() {
         let testxml = r#"<primitive class="stonith" id="st-fencing" type="fence_chroma"/>"#;
         assert_eq!(
-            _check_stonith(&testxml.as_bytes()).unwrap(),
+            _check_stonith(&testxml.as_bytes(), &"host0".to_string()).unwrap(),
             (false, "fence_chroma - unconfigured".to_string())
         );
-
-        Ok(())
     }
 
     #[test]
-    fn test_stonith_bad_xml() -> agent_error::Result<()> {
+    fn test_stonith_bad_xml() {
         let testxml = r#"<badtag class="stonith" id="st-fencing" type="fence_chroma"/>"#;
         assert_eq!(
-            format!("{}", _check_stonith(&testxml.as_bytes()).unwrap_err()),
+            format!("{}", _check_stonith(&testxml.as_bytes(), &"host0".to_string()).unwrap_err()),
             "Unknown first tag badtag".to_string()
         );
-
-        Ok(())
     }
 
     #[test]
-    fn test_stonith_fence_vbox() -> agent_error::Result<()> {
+    fn test_stonith_fence_vbox() {
         let testxml = r#"<primitive class="stonith" id="vboxfence" type="fence_vbox">
     <instance_attributes id="vboxfence-instance_attributes">
       <nvpair id="vboxfence-instance_attributes-identity_file" name="identity_file" value="/home/nclark/.ssh/id_rsa"/>
@@ -108,14 +171,13 @@ mod tests {
   </primitive>
 "#;
         assert_eq!(
-            _check_stonith(&testxml.as_bytes()).unwrap(),
+            _check_stonith(&testxml.as_bytes(), &"host0".to_string()).unwrap(),
             (true, "fence_vbox".to_string())
         );
-        Ok(())
     }
 
     #[test]
-    fn test_stonith_multi_fence_ipmilan() -> agent_error::Result<()> {
+    fn test_stonith_multi_fence_ipmilan() {
         let testxml = r#"<xpath-query>
       <primitive id="stonith-host0" class="stonith" type="fence_ipmilan">
         <instance_attributes id="stonith-host0-instance_attributes">
@@ -142,7 +204,7 @@ mod tests {
       </primitive>
       <primitive id="stonith-host1" class="stonith" type="fence_ipmilan">
         <instance_attributes id="stonith-host1-instance_attributes">
-          <nvpair name="pcmk_host_list" value="host1" id="stonith-host1-instance_attributes-pcmk_host_list"/>
+          <nvpair name="pcmk_host_list" value="host2,host1" id="stonith-host1-instance_attributes-pcmk_host_list"/>
           <nvpair name="ipaddr" value="10.0.1.11" id="stonith-host1-instance_attributes-ipaddr"/>
           <nvpair name="login" value="root" id="stonith-host1-instance_attributes-login"/>
           <nvpair name="passwd" value="****" id="stonith-host1-instance_attributes-passwd"/>
@@ -166,9 +228,16 @@ mod tests {
 </xpath-query>
 "#;
         assert_eq!(
-            _check_stonith(&testxml.as_bytes()).unwrap(),
+            _check_stonith(&testxml.as_bytes(), &"host0".to_string()).unwrap(),
             (true, "fence_ipmilan".to_string())
         );
-        Ok(())
+        assert_eq!(
+            _check_stonith(&testxml.as_bytes(), &"host1".to_string()).unwrap(),
+            (true, "fence_ipmilan".to_string())
+        );
+        assert_eq!(
+            _check_stonith(&testxml.as_bytes(), &"badhost".to_string()).unwrap(),
+            (false, "No working fencing agents".to_string())
+        );
     }
 }
