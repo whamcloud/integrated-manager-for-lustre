7e4069384656cabe19ed44b3728ef9654e9ca3f8
Joe Grund <jgrund@whamcloud.io>
2019-10-24 12:42:25 -0400

    Asyncify
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- iml-agent/src/action_plugins/ostpool.rs
+++ iml-agent/src/action_plugins/ostpool.rs
@@ -3,6 +3,11 @@
 // license that can be found in the LICENSE file.
 
 use crate::{agent_error::ImlAgentError, cmd::lctl};
+use futures::{
+    compat::Future01CompatExt,
+    future::try_join_all,
+    future::{FutureExt, TryFutureExt},
+};
 use futures01::Future;
 use iml_wire_types::OstPool;
 
@@ -46,14 +51,12 @@ pub fn pool_destroy(
     lctl(&["pool_destroy", format!("{}.{}", filesystem, name).as_str()]).map(drop)
 }
 
-fn pool_list(
-    filesystem: &String,
-    pool: &String,
-) -> impl Future<Item = Vec<String>, Error = ImlAgentError> {
-    lctl(&["pool_list", format!("{}.{}", filesystem, pool).as_str()])
+async fn pool_list(filesystem: &str) -> Result<Vec<String>, ImlAgentError> {
+    lctl(&["pool_list", &filesystem])
+        .compat()
+        .await
         .map(|o| {
             String::from_utf8_lossy(&o.stdout)
-                .to_string()
                 .lines()
                 .skip(1)
                 .map(|s| s.rsplit('.').next().unwrap().to_string())
@@ -72,30 +75,32 @@ fn pool_list(
         })
 }
 
-pub fn pools(filesystem: String) -> impl Future<Item = Vec<OstPool>, Error = ImlAgentError> {
-    lctl(&["pool_list", filesystem.as_str()])
-        .map(|o| {
-            String::from_utf8_lossy(&o.stdout)
-                .to_string()
-                .lines()
-                .skip(1)
-                .map(|s| s.rsplit('.').next().unwrap())
-                .map(move |pool| OstPool {
-                    filesystem: filesystem.clone(),
-                    name: pool.to_string(),
-                    osts: vec![] as Vec<String>, // @@ Do lctl "pool_list", filename.pool for these OSTs
-                })
-                .collect()
-        })
-        .or_else(|e| {
-            if let ImlAgentError::CmdOutputError(cerr) = e {
-                if cerr.status.code() == Some(2) {
-                    Ok(vec![])
-                } else {
-                    Err(ImlAgentError::CmdOutputError(cerr))
-                }
-            } else {
-                Err(e)
-            }
-        })
+pub async fn pools_async(filesystem: String) -> Result<Vec<OstPool>, ImlAgentError> {
+    let xs = pool_list(&filesystem).await?;
+
+    let xs = xs.into_iter().map(|pool| {
+        let filesystem = filesystem.clone();
+
+        async move {
+            let p = format!("{}.{}", filesystem, pool);
+
+            let osts = {
+                pool_list(&p)
+            };
+
+            Ok::<_, ImlAgentError>(OstPool {
+                name: pool.to_string(),
+                filesystem,
+                osts: osts.await?,
+            })
+        }
+    });
+
+    let xs = try_join_all(xs).await?;
+
+    Ok(xs)
+}
+
+pub fn pools(x: String) -> impl Future<Item = Vec<OstPool>, Error = ImlAgentError> {
+    pools_async(x).boxed().compat()
 }
