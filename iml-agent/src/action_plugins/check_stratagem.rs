f449ec687ff940ed538de10cef2579e958b8f2b7
Michael Pankov <work@michaelpankov.com>
2019-11-14 15:31:52 +0300

    Change the package name
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-agent/src/action_plugins/check_stratagem.rs
+++ iml-agent/src/action_plugins/check_stratagem.rs
@@ -1,7 +1,7 @@
 use crate::{agent_error::ImlAgentError, cmd::cmd_output};
 
 pub async fn check_stratagem(_: ()) -> Result<bool, ImlAgentError> {
-    let rpm = cmd_output("rpm", vec!["--query", "rust-iml-stratagem"]).await?;
+    let rpm = cmd_output("rpm", vec!["--query", "lipe"]).await?;
     if rpm.status.success() {
         Ok(true)
     } else {
