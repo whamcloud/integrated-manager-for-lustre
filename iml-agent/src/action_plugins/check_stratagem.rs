314efc076a28efcbf16a3d3af28f189c9b024ce0
Michael Pankov <work@michaelpankov.com>
2019-11-19 09:49:22 +0300

    Use librpm.rs

--- iml-agent/src/action_plugins/check_stratagem.rs
+++ iml-agent/src/action_plugins/check_stratagem.rs
@@ -1,8 +1,12 @@
 use crate::{agent_error::ImlAgentError, cmd::cmd_output};
+use librpm::Index;
 
 pub async fn check_stratagem(_: ()) -> Result<bool, ImlAgentError> {
-    let rpm = cmd_output("rpm", vec!["--query", "lipe"]).await?;
-    if rpm.status.success() {
+    librpm::config::read_file(None).unwrap();
+
+    let mut matches = Index::Name.find("lipe");
+    let package = matches.next();
+    if package.is_some() {
         Ok(true)
     } else {
         Ok(false)
