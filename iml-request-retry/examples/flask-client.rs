4601beb512a12fd92a5de1edb8851a17b98099cb
Nick Linker <nlinker@gmail.com>
2019-11-19 22:04:47 +0600

    Apply rustfmt
    Signed-off-by: Nick Linker <nlinker@gmail.com>

--- iml-request-retry/examples/flask-client.rs
+++ iml-request-retry/examples/flask-client.rs
@@ -2,37 +2,36 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-use std::io;
-use std::collections::HashMap;
+use iml_request_retry::{retry_future, retry_future_gen, RetryAction};
 use reqwest::{Client, Response, Url};
-use iml_request_retry::{RetryAction, retry_future, retry_future_gen};
-use std::time::Duration;
+use std::collections::HashMap;
 use std::fmt::Debug;
+use std::io;
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-
-    let mut policy_1 = |k: u32, e| {
-        match k {
-            0 => RetryAction::RetryNow,
-            k if k < 3 => RetryAction::WaitFor(Duration::from_secs((2 * k) as u64)),
-            _ => RetryAction::ReturnError(e),
-        }
+    let mut policy_1 = |k: u32, e| match k {
+        0 => RetryAction::RetryNow,
+        k if k < 3 => RetryAction::WaitFor(Duration::from_secs((2 * k) as u64)),
+        _ => RetryAction::ReturnError(e),
     };
 
     let r = retry_future(|| http_call(42), &mut policy_1).await;
     println!("r = {:?}", r);
 
     fn policy_2<E: Debug>(k: u32, _e: E) -> RetryAction<E> {
-        if k == 0 { RetryAction::RetryNow }
-        else { RetryAction::WaitFor(Duration::from_secs(5)) }
+        if k == 0 {
+            RetryAction::RetryNow
+        } else {
+            RetryAction::WaitFor(Duration::from_secs(5))
+        }
     }
 
     let r = retry_future_gen(|| http_call(42), policy_2).await;
     println!("r = {:?}", r);
     Ok(())
 }
-
 
 pub async fn http_call(_n: u32) -> reqwest::Result<String> {
     let uri = Url::parse(&"http://127.0.0.1:5000/fail5").unwrap();
@@ -42,21 +41,14 @@ pub async fn http_call(_n: u32) -> reqwest::Result<String> {
 
     // GET request with body
     let client: Client = reqwest::Client::new();
-    let resp: Response = client
-        .get(uri)
-        .json(&map)
-        .send()
-        .await?;
+    let resp: Response = client.get(uri).json(&map).send().await?;
 
     match resp.error_for_status() {
         Ok(resp) => {
-            let resp_body = resp
-                .text()
-                .await?
-                .to_string();
+            let resp_body = resp.text().await?.to_string();
             Ok(resp_body)
         }
-        Err(err) => Err(err)
+        Err(err) => Err(err),
     }
 }
 
@@ -69,7 +61,6 @@ pub enum MyError {
 }
 
 //pub type Result<T> = ::std::result::Result<T, MyError>;
-
 
 impl From<io::Error> for MyError {
     fn from(err: io::Error) -> MyError {
