8168a98f48f00e34c597cad584ff42bd34a9e6cc
Michael Pankov <work@michaelpankov.com>
2019-11-21 10:37:45 +0300

    Factor out rpm querying to a module
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-agent/src/action_plugins/check_stratagem.rs
+++ iml-agent/src/action_plugins/check_stratagem.rs
@@ -1,10 +1,5 @@
-use crate::{agent_error::ImlAgentError, cmd::cmd_output};
+use crate::{agent_error::ImlAgentError, rpm};
 
 pub async fn check_stratagem(_: ()) -> Result<bool, ImlAgentError> {
-    let rpm = cmd_output("rpm", vec!["--query", "lipe"]).await?;
-    if rpm.status.success() {
-        Ok(true)
-    } else {
-        Ok(false)
-    }
+    rpm::query("lipe").await
 }
