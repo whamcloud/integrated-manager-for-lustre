d15aad500e3d6ff3cc7406a49887879425199823
Joe Grund <jgrund@whamcloud.io>
2019-11-21 11:17:18 -0500

    Add tooltip component (#1345)
    Add the tooltip component to be usable within the iml-gui.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- iml-gui/crate/src/components/mod.rs
+++ iml-gui/crate/src/components/mod.rs
@@ -1,3 +1,7 @@
 pub(crate) mod font_awesome;
+pub(crate) mod tooltip;
 
 pub(crate) use font_awesome::font_awesome;
+pub(crate) use tooltip::{
+    color_tooltip, error_tooltip, tooltip, tooltip_container, TooltipPlacement,
+};
