e18fe57d949d3e3da29e5b0c1966deb1432e9883
Michael Pankov <work@michaelpankov.com>
2019-12-05 10:36:23 +0300

    Format code
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-agent/src/rpm.rs
+++ iml-agent/src/rpm.rs
@@ -22,7 +22,9 @@ async fn parse(output: Output) -> StdResult<Option<Version>, ImlAgentError> {
         if output.stderr.len() > 0 {
             Err(ImlAgentError::CmdOutputError(output))
         } else {
-            Ok(Some(Version(String::from_utf8_lossy(&output.stdout).to_string())))
+            Ok(Some(Version(
+                String::from_utf8_lossy(&output.stdout).to_string(),
+            )))
         }
     } else {
         let stdout = output.stdout.clone();
