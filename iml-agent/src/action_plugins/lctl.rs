36fe8f58ba5f2a870d06a23cd0cf05a1d4b56c70
Nathaniel Clark <nclark@whamcloud.com>
2019-12-06 15:37:35 -0500

    agent: Add lctl action plugin (#1383)
    * agent: Add lctl action plugin
    
    lctl(Vec<String>) -> String
    
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>
    
    * Review comments.
    
    Co-Authored-By: Joe Grund <jgrund@whamcloud.io>
    
    * Fix formatting
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

---
+++ iml-agent/src/action_plugins/lctl.rs
@@ -0,0 +1,12 @@
+// Copyright (c) 2019 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+use crate::{agent_error::ImlAgentError, cmd};
+
+/// Runs lctl with given arguments
+pub async fn lctl(args: Vec<String>) -> Result<String, ImlAgentError> {
+    cmd::lctl(args.iter().map(|s| s.as_ref()).collect())
+        .await
+        .map(|o| String::from_utf8_lossy(&o.stdout).to_string())
+}
