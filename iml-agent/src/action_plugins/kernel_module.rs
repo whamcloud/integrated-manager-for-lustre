2fa3116176a62fa0b8f1d390df63aa5cea965754
Michael Pankov <work@michaelpankov.com>
2019-12-06 10:18:17 +0300

    Drop the draft code of search
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-agent/src/action_plugins/kernel_module.rs
+++ iml-agent/src/action_plugins/kernel_module.rs
@@ -5,15 +5,5 @@ use kmod;
 pub async fn loaded(module: String) -> Result<bool, ImlAgentError> {
     let ctx = kmod::Context::new()?;
 
-    let mut result = false;
-    for m in ctx.modules_loaded()? {
-        let name = m.name();
-
-        if name == module {
-            result = true;
-        } else {
-            continue;
-        }
-    }
-    Ok(result)
+    Ok(ctx.modules_loaded()?.find(|m| m.name() == module).is_some())
 }
