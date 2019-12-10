6eb95f6b6e20d311b6336437e35cef3f5af1d1b9
Michael Pankov <work@michaelpankov.com>
2019-12-10 10:00:21 +0300

    Use blocking executor
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-agent/src/action_plugins/kernel_module.rs
+++ iml-agent/src/action_plugins/kernel_module.rs
@@ -1,9 +1,12 @@
 use crate::agent_error::ImlAgentError;
-
 use kmod;
+use tokio_executor::blocking::run;
 
 pub async fn loaded(module: String) -> Result<bool, ImlAgentError> {
-    let ctx = kmod::Context::new()?;
+    run(move || {
+        let ctx = kmod::Context::new()?;
 
-    Ok(ctx.modules_loaded()?.find(|m| m.name() == module).is_some())
+        Ok(ctx.modules_loaded()?.find(|m| m.name() == module).is_some())
+    })
+    .await
 }
