406df9e0ed64203d4d64bd0e772796c6189adf20
Michael Pankov <work@michaelpankov.com>
2019-12-10 17:57:49 +0300

    Factor out common prefix
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-agent/src/action_plugins/ltuer.rs
+++ iml-agent/src/action_plugins/ltuer.rs
@@ -3,12 +3,15 @@
 // license that can be found in the LICENSE file.
 
 use crate::agent_error::ImlAgentError;
+use std::path::PathBuf;
 use tokio_fs as fs;
+
+static CONFIGURATION_DIR: &str = "/etc/iml";
 
 pub async fn create_ltuer_conf(
     (mailbox_path, fs_name, cold_pool): (String, String, String),
 ) -> Result<(), ImlAgentError> {
-    fs::create_dir_all("/etc/iml").await?;
+    fs::create_dir_all(CONFIGURATION_DIR).await?;
 
     let contents = format!(
         "mailbox={}
@@ -17,12 +20,16 @@ cold_pool={}
 ",
         mailbox_path, fs_name, cold_pool
     );
-    fs::write("/etc/iml/ltuer.conf", contents)
-        .await
-        .map_err(|e| e.into())
+
+    let mut path = PathBuf::from(CONFIGURATION_DIR);
+    path.push("ltuer.conf");
+
+    fs::write(path, contents).await.map_err(|e| e.into())
 }
 
 #[tokio::test]
 async fn ltuer() {
-    create_ltuer_conf(("foo".into(), "bar".into(), "baz".into())).await.unwrap();
+    create_ltuer_conf(("foo".into(), "bar".into(), "baz".into()))
+        .await
+        .unwrap();
 }
