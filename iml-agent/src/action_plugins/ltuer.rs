b4dea9e9229ca92cb2f57df2c07f74383d85dbf9
Michael Pankov <work@michaelpankov.com>
2019-12-11 10:23:03 +0300

    Implement a better test
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-agent/src/action_plugins/ltuer.rs
+++ iml-agent/src/action_plugins/ltuer.rs
@@ -3,16 +3,13 @@
 // license that can be found in the LICENSE file.
 
 use crate::agent_error::ImlAgentError;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use tokio::io::{AsyncWrite, AsyncWriteExt};
 use tokio_fs as fs;
 
-#[cfg(not(test))]
 static CONFIGURATION_DIR: &str = "/etc/iml";
-#[cfg(test)]
-static CONFIGURATION_DIR: &str = "/tmp/etc/iml";
 
-async fn create_ltuer_conf_internal<W>(
+async fn create_ltuer_conf_internal_<W>(
     (mailbox_path, fs_name, cold_pool): (String, String, String),
     writer: &mut W,
 ) -> Result<(), ImlAgentError>
@@ -33,33 +30,49 @@ where
         .map_err(|e| e.into())
 }
 
-pub async fn create_ltuer_conf(
+async fn create_ltuer_conf_internal<P>(
     (mailbox_path, fs_name, cold_pool): (String, String, String),
-) -> Result<(), ImlAgentError> {
-    fs::create_dir_all(CONFIGURATION_DIR).await?;
+    dir_path: P,
+) -> Result<(), ImlAgentError>
+where
+    P: AsRef<Path>,
+    PathBuf: From<P>,
+{
+    fs::create_dir_all(&dir_path).await?;
 
-    let mut path = PathBuf::from(CONFIGURATION_DIR);
+    let mut path = PathBuf::from(dir_path);
     path.push("ltuer.conf");
 
     let mut file = fs::File::create(path).await?;
 
-    create_ltuer_conf_internal((mailbox_path, fs_name, cold_pool), &mut file).await
+    create_ltuer_conf_internal_((mailbox_path, fs_name, cold_pool), &mut file).await
 }
 
-// Warning: only one test at a time can be executing,
-// because they write to the same one file
-#[tokio::test]
-async fn test_create_ltuer_conf() {
-    create_ltuer_conf(("foo".into(), "bar".into(), "baz".into()))
-        .await
-        .unwrap();
+pub async fn create_ltuer_conf(
+    (mailbox_path, fs_name, cold_pool): (String, String, String),
+) -> Result<(), ImlAgentError> {
+    create_ltuer_conf_internal((mailbox_path, fs_name, cold_pool), CONFIGURATION_DIR).await
+}
 
-    let mut path = PathBuf::from(CONFIGURATION_DIR);
-    path.push("ltuer.conf");
+#[cfg(test)]
+mod tests {
+    use tempfile::tempdir;
+    use super::*;
 
-    let contents = fs::read(path).await.unwrap();
-    assert_eq!(
-        String::from_utf8_lossy(&contents),
-        "mailbox=foo\nfs_name=bar\ncold_pool=baz\n"
-    );
+    #[tokio::test]
+    async fn test_create_ltuer_conf() {
+        let dir = tempdir().unwrap();
+
+        create_ltuer_conf_internal(("foo".into(), "bar".into(), "baz".into()), dir.path())
+            .await
+            .unwrap();
+
+        let path = dir.path().join("ltuer.conf");
+
+        let contents = fs::read(path).await.unwrap();
+        assert_eq!(
+            String::from_utf8_lossy(&contents),
+            "mailbox=foo\nfs_name=bar\ncold_pool=baz\n"
+        );
+    }
 }
