b5ce99c98a79ce11e02ffb6ae178a03e9fc7a2f6
Michael Pankov <work@michaelpankov.com>
2019-12-13 13:52:50 +0300

    Try to slightly improve iterators
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-agent/src/action_plugins/kernel_module.rs
+++ iml-agent/src/action_plugins/kernel_module.rs
@@ -10,21 +10,16 @@ pub async fn loaded(module: String) -> Result<bool, ImlAgentError> {
     if output.status.success() {
         let stdout = String::from_utf8_lossy(&output.stdout);
         let mut modules = stdout.lines().into_iter().filter_map(|m| {
-            println!("{:?}", m);
             let mut fields = m.split(' ').filter(|s| *s != "");
             let (name, _, refcount) = (fields.next(), fields.next(), fields.next());
-            println!("{:?} {:?}", name, refcount);
-            if let (Some(name), Some(refcount)) = (name, refcount) {
-                if let Ok(refcount) = refcount.parse::<u32>() {
-                    dbg!(Some((name, refcount)))
-                } else {
-                    dbg!(None)
-                }
+            let refcount = refcount.map(|x| x.parse::<u32>());
+            if let (Some(name), Some(Ok(refcount))) = (name, refcount) {
+                Some((name, refcount))
             } else {
-                dbg!(None)
+                None
             }
         });
-        println!("{:#?}", modules);
+
         let module = modules.find(|(m, refcount)| *m == module && *refcount > 0);
 
         if module.is_some() {
