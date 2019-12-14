f6dcdca23f5c0bcf5326c5c20141556c80b68be8
johnsonw <wjohnson@whamcloud.com>
2019-12-14 08:37:49 -0500

    - more updates
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- docker/influxdb/setup-influxdb.sh
+++ docker/influxdb/setup-influxdb.sh
@@ -1,2 +1,5 @@
 #! /bin/sh
-setup-influxdb && influxd -config /etc/influxdb/influxdb.conf
+/entrypoint.sh influxd \
+  && timeout -t 60 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' influxdb:8086/ping)" != "201" ]]; do echo "Testing influxdb connection" && sleep 5; done' || false \
+  && setup-influxdb
+
