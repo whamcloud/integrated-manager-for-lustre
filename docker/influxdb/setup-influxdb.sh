5a2d6ef96fa51acef79df86bc45acf94de4a41c5
johnsonw <wjohnson@whamcloud.com>
2019-12-14 10:08:06 -0500

    - run entrypoint in background
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- docker/influxdb/setup-influxdb.sh
+++ docker/influxdb/setup-influxdb.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-/entrypoint.sh influxd \
-  && timeout -t 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' influxdb:8086/ping)" != "204" ]]; do echo "Testing influxdb connection" && sleep 5; done' || false \
+/entrypoint.sh influxd &
+timeout -t 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' influxdb:8086/ping)" != "204" ]]; do echo "Testing influxdb connection" && sleep 5; done' || false \
   && setup-influxdb
 
