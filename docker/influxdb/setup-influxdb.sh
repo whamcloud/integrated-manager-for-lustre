ebedf8c12dbb106909300e3e6654ae0a08d82016
johnsonw <wjohnson@whamcloud.com>
2019-12-15 09:27:49 -0500

    - remove influxdb.dockerfile. I was able to get it working with the  stock image
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- docker/influxdb/setup-influxdb.sh
+++ docker/influxdb/setup-influxdb.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
-timeout -t 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' influxdb:8086/ping)" != "204" ]]; do echo "Testing influxdb connection" && sleep 5; done' || false \
+apk add curl python \
+  && timeout -t 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' influxdb:8086/ping)" != "204" ]]; do echo "Testing influxdb connection" && sleep 5; done' || false \
   && setup-influxdb
