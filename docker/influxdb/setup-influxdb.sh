95ab0783fd6282ae2f73b39d9768878e3e0d4dfb
johnsonw <wjohnson@whamcloud.com>
2019-12-15 09:37:14 -0500

    - more cleanups
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- docker/influxdb/setup-influxdb.sh
+++ docker/influxdb/setup-influxdb.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
-apk add curl python \
-  && timeout -t 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' influxdb:8086/ping)" != "204" ]]; do echo "Testing influxdb connection" && sleep 5; done' || false \
-  && setup-influxdb
+apk add curl \
+  && timeout -t 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' 127.0.0.1:8086/ping)" != "204" ]]; do echo "Testing influxdb connection" && sleep 5; done' || false \
+  && influx -execute "CREATE DATABASE $INFLUXDB_IML_DB"
+  && influx -execute "CREATE DATABASE $INFLUXDB_STRATAGEM_SCAN_DB"
