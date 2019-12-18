5dbd250dee44f845e662d95e3b40fd8b93fc502a
johnsonw <wjohnson@whamcloud.com>
2019-12-18 11:11:14 -0500

    - Remove conf in favor of environment variables
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- docker/influxdb/setup-influxdb.sh
+++ docker/influxdb/setup-influxdb.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 apk add curl \
-  && timeout -t 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' 127.0.0.1:8086/ping)" != "204" ]]; do echo "Testing influxdb connection" && sleep 5; done' || false \
+  && [[ $(curl -s -o /dev/null -w '%{http_code}' --connect-timeout 5 --max-time 5 --retry 10 --retry-delay 0 http://influxdb:8086/ping) == "204" ]] || false \
   && influx -execute "CREATE DATABASE $INFLUXDB_IML_DB" \
   && influx -execute "CREATE DATABASE $INFLUXDB_STRATAGEM_SCAN_DB"
