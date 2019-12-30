3775cd21a618fc228fcd221d7b5a8e2cddd387ab
Nick Linker <nlinker@gmail.com>
2019-12-31 04:47:05 +0600

    Add --force flag
    Signed-off-by: Nick Linker <nlinker@gmail.com>

--- iml-agent/src/action_plugins/lamigo.rs
+++ iml-agent/src/action_plugins/lamigo.rs
@@ -4,9 +4,13 @@
 
 use crate::agent_error::ImlAgentError;
 use futures::future::TryFutureExt;
-use std::fmt;
+use std::{fmt, io};
 use std::path::Path;
 use tokio::fs;
+use tokio::process::{Command, Child};
+use std::process::Output;
+
+static SYSTEMD_DIR: &str = "/Users/nick/tmp/system";
 
 #[derive(serde::Deserialize, structopt::StructOpt, Clone, Debug)]
 pub struct Config {
@@ -35,8 +39,18 @@ pub struct Config {
     min_age: u32,
 
     #[structopt(long)]
-    /// Lustre client mount point, e.g. /mnt/lustre
+    /// Lustre client mount point, e.g. `/mnt/lustre`
     mount_point: String,
+
+    #[structopt(long)]
+    /// Lustre device to be mounted, e.g. `192.168.0.100@tcp0:/spfs`
+    lustre_device: String,
+
+    #[structopt(long)]
+    /// (Optional argument)
+    /// By default, nothing is performed, if the mount point is already mounted.
+    /// This flag is to force the mount execution.
+    force: bool,
 }
 
 impl fmt::Display for Config {
@@ -65,11 +79,50 @@ impl fmt::Display for Config {
 }
 
 pub async fn create_lamigo_service_unit(c: Config) -> Result<(), ImlAgentError> {
-    create_lamigo_service_unit_internal("/etc/systemd/system", &c).err_into().await
+    if c.force {
+        create_lamigo_service_unit_internal(SYSTEMD_DIR, &c).err_into().await
+    } else {
+        let is_mounted = is_filesystem_mounted(&c.mount_point).await?;
+        if !is_mounted {
+            mount_filesystem(&c).await?;
+        }
+        create_lamigo_service_unit_internal(SYSTEMD_DIR, &c).await?;
+        Ok(())
+    }
 }
 
-pub async fn is_point_mounted(mount_point: String) -> std::io::Result<bool> {
-    Ok(true)
+pub async fn mount_filesystem(c: &Config) -> std::io::Result<()> {
+    // according to http://wiki.lustre.org/Mounting_a_Lustre_File_System_on_Client_Nodes
+    // we need to execute mount command
+    //   mount -t lustre \
+    //     [-o <options> ] \
+    //     <MGS NID>[:<MGS NID>]:/<fsname> \
+    //     /lustre/<fsname>
+    let process: Child = Command::new("mount")
+        .arg("-t")
+        .arg("lustre")
+        .arg(c.lustre_device.clone())
+        .arg(c.mount_point.clone())
+        .spawn()?;
+    let output: Output = process.wait_with_output().await?;
+    match output.status.success() {
+        true => Ok(()),
+        false => Err(io::Error::new(io::ErrorKind::Other,
+                                    format!("Cannot execute command \'mount -t lustre {} {}\'",
+                                            c.lustre_device,
+                                            c.mount_point))),
+    }
+}
+
+pub async fn is_filesystem_mounted(mount_point: &str) -> std::io::Result<bool> {
+    // `mountpoint -q /mnt/something` produces no output,
+    // but signals the caller with the exit code
+    let process: Child = Command::new("mountpoint")
+        .arg("-q")
+        .arg(mount_point)
+        .spawn()?;
+    let output: Output = process.wait_with_output().await?;
+    Ok(output.status.success())
 }
 
 async fn create_lamigo_service_unit_internal<P: AsRef<Path>>(dir: P, c: &Config) -> std::io::Result<()> {
@@ -89,14 +142,20 @@ mod lamigo_tests {
 
     #[tokio::test]
     async fn test_works() {
+        // cargo run --package iml-agent --bin iml-agent -- lamigo
+        // --cold_pool COLD_POOL --hot_pool HOT_POOL
+        // --fs LU_TEST --lustre_device 192.168.0.100@tcp0:/spfs
+        // --mdt MDT000 --min_age 10000 --user nick --mount_point /mnt/lustre
         let config = Config {
             fs: "lu_test".into(),
             mdt: "lustre-MDT0000".into(),
             user: "cl1".into(),
             min_age: 35353,
             mount_point: "/mnt/lustre".into(),
+            lustre_device: "192.168.0.100@tcp0:/spfs".into(),
             hot_pool: "fast_pool".into(),
             cold_pool: "slow_pool".into(),
+            force: true,
         };
 
         let dir = tempdir().expect("could not create tmpdir");
