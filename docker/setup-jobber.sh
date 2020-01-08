dc57d14fa356d4bb1a01af4ed9b8d71877874908
johnsonw <wjohnson@whamcloud.com>
2020-01-07 23:00:40 -0500

    - only run iml-jobber to see if the container will stay running
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- docker/setup-jobber.sh
+++ docker/setup-jobber.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Start the jobber daemon
-/usr/libexec/jobberrunner -u /var/jobber/1000/cmd.sock /home/jobberuser/.jobber -D
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start jobber daemon: $status"
-  exit $status
-fi
+# /usr/libexec/jobberrunner -u /var/jobber/1000/cmd.sock /home/jobberuser/.jobber -D
+# status=$?
+# if [ $status -ne 0 ]; then
+#   echo "Failed to start jobber daemon: $status"
+#   exit $status
+# fi
 
 # Start the iml-jobber http server
 ./iml-jobber -D
@@ -23,14 +23,17 @@ fi
 # Otherwise it loops forever, waking up every 60 seconds
 
 while sleep 60; do
-  ps aux |grep jobberd |grep -q -v grep
-  PROCESS_1_STATUS=$?
+  # ps aux |grep jobberd |grep -q -v grep
+  # PROCESS_1_STATUS=$?
   ps aux |grep iml-jobber |grep -q -v grep
   PROCESS_2_STATUS=$?
   # If the greps above find anything, they exit with 0 status
   # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
-    echo "One of the jobber processes exited."
+  # if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 ]; then
+  #   echo "One of the jobber processes exited."
+  #   exit 1
+  # fi
+  if [ $PROCESS_2_STATUS -ne 0 ]; then
     exit 1
   fi
 done
