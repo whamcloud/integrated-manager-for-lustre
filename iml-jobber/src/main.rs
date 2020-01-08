350ef17f0da151a35194b169819c7e52f3971e15
johnsonw <wjohnson@whamcloud.com>
2020-01-08 14:51:53 -0500

    - Update port
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- iml-jobber/src/main.rs
+++ iml-jobber/src/main.rs
@@ -80,5 +80,5 @@ async fn main() {
             _ => warp::reply::with_status(warp::reply(), status),
         });
 
-    warp::serve(config_route).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(config_route).run(([127, 0, 0, 1], 8892)).await;
 }
