436f9ba2042659feefa223892a92cd5047082a09
johnsonw <wjohnson@whamcloud.com>
2020-01-07 22:36:19 -0500

    - Update jobber command
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- docker/setup-jobber.sh
+++ docker/setup-jobber.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start the jobber daemon
-./jobberd -D
+/usr/libexec/jobberrunner -u /var/jobber/1000/cmd.sock /home/jobberuser/.jobber -D
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start jobber daemon: $status"
