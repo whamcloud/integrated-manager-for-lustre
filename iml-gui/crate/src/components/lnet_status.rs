27761bc9f511f6a9983da9d9184ade26c5f5f81d
Joe Grund <jgrund@whamcloud.io>
2020-01-14 09:19:41 -0500

    extra call not needed
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- iml-gui/crate/src/components/lnet_status.rs
+++ iml-gui/crate/src/components/lnet_status.rs
@@ -1,13 +1,10 @@
-use crate::{components::font_awesome, generated::css_classes::C, MergeAttrs};
+use crate::{components::font_awesome, generated::css_classes::C};
 use iml_wire_types::db::LnetConfigurationRecord;
 use seed::{prelude::*, *};
 
 fn network<T>(color: impl Into<Option<&'static str>>) -> Node<T> {
     if let Some(color) = color.into() {
-        font_awesome(
-            class![C.w_4, C.h_4, C.inline, C.mr_1].merge_attrs(class![color]),
-            "network-wired",
-        )
+        font_awesome(class![C.w_4, C.h_4, C.inline, C.mr_1, color], "network-wired")
     } else {
         empty![]
     }
