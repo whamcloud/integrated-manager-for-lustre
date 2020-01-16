27ebbd81e7ba44988c1d00106e33b313d745cfa2
johnsonw <wjohnson@whamcloud.com>
2020-01-16 14:23:44 -0500

    - move sed statement
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- docker/grafana/setup-grafana.sh
+++ docker/grafana/setup-grafana.sh
@@ -1,2 +1,6 @@
 #! /bin/sh
-/usr/share/grafana/setup-grafana && /run.sh
+
+# Remove the whitelist from the config file
+sed -i '/^whitelist/d' /etc/grafana/grafana.ini \
+  && /usr/share/grafana/setup-grafana \
+  && /run.sh
