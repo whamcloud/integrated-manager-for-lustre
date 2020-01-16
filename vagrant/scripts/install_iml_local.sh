16715db4dfbfcac8fb13f18df272f63b8a846f09
Joe Grund <jgrund@whamcloud.io>
2020-01-16 13:48:31 -0500

    ensure we use latest rpmdevtools
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- vagrant/scripts/install_iml_local.sh
+++ vagrant/scripts/install_iml_local.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 yum copr enable -y managerforlustre/manager-for-lustre-devel
-yum install -y rpmdevtools git ed epel-release python-setuptools gcc openssl-devel
+# Add buildtools repo to get latest rpmdevtools
+yum-config-manager --add-repo https://copr.fedorainfracloud.org/coprs/managerforlustre/buildtools/repo/epel-8/managerforlustre-buildtools-epel-8.repo
+yum install -y rpmdevtools git ed epel-release python-setuptools gcc openssl-devel postgresql-devel
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 cd /integrated-manager-for-lustre
@@ -18,15 +20,15 @@ TS=$(date +%s)
 V=$(rpmspec -q --srpm --queryformat='%{VERSION}' /integrated-manager-for-lustre/rust-iml.spec)
 rpmdev-bumpspec -n $V.$TS rust-iml.spec
 
-V=$(rpmspec -q --srpm --queryformat='%{VERSION}' /integrated-manager-for-lustre/python-iml-manager.spec)
-rpmdev-bumpspec -n $V.$TS python-iml-manager.spec
-
 make all
+
+yum autoremove -y rpmdevtools
 
 rm -rf /tmp/{manager,agent}-rpms
 mkdir -p /tmp/{manager,agent}-rpms
 
-cp /integrated-manager-for-lustre/_topdir/RPMS/x86_64/rust-iml-{action-runner,cli,ostpool,stratagem,agent-comms,mailbox,warp-drive}-*.rpm,python2-iml-manager-* /tmp/manager-rpms/
+cp /integrated-manager-for-lustre/_topdir/RPMS/x86_64/rust-iml-{action-runner,cli,ostpool,stratagem,agent-comms,mailbox,warp-drive}-*.rpm /tmp/manager-rpms/
+cp /integrated-manager-for-lustre/_topdir/RPMS/noarch/python2-iml-manager-*.rpm /tmp/manager-rpms/
 cp /integrated-manager-for-lustre/_topdir/RPMS/x86_64/rust-iml-agent-[0-9]*.rpm /tmp/agent-rpms
 cp /integrated-manager-for-lustre/chroma_support.repo /etc/yum.repos.d/
 
