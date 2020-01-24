1db0d5a375a2d5e71f6eaf845cba854c591be25d
Igor Pashev <pashev.igor@gmail.com>
2020-01-24 18:35:24 +0300

    Use class _mt_1 for font_awesome where needed (#1441)
    Signed-off-by: Igor Pashev <pashev.igor@gmail.com>

--- iml-gui/crate/src/components/activity_indicator.rs
+++ iml-gui/crate/src/components/activity_indicator.rs
@@ -35,7 +35,7 @@ pub fn activity_indicator<T>(activity_health: &ActivityHealth) -> Node<T> {
         class![C.mr_3, C.text_green_500 => activity_health.health == AlertSeverity::INFO,
                             C.text_yellow_500 => activity_health.health == AlertSeverity::WARNING,
                             C.text_red_500 => activity_health.health == AlertSeverity::ERROR],
-        font_awesome(class![C.h_6, C.w_6, C.mr_1, C.inline], "bell"),
+        font_awesome(class![C.h_6, C.w_6, C.mr_1, C.inline, C._mt_1], "bell"),
         sup![activity_health.count.to_string()]
     ]
 }
