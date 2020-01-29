dd3e00aafbb263a99b4232705f5a6dbac6458009
Nathaniel Clark <nclark@whamcloud.com>
2020-01-29 11:46:31 -0500

    Ticket API
    Add tickets to tastypie API
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- chroma_api/urls.py
+++ chroma_api/urls.py
@@ -64,6 +64,7 @@ import chroma_api.pacemaker
 import chroma_api.action
 import chroma_api.stratagem
 import chroma_api.ntp
+import chroma_api.ticket
 
 api.register(chroma_api.host.HostResource())
 api.register(chroma_api.host.ServerProfileResource())
@@ -107,5 +108,6 @@ api.register(chroma_api.action.ActionResource())
 api.register(chroma_api.stratagem.StratagemConfigurationResource())
 api.register(chroma_api.stratagem.RunStratagemResource())
 api.register(chroma_api.ntp.NtpConfigurationResource())
+api.register(chroma_api.ticket.TicketResource())
 
 urlpatterns = [url(r"^", include(api.urls))]
