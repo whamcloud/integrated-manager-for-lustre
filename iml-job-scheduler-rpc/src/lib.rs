359dd229c218600c950502d629fe13ee73872885
Joe Grund <jgrund@whamcloud.io>
2020-02-10 16:13:35 -0500

    Add rust interface to call job_scheduler
    Add a way to call the job_scheduler asynchronously from Rust.
    
    This will allow invocation of any job_scheduler RPCs without
    dropping into python.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

---
+++ iml-job-scheduler-rpc/src/lib.rs
@@ -0,0 +1,130 @@
+// Copyright (c) 2020 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+//! # Iml Job Scheduler RPC client
+//!
+//! This module provides a client to make rpc calls to the `job_scheduler`.
+//!
+//! It should be able to call into the job scheduler and recieve results the same as `job_scheduler_client`.
+
+mod request;
+mod response;
+
+use iml_rabbit::{
+    basic_consume_one, basic_publish, bind_queue, create_channel, declare_transient_exchange,
+    declare_transient_queue, delete_queue, BasicConsumeOptions, Client, ExchangeKind,
+    ImlRabbitError,
+};
+use request::Request;
+use response::Response;
+use std::{collections::HashMap, fmt::Debug};
+use uuid::Uuid;
+
+static JOB_SCHEDULER_RPC: &str = "JobSchedulerRpc";
+static RPC: &str = "rpc";
+
+#[derive(Debug)]
+pub enum ImlJobSchedulerRpcError {
+    ImlRabbitError(ImlRabbitError),
+    RpcError(String),
+    SerdeJsonError(serde_json::error::Error),
+}
+
+impl std::fmt::Display for ImlJobSchedulerRpcError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            ImlJobSchedulerRpcError::ImlRabbitError(ref err) => write!(f, "{}", err),
+            ImlJobSchedulerRpcError::RpcError(ref err) => write!(f, "{}", err),
+            ImlJobSchedulerRpcError::SerdeJsonError(ref err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl std::error::Error for ImlJobSchedulerRpcError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+            ImlJobSchedulerRpcError::ImlRabbitError(ref err) => Some(err),
+            ImlJobSchedulerRpcError::RpcError(_) => None,
+            ImlJobSchedulerRpcError::SerdeJsonError(ref err) => Some(err),
+        }
+    }
+}
+
+impl From<ImlRabbitError> for ImlJobSchedulerRpcError {
+    fn from(err: ImlRabbitError) -> Self {
+        ImlJobSchedulerRpcError::ImlRabbitError(err)
+    }
+}
+
+impl From<String> for ImlJobSchedulerRpcError {
+    fn from(err: String) -> Self {
+        ImlJobSchedulerRpcError::RpcError(err)
+    }
+}
+
+impl From<serde_json::error::Error> for ImlJobSchedulerRpcError {
+    fn from(err: serde_json::error::Error) -> Self {
+        ImlJobSchedulerRpcError::SerdeJsonError(err)
+    }
+}
+
+/// Performs an RPC call to the `job_scheduler`.
+pub async fn call<I: Debug + serde::Serialize, T: serde::de::DeserializeOwned>(
+    client: Client,
+    method: impl Into<String>,
+    args: impl Into<Option<Vec<I>>>,
+    kwargs: impl Into<Option<HashMap<String, String>>>,
+) -> Result<T, ImlJobSchedulerRpcError> {
+    let response_key = format!(
+        "{}.responses_{}",
+        JOB_SCHEDULER_RPC,
+        Uuid::new_v4().to_hyphenated().to_string()
+    );
+
+    let method = method.into();
+
+    let req = Request::new(
+        &method,
+        &response_key,
+        args.into().unwrap_or_default(),
+        kwargs.into().unwrap_or_default(),
+    );
+
+    tracing::debug!("--> to job scheduler {} {:?}", method, req);
+
+    let channel = create_channel(client).await?;
+    let channel = declare_transient_exchange(channel, RPC, ExchangeKind::Topic).await?;
+    let (channel, queue) = declare_transient_queue(channel, &response_key).await?;
+    let channel = bind_queue(channel, RPC, &response_key, &response_key).await?;
+
+    let channel =
+        basic_publish(channel, RPC, format!("{}.requests", JOB_SCHEDULER_RPC), req).await?;
+
+    let (delivery, channel) = basic_consume_one(
+        channel,
+        queue,
+        &response_key,
+        Some(BasicConsumeOptions {
+            no_ack: true,
+            ..BasicConsumeOptions::default()
+        }),
+    )
+    .await?;
+
+    tracing::debug!(
+        "<- from job scheduler {} {:?}",
+        method,
+        std::str::from_utf8(&delivery.data)
+    );
+
+    delete_queue(channel, &response_key).await?;
+
+    let resp: Response<T> = serde_json::from_slice(&delivery.data)?;
+
+    if let Some(e) = resp.exception {
+        return Err(e.into());
+    }
+
+    Ok(resp.result)
+}
