ad498e694a4ed3b5731173e6cd7705c71000adc9
Michael Pankov <work@michaelpankov.com>
2020-02-12 10:35:42 +0300

    Assert for correct fixture
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-services/iml-devices/src/virtual_device.rs
+++ iml-services/iml-devices/src/virtual_device.rs
@@ -412,7 +412,10 @@ mod test {
         let (fqdn, incoming_devices, incoming_device_hosts, db_devices, db_device_hosts) =
             deser_fixture(test_name);
 
-        // TODO: Assert that fqdn == incoming_device_hosts[0].fqdn
+        for ((_, f), dh) in incoming_device_hosts.iter() {
+            assert_eq!(&fqdn, f);
+            assert_eq!(fqdn, dh.fqdn);
+        }
 
         let updates = super::compute_virtual_device_changes(
             &fqdn,
