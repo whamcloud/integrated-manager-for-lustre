4d83a3c64d9cd3deaa59f5b7241d3e45bd2339bd
Nathaniel Clark <nclark@whamcloud.com>
2020-02-28 09:07:34 -0500

    Add repos/profiles during setup
    Setup dir: /etc/iml-docker/setup
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
- set -e
+set -e
 
 echo "Starting db check"
 
@@ -7,6 +7,21 @@ until psql -h 'postgres' -U 'chroma' -c '\q'; do
   echo "Waiting for postgres"
   sleep 5
 done
-  echo "Starting setup"
-  PW=$(cat /run/secrets/iml_pw)
-  chroma-config container-setup admin $PW
+
+echo "Starting setup"
+PW=$(cat /run/secrets/iml_pw)
+chroma-config container-setup admin $PW
+
+echo "Registering Repos/Profiles"
+SETUPDIR=/var/lib/chroma-setup
+DATADIR=/var/lib/chroma
+
+for repo in $(ls $SETUPDIR/ |grep .repo$); do
+    mkdir -p $DATADIR/repo
+    cp $repo $DATADIR/repo/
+    chroma-config repos register $DATADIR/repo/$(basename $repo)
+done
+
+for profile in $(ls $SETUPDIR/ |grep .profile$); do
+    chroma-config profile register $profile
+done
