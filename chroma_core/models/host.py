d4ee0cacf01d71d2d755976c3adbfd765be6fb24
Michael Pankov <work@michaelpankov.com>
2020-03-09 10:21:02 +0300

    Implement first version of models
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- chroma_core/models/host.py
+++ chroma_core/models/host.py
@@ -479,6 +479,34 @@ class VolumeNode(models.Model):
         return "%s:%s" % (self.host, self.path)
 
 
+class Device(models.Model):
+    id = models.CharField(help_text=b"Unique identifer per-device", max_length=255, primary_key=True, serialize=False)
+    size = models.CharField(help_text=b"The size of the device in bytes", max_length=64)
+    usable_for_lustre = models.BooleanField(help_text=b"Is this storage device usable for Lustre")
+    device_type = models.CharField(help_text=b"The type of block or virtual device", max_length=64)
+    parents = django.contrib.postgres.fields.ArrayField(
+        base_field=models.CharField(max_length=255), help_text=b"", size=None
+    )
+    children = django.contrib.postgres.fields.ArrayField(
+        base_field=models.CharField(max_length=255), help_text=b"", size=None
+    )
+    max_depth = models.SmallIntegerField(default=0, help_text=b"")
+
+
+class DeviceHost(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")
+    device_id = models.CharField(help_text=b"Unique identifer per-device", max_length=255)
+    fqdn = models.CharField(help_text=b"The size of the device in bytes", max_length=255)
+    local = models.BooleanField(help_text=b"Is this storage device usable for Lustre")
+    paths = django.contrib.postgres.fields.ArrayField(
+        base_field=models.CharField(max_length=255), help_text=b"", size=None
+    )
+    mount_path = models.CharField(help_text=b"", max_length=255, null=True)
+    fs_type = models.CharField(help_text=b"", max_length=255, null=True)
+    fs_label = models.CharField(help_text=b"", max_length=255, null=True)
+    fs_uuid = models.CharField(help_text=b"", max_length=255, null=True)
+
+
 class RemoveServerConfStep(Step):
     idempotent = True
 
