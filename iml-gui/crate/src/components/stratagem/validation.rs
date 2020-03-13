1d95fa74c87b8bfefb321cd77817b83c23578ab8
johnsonw <wjohnson@whamcloud.com>
2020-03-13 09:55:22 -0400

    - Add validation to scan stratagem modal
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

---
+++ iml-gui/crate/src/components/stratagem/validation.rs
@@ -0,0 +1,52 @@
+// Copyright (c) 2020 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+use crate::{components::duration_picker, environment};
+
+pub fn max_value_validation(ms: u64, unit: duration_picker::Unit) -> Option<String> {
+    if ms > environment::MAX_SAFE_INTEGER {
+        Some(format!(
+            "Duration cannot be greater than {} {}",
+            duration_picker::convert_ms_to_unit(unit, environment::MAX_SAFE_INTEGER),
+            unit
+        ))
+    } else {
+        None
+    }
+}
+
+pub fn validate(
+    scan_duration_picker: &mut duration_picker::Model,
+    report_duration_picker: &mut duration_picker::Model,
+    purge_duration_picker: &mut duration_picker::Model,
+) {
+    scan_duration_picker.validation_message = match scan_duration_picker.value_as_ms() {
+        Some(ms) => {
+            if ms < 1 {
+                Some("Value must be greater than or equal to 1.".into())
+            } else {
+                max_value_validation(ms, scan_duration_picker.unit)
+            }
+        }
+        None => Some("Please fill out this field.".into()),
+    };
+
+    validate_report_and_purge(report_duration_picker, purge_duration_picker);
+}
+
+pub fn validate_report_and_purge(
+    report_duration_picker: &mut duration_picker::Model,
+    purge_duration_picker: &mut duration_picker::Model,
+) {
+    let check = report_duration_picker
+        .value
+        .and_then(|r| purge_duration_picker.value.map(|p| r >= p))
+        .unwrap_or(false);
+
+    if check {
+        report_duration_picker.validation_message = Some("Report duration must be less than Purge duration.".into());
+    } else {
+        report_duration_picker.validation_message = None;
+    }
+}
