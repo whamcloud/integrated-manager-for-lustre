1ee08f99874029decacdb79dc9230aac03528e17
johnsonw <wjohnson@whamcloud.com>
2020-03-20 19:54:17 -0400

    Update system integration test framework
    - Update the test framework to support a docker setup
    
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- iml-system-rpm-tests/tests/vagrant_integration_test.rs
+++ iml-system-rpm-tests/tests/vagrant_integration_test.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-mod vagrant;
+use iml_system_test_utils::{docker, iml, vagrant};
 
 #[tokio::test]
 async fn test_setup() -> Result<(), Box<dyn std::error::Error>> {
---
+++ iml-system-test-utils/src/vagrant.rs
@@ -0,0 +1,346 @@
+// Copyright (c) 2020 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+use crate::{iml, try_command_n_times};
+use iml_cmd::{CheckedCommandExt, CmdError};
+use std::{collections::HashMap, io, str, time::Duration};
+use tokio::{fs, process::Command, time::delay_for};
+
+pub enum NtpServer {
+    HostOnly,
+    Adm,
+}
+
+pub enum FsType {
+    LDISKFS,
+    ZFS,
+}
+
+async fn vagrant() -> Result<Command, io::Error> {
+    let mut x = Command::new("vagrant");
+
+    let path = fs::canonicalize("../vagrant/").await?;
+    println!("Setting current path for vagrant to {:?}", path);
+
+    x.current_dir(path);
+
+    Ok(x)
+}
+
+pub async fn up<'a>() -> Result<Command, io::Error> {
+    let mut x = vagrant().await?;
+
+    x.arg("up");
+
+    Ok(x)
+}
+
+pub async fn destroy<'a>() -> Result<(), io::Error> {
+    let mut x = vagrant().await?;
+
+    x.arg("destroy").arg("-f");
+
+    try_command_n_times(3, &mut x).await
+}
+
+pub async fn halt() -> Result<Command, io::Error> {
+    let mut x = vagrant().await?;
+    x.arg("halt");
+
+    Ok(x)
+}
+
+pub async fn reload() -> Result<Command, io::Error> {
+    let mut x = vagrant().await?;
+    x.arg("reload");
+
+    Ok(x)
+}
+
+async fn snapshot() -> Result<Command, io::Error> {
+    let mut x = vagrant().await?;
+
+    x.arg("snapshot");
+
+    Ok(x)
+}
+
+pub async fn snapshot_save(host: &str, name: &str) -> Result<Command, io::Error> {
+    let mut x = snapshot().await?;
+
+    x.arg("save").arg("-f").arg(host).arg(name);
+
+    Ok(x)
+}
+
+pub async fn snapshot_restore(host: &str, name: &str) -> Result<Command, io::Error> {
+    let mut x = snapshot().await?;
+
+    x.arg("restore").arg(host).arg(name);
+
+    Ok(x)
+}
+
+pub async fn snapshot_delete(host: &str, name: &str) -> Result<Command, io::Error> {
+    let mut x = snapshot().await?;
+
+    x.arg("delete").arg("-f").arg(host).arg(name);
+
+    Ok(x)
+}
+
+pub async fn provision(name: &str) -> Result<Command, io::Error> {
+    let mut x = vagrant().await?;
+
+    x.arg("provision").arg("--provision-with").arg(name);
+
+    Ok(x)
+}
+
+pub async fn run_vm_command(node: &str, cmd: &str) -> Result<Command, io::Error> {
+    let mut x = vagrant().await?;
+
+    x.arg("ssh").arg("-c").arg(&cmd).arg(node);
+
+    Ok(x)
+}
+
+pub async fn setup_bare(
+    hosts: &[&str],
+    config: &ClusterConfig,
+) -> Result<(), Box<dyn std::error::Error>> {
+    up().await?.args(hosts).checked_status().await?;
+
+    provision("yum-update")
+        .await?
+        .args(hosts)
+        .checked_status()
+        .await?;
+
+    configure_ntp_for_host_only_if(&config.storage_servers()).await?;
+
+    halt().await?.args(hosts).checked_status().await?;
+
+    for x in hosts {
+        snapshot_save(x, "bare").await?.checked_status().await?;
+    }
+
+    up().await?.args(hosts).checked_status().await?;
+
+    Ok(())
+}
+
+pub async fn setup_iml_install(
+    hosts: &[&str],
+    config: &ClusterConfig,
+) -> Result<(), Box<dyn std::error::Error>> {
+    setup_bare(hosts, &config).await?;
+
+    provision("install-iml-local")
+        .await?
+        .args(hosts)
+        .checked_status()
+        .await?;
+
+    halt().await?.args(hosts).checked_status().await?;
+
+    for host in hosts {
+        snapshot_save(host, "iml-installed")
+            .await?
+            .checked_status()
+            .await?;
+    }
+
+    up().await?.args(hosts).checked_status().await?;
+    delay_for(Duration::from_secs(30)).await;
+
+    Ok(())
+}
+
+pub async fn setup_deploy_servers(
+    config: &ClusterConfig,
+    profile: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    setup_iml_install(&config.all(), &config).await?;
+
+    run_vm_command(
+        "adm",
+        &format!(
+            "iml server add -h {} -p {}",
+            config.storage_servers().join(","),
+            profile
+        ),
+    )
+    .await?
+    .checked_status()
+    .await?;
+
+    halt().await?.args(config.all()).checked_status().await?;
+
+    for host in config.all() {
+        snapshot_save(host, "iml-deployed")
+            .await?
+            .checked_status()
+            .await?;
+    }
+
+    up().await?.args(config.all()).checked_status().await?;
+
+    Ok(())
+}
+
+pub async fn add_servers<S: std::hash::BuildHasher>(
+    config: &ClusterConfig,
+    server_map: &HashMap<String, &[String], S>,
+) -> Result<(), CmdError> {
+    iml::server_add(&server_map).await?;
+
+    halt()
+        .await?
+        .args(config.iscsi_and_storage_servers())
+        .checked_status()
+        .await?;
+
+    for host in config.iscsi_and_storage_servers() {
+        snapshot_save(host, "servers-deployed")
+            .await?
+            .checked_status()
+            .await?;
+    }
+
+    up().await?
+        .args(&config.iscsi_and_storage_servers())
+        .checked_status()
+        .await
+}
+
+pub async fn setup_deploy_docker_servers<S: std::hash::BuildHasher>(
+    config: &ClusterConfig,
+    server_map: HashMap<String, &[String], S>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    setup_bare(&config.iscsi_and_storage_servers(), &config).await?;
+
+    delay_for(Duration::from_secs(30)).await;
+
+    let storage_servers: Vec<&str> = config.storage_servers();
+    for host in &storage_servers {
+        configure_docker_network(host).await?;
+    }
+
+    add_servers(&config, &server_map).await?;
+
+    Ok(())
+}
+
+pub async fn configure_docker_network(host: &str) -> Result<(), CmdError> {
+    provision("configure-docker-network")
+        .await?
+        .arg(host)
+        .checked_status()
+        .await?;
+
+    Ok(())
+}
+
+pub async fn configure_ntp(hosts: &[&str], ntp_server: NtpServer) -> Result<(), CmdError> {
+    match ntp_server {
+        NtpServer::HostOnly => {
+            provision("configure-ntp-docker")
+                .await?
+                .args(hosts)
+                .checked_status()
+                .await?
+        }
+        NtpServer::Adm => {
+            provision("configure-ntp")
+                .await?
+                .args(hosts)
+                .checked_status()
+                .await?
+        }
+    };
+
+    Ok(())
+}
+
+pub async fn configure_ntp_for_host_only_if(hosts: &[&str]) -> Result<(), CmdError> {
+    configure_ntp(&hosts, NtpServer::HostOnly).await?;
+
+    Ok(())
+}
+
+pub async fn configure_ntp_for_adm(hosts: &[&str]) -> Result<(), CmdError> {
+    configure_ntp(&hosts, NtpServer::Adm).await?;
+
+    Ok(())
+}
+
+async fn create_monitored_ldiskfs() -> Result<(), CmdError> {
+    provision("install-ldiskfs-no-iml,configure-lustre-network,create-ldiskfs-fs,create-ldiskfs-fs2,mount-ldiskfs-fs,mount-ldiskfs-fs2")
+        .await?
+        .checked_status()
+        .await
+}
+
+async fn create_monitored_zfs() -> Result<(), CmdError> {
+    provision("install-zfs-no-iml,configure-lustre-network,create-pools,zfs-params,create-zfs-fs")
+        .await?
+        .checked_status()
+        .await
+}
+
+pub async fn create_fs(fs_type: FsType) -> Result<(), CmdError> {
+    match fs_type {
+        FsType::LDISKFS => create_monitored_ldiskfs().await?,
+        FsType::ZFS => create_monitored_zfs().await?,
+    };
+
+    Ok(())
+}
+
+pub struct ClusterConfig {
+    manager: &'static str,
+    mds: Vec<&'static str>,
+    oss: Vec<&'static str>,
+    clients: Vec<&'static str>,
+    iscsi: &'static str,
+}
+
+impl Default for ClusterConfig {
+    fn default() -> Self {
+        ClusterConfig {
+            manager: "adm",
+            mds: vec!["mds1", "mds2"],
+            oss: vec!["oss1", "oss2"],
+            clients: vec!["c1"],
+            iscsi: "iscsi",
+        }
+    }
+}
+
+impl ClusterConfig {
+    pub fn all(&self) -> Vec<&str> {
+        let mut xs = vec![self.iscsi, self.manager];
+
+        xs.extend(self.storage_servers());
+        xs.extend(&self.clients);
+
+        xs
+    }
+    pub fn storage_servers(&self) -> Vec<&str> {
+        [&self.mds[..], &self.oss[..]].concat()
+    }
+    pub fn iscsi_and_storage_servers(&self) -> Vec<&str> {
+        [&[self.iscsi][..], &self.storage_servers()].concat()
+    }
+    pub fn get_mds_servers(&self) -> Vec<&str> {
+        [&self.mds[..]].concat()
+    }
+    pub fn get_oss_servers(&self) -> Vec<&str> {
+        [&self.oss[..]].concat()
+    }
+    pub fn get_client_servers(&self) -> Vec<&str> {
+        [&self.clients[..]].concat()
+    }
+}
