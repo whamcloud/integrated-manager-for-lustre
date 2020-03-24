a955c369bb435d736459210377aa98dc78e9d132
Joe Grund <jgrund@whamcloud.io>
2020-03-24 13:44:55 -0400

    black fixes
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- chroma_core/models/host.py
+++ chroma_core/models/host.py
@@ -746,9 +746,7 @@ class InstallPackagesStep(Step):
         # they could be things the agent uses/imports or API changes, specifically to kernel_status() below
         self.invoke_agent(host, "restart_agent")
 
-        AgentRpc.await_restart(
-            host.fqdn, timeout=settings.AGENT_RESTART_TIMEOUT, old_session_id=old_session_id
-        )
+        AgentRpc.await_restart(host.fqdn, timeout=settings.AGENT_RESTART_TIMEOUT, old_session_id=old_session_id)
 
         if old_rust_session_id:
             self.invoke_rust_local_action_expect_result(
