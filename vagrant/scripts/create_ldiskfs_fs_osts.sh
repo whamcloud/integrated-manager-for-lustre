ffb475f3c954388093d25b340e91e00c7b06b630
Nathaniel Clark <nclark@whamcloud.com>
2020-04-01 07:51:17 -0400

    Update vagrant/scripts/create_ldiskfs_fs_osts.sh
    Co-Authored-By: Joe Grund <jgrund@whamcloud.io>

--- vagrant/scripts/create_ldiskfs_fs_osts.sh
+++ vagrant/scripts/create_ldiskfs_fs_osts.sh
@@ -6,6 +6,6 @@ IDX=$3
 FSNAME=$4
 
 for x in $(eval echo "{$START..$END}"); do
-     mkfs.lustre --ost --reformat --servicenode=10.73.20.21@tcp --servicenode=10.73.20.22@tcp --index=$IDX --mgsnode=10.73.20.11@tcp:10.73.20.11@tcp --fsname=$FSNAME /dev/mapper/mpath$x
+     mkfs.lustre --ost --reformat --servicenode=10.73.20.21@tcp --servicenode=10.73.20.22@tcp --index=$IDX --mgsnode=10.73.20.11@tcp:10.73.20.12@tcp --fsname=$FSNAME /dev/mapper/mpath$x
      ((IDX++))
 done
