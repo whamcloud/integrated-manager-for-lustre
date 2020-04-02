7215e3459c49beb45c59d08d4af88ed8b31f60ba
Michael Pankov <work@michaelpankov.com>
2020-04-02 15:13:04 +0300

    Expand Devices UI a bit
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-gui/crate/src/page/devices.rs
+++ iml-gui/crate/src/page/devices.rs
@@ -69,12 +69,20 @@ pub fn view(model: &Model) -> impl View<Msg> {
             ]
         } else {
             table::wrapper_view(vec![
-                table::thead_view(vec![table::th_view(plain!["Id"]), th![]]),
-                tbody![model.device.iter().map(|x| tr![table::td_center(vec![a![
-                    class![C.text_blue_500, C.hover__underline],
-                    attrs! {At::Href => Route::Device(RouteId::from(x.record_id)).to_href()},
-                    &x.device.id
-                ],]),])],
+                table::thead_view(vec![
+                    table::th_view(plain!["Id"]),
+                    table::th_view(plain!["Size"]),
+                    table::th_view(plain!["Type"]),
+                ]),
+                tbody![model.device.iter().map(|x| tr![
+                    table::td_center(vec![a![
+                        class![C.text_blue_500, C.hover__underline],
+                        attrs! {At::Href => Route::Device(RouteId::from(x.record_id)).to_href()},
+                        &x.device.id
+                    ],],),
+                    table::td_view(vec![plain![format!("{}", x.device.size.0)],]),
+                    table::td_view(vec![plain![format!("{}", x.device.device_type)],]),
+                ])],
             ])
         }
     ]
