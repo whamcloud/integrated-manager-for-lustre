6f0dc6aff91b2d1dae2335210782911e13207aa7
Nathaniel Clark <nclark@whamcloud.com>
2020-04-03 10:00:47 -0400

    Vagrant: Add ldiskfs-lvm-fs provisioners (#1751)
    * Vagrant: Add ldiskfs-lvm-fs provisioners
    
    Add provisioners to create MDT and MGT on LVMs
    
    To create an ES-like system provision with the following steps:
    create-ldiskfs-lvm-fs
    ha-ldiskfs-lvm-fs-step1
    ha-ldiskfs-lvm-fs-step2
    
    Set lustre version to 2.12.4
    
    Add documentation to README.md
    
    Fixes #1746
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>
    Co-authored-by: Joe Grund <jgrund@whamcloud.io>

---
+++ vagrant/scripts/create_ldiskfs_lvm_oss_ha_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# ARGS: MPATH_LIST IDX VBOX_USER VBOX_PASSWD VBOX_SSHKEY
+#
+LIST=$1
+IDX=$2
+
+USER=$3
+PASSWD=$4
+SSHKEY=$5
+
+pcs cluster auth oss1.local oss2.local -u hacluster -p lustre
+pcs cluster setup --start --name oss-cluster oss1.local oss2.local --enable --token 17000
+pcs stonith create vboxfence fence_vbox ipaddr=10.0.2.2 login=${USER} ${PASSWD:+passwd=${PASSWD}} ${SSHKEY:+identity_file=${SSHKEY}}
+
+for x in $(eval "echo $LIST"); do
+    pcs resource create ost$IDX ocf:lustre:Lustre target=/dev/mapper/mpath$x mountpoint=/mnt/ost$IDX op start timeout=900 op stop timeout=120
+    ((IDX++))
+done
