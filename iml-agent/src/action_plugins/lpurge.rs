a2e469790bc0ceee0b9931801e025546e4fbb281
Nathaniel Clark <nclark@whamcloud.com>
2020-04-03 20:23:28 -0400

    Update lpurge action plugin for ES5.1 code
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- iml-agent/src/action_plugins/lpurge.rs
+++ iml-agent/src/action_plugins/lpurge.rs
@@ -2,20 +2,19 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-use crate::agent_error::ImlAgentError;
+use crate::{agent_error::ImlAgentError, daemon_plugins::postoffice, env};
 use futures::future::TryFutureExt;
-use std::fmt;
-use std::path::Path;
+use std::{collections::HashMap, fmt, path::PathBuf};
 use tokio::fs;
 
 #[derive(serde::Deserialize, structopt::StructOpt, Debug)]
 pub struct Config {
     #[structopt(long)]
-    /// Filesystem name
+    /// Filesystem Name
     fs: String,
     #[structopt(long)]
-    /// Object Storage Target name
-    ost: String,
+    /// Ost Index
+    ost: u32,
     #[structopt(long)]
     /// OST pool name
     pool: String,
@@ -23,6 +22,8 @@ pub struct Config {
     freelo: u8,
     #[structopt(long)]
     freehi: u8,
+    #[structopt(long)]
+    mailbox: String,
 }
 
 impl fmt::Display for Config {
@@ -30,31 +31,54 @@ impl fmt::Display for Config {
         write!(
             f,
             "\
-             device={fs}-{ost}\n\
+             device={fs}-OST{ost:04x}\n\
              dryrun=true\n\
              freehi={freehi}\n\
              freelo={freelo}\n\
-             listen_socket=/run/lpurge-{fs}-{ost}-{pool}.sock\n\
+             listen_socket={socket}\n\
              max_jobs=0\n\
              pool={fs}.{pool}\n\
              ",
             freehi = self.freehi,
             freelo = self.freelo,
-            fs = self.fs,
+            socket = postoffice::socket_name(&self.mailbox),
             ost = self.ost,
+            fs = self.fs,
             pool = self.pool,
         )
     }
 }
 
-pub async fn create_lpurge_conf(c: Config) -> Result<(), ImlAgentError> {
-    write("/etc/lpurge", &c).err_into().await
+fn expand_path_fmt(path_fmt: &str, c: &Config) -> strfmt::Result<String> {
+    let mut vars = HashMap::new();
+    let ost = format!("OST{:04x}", c.ost);
+    let freehi = c.freehi.to_string();
+    let freelo = c.freelo.to_string();
+
+    vars.insert("fs".to_string(), &c.fs);
+    vars.insert("ost".to_string(), &ost);
+    vars.insert("pool".to_string(), &c.pool);
+    vars.insert("freehi".to_string(), &freehi);
+    vars.insert("freelo".to_string(), &freelo);
+    vars.insert("mailbox".to_string(), &c.mailbox);
+    strfmt::strfmt(&path_fmt, &vars)
 }
 
-async fn write<P: AsRef<Path>>(dir: P, c: &Config) -> std::io::Result<()> {
-    fs::create_dir_all(&dir).await?;
+pub async fn create_lpurge_conf(c: Config) -> Result<(), ImlAgentError> {
+    let file = conf_name(&c).await?;
+    write(file, &c).err_into().await
+}
 
-    let file = dir.as_ref().join(format!("{}.conf", c.ost));
+async fn conf_name(c: &Config) -> Result<PathBuf, ImlAgentError> {
+    let path_fmt = env::get_var("LPURGE_CONF_PATH");
+    let path = PathBuf::from(expand_path_fmt(&path_fmt, c)?);
+    Ok(path)
+}
+
+async fn write(file: PathBuf, c: &Config) -> std::io::Result<()> {
+    if let Some(parent) = file.parent() {
+        fs::create_dir_all(&parent).await?;
+    }
     let cnt = format!("{}", c);
     fs::write(file, cnt.as_bytes()).await
 }
@@ -64,27 +88,48 @@ mod lpurge_conf_tests {
     use super::*;
     use insta::assert_display_snapshot;
     use tempfile::tempdir;
+    use std::env;
 
     #[tokio::test]
     async fn works() {
+        // for postoffice::socket_name()
+        env::set_var("SOCK_DIR", "/run/iml");
         let cfg = Config {
             fs: "lima".to_string(),
             pool: "santiago".to_string(),
-            ost: "montevideo".to_string(),
+            ost: 16,
             freehi: 123,
             freelo: 60,
+            mailbox: "foobar".to_string(),
         };
 
-        let dir = tempdir().expect("could not create tmpdir");
-        let expected_file = dir.path().join("montevideo.conf");
-
-        write(&dir, &cfg).await.expect("could not write");
+        let dir = tempdir().expect("could not create temp file");
+        let file = dir.path().join("config");
+        let file2 = file.clone();
+        
+        write(file, &cfg).await.expect("could not write");
 
         let cnt = String::from_utf8(
-            std::fs::read(&expected_file).expect(expected_file.to_str().unwrap()),
+            std::fs::read(&file2).expect("could not read file"),
         )
         .unwrap();
 
         assert_display_snapshot!(cnt);
+    }
+
+    #[tokio::test]
+    async fn config_name() {
+        env::set_var("LPURGE_CONF_PATH", "/etc/lpurge/{fs}/{ost}-{pool}.conf");
+        let cfg = Config {
+            fs: "lima".to_string(),
+            pool: "santiago".to_string(),
+            ost: 16,
+            freehi: 123,
+            freelo: 60,
+            mailbox: "foobar".to_string(),
+        };
+        let file = conf_name(&cfg).await.expect("name could not be created");
+
+        assert_eq!(file, PathBuf::from("/etc/lpurge/lima/OST0010-santiago.conf"));
     }
 }
