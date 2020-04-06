60c2f6bf12f40333237933b65e1a5138b77e59f5
Nathaniel Clark <nclark@whamcloud.com>
2020-04-06 11:28:43 -0400

    Make lustre version configurable and singular (#1765)
    Set ENV override of Lustre version
    Set lustre version in single location
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- vagrant/scripts/install_ldiskfs_no_iml.sh
+++ vagrant/scripts/install_ldiskfs_no_iml.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-yum-config-manager --add-repo=https://downloads.whamcloud.com/public/lustre/lustre-2.12.4/el7/patchless-ldiskfs-server/
+LUSTRE=$1
+
+yum-config-manager --add-repo=https://downloads.whamcloud.com/public/lustre/lustre-$LUSTRE/el7/patchless-ldiskfs-server/
 yum-config-manager --add-repo=https://downloads.whamcloud.com/public/e2fsprogs/latest/el7/
 yum install -y --nogpgcheck lustre kmod-lustre-osd-ldiskfs
