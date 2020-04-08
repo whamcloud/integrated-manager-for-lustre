4b921f156151618b44ba7030c79dfc9d70182099
johnsonw <wjohnson@whamcloud.com>
2020-04-08 15:33:49 -0400

    - RPM test updates
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- iml-system-rpm-tests/tests/vagrant_integration_test.rs
+++ iml-system-rpm-tests/tests/vagrant_integration_test.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-use iml_system_test_utils::{get_local_server_names, iml, vagrant};
+use iml_system_test_utils::{get_local_server_names, iml, vagrant, SetupConfig};
 use std::{
     collections::{hash_map::RandomState, HashMap},
     time::Duration,
@@ -17,15 +17,13 @@ async fn setup() -> Result<(), Box<dyn std::error::Error>> {
 
 async fn run_fs_test<S: std::hash::BuildHasher>(
     config: &vagrant::ClusterConfig,
+    setup_config: &SetupConfig,
     server_map: HashMap<String, &[String], S>,
     fs_type: vagrant::FsType,
 ) -> Result<(), Box<dyn std::error::Error>> {
     setup().await?;
 
-    vagrant::setup_deploy_servers(
-        &config, 
-        server_map,
-    ).await?;
+    vagrant::setup_deploy_servers(&config, &setup_config, server_map).await?;
 
     vagrant::create_fs(fs_type).await?;
 
@@ -37,15 +35,72 @@ async fn run_fs_test<S: std::hash::BuildHasher>(
 }
 
 #[tokio::test]
-async fn test_setup() -> Result<(), Box<dyn std::error::Error>> {
+async fn test_ldiskfs_setup() -> Result<(), Box<dyn std::error::Error>> {
     let config = vagrant::ClusterConfig::default();
     let server_names = get_local_server_names(&config.storage_servers());
 
     run_fs_test(
         &config,
+        &SetupConfig {
+            use_stratagem: false,
+            branding: iml_wire_types::Branding::Whamcloud,
+        },
         vec![("base_monitored".into(), &server_names[..])]
             .into_iter()
             .collect::<HashMap<String, &[String], RandomState>>(),
+        vagrant::FsType::LDISKFS,
+    )
+    .await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_zfs_setup() -> Result<(), Box<dyn std::error::Error>> {
+    let config = vagrant::ClusterConfig::default();
+    let server_names = get_local_server_names(&config.storage_servers());
+
+    run_fs_test(
+        &config,
+        &SetupConfig {
+            use_stratagem: false,
+            branding: iml_wire_types::Branding::Whamcloud,
+        },
+        vec![("base_monitored".into(), &server_names[..])]
+            .into_iter()
+            .collect::<HashMap<String, &[String], RandomState>>(),
+        vagrant::FsType::ZFS,
+    )
+    .await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_stratagem_setup() -> Result<(), Box<dyn std::error::Error>> {
+    let config = vagrant::ClusterConfig::default();
+    run_fs_test(
+        &config,
+        &SetupConfig {
+            use_stratagem: true,
+            branding: iml_wire_types::Branding::Whamcloud,
+        },
+        vec![
+            (
+                "stratagem_server".into(),
+                &get_local_server_names(&config.get_mds_servers())[..],
+            ),
+            (
+                "base_monitored".into(),
+                &get_local_server_names(&config.get_oss_servers())[..],
+            ),
+            (
+                "stratagem_client".into(),
+                &get_local_server_names(&config.get_client_servers())[..],
+            ),
+        ]
+        .into_iter()
+        .collect::<HashMap<String, &[String], RandomState>>(),
         vagrant::FsType::LDISKFS,
     )
     .await?;
