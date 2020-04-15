bb3005ac408b2522eab31e649cd1e1afb8276ea3
Michael Pankov <work@michaelpankov.com>
2020-04-15 16:21:17 +0300

    Make fqdn unique
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- chroma_core/models/devices.py
+++ chroma_core/models/devices.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.postgres import fields
 
 class Device(models.Model):
-    fqdn = models.CharField(max_length=255)
+    fqdn = models.CharField(max_length=255, unique=True)
     device = fields.JSONField()
