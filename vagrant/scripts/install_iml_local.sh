f4757a2de6842ea42fab312940040e33d8e34019
Nathaniel Clark <nclark@whamcloud.com>
2020-05-06 09:05:24 -0400

    Add "make local" target to create dated releases (#1867)
    * Add "make local" target to create dated releases
    
    This will set the rpm dist to 10.UNIX_EPOCH
    
    Use this build target in vargrant local build
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>
    
    * Ensure rpm-build is present
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- vagrant/scripts/install_iml_local.sh
+++ vagrant/scripts/install_iml_local.sh
@@ -3,10 +3,8 @@
 set -ex
 
 yum copr enable -y managerforlustre/manager-for-lustre-devel
-# Get latest rpmdevtools
-yum install -y https://copr-be.cloud.fedoraproject.org/results/managerforlustre/buildtools/epel-8-x86_64/01152137-rpmdevtools/rpmdevtools-8.10-7.el8.noarch.rpm
 yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-yum install -y rpmdevtools git ed epel-release python-setuptools gcc openssl-devel postgresql96-devel
+yum install -y rpm-build git ed epel-release python-setuptools gcc openssl-devel postgresql96-devel
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 rustup update
@@ -14,21 +12,7 @@ rustc --version
 cargo --version
 cd /integrated-manager-for-lustre
 
-# Bump the release number. This should ensure we get picked 
-#even if copr-devel has something newer
-rpmdev-bumpspec rust-iml.spec
-rpmdev-bumpspec python-iml-manager.spec
-
-# Use current timestamp to ensure running this again
-# causes a re-install
-TS=$(date +%s)
-
-V=$(rpmspec -q --srpm --queryformat='%{VERSION}' /integrated-manager-for-lustre/rust-iml.spec)
-rpmdev-bumpspec -n $V.$TS rust-iml.spec
-
-make all
-
-yum autoremove -y rpmdevtools
+make local
 
 rm -rf /tmp/{manager,agent}-rpms
 mkdir -p /tmp/{manager,agent}-rpms
