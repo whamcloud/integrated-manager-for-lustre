f5761d0b3623db1f3707eb88918571e07ca0bf2a
Joe Grund <jgrund@whamcloud.io>
2020-05-06 13:27:14 -0400

    Remove outdated testing
    Remove outdated testing that is no longer being run.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- tests/integration/utils/test_filesystems/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2020 DDN. All rights reserved.
-# Use of this source code is governed by a MIT-style
-# license that can be found in the LICENSE file.
-
-
-import test_filesystem_ldiskfs
-import test_filesystem_zfs
