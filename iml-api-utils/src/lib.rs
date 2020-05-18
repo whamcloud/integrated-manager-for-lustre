3b4fbdf51163e4f5b83b5103d1ff1eaa8ee10d53
Nick Linker <nlinker@gmail.com>
2020-05-18 20:55:15 +0600

    Share dependency_tree with IML CLI too
    Signed-off-by: Nick Linker <nlinker@gmail.com>

--- iml-api-utils/src/lib.rs
+++ iml-api-utils/src/lib.rs
@@ -1,6 +1,7 @@
 // Copyright (c) 2020 DDN. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
+pub mod dependency_tree;
 
 use lazy_static::lazy_static;
 use regex::Regex;
