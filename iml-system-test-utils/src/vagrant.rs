d95b2ce57cc850ba6843febdef6b26cfc7ac0974
johnsonw <wjohnson@whamcloud.com>
2020-05-19 16:21:54 -0400

    - Add `destroy_list` to ClusterConfig struct.

--- iml-system-test-utils/src/vagrant.rs
+++ iml-system-test-utils/src/vagrant.rs
@@ -59,8 +59,7 @@ pub async fn up<'a>() -> Result<Command, CmdError> {
 }
 
 pub async fn destroy<'a>(config: &ClusterConfig) -> Result<(), CmdError> {
-    let mut nodes = config.all();
-    nodes.reverse();
+    let nodes = config.destroy_list();
 
     for node in &nodes {
         let mut suspend_cmd = suspend().await?;
@@ -613,6 +612,12 @@ impl ClusterConfig {
         xs.extend(&self.clients);
 
         xs
+    }
+    pub fn destroy_list(&self) -> Vec<&str> {
+        let mut to_destroy = self.all();
+        to_destroy.reverse();
+
+        to_destroy
     }
     pub fn all_but_adm(&self) -> Vec<&str> {
         let mut xs = vec![self.iscsi];
