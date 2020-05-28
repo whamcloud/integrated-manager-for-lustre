e0215bdf3e42d9bb119a94b4c4dabbb8224b4131
Nathaniel Clark <nclark@whamcloud.com>
2020-05-28 13:57:45 -0400

    Address reviews
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- iml-action-client/src/lib.rs
+++ iml-action-client/src/lib.rs
@@ -2,53 +2,113 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-use futures::{future, StreamExt, TryStreamExt};
+use futures::{
+    channel::oneshot,
+    future::{self, Either},
+    Future,
+    FutureExt,
+    TryFutureExt,
+};
 use iml_action_runner::ActionType;
-use iml_fs::stream_file_lines;
 use iml_manager_env::{get_action_runner_connect, get_action_runner_socket};
-use iml_wire_types::{Action, ActionId, ActionName, AgentResult, Fqdn};
+use iml_wire_types::{Action, ActionId, ActionName, Fqdn};
+use lazy_static::lazy_static;
+use std::io::BufRead;
+use thiserror::Error;
 use uuid::Uuid;
 
-async fn running_in_docker() -> bool {
-    stream_file_lines("/proc/self/cgroup")
-        .boxed()
-        .try_filter(|l| future::ready(l.split(':').nth(1) == Some("docker")))
-        .try_next()
-        .await
-        .unwrap_or(None)
-        .is_some()
+lazy_static! {
+    static ref RUNNING_IN_DOCKER: bool = {
+        match std::fs::File::open("/proc/self/cgroup") {
+            Err(_) => false,
+            Ok(file) => {
+                let reader = std::io::BufReader::new(file);
+                reader
+                    .lines()
+                    .filter(|l| {
+                        l.as_ref().unwrap_or(&"".to_string()).split(':').nth(1) == Some("docker")
+                    })
+                    .next()
+                    .is_some()
+            }
+        }
+    };
 }
 
-pub async fn invoke_rust_agent(
-    host: impl Into<Fqdn>,
+#[derive(Error, Debug)]
+pub enum ImlActionClientError {
+    #[error(transparent)]
+    ReqwestError(#[from] reqwest::Error),
+    CancelledRequest,
+}
+
+impl std::fmt::Display for ImlActionClientError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            ImlActionClientError::ReqwestError(ref err) => write!(f, "{}", err),
+            ImlActionClientError::CancelledRequest => write!(f, "Request Cancelled"),
+        }
+    }
+}
+
+pub fn invoke_rust_agent(
+    host: impl Into<Fqdn> + Clone,
     command: impl Into<ActionName>,
-    args: serde_json::Value,
-) -> AgentResult {
+    args: impl serde::Serialize,
+) -> (oneshot::Sender<()>, impl Future<Output = Result<serde_json::Value, ImlActionClientError>>) {
     let request_id = Uuid::new_v4().to_hyphenated().to_string();
-    let conn = if running_in_docker().await {
+    let conn = if *RUNNING_IN_DOCKER {
         get_action_runner_connect()
     } else {
         get_action_runner_socket()
     };
+    let conn2 = conn.clone();
 
     let action = ActionType::Remote((
-        host.into(),
+        host.clone().into(),
         Action::ActionStart {
             action: command.into(),
-            args,
-            id: ActionId(request_id),
+            args: serde_json::json!(args),
+            id: ActionId(request_id.clone()),
         },
     ));
 
     let client = reqwest::Client::new();
 
-    client
-        .post(&conn)
-        .json(&action)
-        .send()
-        .await
-        .map_err(|e| format!("Send Failed: {:?}", e))?
-        .json()
-        .await
-        .map_err(|e| format!("Jsonify Failed: {:?}", e))
+    let (p, c) = oneshot::channel::<()>();
+
+    let post = async move {
+        client
+            .post(&conn)
+            .json(&action)
+            .send()
+            .await?
+            .json()
+            .err_into()
+            .await
+    }.boxed();
+
+    let fut = future::select(c, post)
+        .then(|either| async move {
+            if let Either::Right((_,_)) = either {
+                let cancel = ActionType::Remote((
+                    host.into(),
+                    Action::ActionCancel {
+                        id: ActionId(request_id),
+                    }
+                ));
+                let client = reqwest::Client::new();
+                let _rc = client
+                    .post(&conn2)
+                    .json(&cancel)
+                    .send()
+                    .await;
+            }
+            either
+        }).map(|either| match either {
+            Either::Left((_, b)) => { drop(b); Err(ImlActionClientError::CancelledRequest) }
+            Either::Right((a, _)) => a,
+        });
+
+    (p, fut)
 }
