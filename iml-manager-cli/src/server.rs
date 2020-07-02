481fdc577aa6854fb0aab5d24eb3112d0a7c360e
Igor Pashev <pashev.igor@gmail.com>
2020-07-02 20:10:50 +0200

    Make listing servers default command
    That is:
    
        iml server [list]
    
    Signed-off-by: Igor Pashev <pashev.igor@gmail.com>

--- iml-manager-cli/src/server.rs
+++ iml-manager-cli/src/server.rs
@@ -56,7 +56,7 @@ pub struct AddHosts {
 
 #[derive(Debug, StructOpt)]
 pub enum ServerCommand {
-    /// List all configured storage servers
+    /// List all configured storage servers (default)
     #[structopt(name = "list")]
     List {
         /// Set the display type
@@ -241,14 +241,18 @@ fn get_confirm() -> bool {
         .unwrap_or(false)
 }
 
-fn list_server(hosts: Vec<Host>, display_type: DisplayType) {
+async fn list_server(display_type: DisplayType) -> Result<(), ImlManagerCliError> {
+    let hosts: ApiList<Host> = wrap_fut("Fetching hosts...", get_hosts()).await?;
+
     let term = Term::stdout();
 
     tracing::debug!("Hosts: {:?}", hosts);
 
-    let x = hosts.into_display_type(display_type);
+    let x = hosts.objects.into_display_type(display_type);
 
     term.write_line(&x).unwrap();
+
+    Ok(())
 }
 
 fn get_profile_by_name<'a>(xs: &'a [ServerProfile], name: &str) -> Option<&'a ServerProfile> {
@@ -535,14 +539,12 @@ async fn add_server(config: AddHosts) -> Result<(), ImlManagerCliError> {
     Ok(())
 }
 
-pub async fn server_cli(command: ServerCommand) -> Result<(), ImlManagerCliError> {
+pub async fn server_cli(command: Option<ServerCommand>) -> Result<(), ImlManagerCliError> {
     match command {
-        ServerCommand::List { display_type } => {
-            let hosts: ApiList<Host> = wrap_fut("Fetching hosts...", get_hosts()).await?;
-            list_server(hosts.objects, display_type);
-        }
-        ServerCommand::Add(config) => add_server(config).await?,
-        ServerCommand::ForceRemove { hosts } => {
+        None => list_server(DisplayType::Tabular).await?,
+        Some(ServerCommand::List { display_type }) => list_server(display_type).await?,
+        Some(ServerCommand::Add(config)) => add_server(config).await?,
+        Some(ServerCommand::ForceRemove { hosts }) => {
             let remove_hosts = parse_hosts(&hosts)?;
 
             tracing::debug!("Parsed hosts {:?}", remove_hosts);
@@ -612,7 +614,7 @@ pub async fn server_cli(command: ServerCommand) -> Result<(), ImlManagerCliError
 
             wait_for_cmds_success(&[command]).await?;
         }
-        ServerCommand::Remove { hosts } => {
+        Some(ServerCommand::Remove { hosts }) => {
             let remove_hosts = parse_hosts(&hosts)?;
 
             tracing::debug!("Parsed hosts {:?}", remove_hosts);
@@ -684,7 +686,7 @@ pub async fn server_cli(command: ServerCommand) -> Result<(), ImlManagerCliError
 
             wait_for_cmds_success(&commands).await?;
         }
-        ServerCommand::Profile { cmd } => profile::cmd(cmd).await?,
+        Some(ServerCommand::Profile { cmd }) => profile::cmd(cmd).await?,
     };
 
     Ok(())
