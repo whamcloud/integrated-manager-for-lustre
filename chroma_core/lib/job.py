2a65ab410445e1cc6dc38dedddfa9814713267a2
johnsonw <wjohnson@whamcloud.com>
2020-07-14 09:19:56 -0400

    Fix formatting
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- chroma_core/lib/job.py
+++ chroma_core/lib/job.py
@@ -221,7 +221,11 @@ class Step(object):
         try:
             result = json.loads(result)
         except ValueError as e:
-            raise LocalActionException(command, args, "Error parsing json: {}; result: {}; command: {}; args: {}".format(e, result, command, args))
+            raise LocalActionException(
+                command,
+                args,
+                "Error parsing json: {}; result: {}; command: {}; args: {}".format(e, result, command, args),
+            )
 
         if "Err" in result:
             self.log(json.dumps(result["Err"], indent=2))
@@ -249,7 +253,12 @@ class Step(object):
         try:
             result = json.loads(result)
         except ValueError as e:
-            raise AgentException(host, command, args, "Error parsing json: {}; result: {}; command: {}; args: {}".format(e, result, command, args))
+            raise AgentException(
+                host,
+                command,
+                args,
+                "Error parsing json: {}; result: {}; command: {}; args: {}".format(e, result, command, args),
+            )
 
         if "Err" in result:
             self.log(json.dumps(result["Err"], indent=2))
