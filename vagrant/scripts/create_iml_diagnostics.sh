eefc9ecae5714ecbf811d6f9beeac8d80edf65e4
Will Johnson <wjohnson@whamcloud.com>
2020-07-15 09:43:28 -0400

    Update sos reports (#2069)
    * Update sos reports
    
    SOS reports use a docker plugin but this plugin does not include docker
    logs by default. Ensure that all portions of docker are included.
    
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>
    
    * remove -n docker
    
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>
    
    * Update params
    
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- vagrant/scripts/create_iml_diagnostics.sh
+++ vagrant/scripts/create_iml_diagnostics.sh
@@ -5,6 +5,6 @@ PREFIX=$1
 # Clean out any existing sos reports on this node
 yum install -y sos
 rm -f /var/tmp/*sosreport*
-sosreport --batch
+sosreport --batch -k docker.all=on -k docker.logs=on
 cd /var/tmp || exit 1
 for f in *sosreport*.tar.xz; do mv "$f" "$PREFIX"_"$f"; done
