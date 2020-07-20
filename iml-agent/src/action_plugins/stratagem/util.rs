5906686c30fe0def9ae385d52576132e143fa620
Nathaniel Clark <nclark@whamcloud.com>
2020-07-20 10:29:53 -0400

    Add Mirror actions for HPv2
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

---
+++ iml-agent/src/action_plugins/stratagem/util.rs
@@ -0,0 +1,15 @@
+// Copyright (c) 2020 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+use crate::agent_error::ImlAgentError;
+use futures::future::TryFutureExt;
+use liblustreapi::LlapiFid;
+use tokio::task::spawn_blocking;
+
+pub(crate) async fn search_rootpath(device: String) -> Result<LlapiFid, ImlAgentError> {
+    spawn_blocking(move || LlapiFid::create(&device).map_err(ImlAgentError::from))
+        .err_into()
+        .await
+        .and_then(std::convert::identity)
+}
