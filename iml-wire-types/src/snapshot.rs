2ef3f4381e6d9827d299044af91fde9697d9af7b
Igor Pashev <pashev.igor@gmail.com>
2020-07-27 21:51:36 +0200

    Implement creating and destroying snapshots
        [root@mds1 ~]# iml-agent snapshot create zfsmo foo -c 'hello world'
        [root@mds1 ~]# lctl snapshot_list --fsname zfsmo
    
        filesystem_name: zfsmo
        snapshot_name: foo
        modify_time: Mon Jul 27 19:55:18 2020
        create_time: Mon Jul 27 19:55:18 2020
        comment: hello world
        snapshot_fsname: 6c80c077
        status: not mount
    
        filesystem_name: zfsmo
        snapshot_name: test2
        snapshot_fsname: 21fd4967
        modify_time: Mon Jul 27 19:24:14 2020
        create_time: Mon Jul 27 19:24:14 2020
        status: not mount
        [root@mds1 ~]# iml-agent snapshot destroy zfsmo test2
        [root@mds1 ~]# lctl snapshot_list --fsname zfsmo
    
        filesystem_name: zfsmo
        snapshot_name: foo
        modify_time: Mon Jul 27 19:55:18 2020
        create_time: Mon Jul 27 19:55:18 2020
        comment: hello world
        snapshot_fsname: 6c80c077
        status: not mount
    
    Signed-off-by: Igor Pashev <pashev.igor@gmail.com>

---
+++ iml-wire-types/src/snapshot.rs
@@ -0,0 +1,38 @@
+// Copyright (c) 2020 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+//! Data structures for communicating with the agent regarding lustre snapshots.
+
+#[cfg(feature = "cli")]
+use structopt::StructOpt;
+
+#[derive(serde::Deserialize, Debug)]
+#[cfg(feature = "cli")]
+#[derive(StructOpt)]
+/// Ask agent to create a snapshot
+pub struct Create {
+    /// Filesystem name
+    pub fsname: String,
+    /// Snapshot name
+    pub name: String,
+
+    /// Optional comment for the snapshot
+    #[structopt(short = "c", long = "comment")]
+    pub comment: Option<String>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+#[cfg(feature = "cli")]
+#[derive(StructOpt)]
+/// Ask agent to destroy the snapshot
+pub struct Destroy {
+    /// Filesystem name
+    pub fsname: String,
+    /// Name of the snapshot to destroy
+    pub name: String,
+
+    /// Destroy the snapshot by force
+    #[structopt(short = "f", long = "--force")]
+    pub force: bool,
+}
