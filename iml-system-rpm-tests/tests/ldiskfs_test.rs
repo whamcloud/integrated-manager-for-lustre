d5d3f5b51822f3090f5d6bc9dcf6323162650e89
Nathaniel Clark <nclark@whamcloud.com>
2020-07-28 17:09:36 -0400

    Tests: Add clients to ldiskfs zfs profile list
    Without profiles for clients, they nothing is installed, and no
    Bare snapshot is taken of them.  Any subsequent test that requires
    a Bare snapshot will be without clients.
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- iml-system-rpm-tests/tests/ldiskfs_test.rs
+++ iml-system-rpm-tests/tests/ldiskfs_test.rs
@@ -10,7 +10,7 @@ async fn test_ldiskfs_setup() -> Result<(), TestError> {
     let config: Config = Config::default();
 
     let config = Config {
-        profile_map: vec![("base_monitored".into(), config.storage_servers())],
+        profile_map: vec![("base_monitored".into(), config.storage_servers()), ("base_client".into(), config.client_servers())],
         ..config
     };
 
