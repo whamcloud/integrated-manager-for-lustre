b0f89e8e0116204cc9dc70e9e513f6d01602f939
Michael Pankov <work@michaelpankov.com>
2020-07-28 12:16:13 +0300

    Implement core of unmounting snapshot
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-agent/src/action_plugins/snapshot.rs
+++ iml-agent/src/action_plugins/snapshot.rs
@@ -8,9 +8,19 @@ use iml_cmd::{CheckedCommandExt, Command};
 pub async fn mount(filesystem_name: String, snapshot_name: String) -> Result<(), ImlAgentError> {
     Command::new("lctl")
         .arg("snapshot_mount")
-        .arg("-F")
+        .arg("--fsname")
         .arg(filesystem_name)
-        .arg("-n")
+        .arg("--name")
+        .arg(snapshot_name)
+        .checked_status()
+        .await
+        .map_err(|e| e.into())
+}
+
+pub async fn unmount(snapshot_name: String) -> Result<(), ImlAgentError> {
+    Command::new("lctl")
+        .arg("snapshot_unmount")
+        .arg("--name")
         .arg(snapshot_name)
         .checked_status()
         .await
