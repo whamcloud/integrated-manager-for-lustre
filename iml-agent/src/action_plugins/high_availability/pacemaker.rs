a0d814e648296e35ccfc95c79382db0d2fede207
Nathaniel Clark <nclark@whamcloud.com>
2020-08-07 16:21:33 -0400

    agent: Add start/stop resource to rust agent
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

---
+++ iml-agent/src/action_plugins/high_availability/pacemaker.rs
@@ -0,0 +1,26 @@
+// Copyright (c) 2020 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+/// The order of Role is order of preference for returning in a cloned
+/// pacemaker resource.
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
+pub enum Role {
+    Error(String),
+    Started,
+    Starting,
+    Stopping,
+    Stopped,
+}
+
+impl From<&str> for Role {
+    fn from(role: &str) -> Self {
+        match role {
+            "Started" => Role::Started,
+            "Starting" => Role::Starting,
+            "Stopping" => Role::Stopping,
+            "Stopped" => Role::Stopped,
+            other => Role::Error(other.to_string()),
+        }
+    }
+}
