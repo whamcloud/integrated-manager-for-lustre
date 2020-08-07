440ee673467d5c1473420640af08366bb5b987f4
Nathaniel Clark <nclark@whamcloud.com>
2020-08-07 16:47:47 -0400

    Quote arguments passed into docker iml
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- docker/iml-cli-proxy.sh
+++ docker/iml-cli-proxy.sh
@@ -3,4 +3,4 @@
 trailer=$(docker service ps -f 'name=iml_iml-manager-cli.1' iml_iml-manager-cli -q --no-trunc | head -n1)
 TTYOPT=$(tty -s && echo "-t")
 
-docker exec -i $TTYOPT iml_iml-manager-cli.1.$trailer iml ${@:1}
+docker exec -i $TTYOPT iml_iml-manager-cli.1.$trailer iml $(printf "'%s' " "${@:1}")
