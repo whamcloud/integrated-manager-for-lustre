ff8d146fd82a190fb86bf3ab2fa5f408dc5ce071
Michael Pankov <work@michaelpankov.com>
2020-08-20 16:29:42 +0300

    [WIP]
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-api/src/snapshot.rs
+++ iml-api/src/snapshot.rs
@@ -2,42 +2,60 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-use crate::error::ImlApiError;
-use futures::{future::try_join, TryFutureExt};
+use crate::error;
 use iml_action_client::invoke_rust_agent;
-use iml_job_scheduler_rpc::{available_jobs, available_transitions, Job, Transition};
+use iml_postgres::{sqlx, PgPool};
 use iml_rabbit::Connection;
-use iml_wire_types::{snapshot, ApiList, AvailableAction, CompositeId};
-use std::convert::TryFrom;
+use iml_wire_types::snapshot;
 use warp::{reply::Json, Filter};
 
-type CompositeIds = Vec<CompositeId>;
+async fn get_snapshots_internal(
+    args: snapshot::List,
+    conn: Connection,
+    pool: PgPool,
+) -> Result<(), error::ImlApiError> {
+    drop(conn);
 
-fn composite_ids() -> impl Filter<Extract = (CompositeIds,), Error = warp::Rejection> + Clone + Send
-{
-    warp::query().map(|q: Vec<(String, String)>| {
-        q.into_iter()
-            .filter(|(x, _)| x == "composite_ids")
-            .filter_map(|(_, y)| CompositeId::try_from(y).ok())
-            .collect()
-    })
+    let mgs_id = sqlx::query!(
+        r#"
+        select mgs_id from chroma_core_managedfilesystem where name=$1
+        "#,
+        args.fsname
+    )
+    .fetch_one(&pool)
+    .await?
+    .mgs_id;
+
+    let mgs = sqlx::query!(
+        r#"
+        select * from chroma_core_managedtarget where id=$1
+        "#,
+        mgs_id
+    )
+    .fetch_one(&pool)
+    .await?;
+
+    Ok(())
 }
 
 async fn get_snapshots(
     args: snapshot::List,
     conn: Connection,
+    pool: PgPool,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    drop(conn);
+    let snapshots = get_snapshots_internal(args, conn, pool).await?;
 
-    Ok(warp::reply::json(&()))
+    Ok(warp::reply::json(&snapshots))
 }
 
 pub(crate) fn endpoint(
     client_filter: impl Filter<Extract = (Connection,), Error = warp::Rejection> + Clone + Send,
+    pool_filter: impl Filter<Extract = (PgPool,), Error = std::convert::Infallible> + Clone + Send,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path!("snapshot" / "list")
         .and(warp::query())
         .and(warp::get())
         .and(client_filter)
+        .and(pool_filter)
         .and_then(get_snapshots)
 }
