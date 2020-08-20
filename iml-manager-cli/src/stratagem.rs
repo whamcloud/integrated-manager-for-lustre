f724c14f445834ce5b8f72ee4e28fffe5e6098ef
Ben Evans <beevans@whamcloud.com>
2020-08-20 13:58:34 +0000

    Fix cargo fmt errors
    Signed-off-by: Ben Evans <beevans@whamcloud.com>

--- iml-manager-cli/src/stratagem.rs
+++ iml-manager-cli/src/stratagem.rs
@@ -208,25 +208,22 @@ pub async fn stratagem_cli(command: StratagemCommand) -> Result<(), ImlManagerCl
 
             display_cmd_state(&command);
         }
-	StratagemCommand::Filesync(data) => {
-	    let r = post("run_stratagem", data).await?;
+        StratagemCommand::Filesync(data) => {
+            let r = post("run_stratagem", data).await?;
 
-	    tracing::error!("fs {:?}", r);
-
-	    let CmdWrapper { command } = handle_cmd_resp(r).await?;
-
-	    tracing::error!("fs {:?}", command);
+	    tracing::error!("run_filesync: {:?}", r);
 	    
-	    let stop_spinner = start_spinner(&command.message);
+            let CmdWrapper { command } = handle_cmd_resp(r).await?;
 
-	    let command = wait_for_cmd(command).await?;
-
-	    tracing::error!("fs {:?}", command);
+	    tracing::error!("run_filesync: {:?}", command);
 	    
-	    stop_spinner(None);
+            let stop_spinner = start_spinner(&command.message);
+            let command = wait_for_cmd(command).await?;
 
-	    display_cmd_state(&command);
-	}
+	    tracing::error!("wait_done: {:?}", command);
+            stop_spinner(None);
+            display_cmd_state(&command);
+        }
         StratagemCommand::StratagemInterval(x) => match x {
             StratagemInterval::List { display_type } => {
                 let stop_spinner = start_spinner("Finding existing intervals...");
