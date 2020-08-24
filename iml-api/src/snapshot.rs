f87cd5ea04b69428e36f9550fc2b434ba584a23a
Michael Pankov <work@michaelpankov.com>
2020-08-24 12:54:19 +0300

    Implement destroy
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-api/src/snapshot.rs
+++ iml-api/src/snapshot.rs
@@ -82,7 +82,7 @@ async fn get_snapshots(
     Ok(warp::reply::json(&snapshots))
 }
 
-async fn post_snapshot_internal(
+async fn create_snapshot_internal(
     args: snapshot::Create,
     conn: Connection,
     pool: PgPool,
@@ -140,38 +140,118 @@ async fn post_snapshot_internal(
     Ok(result)
 }
 
-async fn post_snapshot(
+async fn create_snapshot(
     args: snapshot::Create,
     conn: Connection,
     pool: PgPool,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    let snapshots = post_snapshot_internal(args, conn, pool)
+    let results = create_snapshot_internal(args, conn, pool)
         .await
         .map_err(|e| match e {
             error::ImlApiError::FileSystemNotFound(_) => warp::reject::not_found(),
             _ => e.into(),
         })?;
 
-    Ok(warp::reply::json(&snapshots))
+    Ok(warp::reply::json(&results))
+}
+
+async fn destroy_snapshot_internal(
+    args: snapshot::Destroy,
+    conn: Connection,
+    pool: PgPool,
+) -> Result<serde_json::Value, error::ImlApiError> {
+    drop(conn);
+
+    let mgs_id = sqlx::query!(
+        r#"
+        select mgs_id from chroma_core_managedfilesystem where name=$1
+        "#,
+        args.fsname
+    )
+    .fetch_one(&pool)
+    .await
+    .map_err(|e| match e {
+        sqlx::Error::RowNotFound => error::ImlApiError::FileSystemNotFound(args.fsname.clone()),
+        _ => e.into(),
+    })?
+    .mgs_id;
+
+    let mgs_uuid = sqlx::query!(
+        r#"
+        select uuid from chroma_core_managedtarget where id=$1
+        "#,
+        mgs_id
+    )
+    .fetch_one(&pool)
+    .await?
+    .uuid;
+
+    let active_mgs_host_id = sqlx::query!(
+        r#"
+        select active_host_id from targets where uuid=$1
+        "#,
+        mgs_uuid
+    )
+    .fetch_one(&pool)
+    .await?
+    .active_host_id;
+
+    let active_mgs_host_fqdn = sqlx::query!(
+        r#"
+        select fqdn from chroma_core_managedhost where id=$1
+        "#,
+        active_mgs_host_id
+    )
+    .fetch_one(&pool)
+    .await?
+    .fqdn;
+
+    tracing::info!("{}", active_mgs_host_fqdn);
+
+    let result = invoke_rust_agent(active_mgs_host_fqdn, "snapshot_destroy", args).await?;
+
+    Ok(result)
+}
+
+async fn destroy_snapshot(
+    args: snapshot::Destroy,
+    conn: Connection,
+    pool: PgPool,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let results = destroy_snapshot_internal(args, conn, pool)
+        .await
+        .map_err(|e| match e {
+            error::ImlApiError::FileSystemNotFound(_) => warp::reject::not_found(),
+            _ => e.into(),
+        })?;
+
+    Ok(warp::reply::json(&results))
 }
 
 pub(crate) fn endpoint(
     client_filter: impl Filter<Extract = (Connection,), Error = warp::Rejection> + Clone + Send,
     pool_filter: impl Filter<Extract = (PgPool,), Error = std::convert::Infallible> + Clone + Send,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    let get = warp::path!("snapshot")
+    let list = warp::path!("snapshot")
         .and(warp::get())
         .and(warp::query())
         .and(client_filter.clone())
         .and(pool_filter.clone())
         .and_then(get_snapshots);
 
-    let post = warp::path!("snapshot")
+    let create = warp::path!("snapshot")
+        .and(warp::post())
+        .and(warp::query())
+        .and(client_filter.clone())
+        .and(pool_filter.clone())
+        .and_then(create_snapshot);
+
+    let destroy = warp::path!("snapshot" / "destroy")
         .and(warp::post())
         .and(warp::query())
         .and(client_filter)
         .and(pool_filter)
-        .and_then(post_snapshot);
+        .and_then(destroy_snapshot);
 
-    get.or(post)
+    list.or(create).or(destroy)
 }
