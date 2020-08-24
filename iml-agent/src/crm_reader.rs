c41e593761332a23ce58e67569004632ad2e71dd
Joe Grund <jgrund@whamcloud.io>
2020-08-24 11:24:34 -0400

    Implement a crm_mon XML reader
    Implement a reader for `crm_mon -1 -r -X`.
    
    It uses streaming XML parsing to build up a `Cluster` struct.
    The `Cluster` struct just contains a list of `Nodes` for now, but can be
    expanded in the future.
    
    In addition, use thiserror for iml_agent errors.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

---
+++ iml-agent/src/crm_reader.rs
@@ -0,0 +1,158 @@
+// Copyright (c) 2020 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+use crate::agent_error::ImlAgentError;
+use iml_cmd::{CheckedCommandExt, Command};
+use iml_fs::file_exists;
+use iml_wire_types::high_availability::{Cluster, Node};
+use quick_xml::{
+    events::{attributes::Attributes, Event},
+    Reader,
+};
+use std::{collections::HashMap, convert::TryInto, io};
+
+static CRM_MON_PATH: &'static str = "/usr/sbin/crm_mon";
+
+pub async fn get_crm_mon() -> Result<Option<Cluster>, ImlAgentError> {
+    if !file_exists(CRM_MON_PATH).await {
+        return Ok(None);
+    }
+
+    let x = Command::new(CRM_MON_PATH)
+        .arg("-1")
+        .arg("-r")
+        .arg("-X")
+        .checked_output()
+        .await?;
+
+    let x = read_crm_output(&x.stdout)?;
+
+    Ok(Some(x))
+}
+
+fn required_arg<'a>(arg: &str, x: &'a HashMap<&str, String>) -> Result<&'a str, ImlAgentError> {
+    x.get(arg)
+        .map(|x| x.as_str())
+        .ok_or_else(|| ImlAgentError::MissingArgument(arg.into()))
+}
+
+fn required_bool(arg: &str, x: &HashMap<&str, String>) -> Result<bool, ImlAgentError> {
+    required_arg(arg, x).and_then(str_to_bool)
+}
+
+fn str_to_bool(x: &str) -> Result<bool, ImlAgentError> {
+    match x {
+        "true" => Ok(true),
+        "false" => Ok(false),
+        _ => Err(ImlAgentError::Io(io::Error::new(
+            io::ErrorKind::NotFound,
+            "Could not convert to bool",
+        ))),
+    }
+}
+
+fn from_map(x: &HashMap<&str, String>) -> Result<Node, ImlAgentError> {
+    Ok(Node {
+        name: required_arg("name", x)?.to_string(),
+        id: required_arg("id", x)?.to_string(),
+        online: required_bool("online", x)?,
+        standby: required_bool("standby", x)?,
+        standby_onfail: required_bool("standby_onfail", x)?,
+        maintenance: required_bool("maintenance", x)?,
+        pending: required_bool("pending", x)?,
+        unclean: required_bool("unclean", x)?,
+        shutdown: required_bool("shutdown", x)?,
+        expected_up: required_bool("expected_up", x)?,
+        is_dc: required_bool("is_dc", x)?,
+        resources_running: required_arg("resources_running", x)?.parse::<u32>()?,
+        r#type: required_arg("type", x)?.try_into()?,
+    })
+}
+
+fn attrs_to_hashmap<'a>(
+    mut attrs: Attributes<'a>,
+    reader: &Reader<&[u8]>,
+) -> Result<HashMap<&'a str, String>, ImlAgentError> {
+    attrs.try_fold(HashMap::new(), |mut acc, x| {
+        let x = x?;
+
+        acc.insert(
+            std::str::from_utf8(x.key)?,
+            x.unescape_and_decode_value(reader)?,
+        );
+
+        Ok(acc)
+    })
+}
+
+fn read_nodes(reader: &mut Reader<&[u8]>) -> Result<Vec<Node>, ImlAgentError> {
+    let mut buf = vec![];
+    let mut xs = vec![];
+
+    loop {
+        buf.clear();
+
+        match reader.read_event(&mut buf)? {
+            Event::Empty(x) => match x.name() {
+                b"node" => {
+                    let x = attrs_to_hashmap(x.attributes(), reader)?;
+                    let x = from_map(&x)?;
+
+                    xs.push(x);
+                }
+                _ => {}
+            },
+            Event::End(x) => {
+                if x.name() == b"nodes" {
+                    break;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    Ok(xs)
+}
+
+fn read_crm_output(crm_output: &[u8]) -> Result<Cluster, ImlAgentError> {
+    let x = std::str::from_utf8(crm_output)?;
+
+    let mut reader = Reader::from_str(x);
+    reader.trim_text(true);
+
+    let mut buf = vec![];
+
+    let mut cluster = Cluster::default();
+
+    loop {
+        match reader.read_event(&mut buf)? {
+            Event::Start(ref x) => match x.name() {
+                b"nodes" => {
+                    cluster.nodes = read_nodes(&mut reader)?;
+                }
+                _ => {}
+            },
+            Event::Eof => break,
+            _ => {}
+        };
+
+        buf.clear();
+    }
+
+    Ok(cluster)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static ES_FIXTURE: &'static [u8] = include_bytes!("./fixtures/es_fixture.xml");
+
+    #[test]
+    fn test_read_es() {
+        let output = read_crm_output(ES_FIXTURE).unwrap();
+
+        insta::assert_debug_snapshot!(output);
+    }
+}
---
+++ iml-wire-types/src/high_availability.rs
@@ -0,0 +1,52 @@
+// Copyright (c) 2020 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+use std::{convert::TryFrom, io};
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub enum NodeType {
+    Unknown,
+    Member,
+    Remote,
+    Ping,
+}
+
+impl TryFrom<&str> for NodeType {
+    type Error = io::Error;
+
+    fn try_from(x: &str) -> Result<Self, Self::Error> {
+        match x {
+            "unknown" => Ok(Self::Unknown),
+            "member" => Ok(Self::Member),
+            "remote" => Ok(Self::Remote),
+            "ping" => Ok(Self::Ping),
+            x => Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!("Invalid node type {}", x),
+            )),
+        }
+    }
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Node {
+    pub name: String,
+    pub id: String,
+    pub online: bool,
+    pub standby: bool,
+    pub standby_onfail: bool,
+    pub maintenance: bool,
+    pub pending: bool,
+    pub unclean: bool,
+    pub shutdown: bool,
+    pub expected_up: bool,
+    pub is_dc: bool,
+    pub resources_running: u32,
+    pub r#type: NodeType,
+}
+
+#[derive(Debug, Default, serde::Serialize, serde::Deserialize)]
+pub struct Cluster {
+    pub nodes: Vec<Node>,
+}
