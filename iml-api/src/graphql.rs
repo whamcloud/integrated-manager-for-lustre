9325dc8b2013389632c961b3f53eaf34418f9013
Joe Grund <jgrund@whamcloud.io>
2020-08-25 08:26:58 -0400

    Implement Experimental GraphQL support
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

---
+++ iml-api/src/graphql.rs
@@ -0,0 +1,128 @@
+// Copyright (c) 2020 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+use crate::error::ImlApiError;
+use iml_postgres::{sqlx, PgPool};
+use juniper::{
+    http::{graphiql::graphiql_source, GraphQLRequest},
+    EmptyMutation, EmptySubscription, GraphQLEnum, RootNode,
+};
+use std::ops::Deref;
+use std::{convert::Infallible, sync::Arc};
+use warp::Filter;
+
+#[derive(juniper::GraphQLObject)]
+/// A Corosync Node found in `crm_mon`
+struct CorosyncNode {
+    /// The name of the node
+    name: String,
+    /// The id of the node as reported by `crm_mon`
+    id: String,
+    online: bool,
+    standby: bool,
+    standby_onfail: bool,
+    maintenance: bool,
+    pending: bool,
+    unclean: bool,
+    shutdown: bool,
+    expected_up: bool,
+    is_dc: bool,
+    resources_running: i32,
+    r#type: String,
+}
+
+pub(crate) struct QueryRoot;
+
+#[derive(GraphQLEnum)]
+enum SortDir {
+    Asc,
+    Desc,
+}
+
+impl Default for SortDir {
+    fn default() -> Self {
+        Self::Asc
+    }
+}
+
+impl Deref for SortDir {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Self::Asc => "asc",
+            Self::Desc => "desc",
+        }
+    }
+}
+
+#[juniper::graphql_object(Context = Context)]
+impl QueryRoot {
+    #[graphql(arguments(
+        limit(description = "paging limit, defaults to 20"),
+        offset(description = "Offset into items, defaults to 0"),
+        dir(description = "Sort direction, defaults to asc")
+    ))]
+    async fn corosync_nodes(
+        context: &Context,
+        limit: Option<i32>,
+        offset: Option<i32>,
+        dir: Option<SortDir>,
+    ) -> juniper::FieldResult<Vec<CorosyncNode>> {
+        let dir = dir.unwrap_or_default();
+
+        let xs = sqlx::query_as!(
+            CorosyncNode,
+            r#"
+                SELECT * FROM corosync_node n
+                ORDER BY
+                    CASE WHEN $3 = 'asc' THEN (n.id, n.name) END ASC,
+                    CASE WHEN $3 = 'desc' THEN (n.id, n.name) END DESC
+                OFFSET $1 LIMIT $2"#,
+            offset.unwrap_or(0) as i64,
+            limit.unwrap_or(20) as i64,
+            dir.deref()
+        )
+        .fetch_all(&context.client)
+        .await?;
+
+        Ok(xs)
+    }
+}
+
+pub(crate) type Schema =
+    RootNode<'static, QueryRoot, EmptyMutation<Context>, EmptySubscription<Context>>;
+
+pub(crate) struct Context {
+    pub(crate) client: PgPool,
+}
+
+pub(crate) async fn graphql(
+    schema: Arc<Schema>,
+    ctx: Arc<Context>,
+    req: GraphQLRequest,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let res = req.execute(&schema, &ctx).await;
+    let json = serde_json::to_string(&res).map_err(ImlApiError::SerdeJsonError)?;
+
+    Ok(json)
+}
+
+pub(crate) fn endpoint(
+    schema_filter: impl Filter<Extract = (Arc<Schema>,), Error = Infallible> + Clone + Send,
+    ctx_filter: impl Filter<Extract = (Arc<Context>,), Error = Infallible> + Clone + Send,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    let graphql_route = warp::path!("graphql")
+        .and(warp::post())
+        .and(schema_filter)
+        .and(ctx_filter)
+        .and(warp::body::json())
+        .and_then(graphql);
+
+    let graphiql_route = warp::path!("graphiql")
+        .and(warp::get())
+        .map(|| warp::reply::html(graphiql_source("graphql", None)));
+
+    graphql_route.or(graphiql_route)
+}
