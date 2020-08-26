036757db1583e8892a4827a589882ef79ace8ba0
Nathaniel Clark <nclark@whamcloud.com>
2020-08-26 16:54:15 -0400

    Add hpv2 stuff to ObjectCache
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>
    
    Fix HA Labels for systemd resources
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>
    
    Fix typo
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>
    
    Fix call to create clone client
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- chroma_core/services/job_scheduler/job_scheduler.py
+++ chroma_core/services/job_scheduler/job_scheduler.py
@@ -2016,6 +2016,8 @@ class JobScheduler(object):
             hotpool = OstPool.objects.get(pk=hotpool_data["hotpool"])
             coldpool = OstPool.objects.get(pk=hotpool_data["coldpool"])
 
+            purges = []
+            amigos = []
             with transaction.atomic():
 
                 # Create HotpoolConfig object
@@ -2072,7 +2074,7 @@ class JobScheduler(object):
                         "configuration": lamigoconf,
                         "mdt": mdt.downcast(),
                     }
-                    lamigo = Lamigo.objects.create(**data)
+                    amigos.append(Lamigo.objects.create(**data))
 
                 # Create LPurge
                 task_data = {
@@ -2101,8 +2103,15 @@ class JobScheduler(object):
                         "configuration": lpurgeconf,
                         "ost": ost.downcast(),
                     }
-                    Lpurge.objects.create(**data)
+                    purges.append(Lpurge.objects.create(**data))
 
+            for lp in purges:
+                ObjectCache.add(Lpurge, lp)
+            for la in amigos:
+                ObjectCache.add(Lamigo, la)
+            ObjectCache.add(HotpoolConfiguration, hpconf)
+
+            with transaction.atomic():
                 command_id = self.CommandPlan.command_run_jobs(job_list, help_text["create_hotpool"],)
 
         self.progress.advance()
