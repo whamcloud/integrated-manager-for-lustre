5661401f543cc07266d9c12bd400b38229916dbc
Michael Pankov <work@michaelpankov.com>
2020-08-26 14:25:36 +0300

    [WIP] Use job_scheduler
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-api/src/snapshot.rs
+++ iml-api/src/snapshot.rs
@@ -4,9 +4,12 @@
 
 use crate::error;
 use error::ImlApiError;
+use futures::TryFutureExt;
 use iml_action_client::invoke_rust_agent;
 use iml_postgres::{sqlx, PgPool};
+use iml_rabbit::Connection;
 use iml_wire_types::snapshot;
+use std::collections::HashMap;
 use warp::Filter;
 
 async fn active_mgs_host_fqdn(fsname: &str, pool: PgPool) -> Result<String, error::ImlApiError> {
@@ -80,19 +83,21 @@ async fn get_snapshots(
 
 async fn create_snapshot(
     args: snapshot::Create,
+    client: Connection,
     pool: PgPool,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    let active_mgs_host_fqdn =
-        active_mgs_host_fqdn(&args.fsname, pool)
-            .await
-            .map_err(|e| match e {
-                error::ImlApiError::FileSystemNotFound(_) => warp::reject::not_found(),
-                _ => e.into(),
-            })?;
+    let mut kwargs = HashMap::new();
+    kwargs.insert("filesystem".to_string(), "1".to_string());
 
-    let results = invoke_rust_agent(active_mgs_host_fqdn, "snapshot_create", args)
-        .await
-        .map_err(|e| ImlApiError::from(e))?;
+    let job = serde_json::json!({
+        "class_name": "CreateSnapshotJob",
+        "args": args,
+        "kwargs": kwargs,
+    });
+    let results: Vec<i32> =
+        iml_job_scheduler_rpc::call(&client, "run_jobs", vec![job], Some(kwargs))
+            .map_err(ImlApiError::ImlJobSchedulerRpcError)
+            .await?;
 
     Ok(warp::reply::json(&results))
 }
@@ -155,6 +160,7 @@ async fn unmount_snapshot(
 }
 
 pub(crate) fn endpoint(
+    client_filter: impl Filter<Extract = (Connection,), Error = warp::Rejection> + Clone + Send,
     pool_filter: impl Filter<Extract = (PgPool,), Error = std::convert::Infallible> + Clone + Send,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     let list = warp::path!("snapshot")
@@ -166,6 +172,7 @@ pub(crate) fn endpoint(
     let create = warp::path!("snapshot")
         .and(warp::post())
         .and(warp::query())
+        .and(client_filter.clone())
         .and(pool_filter.clone())
         .and_then(create_snapshot);
 
