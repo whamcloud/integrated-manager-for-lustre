5810571810569c4fa916de15614878eb3ca1421c
Joe Grund <jgrund@whamcloud.io>
2020-08-31 10:54:11 -0400

    Use IML cli inside iml-timer
    The iml-timer service currently uses it's own internal CLI for stratagem
    scans instead of reusing the iml cli.
    
    If we follow this pattern, it will mean we need to create a separate
    binary for each service we want to run as a timer inside of docker.
    
    Instead, we should embed the iml cli inside the container and call it
    the same way we do outside the container.
    
    This will allow us to write a single cli command for running snapshot
    commands instead of having to maintain two commands.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- iml-timer/src/error.rs
+++ iml-timer/src/error.rs
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-use serde::Serialize;
-use warp::{self, reject};
+use warp::reject;
 
 #[derive(Debug)]
 pub enum TimerError {
@@ -12,7 +11,7 @@ pub enum TimerError {
 
 impl reject::Reject for TimerError {}
 
-#[derive(Serialize)]
+#[derive(serde::Serialize)]
 struct ErrorMessage<'a> {
     code: u16,
     message: &'a str,
