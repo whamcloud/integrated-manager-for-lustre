1b2e695a6c9e22ee6c3265cf0ee02ecd25ee598f
Michael Pankov <work@michaelpankov.com>
2020-09-03 11:16:33 +0300

    Remove unnecessary changes
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-api/src/error.rs
+++ iml-api/src/error.rs
@@ -3,7 +3,6 @@
 // license that can be found in the LICENSE file.
 
 use futures::channel::oneshot;
-use iml_action_client::ImlActionClientError;
 use iml_job_scheduler_rpc::ImlJobSchedulerRpcError;
 use iml_postgres::sqlx;
 use iml_rabbit::{self, ImlRabbitError};
@@ -12,8 +11,6 @@ use warp::reject;
 
 #[derive(Debug, Error)]
 pub enum ImlApiError {
-    #[error(transparent)]
-    ImlActionClientError(#[from] ImlActionClientError),
     #[error(transparent)]
     ImlJobSchedulerRpcError(#[from] ImlJobSchedulerRpcError),
     #[error(transparent)]
@@ -26,8 +23,6 @@ pub enum ImlApiError {
     SerdeJsonError(#[from] serde_json::error::Error),
     #[error(transparent)]
     SqlxError(#[from] sqlx::Error),
-    #[error("filesystem {0} not found")]
-    FileSystemNotFound(String),
 }
 
 impl reject::Reject for ImlApiError {}
