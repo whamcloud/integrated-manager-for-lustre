1b50457ce9ac87a515b097236d5cc003407c158c
Michael Pankov <work@michaelpankov.com>
2020-09-04 09:46:47 +0300

    Address review
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-api/src/graphql.rs
+++ iml-api/src/graphql.rs
@@ -322,9 +322,9 @@ impl QueryRoot {
         .map_err(ImlApiError::ImlJobSchedulerRpcError)
         .await?;
 
-        let command = get_command(&context.pg_pool, command_id).await?;
-
-        Ok(command)
+        get_command(&context.pg_pool, command_id)
+            .await
+            .map_err(|e| e.into())
     }
     #[graphql(arguments(args(description = "Snapshot unmounting arguments")))]
     async fn unmount_snapshot(context: &Context, args: Unmount) -> juniper::FieldResult<Command> {
@@ -352,9 +352,9 @@ impl QueryRoot {
         .map_err(ImlApiError::ImlJobSchedulerRpcError)
         .await?;
 
-        let command = get_command(&context.pg_pool, command_id).await?;
-
-        Ok(command)
+        get_command(&context.pg_pool, command_id)
+            .await
+            .map_err(|e| e.into())
     }
 }
 
