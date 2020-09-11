5400d689826e6ce0d14ca086fe070a1d2759fab7
Joe Grund <jgrund@whamcloud.io>
2020-09-11 14:14:08 -0400

    fixups
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- iml-graphql-queries/src/lib.rs
+++ iml-graphql-queries/src/lib.rs
@@ -11,3 +11,33 @@ pub struct Query<T: serde::Serialize> {
     query: String,
     variables: Option<T>,
 }
+
+#[derive(Debug, Clone, serde::Deserialize)]
+pub struct Data<T> {
+    pub data: T,
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+pub struct Location {
+    line: u32,
+    column: u32,
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+pub struct Error {
+    message: String,
+    locations: Vec<Location>,
+    path: Vec<String>,
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+pub struct Errors {
+    errors: Vec<Error>,
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+#[serde(untagged)]
+pub enum Response<T> {
+    Data(Data<T>),
+    Errors(Errors),
+}
