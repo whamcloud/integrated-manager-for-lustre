c9913a522e0fd7fe4b981e40b77f71271f84ba85
Joe Grund <jgrund@whamcloud.io>
2020-09-17 12:57:10 -0400

    fixup realtime intervals
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- migrations/20200831174542_snapshot.sql
+++ migrations/20200831174542_snapshot.sql
@@ -10,19 +10,6 @@ CREATE TABLE IF NOT EXISTS snapshot (
   UNIQUE (filesystem_name, snapshot_name)
 );
 
-CREATE OR REPLACE FUNCTION table_update_notify() RETURNS TRIGGER AS $$
-BEGIN
-  IF TG_OP = 'INSERT' THEN
-    PERFORM pg_notify('table_update', '[ "' || TG_OP || '", "' || TG_TABLE_NAME || '", ' || row_to_json(NEW) || ']');
-  ELSEIF TG_OP = 'UPDATE' AND OLD IS DISTINCT FROM NEW THEN
-    PERFORM pg_notify('table_update', '[ "' || TG_OP || '", "' || TG_TABLE_NAME || '", ' || row_to_json(NEW) || ']');
-  ELSE
-    PERFORM pg_notify('table_update','[ "' || TG_OP || '", "' || TG_TABLE_NAME || '", ' || row_to_json(OLD) || ']');
-  END IF;
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
 DROP TRIGGER IF EXISTS snapshot_notify_update ON snapshot;
 
 DROP TRIGGER IF EXISTS snapshot_notify_insert ON snapshot;
@@ -49,6 +36,27 @@ CREATE TABLE IF NOT EXISTS snapshot_interval (
   UNIQUE (filesystem_name, interval)
 );
 
+CREATE OR REPLACE FUNCTION table_update_notify_snapshot_interval() RETURNS TRIGGER 
+  AS $$
+    BEGIN
+      IF TG_OP = 'INSERT' THEN PERFORM pg_notify(
+        'table_update',
+        notify_row(TG_OP, TG_TABLE_NAME, json_build_object('id', NEW.id, 'filesystem_name', NEW.filesystem_name, 'use_barrier', NEW.use_barrier, 'last_run', NEW.last_run, 'interval', interval_to_seconds(NEW.interval)))
+      );
+      ELSEIF TG_OP = 'UPDATE' AND OLD IS DISTINCT FROM NEW THEN PERFORM pg_notify(
+        'table_update',
+        notify_row(TG_OP, TG_TABLE_NAME, json_build_object('id', NEW.id, 'filesystem_name', NEW.filesystem_name, 'use_barrier', NEW.use_barrier, 'last_run', NEW.last_run, 'interval', interval_to_seconds(NEW.interval)))
+      );
+      ELSE PERFORM pg_notify(
+        'table_update',
+        notify_row(TG_OP, TG_TABLE_NAME, json_build_object('id', OLD.id, 'filesystem_name', OLD.filesystem_name, 'use_barrier', OLD.use_barrier, 'last_run', OLD.last_run, 'interval', interval_to_seconds(OLD.interval)))
+      );
+      END IF;
+
+      RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
 DROP TRIGGER IF EXISTS snapshot_interval_notify_update ON snapshot_interval;
 
 DROP TRIGGER IF EXISTS snapshot_interval_notify_insert ON snapshot_interval;
@@ -57,14 +65,14 @@ DROP TRIGGER IF EXISTS snapshot_interval_notify_delete ON snapshot_interval;
 
 CREATE TRIGGER snapshot_interval_notify_update
 AFTER
-UPDATE ON snapshot_interval FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+UPDATE ON snapshot_interval FOR EACH ROW EXECUTE PROCEDURE table_update_notify_snapshot_interval();
 
 CREATE TRIGGER snapshot_interval_notify_insert
 AFTER
-INSERT ON snapshot_interval FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+INSERT ON snapshot_interval FOR EACH ROW EXECUTE PROCEDURE table_update_notify_snapshot_interval();
 
 CREATE TRIGGER snapshot_interval_notify_delete
-AFTER DELETE ON snapshot_interval FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
+AFTER DELETE ON snapshot_interval FOR EACH ROW EXECUTE PROCEDURE table_update_notify_snapshot_interval();
 
 CREATE TYPE snapshot_delete_unit AS ENUM ('percent', 'gibibytes', 'tebibytes');
 
