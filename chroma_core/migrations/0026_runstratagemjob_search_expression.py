a6fec8caa665dd216917b1d0fc38b8e7a0e03c26
beevans <64284623+beevans@users.noreply.github.com>
2020-10-01 19:02:51 -0400

    Ex-1842 filesync: remove not null requirement (#2296)
    Remove not null requirement for search_expression
    
    Signed-off-by: Ben Evans <beevans@whamcloud.com>

--- chroma_core/migrations/0026_runstratagemjob_search_expression.py
+++ chroma_core/migrations/0026_runstratagemjob_search_expression.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="runstratagemjob",
             name="search_expression",
-            field=models.TextField(default=""),
+            field=models.TextField(null=True, default=""),
             preserve_default=False,
         ),
         migrations.AddField(
