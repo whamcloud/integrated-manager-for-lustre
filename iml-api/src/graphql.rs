c7b81fe8efb98c68a95c34f33188241c24eb81d4
Michael Pankov <work@michaelpankov.com>
2020-10-02 12:02:31 +0300

    Remove substitutions
    Signed-off-by: Michael Pankov <work@michaelpankov.com>

--- iml-api/src/graphql.rs
+++ iml-api/src/graphql.rs
@@ -137,14 +137,6 @@ impl Deref for SortDir {
     }
 }
 
-#[derive(juniper::GraphQLObject)]
-struct Substitution {
-    pub start: String,
-    pub end: String,
-    pub label: String,
-    pub resource_uri: String,
-}
-
 #[derive(GraphQLEnum, sqlx::Type)]
 #[repr(i16)]
 enum MessageClass {
@@ -207,7 +199,6 @@ struct LogMessage {
     pub message: String,
     pub message_class: MessageClass,
     pub severity: LogSeverity,
-    pub substitutions: Vec<Substitution>,
     pub tag: String,
 }
 
@@ -221,7 +212,6 @@ impl From<LogMessageRecord> for LogMessage {
             message: record.message,
             message_class: MessageClass::from(record.message_class),
             severity: LogSeverity::from(record.severity),
-            substitutions: vec![],
             tag: record.tag,
         }
     }
