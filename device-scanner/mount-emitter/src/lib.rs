6919b4c5e7c5bf8fc240fb67d81dfde6d7be39e6
Joe Grund <jgrund@whamcloud.io>
2020-10-14 15:27:25 -0400

    Remove swap-emitter.timer
    Due to systemd/systemd#9328, there is no way to silence a systemd timer.
    
    To workaround this limitation, create a new daemon that will list swap
    devices one per minute, but without logging each tick to the journal.
    
    In addition, update mount-emitter to use latest futures and tokio.
    
    Fixes #2232
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- device-scanner/mount-emitter/src/lib.rs
+++ device-scanner/mount-emitter/src/lib.rs
@@ -8,36 +8,55 @@
 //! into a `MountCommand` variant and send the serialized result to `device-scanner`.
 //!
 
-use std::convert::AsRef;
-
 use device_types::{
     mount::{FsType, MountCommand, MountOpts, MountPoint},
     Command, DevicePath,
 };
-use futures::{future, Future};
-use std::{
-    collections::HashMap, io::BufRead, os::unix::net::UnixStream as NetUnixStream, process::exit,
-    str,
-};
+use futures::Future;
+use std::{collections::HashMap, convert::AsRef, os::unix::net::UnixStream as NetUnixStream, str};
 use tokio::{
-    io::{AsyncRead, AsyncWrite},
+    io::{self, AsyncBufRead, AsyncBufReadExt, AsyncWrite, AsyncWriteExt},
     net::UnixStream,
-    reactor::Handle,
 };
 
-fn line_to_command(x: &[u8]) -> MountCommand {
-    let mut x: IntermediateMap = line_to_hashmap(&x);
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    SerdeJson(#[from] serde_json::Error),
+    #[error(transparent)]
+    Utf8Error(#[from] std::str::Utf8Error),
+    #[error("missing {0}")]
+    Missing(&'static str),
+    #[error("unexpected {0}")]
+    Unexpected(String),
+}
+
+pub fn line_to_command(x: &[u8]) -> Result<MountCommand, Error> {
+    let mut x: IntermediateMap = line_to_hashmap(&x)?;
 
     let (target, source, fstype, mount_opts, old_opts, old_target) = (
-        MountPoint(x.remove("TARGET").expect("missing TARGET").into()),
-        DevicePath(x.remove("SOURCE").expect("missing SOURCE").into()),
-        FsType(x.remove("FSTYPE").expect("missing FSTYPE")),
-        MountOpts(x.remove("OPTIONS").expect("missing OPTIONS")),
+        MountPoint(
+            x.remove("TARGET")
+                .ok_or_else(|| Error::Missing("TARGET"))?
+                .into(),
+        ),
+        DevicePath(
+            x.remove("SOURCE")
+                .ok_or_else(|| Error::Missing("SOURCE"))?
+                .into(),
+        ),
+        FsType(x.remove("FSTYPE").ok_or_else(|| Error::Missing("FSTYPE"))?),
+        MountOpts(
+            x.remove("OPTIONS")
+                .ok_or_else(|| Error::Missing("OPTIONS"))?,
+        ),
         x.get("OLD-OPTIONS"),
         x.get("OLD-TARGET"),
     );
 
-    match x.get("ACTION").map(AsRef::as_ref) {
+    let x = match x.get("ACTION").map(AsRef::as_ref) {
         Some("mount") | None => MountCommand::AddMount(target, source, fstype, mount_opts),
         Some("umount") => MountCommand::RemoveMount(target, source, fstype, mount_opts),
         Some("remount") => MountCommand::ReplaceMount(
@@ -45,30 +64,38 @@ fn line_to_command(x: &[u8]) -> MountCommand {
             source,
             fstype,
             mount_opts,
-            MountOpts(old_opts.expect("missing OLD-OPTIONS").to_string()),
+            MountOpts(
+                old_opts
+                    .ok_or_else(|| Error::Missing("OLD-OPTIONS"))?
+                    .to_string(),
+            ),
         ),
         Some("move") => MountCommand::MoveMount(
             target,
             source,
             fstype,
             mount_opts,
-            MountPoint(old_target.expect("missing OLD-TARGET").into()),
+            MountPoint(
+                old_target
+                    .ok_or_else(|| Error::Missing("OLD-TARGET"))?
+                    .into(),
+            ),
         ),
-        Some(x) => {
-            eprintln!("Unexpected ACTION: {}", x);
-            exit(1);
-        }
-    }
+        Some(x) => return Err(Error::Unexpected(x.to_string())),
+    };
+
+    Ok(x)
 }
 
 type IntermediateMap = HashMap<String, String>;
 
-fn line_to_hashmap(x: &[u8]) -> IntermediateMap {
-    str::from_utf8(x)
-        .expect("Did not convert from utf8")
+fn line_to_hashmap(x: &[u8]) -> Result<IntermediateMap, Error> {
+    let x = str::from_utf8(x)?
         .trim()
         .split(' ')
-        .fold(HashMap::new(), build_hashmap)
+        .fold(HashMap::new(), build_hashmap);
+
+    Ok(x)
 }
 
 fn build_hashmap(mut acc: IntermediateMap, x: &str) -> IntermediateMap {
@@ -87,78 +114,81 @@ fn build_hashmap(mut acc: IntermediateMap, x: &str) -> IntermediateMap {
 /// Creates a stream that implements `AsyncWrite`
 /// In this case, we use `tokio::net::UnixStream`,
 /// But we can substitute this fn for integration testing
-pub fn get_write_stream() -> impl AsyncWrite {
-    let stream = NetUnixStream::connect("/var/run/device-scanner.sock")
-        .expect("Unable to connect to device-scanner.sock");
+pub fn get_write_stream() -> Result<impl AsyncWrite, Error> {
+    let stream = NetUnixStream::connect("/var/run/device-scanner.sock")?;
 
-    UnixStream::from_std(stream, &Handle::default()).expect("Unable to consume device-scanner.sock")
+    let x = UnixStream::from_std(stream)?;
+
+    Ok(x)
 }
 
 /// Writes a given buffer to the tokio runtime.
-pub fn write_all<A, T>(a: A, buf: T) -> impl Future<Item = (), Error = std::io::Error>
+pub async fn write_all<A, T>(mut a: A, buf: T) -> Result<(), std::io::Error>
 where
-    A: AsyncWrite,
+    A: AsyncWrite + std::marker::Unpin,
     T: AsRef<[u8]>,
 {
-    tokio::io::write_all(a, buf).map(|_| ())
+    a.write_all(buf.as_ref()).await?;
+
+    Ok(())
 }
 
 /// convert stdin into a nonblocking file;
-/// this is the only part that makes use of tokio_file_unix
-pub fn stdin_to_file() -> impl AsyncRead + BufRead {
-    let file = tokio_file_unix::raw_stdin().unwrap();
-    let file = tokio_file_unix::File::new_nb(file).unwrap();
-    file.into_reader(&Handle::default()).unwrap()
+pub fn stdin_to_file() -> impl AsyncBufRead {
+    io::BufReader::new(io::stdin())
 }
 
 /// Loops over lines streaming from STDIN.
 /// This has been extracted for integration
 /// testing purposes.
-pub fn looper<R, R1, W, W1, F, W2>(
+pub async fn looper<R, R1, W, W1, F, W2>(
     read_fn: R,
     write_fn: W,
     write_out: W2,
-) -> impl Future<Item = (), Error = ()>
+) -> Result<(), Error>
 where
     R: Fn() -> R1,
-    R1: AsyncRead + BufRead + Send + 'static,
-    W: Fn() -> W1 + Send + 'static,
+    R1: AsyncBufRead + std::marker::Unpin + Send + 'static,
+    W: Fn() -> Result<W1, Error>,
     W1: AsyncWrite + Send + Sized + 'static,
-    F: Future<Item = (), Error = std::io::Error> + Send + 'static,
+    F: Future<Output = Result<(), std::io::Error>> + Send + 'static,
     W2: Fn(W1, String) -> F + Send + Sync + 'static,
 {
-    let file = read_fn();
+    let mut file = read_fn();
 
-    future::loop_fn(
-        (file, Vec::new(), write_fn, write_out),
-        |(file, line, write_fn, write_out)| {
-            // read each line
-            tokio::io::read_until(file, b'\n', line).and_then(|(file, mut line)| {
-                if str::from_utf8(&line).unwrap() == "" {
-                    return future::Either::A(future::done(Ok(future::Loop::Break(()))));
-                }
+    let mut line = vec![];
 
-                let mount_command: MountCommand = line_to_command(&line);
+    loop {
+        file.read_until(b'\n', &mut line).await?;
 
-                let x = serde_json::to_string(&Command::MountCommand(mount_command))
-                    .expect("Could not serialize mount command");
+        tracing::debug!("Read line: {}", String::from_utf8_lossy(&line));
 
-                let s = write_fn();
+        if str::from_utf8(&line)? == "" {
+            break;
+        }
 
-                let wo = write_out(s, x);
+        let mount_command: MountCommand = line_to_command(&line)?;
 
-                let next = if line.ends_with(b"\n") {
-                    line.clear();
-                    future::Loop::Continue((file, line, write_fn, write_out))
-                } else {
-                    future::Loop::Break(())
-                };
+        tracing::debug!("Parsed mount_command {:?}", mount_command);
 
-                future::Either::B(wo.map(|_| next))
-            })
-        },
-    )
-    .map_err(|e| panic!("{:?}", e))
+        let x = serde_json::to_string(&Command::MountCommand(mount_command))?;
+
+        let s = write_fn()?;
+
+        let wo = write_out(s, x);
+
+        wo.await?;
+
+        tracing::debug!("Wrote line");
+
+        if line.ends_with(b"\n") {
+            line.clear();
+        } else {
+            break;
+        };
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -180,62 +210,62 @@ mod tests {
     fn test_line_to_hashmap_swap() {
         let line = b"TARGET=\"swap\" SOURCE=\"/dev/mapper/centos-swap\" FSTYPE=\"swap\" OPTIONS=\"defaults\"\n";
 
-        assert_debug_snapshot!(map_to_sorted_vec(line_to_hashmap(line)))
+        assert_debug_snapshot!(map_to_sorted_vec(line_to_hashmap(line).unwrap()))
     }
 
     #[test]
     fn test_line_to_hashmap_mount() {
         let line = b"ACTION=\"mount\" TARGET=\"/mnt/part1\" SOURCE=\"/dev/sde1\" FSTYPE=\"ext4\" OPTIONS=\"rw,relatime,data=ordered\" OLD-TARGET=\"\" OLD-OPTIONS=\"\"\n";
 
-        assert_debug_snapshot!(map_to_sorted_vec(line_to_hashmap(line)))
+        assert_debug_snapshot!(map_to_sorted_vec(line_to_hashmap(line).unwrap()))
     }
 
     #[test]
     fn test_swap() {
         let line = b"TARGET=\"swap\" SOURCE=\"/dev/mapper/centos-swap\" FSTYPE=\"swap\" OPTIONS=\"defaults\"\n";
 
-        assert_debug_snapshot!(line_to_command(line))
+        assert_debug_snapshot!(line_to_command(line).unwrap())
     }
 
     #[test]
     fn test_poll_mount() {
         let line = b"ACTION=\"mount\" TARGET=\"/mnt/part1\" SOURCE=\"/dev/sde1\" FSTYPE=\"ext4\" OPTIONS=\"rw,relatime,data=ordered\" OLD-TARGET=\"\" OLD-OPTIONS=\"\"\n";
 
-        assert_debug_snapshot!(line_to_command(line))
+        assert_debug_snapshot!(line_to_command(line).unwrap())
     }
 
     #[test]
     fn test_poll_umount() {
         let line = b"ACTION=\"umount\" TARGET=\"/testPool4\" SOURCE=\"testPool4\" FSTYPE=\"zfs\" OPTIONS=\"rw,xattr,noacl\" OLD-TARGET=\"\" OLD-OPTIONS=\"\"\n";
 
-        assert_debug_snapshot!(line_to_command(line))
+        assert_debug_snapshot!(line_to_command(line).unwrap())
     }
 
     #[test]
     fn test_poll_remount() {
         let line = b"ACTION=\"remount\" TARGET=\"/mnt/part1\" SOURCE=\"/dev/sde1\" FSTYPE=\"ext4\" OPTIONS=\"ro,relatime,data=ordered\" OLD-TARGET=\"\" OLD-OPTIONS=\"rw,data=ordered\"\n";
 
-        assert_debug_snapshot!(line_to_command(line))
+        assert_debug_snapshot!(line_to_command(line).unwrap())
     }
 
     #[test]
     fn test_poll_move() {
         let line = b"ACTION=\"move\" TARGET=\"/mnt/part1a\" SOURCE=\"/dev/sde1\" FSTYPE=\"ext4\" OPTIONS=\"rw,relatime,data=ordered\" OLD-TARGET=\"/mnt/part1\" OLD-OPTIONS=\"\"\n";
 
-        assert_debug_snapshot!(line_to_command(line))
+        assert_debug_snapshot!(line_to_command(line).unwrap())
     }
 
     #[test]
     fn test_list_mount() {
         let line = b"TARGET=\"/mnt/part1\" SOURCE=\"/dev/sde1\" FSTYPE=\"ext4\" OPTIONS=\"rw,relatime,data=ordered\"\n";
 
-        assert_debug_snapshot!(line_to_command(line))
+        assert_debug_snapshot!(line_to_command(line).unwrap())
     }
 
     #[test]
     fn test_swap_extra() {
         let line = b"TARGET=\"swap\" SOURCE=\"/dev/mapper/VolGroup00-LogVol01\" FSTYPE=\"swap\" OPTIONS=\"defaults\"";
 
-        assert_debug_snapshot!(line_to_command(line))
+        assert_debug_snapshot!(line_to_command(line).unwrap())
     }
 }
