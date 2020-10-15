77b051e648d01c27101c59d73a29ff5796a52d02
Nathaniel Clark <nclark@whamcloud.com>
2020-10-15 09:23:38 -0400

    chroma: Add graphql helper function (#2290)
    * chroma: Add graphql helper function
    
    Use graphql for fs.get_server_groups()
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>
    
    * Cache API_USER and API_KEY in module variables
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>
    
    * Fix typo
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>
    
    * Fix docker and remove extra imports
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>
    
    * Remove use of global keyword
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>
    
    * Re-black python
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

---
+++ chroma_core/lib/graphql.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import requests
+import settings
+from django.db import connection
+
+# this is a pointer to the module object instance itself.
+this = sys.modules[__name__]
+this.API_CRED = None
+
+
+def get_api_session_request():
+    if not this.API_CRED:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * from api_key()")
+        this.API_CRED = cursor.fetchone()
+        cursor.close()
+    s = requests.Session()
+    s.headers.update({"AUTHORIZATION": "ApiKey {}:{}".format(this.API_CRED[0], this.API_CRED[1])})
+    return s
+
+
+def graphql_query(query):
+    """
+    Issue a GraphQL query
+    """
+    s = get_api_session_request()
+    path = os.path.join(settings.IML_API_PROXY_PASS, "graphql")
+    q = {"query": query}
+    res = s.post(url=path, json=q)
+    return res.json()["data"]
