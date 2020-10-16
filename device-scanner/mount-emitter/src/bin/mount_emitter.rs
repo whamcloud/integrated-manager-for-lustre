d3c6b2749cc8d6632bd176d0b4662b4172569c24
Joe Grund <jgrund@whamcloud.io>
2020-10-16 16:41:58 -0400

    Remove swap-emitter.timer (#2335)
    Due to systemd/systemd#9328, there is no way to silence a systemd timer.
    
    To workaround this limitation, create a new daemon that will list swap
    devices one per minute, but without logging each tick to the journal.
    
    In addition, update mount-emitter to use latest futures and tokio.
    
    Fixes #2232
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- device-scanner/mount-emitter/src/bin/mount_emitter.rs
+++ device-scanner/mount-emitter/src/bin/mount_emitter.rs
@@ -3,7 +3,15 @@
 // license that can be found in the LICENSE file.
 
 use mount_emitter::{get_write_stream, looper, stdin_to_file, write_all};
+use tracing_subscriber::{fmt::Subscriber, EnvFilter};
 
-fn main() {
-    tokio::run(looper(stdin_to_file, get_write_stream, write_all))
+#[tokio::main]
+async fn main() -> Result<(), mount_emitter::Error> {
+    let subscriber = Subscriber::builder()
+        .with_env_filter(EnvFilter::from_default_env())
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    looper(stdin_to_file, get_write_stream, write_all).await
 }
---
+++ device-scanner/mount-emitter/src/bin/swap_emitter.rs
@@ -0,0 +1,61 @@
+// Copyright (c) 2020 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+use mount_emitter::{get_write_stream, line_to_command};
+use std::{io::BufRead, time::Duration};
+use tokio::{io::AsyncWriteExt, process::Command, time::sleep};
+use tracing_subscriber::{fmt::Subscriber, EnvFilter};
+
+#[tokio::main]
+async fn main() -> Result<(), mount_emitter::Error> {
+    let subscriber = Subscriber::builder()
+        .with_env_filter(EnvFilter::from_default_env())
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    loop {
+        let output = Command::new("/usr/bin/findmnt")
+            .arg("-P")
+            .arg("-s")
+            .arg("-e")
+            .arg("-t")
+            .arg("swap")
+            .output()
+            .await?;
+
+        if !output.status.success() {
+            return Err(mount_emitter::Error::Unexpected(format!(
+                "findmnt call failed. exit code: {:?}, stderr: {}",
+                output.status.code(),
+                String::from_utf8_lossy(&output.stderr)
+            )));
+        }
+
+        let xs: Vec<String> = (&output.stdout).lines().collect::<Result<_, _>>()?;
+
+        tracing::debug!("Swap entries: {:?}", xs);
+
+        for x in xs {
+            let x = x.trim();
+
+            if x == "" {
+                continue;
+            }
+
+            let mount_command = line_to_command(x.as_bytes())?;
+
+            let x =
+                serde_json::to_string(&device_types::Command::MountCommand(mount_command))? + "\n";
+
+            let mut s = get_write_stream()?;
+
+            s.write_all(x.as_bytes()).await?;
+        }
+
+        tracing::debug!("Wrote entries");
+
+        sleep(Duration::from_secs(60)).await;
+    }
+}
