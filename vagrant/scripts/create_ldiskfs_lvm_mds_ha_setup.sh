be8e87a024b771a6d3193b1fc40a200c3f435be0
Joe Grund <jgrund@whamcloud.io>
2020-10-22 13:12:16 -0400

    Fix Vbox fencing (#2342)
    * Fix Vbox fencing
    
    Fencing is currently broken for vagrant installs.
    
    This appears to be because the fence agent tries to use node.local as
    the node to fence, but this does not coorespond to any vm name.
    
    Instead it should use just node.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>
    
    * Update mds / oss hostnames so Python code can figure out node <-> corosync relations.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- vagrant/scripts/create_ldiskfs_lvm_mds_ha_setup.sh
+++ vagrant/scripts/create_ldiskfs_lvm_mds_ha_setup.sh
@@ -6,8 +6,8 @@ USER=$1
 PASSWD=$2
 SSHKEY=$3
 
-pcs cluster auth mds1.local mds2.local -u hacluster -p lustre
-pcs cluster setup --start --name mds-cluster mds1.local mds2.local --enable --token 17000
+pcs cluster auth mds1 mds2 -u hacluster -p lustre
+pcs cluster setup --start --name mds-cluster mds1 mds2 --enable --token 17000
 pcs stonith create vboxfence fence_vbox ipaddr=10.0.2.2 login=${USER} ${PASSWD:+passwd=${PASSWD}} ${SSHKEY:+identity_file=${SSHKEY}}
 pcs resource create mgs-vg ocf:heartbeat:LVM volgrpname=mgt_vg exclusive=true op start timeout=120 op stop timeout=120 --group mgs-grp
 pcs resource create mgs ocf:lustre:Lustre target=/dev/mapper/mgt_vg-mgt mountpoint=/mnt/mgs op start timeout=900 op stop timeout=120 --group mgs-grp
