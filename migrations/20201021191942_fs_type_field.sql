2c8d67e3b9c95546d0a8ff8ea3548960380f5ca9
johnsonw <wjohnson@whamcloud.com>
2020-10-26 10:40:24 -0400

    CR Updates
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- migrations/20201021191942_fs_type_field.sql
+++ migrations/20201021191942_fs_type_field.sql
@@ -1,4 +1,4 @@
 -- Add migration script here
 CREATE TYPE fs_type AS ENUM('zfs', 'ldiskfs');
 
-ALTER TABLE target add column fs_type fs_type NOT NULL DEFAULT 'ldiskfs';
+ALTER TABLE target add column fs_type fs_type;
