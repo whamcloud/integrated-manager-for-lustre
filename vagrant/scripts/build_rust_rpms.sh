0eb8adf6c3764644fb1325d480921b467fb5d164
johnsonw <wjohnson@whamcloud.com>
2020-11-13 09:18:20 -0500

    Add rust rpm provisioner to adm node
    Building the rpms locally has been difficult becuase using the docker
    container is significantly slower, even after increasing the available
    ram and cpu's. In contrast, building in the adm vm appears to be nearly
    as fast as building on the host. Furthermore, adding sccache appears to
    maintain dependency builds across different branches, allowing for much
    quicker builds. This patch adds a new provisioner to build the rust rpms
    using the adm virtual machine and takes care of installing all necessary
    dependencies, including sccache. To build the rust rpms, simply run the
    following:
    
    ```
    vagrant provision adm --provision-with build-rust-rpms
    ```
    
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

---
+++ vagrant/scripts/build_rust_rpms.sh
@@ -0,0 +1,9 @@
+yum install -y openssl-devel
+yum install -y cargo rpm-build
+
+[ -f /root/.cargo/bin/sccache ] && echo "sccache already installed. Skipping." || cargo install sccache
+
+rm -rf /integrated-manager-for-lustre/_topdir
+
+cd /integrated-manager-for-lustre \
+    && CARGO_HOME="/root/.cargo" SCCACHE_CACHE_SIZE="40G"  SCCACHE_DIR="/root/.cache" RUSTC_WRAPPER="/root/.cargo/bin/sccache" make copr-rpms
