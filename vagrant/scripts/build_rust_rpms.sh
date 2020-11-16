51a6c6bf5f50f45181eda33f5206ad3b5011a3a2
johnsonw <wjohnson@whamcloud.com>
2020-11-16 09:45:46 -0500

    Move env vars into Makefile
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- vagrant/scripts/build_rust_rpms.sh
+++ vagrant/scripts/build_rust_rpms.sh
@@ -3,5 +3,4 @@ yum install -y cargo rpm-build
 
 [ -f /root/.cargo/bin/sccache ] && echo "sccache already installed. Skipping." || cargo install sccache
 
-cd /integrated-manager-for-lustre \
-    && CARGO_HOME="/root/.cargo" SCCACHE_CACHE_SIZE="40G"  SCCACHE_DIR="/root/.cache" RUSTC_WRAPPER="/root/.cargo/bin/sccache" make copr-rpms
+cd /integrated-manager-for-lustre && make copr-rpms
