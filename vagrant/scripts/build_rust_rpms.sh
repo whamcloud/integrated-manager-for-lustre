d41b49d64a85d02f23851a83d84ee7c0e1a9d105
johnsonw <wjohnson@whamcloud.com>
2020-11-17 10:23:12 -0500

    Update release version between builds
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- vagrant/scripts/build_rust_rpms.sh
+++ vagrant/scripts/build_rust_rpms.sh
@@ -3,4 +3,6 @@ yum install -y cargo rpm-build
 
 [ -f /root/.cargo/bin/sccache ] && echo "sccache already installed. Skipping." || cargo install sccache
 
-cd /integrated-manager-for-lustre && make copr-rpms
+cd /integrated-manager-for-lustre \
+    && sed -i 's/Release: 1.*/Release: 1.'"$(date '+%s')"'/g' rust-iml.spec \
+    && make copr-rpms
