3bd33d5a35841d869ea275a911c9327f807402bc
Nathaniel Clark <nclark@whamcloud.com>
2020-11-17 10:18:43 -0500

    Add create and destroy resource (#2178)
    Related #1464
    
    Add ability to add cloned resources
    Add test for create_resource()
    Add cloned_client and flesh out pacemaker constraints
    Address review comments
    Start normal resources, not cloned client
    Account for missing templates
    Add destroy cloned client
    Add constraint ids to destroy_resource
    Only call umount when the mount is mounted
    Fix missing return
    Fix cloned init check and wait
    Rename PacemakerOperations.is_some for clarity
    Move crm commands to correct module
    
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- iml-agent/src/action_plugins/lustre/client.rs
+++ iml-agent/src/action_plugins/lustre/client.rs
@@ -96,12 +96,16 @@ pub async fn mount_many(xs: Vec<Mount>) -> Result<(), ImlAgentError> {
 pub async fn unmount(unmount: Unmount) -> Result<(), ImlAgentError> {
     delete_fstab_entry(&unmount.mountpoint).await?;
 
-    Command::new("/bin/umount")
-        .arg(unmount.mountpoint)
-        .checked_output()
-        .err_into()
-        .await
-        .map(drop)
+    if is_mounted(&unmount.mountpoint, false).await? {
+        Command::new("/bin/umount")
+            .arg(unmount.mountpoint)
+            .checked_output()
+            .err_into()
+            .await
+            .map(drop)
+    } else {
+        Ok(())
+    }
 }
 
 /// This action will attempt to:
