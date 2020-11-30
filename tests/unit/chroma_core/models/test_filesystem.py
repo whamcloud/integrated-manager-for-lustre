e78acb18d657a301bc5c48e9fe86f4431901fbe6
johnsonw <wjohnson@whamcloud.com>
2020-11-30 12:48:05 -0500

    remove unnecessary tests
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- tests/unit/chroma_core/models/test_filesystem.py
+++ tests/unit/chroma_core/models/test_filesystem.py
@@ -49,38 +49,3 @@ class TestNidStrings(IMLUnitTestCase):
             "othernode": [Nid.Nid("1.2.3.6", "tcp", 0), Nid.Nid("4.3.2.3", "tcp", 1)],
         }
         return synthetic_host(address, host_nids[address])
-
-    def test_one_nid_no_failover(self):
-        mgs0 = self._host_with_nids("primary-mgs")
-        other = self._host_with_nids("othernode")
-
-        self.assertEqual(self.mgt.managedtarget_ptr.nids(), ((u"1.2.3.4@tcp0",),))
-        self.assertEqual(self.fs.mgs_spec(), u"1.2.3.4@tcp0")
-
-    def test_one_nid_with_failover(self):
-        mgs0 = self._host_with_nids("primary-mgs")
-        mgs1 = self._host_with_nids("failover-mgs")
-        other = self._host_with_nids("othernode")
-
-        self.mgt.name = "MGS_ha"
-        self.mgt.save()
-
-        self.assertEqual(self.mgt.managedtarget_ptr.nids(), ((u"1.2.3.4@tcp0",), (u"1.2.3.5@tcp5",)))
-        self.assertEqual(self.fs.mgs_spec(), u"1.2.3.4@tcp0:1.2.3.5@tcp5")
-
-    def test_two_nids_no_failover(self):
-        mgs0 = self._host_with_nids("primary-mgs-twonid")
-        other = self._host_with_nids("othernode")
-
-        self.assertEqual(self.mgt.managedtarget_ptr.nids(), ((u"1.2.3.4@tcp0", u"4.3.2.1@tcp1"),))
-        self.assertEqual(self.fs.mgs_spec(), u"1.2.3.4@tcp0,4.3.2.1@tcp1")
-
-    def test_two_nids_with_failover(self):
-        mgs0 = self._host_with_nids("primary-mgs-twonid")
-        mgs1 = self._host_with_nids("failover-mgs-twonid")
-        other = self._host_with_nids("othernode")
-
-        self.mgt.name = "MGS_ha"
-
-        self.assertEqual(self.mgt.nids(), ((u"1.2.3.4@tcp0", u"4.3.2.1@tcp1"), (u"1.2.3.5@tcp5", u"4.3.2.2@tcp1")))
-        self.assertEqual(self.fs.mgs_spec(), u"1.2.3.4@tcp0,4.3.2.1@tcp1:1.2.3.5@tcp5,4.3.2.2@tcp1")
