45c4edaeccae0804a4ab6f5e1dc3867e0f3cc3f8
johnsonw <wjohnson@whamcloud.com>
2020-12-08 10:07:51 -0500

    Fix listen-notify
    Signed-off-by: johnsonw <wjohnson@whamcloud.com>

--- migrations/20201204185824_corosync_listen_notify.sql
+++ migrations/20201204185824_corosync_listen_notify.sql
@@ -1,8 +1,8 @@
-DROP TRIGGER IF EXISTS corosync_resource_notify_update ON target;
+DROP TRIGGER IF EXISTS corosync_resource_notify_update ON corosync_resource;
 
-DROP TRIGGER IF EXISTS corosync_resource_notify_insert ON target;
+DROP TRIGGER IF EXISTS corosync_resource_notify_insert ON corosync_resource;
 
-DROP TRIGGER IF EXISTS corosync_resource_notify_delete ON target;
+DROP TRIGGER IF EXISTS corosync_resource_notify_delete ON corosync_resource;
 
 CREATE TRIGGER corosync_resource_notify_update
 AFTER
@@ -16,11 +16,11 @@ CREATE TRIGGER corosync_resource_notify_delete
 AFTER DELETE ON corosync_resource FOR EACH ROW EXECUTE PROCEDURE table_update_notify();
 
 
-DROP TRIGGER IF EXISTS corosync_resource_bans_notify_update ON target;
+DROP TRIGGER IF EXISTS corosync_resource_bans_notify_update ON corosync_resource_bans;
 
-DROP TRIGGER IF EXISTS corosync_resource_bans_notify_insert ON target;
+DROP TRIGGER IF EXISTS corosync_resource_bans_notify_insert ON corosync_resource_bans;
 
-DROP TRIGGER IF EXISTS corosync_resource_bans_notify_delete ON target;
+DROP TRIGGER IF EXISTS corosync_resource_bans_notify_delete ON corosync_resource_bans;
 
 CREATE TRIGGER corosync_resource_bans_notify_update
 AFTER
