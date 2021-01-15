4e634c2e699b2616b10ed28709c3bfb15ca36e78
Nathaniel Clark <nclark@whamcloud.com>
2021-01-15 10:40:36 -0400

    Fix Ubuntu Client build (#96)
    Signed-off-by: Nathaniel Clark <nclark@whamcloud.com>

--- es-vagrant/scripts/install_lustre_ubuntu.sh
+++ es-vagrant/scripts/install_lustre_ubuntu.sh
@@ -7,13 +7,16 @@ set -e
 scp node1:/root/EXAScaler-${VER}/lustre_dgx-${VER}.tar.gz .
 tar -axf lustre_dgx-${VER}.tar.gz
 cd lustre_dgx-${VER}/lustre-source
+
 # We manually do steps from dgx_install.py
-sudo apt-get install -y libtool automake wget git make dpkg-dev bc libselinux-dev ed \
-     libssl-dev libreadline-dev debhelper libsnmp-dev \
+apt-get update
+apt-get install -y libtool automake wget git make dpkg-dev bc libselinux-dev ed \
+     libssl-dev libreadline-dev debhelper libsnmp-dev quilt \
      rsync libyaml-dev build-essential debhelper devscripts fakeroot \
      kernel-wedge libudev-dev keyutils libkeyutils-dev krb5-multidev \
      libgssapi-krb5-2 libkrb5-3 libkrb5-dev kmod sg3-utils attr lsof \
-     pkg-config systemd libelf-dev libtool-bin
+     pkg-config systemd libelf-dev libtool-bin module-assistant dpatch mpi-default-dev
+
 ./configure --disable-server
 make debs
-sudo dpkg -i debs/*.deb
+dpkg -i debs/lustre-client*.deb
