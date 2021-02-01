b8326689b0dff91b473fb7808bab1be070d2a3c3
Igor Pashev <pashev.igor@gmail.com>
2021-02-01 19:30:05 +0200

    State Machine Initialization: Schema Validation (#111)
    Signed-off-by: Igor Pashev <pashev.igor@gmail.com>

---
+++ emf-state-machine/src/lib.rs
@@ -0,0 +1,89 @@
+static STATE_SCHEMA_RAW: &str = std::include_str!("state-schema.yml");
+static STATE_SCHEMA_SCHEMA_RAW: &str = std::include_str!("state-schema-schema.json");
+
+lazy_static::lazy_static! {
+
+    pub static ref STATE_SCHEMA: serde_json::Value = load_schema_yaml(STATE_SCHEMA_RAW, STATE_SCHEMA_SCHEMA_RAW).unwrap();
+
+}
+
+#[derive(thiserror::Error, Debug)]
+enum Error {
+    #[error("invalid JSON: {0}")]
+    JSON(#[from] serde_json::Error),
+    #[error("invalid YAML: {0}")]
+    YAML(#[from] serde_yaml::Error),
+    #[error("invalid document: {0:?}")]
+    Schema(Vec<jsonschema_valid::ValidationError>),
+}
+
+impl From<jsonschema_valid::ValidationError> for Error {
+    fn from(err: jsonschema_valid::ValidationError) -> Self {
+        Error::Schema(vec![err])
+    }
+}
+
+impl<'a> From<jsonschema_valid::ErrorIterator<'a>> for Error {
+    fn from(i: jsonschema_valid::ErrorIterator<'a>) -> Self {
+        Error::Schema(i.collect())
+    }
+}
+
+fn load_schema_yaml(data_raw: &str, schema_raw: &str) -> Result<serde_json::Value, Error> {
+    let schema_json = serde_json::from_str(schema_raw)?;
+    let cfg = jsonschema_valid::Config::from_schema(&schema_json, None)?;
+    cfg.validate_schema()?;
+
+    let data_json = serde_yaml::from_str::<serde_json::Value>(data_raw)?;
+    cfg.validate(&data_json)?;
+
+    Ok(data_json)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn mk_err_str(errs: Vec<jsonschema_valid::ValidationError>) -> String {
+        let mut strings: Vec<_> = errs.into_iter().map(|x| format!("{}", x,)).collect();
+        strings.sort();
+        strings.join("\n\n")
+    }
+
+    #[test]
+    fn invalid_state_schema_is_invalid() {
+        let s = r#"
+version: s
+components:
+  lnet:
+    states:
+      up:
+      down:
+    actions:
+      start:
+        state:
+          strt: own
+          end: oops
+      stop:
+        state:
+          start: up
+          end: down
+"#;
+        let state_schema = load_schema_yaml(s, STATE_SCHEMA_SCHEMA_RAW);
+        assert!(state_schema.is_err());
+
+        match state_schema.unwrap_err() {
+            Error::Schema(errs) => {
+                insta::assert_snapshot!(mk_err_str(errs));
+            }
+            e => {
+                panic!("Unexpected error: {:?}", e);
+            }
+        }
+    }
+
+    #[test]
+    fn state_schema_is_valid() -> Result<(), Error> {
+        load_schema_yaml(STATE_SCHEMA_RAW, STATE_SCHEMA_SCHEMA_RAW).map(drop)
+    }
+}
